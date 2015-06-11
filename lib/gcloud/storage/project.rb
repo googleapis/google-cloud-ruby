854d92146d6ed7fc939c575046ea9f6859ebf6e3
Mike Moore <mike@blowmage.com>
2015-06-11 07:58:35 -0600

    Update docs for Storage

--- lib/gcloud/storage/project.rb
+++ lib/gcloud/storage/project.rb
@@ -22,14 +22,21 @@ require "gcloud/storage/file"
 module Gcloud
   module Storage
     ##
-    # Represents the Project that the Buckets and Files belong to.
+    # = Project
+    #
+    # Represents the project that storage buckets and files belong to.
+    # All data in Google Cloud Storage belongs inside a project.
+    # A project consists of a set of users, a set of APIs, billing,
+    # authentication, and monitoring settings for those APIs.
     #
     # Gcloud::Storage::Project is the main object for interacting with
     # Google Storage. Gcloud::Storage::Bucket objects are created,
     # read, updated, and deleted by Gcloud::Storage::Project.
     #
-    #   storage = Gcloud.storage "my-todo-project",
-    #                            "/path/to/keyfile.json"
+    #   require "gcloud/datastore"
+    #
+    #   storage = Gcloud.storage
+    #
     #   bucket = storage.find_bucket "my-bucket"
     #   file = bucket.find_file "path/to/my-file.ext"
     #
@@ -48,7 +55,17 @@ module Gcloud
       end
 
       ##
-      # The project identifier.
+      # The Storage project connected to.
+      #
+      # === Example
+      #
+      #   require "gcloud/datastore"
+      #
+      #   storage = Gcloud.storage "my-todo-project",
+      #                            "/path/to/keyfile.json"
+      #
+      #   storage.project #=> "my-todo-project"
+      #
       def project
         connection.project
       end
@@ -62,13 +79,62 @@ module Gcloud
       ##
       # Retrieves a list of buckets for the given project.
       #
+      # === Parameters
+      #
+      # +options+::
+      #   An optional Hash for controlling additional behavor. (+Hash+)
+      # +options [:prefix]+::
+      #   Filter results to buckets whose names begin with this prefix.
+      #   (+String+)
+      # +options [:token]+::
+      #   A previously-returned page token representing part of the larger set
+      #   of results to view. (+String+)
+      # +options [:max]+::
+      #   Maximum number of buckets to return. (+Integer+)
+      #
+      # === Returns
+      #
+      # Array of Gcloud::Datastore::Bucket (Gcloud::Datastore::Bucket::List)
+      #
+      # === Examples
+      #
+      #   require "gcloud/datastore"
+      #
       #   storage = Gcloud.storage
+      #
       #   buckets = storage.buckets
       #   buckets.each do |bucket|
       #     puts bucket.name
       #   end
       #
-      # See Gcloud::Storage::Bucket
+      # You can also retrieve all buckets whose names begin with a prefix using
+      # the +:prefix+ option:
+      #
+      #   require "gcloud/storage"
+      #
+      #   storage = Gcloud.storage
+      #
+      #   user_buckets = storage.buckets prefix: "user-"
+      #
+      # If you have a significant number of buckets, you may need to paginate
+      # through them: (See Bucket::List#token)
+      #
+      #   require "gcloud/storage"
+      #
+      #   storage = Gcloud.storage
+      #
+      #   all_buckets = []
+      #   tmp_buckets = storage.buckets
+      #   while tmp_buckets.any? do
+      #     tmp_buckets.each do |bucket|
+      #       all_buckets << bucket
+      #     end
+      #     # break loop if no more buckets available
+      #     break if tmp_buckets.token.nil?
+      #     # get the next group of buckets
+      #     tmp_buckets = storage.buckets token: tmp_buckets.token
+      #   end
+      #
       def buckets options = {}
         resp = connection.list_buckets options
         if resp.success?
@@ -81,11 +147,24 @@ module Gcloud
       ##
       # Retrieves bucket by name.
       #
+      # === Parameters
+      #
+      # +bucket_name+::
+      #   Name of a bucket. (+String+)
+      #
+      # === Returns
+      #
+      # Gcloud::Datastore::Bucket or nil if bucket does not exist
+      #
+      # === Example
+      #
+      #   require "gcloud/storage"
+      #
       #   storage = Gcloud.storage
+      #
       #   bucket = storage.find_bucket "my-bucket"
       #   puts bucket.name
       #
-      # See Gcloud::Storage::Bucket
       def find_bucket bucket_name
         resp = connection.get_bucket bucket_name
         if resp.success?
@@ -99,15 +178,38 @@ module Gcloud
       ##
       # Creates a new bucket.
       #
-      #   bucket = project.create_bucket "my-bucket"
+      # === Parameters
+      #
+      # +bucket_name+::
+      #   Name of a bucket. (+String+)
+      # +options+::
+      #   An optional Hash for controlling additional behavor. (+Hash+)
+      # +options [:retries]+::
+      #   The number of times the API call should be retried.
+      #   Default is Gcloud::Backoff.retries. (+Integer+)
+      #
+      # === Returns
+      #
+      # Gcloud::Datastore::Bucket
+      #
+      # === Examples
+      #
+      #   require "gcloud/storage"
+      #
+      #   storage = Gcloud.storage
+      #
+      #   bucket = storage.create_bucket "my-bucket"
       #
       # The API call to create the bucket may be retried under certain
       # conditions. See Gcloud::Backoff to control this behavior, or
-      # specify the wanted behavior in the call:
+      # specify the wanted behavior in the call with the +:retries:+ option:
       #
-      #   bucket = project.create_bucket "my-bucket", retries: 5
+      #   require "gcloud/storage"
       #
-      # See Gcloud::Storage::Bucket
+      #   storage = Gcloud.storage
+      #
+      #   bucket = storage.create_bucket "my-bucket", retries: 5
+      #
       def create_bucket bucket_name, options = {}
         options[:acl] = Bucket::Acl.predefined_rule_for options[:acl]
         default_acl = options[:default_acl]
