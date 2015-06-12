02d12aebddd3e17cb8f6bfbcc1eec758edee3c8a
Mike Moore <mike@blowmage.com>
2015-06-11 19:04:27 -0600

    Add releases to the site
    Releases will be used to navigate across documentation.
    Display the current release on the homepage.
    
    [refs #158]

--- js/main.js
+++ js/main.js
@@ -1,7 +1,34 @@
+---
+---
 $('.nav-current').click(function(){
-    $('.main-nav').toggleClass('open');
+  $('.main-nav').toggleClass('open');
 });
 
 $('.faq-btn').click(function(){
 	$(this).toggleClass('open');
+});
+
+// Releases Dropdown
+$(document).ready(function() {
+  var meta = $('.side-nav--meta');
+  if (meta) {
+    // Add the release switcher HTML
+    meta.prepend($.parseHTML('<div id="release-nav">Browsing Version <select>' +
+      {% for release in site.data.releases %}
+        '<option value="/gcloud-ruby/docs/{{ release.version }}">{{ release.version }}</option>' +
+      {% endfor %}
+      '<option value="/gcloud-ruby/docs/master">master</option>' +
+      '</select></div>'));
+    // Select the current option
+    var switcher = meta.find('#release-nav').find('select');
+    switcher.find('option').each(function(index,element) {
+      if (window.location.pathname.startsWith(element.value)) {
+        element.selected = true;
+      }
+    });
+    // Navigate when selection is changed
+    switcher.change(function() {
+      window.location = this.value;
+    });
+  }
 });
