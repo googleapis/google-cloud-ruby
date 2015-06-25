7a587fd02353f72edab1ee4332c484843b526e9c
Mike Moore <mike@blowmage.com>
2015-06-24 18:19:08 -0600

    Fix raising error
    The error was returned, not raised.

--- lib/gcloud/storage/bucket.rb
+++ lib/gcloud/storage/bucket.rb
@@ -131,7 +131,7 @@ module Gcloud
         if resp.success?
           true
         else
-          ApiError.from_response(resp)
+          fail ApiError.from_response(resp)
         end
       end
 
