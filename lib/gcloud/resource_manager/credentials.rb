ec894b97e9dc0aaa0ca536209317eb33931a190a
Mike Moore <mike@blowmage.com>
2015-10-01 14:54:41 -0600

    Swap default scope
    The Google Cloud documentation states that the cloud-platform scope is required.

--- lib/gcloud/resource_manager/credentials.rb
+++ lib/gcloud/resource_manager/credentials.rb
@@ -20,7 +20,7 @@ module Gcloud
     ##
     # Represents the Oauth2 signing logic for Resource Manager.
     class Credentials < Gcloud::Credentials #:nodoc:
-      SCOPE = ["https://www.googleapis.com/auth/cloudresourcemanager"]
+      SCOPE = ["https://www.googleapis.com/auth/cloud-platform"]
       PATH_ENV_VARS = %w(RESOURCE_MANAGER_KEYFILE
                          GCLOUD_KEYFILE GOOGLE_CLOUD_KEYFILE)
       JSON_ENV_VARS = %w(RESOURCE_MANAGER_KEYFILE_JSON GCLOUD_KEYFILE_JSON
