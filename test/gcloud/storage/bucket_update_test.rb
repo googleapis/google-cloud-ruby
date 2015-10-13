796c9dd2e089a3a4e1603f3af6af5f241478d358
Chris Smith <quartzmo@gmail.com>
2015-10-12 18:32:50 -0600

    Add Bucket::Cors
    [closes #375]

--- test/gcloud/storage/bucket_update_test.rb
+++ test/gcloud/storage/bucket_update_test.rb
@@ -162,4 +162,45 @@ describe Gcloud::Storage::Bucket, :update, :mock_storage do
       b.cors.first["responseHeader"] << "X-Another-Custom-Header"
     end
   end
+
+  it "sets cors rules in a nested block" do
+      mock_connection.patch "/storage/v1/b/#{bucket.name}" do |env|
+        json = JSON.parse env.body
+        rules = json["cors"]
+        rules.count.must_equal 3
+        rules[0]["maxAgeSeconds"].must_equal 1800
+        rules[0]["origin"].must_equal ["http://example.org"]
+        rules[0]["method"].must_equal ["GET"]
+        rules[0]["responseHeader"].must_equal []
+        rules[1]["maxAgeSeconds"].must_equal 300
+        rules[1]["origin"].must_equal ["http://example.org", "https://example.org"]
+        rules[1]["method"].must_equal ["PUT", "DELETE"]
+        rules[1]["responseHeader"].must_equal ["X-My-Custom-Header"]
+        rules[2]["maxAgeSeconds"].must_equal 1800
+        rules[2]["origin"].must_equal ["http://example.com"]
+        rules[2]["method"].must_equal ["*"]
+        rules[2]["responseHeader"].must_equal ["X-Another-Custom-Header"]
+
+        updated_gapi = bucket.gapi.dup
+        updated_gapi["cors"] = json["cors"]
+        [200, { "Content-Type" => "application/json" },
+         random_bucket_hash(bucket_name, bucket_url, bucket_location,
+                                   bucket_storage_class, nil, nil, nil, nil, nil,
+                                   bucket_cors).to_json]
+      end
+
+
+      bucket.update do |b|
+        b.cors do |c|
+          c.add_rule "http://example.org", "GET"
+          c.add_rule ["http://example.org", "https://example.org"],
+                     ["PUT", "DELETE"],
+                     headers: ["X-My-Custom-Header"],
+                     max_age: 300
+          c.add_rule "http://example.com",
+                     "*",
+                     headers: "X-Another-Custom-Header"
+        end
+      end
+    end
 end
