cdd18559df61daf4d951657e77a830f8f3970268
Chris Smith <quartzmo@gmail.com>
2015-11-18 13:34:34 -0700

    Add FieldValue
    This commit refactors code from (and deletes) Fields. In addition,
    it introduces FieldValue and implements the "Option 2" examples in
    
    [refs #413,#418]

--- test/gcloud/search/document_fields_test.rb
+++ test/gcloud/search/document_fields_test.rb
@@ -27,137 +27,100 @@ describe Gcloud::Search::Document, :fields, :mock_search do
   let(:doc_hash) { {"docId" => doc_id, "rank" => doc_rank, "fields" => random_fields_hash } }
   let(:document) { Gcloud::Search::Document.from_hash doc_hash }
 
-  it "sets a number field from float" do
-    document["rating"] = 4.5
+  it "adds a number to a field" do
+    document.add "rating", 4.5
 
-    field = document["rating"]
-    field.must_be_kind_of Numeric
-    field.must_equal 4.5
+    fields = document["rating"]
+    fields.size.must_equal 1
+    field = fields.first
+    field.name.must_equal "rating"
+    field.value.must_equal 4.5
     field.type.must_equal :number
   end
 
-  it "sets a timestamp field from DateTime" do
-    document["posted_at"] = DateTime.new 2001, 2, 3, 4, 5, 6, '+7'
+  it "adds a DateTime to a field" do
+    document.add "posted_at", DateTime.new(2001, 2, 3, 4, 5, 6, '+7')
 
-    field = document["posted_at"]
-    field.must_be_kind_of DateTime
-    field.to_s.must_equal "2001-02-03T04:05:06+07:00"
+    field = document["posted_at"].first
+    field.name.must_equal "posted_at"
+    field.value.must_be_kind_of DateTime
+    field.value.to_s.must_equal "2001-02-03T04:05:06+07:00"
     field.type.must_equal :timestamp
   end
 
-  it "sets a geo field from Hash" do
-    latlon = { latitude: 40.58, longitude: "-111.65" }
-    document["destination"] = latlon
+  it "adds a coordinate to a field with type geo set" do
+    document.add "destination", "40.58, -111.65", type: :geo
 
-    field = document["destination"]
-    field.must_be_kind_of Gcloud::Search::GeoValue
-    field.latitude.must_equal 40.58
-    field.longitude.must_equal -111.65
-    field.to_s.must_equal "40.58, -111.65"
+    field = document["destination"].first
+    field.value.must_equal "40.58, -111.65"
     field.type.must_equal :geo
   end
 
-  it "sets a string default field" do
-    document["serial_number"] = "abc123"
+  it "adds a default string to a field" do
+    document.add "serial_number", "abc123"
 
-    field = document["serial_number"]
-    field.must_be_kind_of String
-    field.must_equal "abc123"
+    field = document["serial_number"].first
+    field.value.must_equal "abc123"
     field.type.must_equal :default
     field.lang.must_be :nil?
   end
 
-  it "returns its rest api representation" do
-    document.fields.must_equal doc_hash["fields"]
+  it "sets a new array of values in a field" do
+    document["body"] = ["new body 1", Gcloud::Search::FieldValue.new("body", "new body 2", type: :html, lang: "en")]
+
+    fields = document["body"]
+    fields[0].name.must_equal "body"
+    fields[0].value.must_equal "new body 1"
+    fields[0].type.must_equal :default
+    fields[0].lang.must_be :nil?
+    fields[1].name.must_equal "body"
+    fields[1].value.must_equal "new body 2"
+    fields[1].type.must_equal :html
+    fields[1].lang.must_equal "en"
   end
 
   it "returns a number field" do
-    field = document["price"]
-    field.must_be_kind_of Numeric
-    field.must_equal 24.95
+    field = document["price"].first
+    field.name.must_equal "price"
+    field.value.must_equal 24.95
     field.type.must_equal :number
   end
 
   it "returns a timestamp field" do
-    field = document["since"]
-    field.must_be_kind_of DateTime
-    field.to_s.must_equal "2015-10-02T15:00:00+00:00"
+    field = document["since"].first
+    field.name.must_equal "since"
+    field.value.must_be_kind_of DateTime
+    field.value.to_s.must_equal "2015-10-02T15:00:00+00:00"
     field.type.must_equal :timestamp
   end
 
   it "returns a geoValue field" do
-    field = document["location"]
-    field.must_be_kind_of Gcloud::Search::GeoValue
-    field.latitude.must_equal -33.857
-    field.longitude.must_equal 151.215
+    field = document["location"].first
+    field.name.must_equal "location"
+    field.value.must_equal "-33.857, 151.215"
     field.type.must_equal :geo
   end
 
   it "returns a string field with lang and type" do
-    field = document["body"]
-    field.must_be_kind_of String
-    field.must_equal "gcloud is a client library"
+    field = document["body"].first
+    field.name.must_equal "body"
+    field.value.must_equal "gcloud is a client library"
     field.type.must_equal :text
     field.lang.must_equal "en"
   end
 
   it "returns all values for a field with values" do
-    values = document["body"].values
+    values = document["body"]
     values.must_be_kind_of Array
-    values[0].must_equal "gcloud is a client library"
+    values[0].value.must_equal "gcloud is a client library"
     values[0].type.must_equal :text
     values[0].lang.must_equal "en"
-    values[1].must_equal "<code>gcloud</code> is a client library"
+    values[1].value.must_equal "<code>gcloud</code> is a client library"
     values[1].type.must_equal :html
     values[1].lang.must_equal "en"
-    values[2].must_equal "<code>gcloud</code> estas kliento biblioteko"
+    values[2].value.must_equal "<code>gcloud</code> estas kliento biblioteko"
     values[2].type.must_equal :html
     values[2].lang.must_equal "eo"
-  end
-
-  def random_fields_hash
-    {
-      "price" => {
-        "values" => [
-          {
-            "numberValue" => 24.95
-          }
-        ]
-      },
-      "since" => {
-        "values" => [
-          {
-            "timestampValue" => "2015-10-02T15:00:00+00:00"
-          }
-        ]
-      },
-      "location" => {
-        "values" => [
-          {
-            "geoValue" => "-33.857, 151.215"
-          }
-        ]
-      },
-      "body" => {
-        "values" => [
-          {
-            "stringFormat" => "TEXT",
-            "lang" => "en",
-            "stringValue" => "gcloud is a client library"
-          },
-          {
-            "stringFormat" => "HTML",
-            "lang" => "en",
-            "stringValue" => "<code>gcloud</code> is a client library"
-          },
-          {
-            "stringFormat" => "HTML",
-            "lang" => "eo",
-            "stringValue" => "<code>gcloud</code> estas kliento biblioteko"
-          }
-        ]
-      }
-    }
   end
 
 end
