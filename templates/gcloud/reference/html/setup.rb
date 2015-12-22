b9b51e03945bc73bc5cb34d7dc7c6da8aa788ef5
Mike Moore <mike@blowmage.com>
2015-12-22 09:12:23 -0700

    Collect methods into groups

--- templates/gcloud/reference/html/setup.rb
+++ templates/gcloud/reference/html/setup.rb
@@ -1,3 +1,34 @@
 def init
   sections :index
 end
+
+def methods_in_groups groups, methods
+  methods = methods.select { |m| m.visibility == :public }
+  methods = methods.reject { |m| m.tag :private }
+
+  others = []
+  if groups
+    group_data = groups.dup
+    methods.each {|m| groups_data |= [m.group] if m.group }
+    group_data.each do |group|
+      items = methods.select {|m| m.group == group }
+      yield(group, items) unless items.empty?
+    end
+    others = methods.select {|m| !m.group || !group_data.include?(m.group) }
+  else
+    group_data = {}
+    methods.each do |meth|
+      if meth.group
+        (group_data[meth.group] ||= []) << meth
+      else
+        others << meth
+      end
+    end
+    group_data.each {|group, items| yield(group, items) unless items.empty? }
+  end
+
+  [:class, :instance].each do |scope|
+    items = others.select {|m| m.scope == scope }
+    yield("#{scope.to_s.capitalize} Methods", items) unless items.empty?
+  end
+end
