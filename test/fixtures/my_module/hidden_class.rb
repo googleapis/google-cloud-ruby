0c9d80f1d2ff6f5d792198a0da4dc244f506844b
Chris Smith <quartzmo@gmail.com>
2016-07-22 10:53:53 -0600

    Exclude private modules

--- test/fixtures/my_module/hidden_class.rb
+++ test/fixtures/my_module/hidden_class.rb
@@ -1,10 +1,18 @@
 module MyModule
   ##
-  # @private This method should not appear in the output.
+  # @private This class should not appear in the output.
   class HiddenClass
     ##
     # This method should not appear in the output.
     def my_hidden_method
+    end
+  end
+  ##
+  # @private This module should not appear in the output.
+  module HiddenModule
+    ##
+    # This method should not appear in the output.
+    def self.my_hidden_module_method
     end
   end
 end
