8c9992bcfff905d3e08fe1d0b0cdb3be07c19713
Mike Moore <mike@blowmage.com>
2016-11-23 17:30:54 -0700

    Add google-cloud-monitoring file

---
+++ google-cloud-monitoring/lib/google-cloud-monitoring.rb
@@ -0,0 +1,24 @@
+# Copyright 2016 Google Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+##
+# This file is here to be autorequired by bundler, so that the .logging and
+# #logging methods can be available, but the library and all dependencies won't
+# be loaded until required and used.
+
+
+gem "google-cloud-core"
+require "google/cloud"
+
+# There is no Google::Cloud integration to add.
