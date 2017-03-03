ee783bfcaaf696a647ed1438248e727d0280176c
Mike Moore <mike@blowmage.com>
2017-03-02 18:21:22 -0700

    Add exit codes to build scripts
    We had some failures that were showing as passing, because the exit code didn't propogate.

--- build/test.rb
+++ build/test.rb
@@ -15,12 +15,14 @@ commands.each_with_index do |command, index|
   # only run the commands that are for the current node
   if node_index == index % node_total
     begin
-      PTY.spawn(command) do |stdout, _stdin, _pid|
+      status = PTY.spawn(command) do |stdout, _stdin, pid|
         begin
           stdout.each_char { |c| print c }
         rescue Errno::EIO
         end
+        Process.wait(pid)
       end
+      exit status if status != 0
     rescue PTY::ChildExited
       puts "The test process exited."
     end
