7cafdb9cc4b3e6022987f6233414c2c6fb6d3772
Heng Xiong <hxiong388@gmail.com>
2017-04-06 17:50:30 -0700

    Improve unit tests performance. Prevent any gRPC calls from unit tests.

--- google-cloud-debugger/test/google/cloud/debugger/transmitter_test.rb
+++ google-cloud-debugger/test/google/cloud/debugger/transmitter_test.rb
@@ -27,18 +27,27 @@ describe Google::Cloud::Debugger::Transmitter, :mock_debugger do
     end
 
     it "doesn't exceeds max_queue_size" do
-      max_queue_size = transmitter.max_queue_size
-      transmitter.start
+      max_queue_size = 3
+      transmitter.max_queue_size = max_queue_size
+      transmitter.instance_variable_set :@lock_cond, OpenStruct.new(broadcast: nil)
 
-      transmitter.instance_variable_get(:@lock_cond).stub :broadcast, nil do
-        max_queue_size.times do |i|
-          transmitter.submit i
-        end
-
-        transmitter.submit nil
-
-        transmitter.instance_variable_get(:@queue).size.must_equal max_queue_size
+      max_queue_size.times do |i|
+        transmitter.submit i
       end
+
+      wait_until_true do
+        transmitter.instance_variable_get(:@queue).size == max_queue_size
+      end
+
+      transmitter.submit nil
+
+      wait_until_true do
+        transmitter.instance_variable_get(:@queue).size == max_queue_size
+      end
+
+      transmitter.instance_variable_get(:@queue).size.must_equal max_queue_size
+
+      transmitter.instance_variable_set :@lock_cond, nil
     end
 
     it "wakes up the child queue to dequeue the breakpoints" do
