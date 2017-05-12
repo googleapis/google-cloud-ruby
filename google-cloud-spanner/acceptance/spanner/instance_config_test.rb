1743e4e7be42e7b371338bdb5a23e07b4d580252
Mike Moore <mike@blowmage.com>
2017-05-12 13:15:45 -0600

    Add acceptance coverage for Admin API

---
+++ google-cloud-spanner/acceptance/spanner/instance_config_test.rb
@@ -0,0 +1,28 @@
+# Copyright 2017 Google Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require "spanner_helper"
+
+describe "Spanner Instance Configs", :spanner do
+  it "lists and gets instance configs" do
+    all_configs = spanner.instance_configs.all.to_a
+    all_configs.wont_be :empty?
+    all_configs.each do |config|
+      config.must_be_kind_of Google::Cloud::Spanner::Instance::Config
+    end
+
+    first_configs = spanner.instance_config all_configs.first.instance_config_id
+    first_configs.must_be_kind_of Google::Cloud::Spanner::Instance::Config
+  end
+end
