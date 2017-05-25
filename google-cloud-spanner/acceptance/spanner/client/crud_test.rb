77902ac98d47689b97ac790469d7aaf32dd28541
Mike Moore <mike@blowmage.com>
2017-05-25 17:02:44 -0600

    Use commit timestamp in acceptance tests

--- google-cloud-spanner/acceptance/spanner/client/crud_test.rb
+++ google-cloud-spanner/acceptance/spanner/client/crud_test.rb
@@ -18,74 +18,85 @@ describe "Spanner Client", :crud, :spanner do
   let(:db) { spanner_client }
 
   before do
-    db.transaction do |tx|
-      existing_ids = tx.read("accounts", ["account_id"]).rows.map { |row| row[:account_id] }
-      tx.delete "accounts", existing_ids
-    end
-  end
-
-  after do
-    db.transaction do |tx|
-      existing_ids = tx.read("accounts", ["account_id"]).rows.map { |row| row[:account_id] }
-      tx.delete "accounts", existing_ids
-    end
+    @setup_timestamp = db.delete "accounts"
   end
 
   it "inserts, updates, upserts, reads, and deletes records" do
-    db.read("accounts", ["account_id"]).rows.count.must_equal 0
+    results = db.read "accounts", ["account_id"], single_use: { timestamp: @setup_timestamp }
+    results.rows.count.must_equal 0
+    results.timestamp.wont_be :nil?
 
     db.insert "accounts", default_account_rows[0]
     db.upsert "accounts", default_account_rows[1]
-    db.insert "accounts", default_account_rows[2]
+    timestamp = db.insert "accounts", default_account_rows[2]
 
-    db.read("accounts", ["account_id"]).rows.count.must_equal 3
+    results = db.read "accounts", ["account_id"], single_use: { timestamp: timestamp }
+    results.rows.count.must_equal 3
+    results.timestamp.wont_be :nil?
 
     active_count_sql = "SELECT COUNT(*) AS count FROM accounts WHERE active = true"
 
-    db.execute(active_count_sql).rows.first[:count].must_equal 2
+    results = db.execute active_count_sql, single_use: { timestamp: timestamp }
+    results.rows.first[:count].must_equal 2
+    results.timestamp.wont_be :nil?
 
     activate_inactive_account = { account_id: 3, active: true }
 
-    db.upsert "accounts", activate_inactive_account
+    timestamp = db.upsert "accounts", activate_inactive_account
 
-    db.execute(active_count_sql).rows.first[:count].must_equal 3
+    results = db.execute active_count_sql, single_use: { timestamp: timestamp }
+    results.rows.first[:count].must_equal 3
+    results.timestamp.wont_be :nil?
 
-    db.delete "accounts", [1, 2, 3]
+    timestamp = db.delete "accounts", [1, 2, 3]
 
-    db.read("accounts", ["account_id"]).rows.count.must_equal 0
+    results = db.read "accounts", ["account_id"], single_use: { timestamp: timestamp }
+    results.rows.count.must_equal 0
+    results.timestamp.wont_be :nil?
   end
 
   it "inserts, updates, upserts, reads, and deletes records using commit" do
-    db.read("accounts", ["account_id"]).rows.count.must_equal 0
+    results = db.read "accounts", ["account_id"], single_use: { timestamp: @setup_timestamp }
+    results.rows.count.must_equal 0
+    results.timestamp.wont_be :nil?
 
-    db.commit do |c|
+    timestamp = db.commit do |c|
       c.insert "accounts", default_account_rows[0]
       c.upsert "accounts", default_account_rows[1]
       c.insert "accounts", default_account_rows[2]
     end
 
-    db.read("accounts", ["account_id"]).rows.count.must_equal 3
+    results = db.read "accounts", ["account_id"], single_use: { timestamp: timestamp }
+    results.rows.count.must_equal 3
+    results.timestamp.wont_be :nil?
 
     active_count_sql = "SELECT COUNT(*) AS count FROM accounts WHERE active = true"
 
-    db.execute(active_count_sql).rows.first[:count].must_equal 2
+    results = db.execute active_count_sql, single_use: { timestamp: timestamp }
+    results.rows.first[:count].must_equal 2
+    results.timestamp.wont_be :nil?
 
     activate_inactive_account = { account_id: 3, active: true }
 
-    db.commit do |c|
+    timestamp = db.commit do |c|
       c.upsert "accounts", activate_inactive_account
     end
 
-    db.execute(active_count_sql).rows.first[:count].must_equal 3
+    results = db.execute active_count_sql, single_use: { timestamp: timestamp }
+    results.rows.first[:count].must_equal 3
+    results.timestamp.wont_be :nil?
 
-    db.commit do |c|
+    timestamp = db.commit do |c|
       c.delete "accounts", [1, 2, 3]
     end
 
-    db.read("accounts", ["account_id"]).rows.count.must_equal 0
+    results = db.read "accounts", ["account_id"], single_use: { timestamp: timestamp }
+    results.rows.count.must_equal 0
+    results.timestamp.wont_be :nil?
   end
 
   it "inserts, updates, upserts, reads, and deletes records in a transaction" do
+    timestamp = @setup_timestamp
     active_count_sql = "SELECT COUNT(*) AS count FROM accounts WHERE active = true"
 
     db.transaction do |tx|
@@ -96,7 +107,7 @@ describe "Spanner Client", :crud, :spanner do
       tx.insert "accounts", default_account_rows[2]
     end
 
-    db.transaction do |tx|
+    timestamp = db.transaction do |tx|
       db.read("accounts", ["account_id"]).rows.count.must_equal 3
 
       tx.execute(active_count_sql).rows.first[:count].must_equal 2
@@ -106,12 +117,14 @@ describe "Spanner Client", :crud, :spanner do
       tx.upsert "accounts", activate_inactive_account
     end
 
-    db.transaction do |tx|
+    timestamp = db.transaction do |tx|
       tx.execute(active_count_sql).rows.first[:count].must_equal 3
 
       tx.delete "accounts", [1, 2, 3]
     end
 
-    db.read("accounts", ["account_id"]).rows.count.must_equal 0
+    results = db.read "accounts", ["account_id"], single_use: { timestamp: timestamp }
+    results.rows.count.must_equal 0
+    results.timestamp.wont_be :nil?
   end
 end
