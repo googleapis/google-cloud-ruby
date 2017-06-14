9fb3c6803d8e57c5f0f5063e373b2fa29fb1cd5c
Heng Xiong <hxiong388@gmail.com>
2017-06-14 16:51:51 -0700

    Create Google::Cloud::Trace::FaradayMiddleware

---
+++ google-cloud-trace/test/google/cloud/trace/faraday_middleware_test.rb
@@ -0,0 +1,78 @@
+# Copyright 2017 Google Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+require "helper"
+require "google/cloud/trace/faraday_middleware"
+require "ostruct"
+
+describe Google::Cloud::Trace::FaradayMiddleware do
+  let(:app) {
+    app = Object.new
+    app.define_singleton_method :call do |_| end
+    app
+  }
+  let(:middleware) do
+    Google::Cloud::Trace::FaradayMiddleware.new app
+  end
+
+  describe "#call" do
+    it "doesn't interfere @app.call even if there are no parent span" do
+      mocked_app = Minitest::Mock.new
+      mocked_app.expect :call, nil, [NilClass]
+
+      middleware.instance_variable_set :@app, mocked_app
+
+      Google::Cloud::Trace.stub :get, nil do
+        middleware.call nil
+      end
+
+      mocked_app.verify
+    end
+  end
+
+  describe "#add_request_labels" do
+    it "sets all the labels" do
+      env = OpenStruct.new request_headers: {user_agent: "test-agent"},
+                           url: OpenStruct.new(host: "test-host",
+                                               scheme: "tcp")
+      env.define_singleton_method(:method) { "test-method" }
+      env.url.define_singleton_method(:to_s) { "full-url" }
+
+      span = OpenStruct.new labels: {}
+
+      middleware.send :add_request_labels, span, env
+
+      span.labels[Google::Cloud::Trace::LabelKey::AGENT].must_equal Google::Cloud::Trace::Middleware::AGENT_NAME
+      span.labels[Google::Cloud::Trace::LabelKey::HTTP_HOST].must_equal "test-host"
+      span.labels[Google::Cloud::Trace::LabelKey::HTTP_METHOD].must_equal "test-method"
+      span.labels[Google::Cloud::Trace::LabelKey::HTTP_CLIENT_PROTOCOL].must_equal "tcp"
+      span.labels[Google::Cloud::Trace::LabelKey::HTTP_USER_AGENT].must_equal "test-agent"
+      span.labels[Google::Cloud::Trace::LabelKey::HTTP_URL].must_equal "full-url"
+      span.labels[Google::Cloud::Trace::LabelKey::PID].must_be_kind_of String
+      span.labels[Google::Cloud::Trace::LabelKey::TID].must_be_kind_of String
+    end
+  end
+
+  describe "#add_response_labels" do
+    it "sets all the labels" do
+      response = OpenStruct.new status: 42
+      span = OpenStruct.new labels: {}
+
+      middleware.send :add_response_labels, span, response
+
+      span.labels[Google::Cloud::Trace::LabelKey::HTTP_STATUS_CODE].must_equal "42"
+    end
+  end
+end
