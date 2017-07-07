ec742f8f2936fce520070def6b79a5c8609996dc
Chris Smith <quartzmo@gmail.com>
2017-07-07 17:08:03 -0600

    Update Translation docs
    Remove mention of premium edition billing.
    
    [closes #1523]

--- google-cloud-translate/lib/google/cloud/translate/translation.rb
+++ google-cloud-translate/lib/google/cloud/translate/translation.rb
@@ -70,18 +70,10 @@ module Google
         alias_method :source, :from
 
         ##
-        # The model used by the service to perform the translation. When this is
-        # set to `nmt`, the translation was performed using premium neural
-        # machine translation model. If it is not set or model is set to `base`,
-        # then the translation was done using standard model. In almost all
-        # cases, the model type in response should match the model type
-        # requested. However, in some limited situations this might not be the
-        # case. In these cases, the request had `nmt` parameter, but the
-        # response has `base` set or model is not returned. This happens when
-        # neural translation did not give a satisfactory translation and we
-        # completed the translation using the standard model. If this happens,
-        # you will charged at the standard edition rate and not at the premium
-        # rate.
+        # The translation model. Can be either `base` for the Phrase-Based
+        # Machine Translation (PBMT) model, or `nmt` for the Neural Machine
+        # Translation (NMT) model. If you did not include a model parameter with
+        # your request, then this field is not included in the response.
         #
         # @return [String]
         attr_reader :model
