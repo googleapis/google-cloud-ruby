724cfa09c01f8b00cc5e58e9abfcb046cb24df45
Mike Moore <mike@blowmage.com>
2017-09-18 10:54:46 -0600

    BigQuery custom error retry and backoff logic
    Disable the Google API Client error retry and handle error retry ourselves.
    The rules for the retry are from the Google BigQuery SLA:
    
        After the first error, there is a minimum back-off interval of 1 second
        and for each consecutive error, the back-off interval increases
        exponentially up to 32 seconds.
    
    https://cloud.google.com/bigquery/sla

---
+++ google-cloud-bigquery/test/google/cloud/bigquery/backoff_delay_test.rb
@@ -0,0 +1,115 @@
+# Copyright 2017 Google Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require "helper"
+
+describe Google::Cloud::Bigquery::Service::Backoff, :delay do
+  class BackoffVerifier
+    def initialize
+      @mock = Minitest::Mock.new
+    end
+
+    def expect meth, ret, args
+      @mock.expect meth, ret, args
+    end
+
+    def verify
+      @mock.verify
+    end
+
+    def sleep num
+      @mock.sleep num
+    end
+
+    # Use the lambda, but give it a new binding context that will use the sleep mock.
+    define_method :backoff, &Google::Cloud::Bigquery::Service::Backoff.backoff
+  end
+
+  it "has a lambda that calls sleep with the delay given 0" do
+    b = BackoffVerifier.new
+    b.expect :sleep, nil, [1]
+    b.backoff 0
+    b.verify
+  end
+
+  it "has a lambda that calls sleep with the delay given 1" do
+    b = BackoffVerifier.new
+    b.expect :sleep, nil, [2]
+    b.backoff 1
+    b.verify
+  end
+
+  it "has a lambda that calls sleep with the delay given 2" do
+    b = BackoffVerifier.new
+    b.expect :sleep, nil, [4]
+    b.backoff 2
+    b.verify
+  end
+
+  it "has a lambda that calls sleep with the delay given 3" do
+    b = BackoffVerifier.new
+    b.expect :sleep, nil, [8]
+    b.backoff 3
+    b.verify
+  end
+
+  it "has a lambda that calls sleep with the delay given 4" do
+    b = BackoffVerifier.new
+    b.expect :sleep, nil, [16]
+    b.backoff 4
+    b.verify
+  end
+
+  it "has a lambda that calls sleep with the delay given 5" do
+    b = BackoffVerifier.new
+    b.expect :sleep, nil, [32]
+    b.backoff 5
+    b.verify
+  end
+
+  it "has a lambda that calls sleep with the delay given 6" do
+    b = BackoffVerifier.new
+    b.expect :sleep, nil, [32]
+    b.backoff 6
+    b.verify
+  end
+
+  it "has a lambda that calls sleep with the delay given 7" do
+    b = BackoffVerifier.new
+    b.expect :sleep, nil, [32]
+    b.backoff 7
+    b.verify
+  end
+
+  it "has a lambda that calls sleep with the delay given 8" do
+    b = BackoffVerifier.new
+    b.expect :sleep, nil, [32]
+    b.backoff 8
+    b.verify
+  end
+
+  it "has a lambda that calls sleep with the delay given 9" do
+    b = BackoffVerifier.new
+    b.expect :sleep, nil, [32]
+    b.backoff 9
+    b.verify
+  end
+
+  it "has a lambda that calls sleep with the delay given 10" do
+    b = BackoffVerifier.new
+    b.expect :sleep, nil, [32]
+    b.backoff 10
+    b.verify
+  end
+end
