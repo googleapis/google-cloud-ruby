5784aea800372f9f20f22bddf71e7eebef8266c9
Mike Moore <mike@blowmage.com>
2017-09-22 15:16:44 -0600

    Update View attributes
    Allow users to specify if the view query is using standard vs. legacy SQL.
    Also allow users to provide user-defined function resourcesfor the query.
    These can be set when creating a view, or updating an existing view.

--- google-cloud-bigquery/test/google/cloud/bigquery/view_update_test.rb
+++ google-cloud-bigquery/test/google/cloud/bigquery/view_update_test.rb
@@ -76,7 +76,7 @@ describe Google::Cloud::Bigquery::View, :update, :mock_bigquery do
     mock.verify
   end
 
-  it "updates its query" do
+  it "updates its query (query=)" do
     new_query = "SELECT name, age FROM `users`"
 
     mock = Minitest::Mock.new
@@ -84,13 +84,15 @@ describe Google::Cloud::Bigquery::View, :update, :mock_bigquery do
     view_hash["view"]["query"] = new_query
     request_view_gapi = Google::Apis::BigqueryV2::Table.new(
       view: Google::Apis::BigqueryV2::ViewDefinition.new(
-        query: new_query
+        query: new_query,
+        use_legacy_sql: false,
+        user_defined_function_resources: []
       ),
       etag: etag
     )
-    mock.expect :patch_table, return_view(view_hash),
+    mock.expect :patch_table, return_view(view_hash, request_view_gapi.view),
       [project, dataset_id, table_id, request_view_gapi, {options: {header: {"If-Match" => etag}}}]
-    mock.expect :get_table, return_view(view_hash), [project, dataset_id, table_id]
+    mock.expect :get_table, return_view(view_hash, request_view_gapi.view), [project, dataset_id, table_id]
     view.service.mocked_service = mock
 
     view.name.must_equal table_name
@@ -103,11 +105,239 @@ describe Google::Cloud::Bigquery::View, :update, :mock_bigquery do
     view.description.must_equal description
     view.schema.fields.count.must_equal schema.fields.count
     view.query.must_equal new_query
+    view.must_be :query_standard_sql?
+    view.wont_be :query_legacy_sql?
+    view.query_udfs.must_be :empty?
 
     mock.verify
   end
 
-  def return_view view_hash
-    Google::Apis::BigqueryV2::Table.from_json(view_hash.to_json)
+  it "updates its query (set_query)" do
+    new_query = "SELECT name, age FROM `users`"
+
+    mock = Minitest::Mock.new
+    view_hash = random_view_hash dataset_id, table_id, table_name, description
+    view_hash["view"]["query"] = new_query
+    request_view_gapi = Google::Apis::BigqueryV2::Table.new(
+      view: Google::Apis::BigqueryV2::ViewDefinition.new(
+        query: new_query,
+        use_legacy_sql: false,
+        user_defined_function_resources: []
+      ),
+      etag: etag
+    )
+    mock.expect :patch_table, return_view(view_hash, request_view_gapi.view),
+      [project, dataset_id, table_id, request_view_gapi, {options: {header: {"If-Match" => etag}}}]
+    mock.expect :get_table, return_view(view_hash, request_view_gapi.view), [project, dataset_id, table_id]
+    view.service.mocked_service = mock
+
+    view.name.must_equal table_name
+    view.description.must_equal description
+    view.schema.fields.count.must_equal schema.fields.count
+
+    view.set_query new_query
+
+    view.name.must_equal table_name
+    view.description.must_equal description
+    view.schema.fields.count.must_equal schema.fields.count
+    view.query.must_equal new_query
+    view.must_be :query_standard_sql?
+    view.wont_be :query_legacy_sql?
+    view.query_udfs.must_be :empty?
+
+    mock.verify
+  end
+
+  it "updates its query with standard_sql" do
+    new_query = "SELECT name, age FROM `users`"
+
+    mock = Minitest::Mock.new
+    view_hash = random_view_hash dataset_id, table_id, table_name, description
+    view_hash["view"]["query"] = new_query
+    request_view_gapi = Google::Apis::BigqueryV2::Table.new(
+      view: Google::Apis::BigqueryV2::ViewDefinition.new(
+        query: new_query,
+        use_legacy_sql: false,
+        user_defined_function_resources: []
+      ),
+      etag: etag
+    )
+    mock.expect :patch_table, return_view(view_hash, request_view_gapi.view),
+      [project, dataset_id, table_id, request_view_gapi, {options: {header: {"If-Match" => etag}}}]
+    mock.expect :get_table, return_view(view_hash, request_view_gapi.view), [project, dataset_id, table_id]
+    view.service.mocked_service = mock
+
+    view.name.must_equal table_name
+    view.description.must_equal description
+    view.schema.fields.count.must_equal schema.fields.count
+
+    view.set_query new_query, standard_sql: true
+
+    view.name.must_equal table_name
+    view.description.must_equal description
+    view.schema.fields.count.must_equal schema.fields.count
+    view.query.must_equal new_query
+    view.must_be :query_standard_sql?
+    view.wont_be :query_legacy_sql?
+    view.query_udfs.must_be :empty?
+
+    mock.verify
+  end
+
+  it "updates its query with legacy_sql" do
+    new_query = "SELECT name, age FROM `users`"
+
+    mock = Minitest::Mock.new
+    view_hash = random_view_hash dataset_id, table_id, table_name, description
+    view_hash["view"]["query"] = new_query
+    request_view_gapi = Google::Apis::BigqueryV2::Table.new(
+      view: Google::Apis::BigqueryV2::ViewDefinition.new(
+        query: new_query,
+        use_legacy_sql: true,
+        user_defined_function_resources: []
+      ),
+      etag: etag
+    )
+    mock.expect :patch_table, return_view(view_hash, request_view_gapi.view),
+      [project, dataset_id, table_id, request_view_gapi, {options: {header: {"If-Match" => etag}}}]
+    mock.expect :get_table, return_view(view_hash, request_view_gapi.view), [project, dataset_id, table_id]
+    view.service.mocked_service = mock
+
+    view.name.must_equal table_name
+    view.description.must_equal description
+    view.schema.fields.count.must_equal schema.fields.count
+
+    view.set_query new_query, legacy_sql: true
+
+    view.name.must_equal table_name
+    view.description.must_equal description
+    view.schema.fields.count.must_equal schema.fields.count
+    view.query.must_equal new_query
+    view.wont_be :query_standard_sql?
+    view.must_be :query_legacy_sql?
+    view.query_udfs.must_be :empty?
+
+    mock.verify
+  end
+
+  it "updates its query with udfs (array)" do
+    new_query = "SELECT name, age FROM `users`"
+
+    mock = Minitest::Mock.new
+    view_hash = random_view_hash dataset_id, table_id, table_name, description
+    view_hash["view"]["query"] = new_query
+    request_view_gapi = Google::Apis::BigqueryV2::Table.new(
+      view: Google::Apis::BigqueryV2::ViewDefinition.new(
+        query: new_query,
+        use_legacy_sql: false,
+        user_defined_function_resources: [
+          Google::Apis::BigqueryV2::UserDefinedFunctionResource.new(inline_code: "return x+1;"),
+          Google::Apis::BigqueryV2::UserDefinedFunctionResource.new(resource_uri: "gs://my-bucket/my-lib.js")
+        ]
+      ),
+      etag: etag
+    )
+    mock.expect :patch_table, return_view(view_hash, request_view_gapi.view),
+      [project, dataset_id, table_id, request_view_gapi, {options: {header: {"If-Match" => etag}}}]
+    mock.expect :get_table, return_view(view_hash, request_view_gapi.view), [project, dataset_id, table_id]
+    view.service.mocked_service = mock
+
+    view.name.must_equal table_name
+    view.description.must_equal description
+    view.schema.fields.count.must_equal schema.fields.count
+
+    view.set_query new_query, udfs: ["return x+1;", "gs://my-bucket/my-lib.js"]
+
+    view.name.must_equal table_name
+    view.description.must_equal description
+    view.schema.fields.count.must_equal schema.fields.count
+    view.query.must_equal new_query
+    view.must_be :query_standard_sql?
+    view.wont_be :query_legacy_sql?
+    view.query_udfs.must_equal ["return x+1;", "gs://my-bucket/my-lib.js"]
+
+    mock.verify
+  end
+
+  it "updates its query with udfs (inline)" do
+    new_query = "SELECT name, age FROM `users`"
+
+    mock = Minitest::Mock.new
+    view_hash = random_view_hash dataset_id, table_id, table_name, description
+    view_hash["view"]["query"] = new_query
+    request_view_gapi = Google::Apis::BigqueryV2::Table.new(
+      view: Google::Apis::BigqueryV2::ViewDefinition.new(
+        query: new_query,
+        use_legacy_sql: false,
+        user_defined_function_resources: [
+          Google::Apis::BigqueryV2::UserDefinedFunctionResource.new(inline_code: "return x+1;")
+        ]
+      ),
+      etag: etag
+    )
+    mock.expect :patch_table, return_view(view_hash, request_view_gapi.view),
+      [project, dataset_id, table_id, request_view_gapi, {options: {header: {"If-Match" => etag}}}]
+    mock.expect :get_table, return_view(view_hash, request_view_gapi.view), [project, dataset_id, table_id]
+    view.service.mocked_service = mock
+
+    view.name.must_equal table_name
+    view.description.must_equal description
+    view.schema.fields.count.must_equal schema.fields.count
+
+    view.set_query new_query, udfs: "return x+1;"
+
+    view.name.must_equal table_name
+    view.description.must_equal description
+    view.schema.fields.count.must_equal schema.fields.count
+    view.query.must_equal new_query
+    view.must_be :query_standard_sql?
+    view.wont_be :query_legacy_sql?
+    view.query_udfs.must_equal ["return x+1;"]
+
+    mock.verify
+  end
+
+  it "updates its query with udfs (url)" do
+    new_query = "SELECT name, age FROM `users`"
+
+    mock = Minitest::Mock.new
+    view_hash = random_view_hash dataset_id, table_id, table_name, description
+    view_hash["view"]["query"] = new_query
+    request_view_gapi = Google::Apis::BigqueryV2::Table.new(
+      view: Google::Apis::BigqueryV2::ViewDefinition.new(
+        query: new_query,
+        use_legacy_sql: false,
+        user_defined_function_resources: [
+          Google::Apis::BigqueryV2::UserDefinedFunctionResource.new(resource_uri: "gs://my-bucket/my-lib.js")
+        ]
+      ),
+      etag: etag
+    )
+    mock.expect :patch_table, return_view(view_hash, request_view_gapi.view),
+      [project, dataset_id, table_id, request_view_gapi, {options: {header: {"If-Match" => etag}}}]
+    mock.expect :get_table, return_view(view_hash, request_view_gapi.view), [project, dataset_id, table_id]
+    view.service.mocked_service = mock
+
+    view.name.must_equal table_name
+    view.description.must_equal description
+    view.schema.fields.count.must_equal schema.fields.count
+
+    view.set_query new_query, udfs: "gs://my-bucket/my-lib.js"
+
+    view.name.must_equal table_name
+    view.description.must_equal description
+    view.schema.fields.count.must_equal schema.fields.count
+    view.query.must_equal new_query
+    view.must_be :query_standard_sql?
+    view.wont_be :query_legacy_sql?
+    view.query_udfs.must_equal ["gs://my-bucket/my-lib.js"]
+
+    mock.verify
+  end
+
+  def return_view view_hash, view_gapi = nil
+    tbl = Google::Apis::BigqueryV2::Table.from_json(view_hash.to_json)
+    tbl.view = view_gapi if view_gapi
+    tbl
   end
 end
