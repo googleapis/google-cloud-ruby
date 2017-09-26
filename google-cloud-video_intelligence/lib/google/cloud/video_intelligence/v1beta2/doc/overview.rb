148e9cca6e6f509f91993263fcb9ed65d7e5b401
Jacob Geiger <jgeiger@google.com>
2017-09-25 17:33:41 -0700

    Regenerate GAPIC

--- google-cloud-video_intelligence/lib/google/cloud/video_intelligence/v1beta2/doc/overview.rb
+++ google-cloud-video_intelligence/lib/google/cloud/video_intelligence/v1beta2/doc/overview.rb
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-require "google/cloud/video_intelligence/v1beta2/video_intelligence_service_client"
-
 module Google
   module Cloud
     # rubocop:disable LineLength
@@ -33,12 +31,17 @@ module Google
     # 2. [Enable the Google Cloud Video Intelligence API.](https://console.cloud.google.com/apis/api/video-intelligence)
     # 3. [Setup Authentication.](https://googlecloudplatform.github.io/google-cloud-ruby/#/docs/google-cloud/master/guides/authentication)
     #
+    # ### Installation
+    # ```
+    # $ gem install google-cloud-video_intelligence
+    # ```
+    #
     # ### Preview
     # #### VideoIntelligenceServiceClient
     # ```rb
-    # require "google/cloud/video_intelligence/v1beta2"
+    # require "google/cloud/video_intelligence"
     #
-    # video_intelligence_service_client = Google::Cloud::VideoIntelligence::V1beta2.new
+    # video_intelligence_service_client = Google::Cloud::VideoIntelligence.new
     # input_uri = "gs://cloud-ml-sandbox/video/chicago.mp4"
     # features_element = :LABEL_DETECTION
     # features = [features_element]
@@ -86,62 +89,6 @@ module Google
     #
     module VideoIntelligence
       module V1beta2
-        # rubocop:enable LineLength
-
-        ##
-        # Service that implements Google Cloud Video Intelligence API.
-        #
-        # @param credentials [Google::Gax::Credentials, String, Hash, GRPC::Core::Channel, GRPC::Core::ChannelCredentials, Proc]
-        #   Provides the means for authenticating requests made by the client. This parameter can
-        #   be many types.
-        #   A `Google::Gax::Credentials` uses a the properties of its represented keyfile for
-        #   authenticating requests made by this client.
-        #   A `String` will be treated as the path to the keyfile to be used for the construction of
-        #   credentials for this client.
-        #   A `Hash` will be treated as the contents of a keyfile to be used for the construction of
-        #   credentials for this client.
-        #   A `GRPC::Core::Channel` will be used to make calls through.
-        #   A `GRPC::Core::ChannelCredentials` for the setting up the RPC client. The channel credentials
-        #   should already be composed with a `GRPC::Core::CallCredentials` object.
-        #   A `Proc` will be used as an updater_proc for the Grpc channel. The proc transforms the
-        #   metadata for requests, generally, to give OAuth credentials.
-        # @param scopes [Array<String>]
-        #   The OAuth scopes for this service. This parameter is ignored if
-        #   an updater_proc is supplied.
-        # @param client_config [Hash]
-        #   A Hash for call options for each method. See
-        #   Google::Gax#construct_settings for the structure of
-        #   this data. Falls back to the default config if not specified
-        #   or the specified config is missing data points.
-        # @param timeout [Numeric]
-        #   The default timeout, in seconds, for calls made through this client.
-        def self.new \
-            service_path: nil,
-            port: nil,
-            channel: nil,
-            chan_creds: nil,
-            updater_proc: nil,
-            credentials: nil,
-            scopes: nil,
-            client_config: nil,
-            timeout: nil,
-            lib_name: nil,
-            lib_version: nil
-          kwargs = {
-            service_path: service_path,
-            port: port,
-            channel: channel,
-            chan_creds: chan_creds,
-            updater_proc: updater_proc,
-            credentials: credentials,
-            scopes: scopes,
-            client_config: client_config,
-            timeout: timeout,
-            lib_name: lib_name,
-            lib_version: lib_version
-          }.select { |_, v| v != nil }
-          Google::Cloud::VideoIntelligence::V1beta2::VideoIntelligenceServiceClient.new(**kwargs)
-        end
       end
     end
   end
