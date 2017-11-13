6bb0e07995ffc217d4a068ad21bace53e4203e09
Mike Moore <mike@blowmage.com>
2017-11-13 04:46:59 -0700

    Add Credentials to Debugger
    * Add Debugger Credentials that inherits from Google::Auth::Credentials.
      (Previously Credentials was private, but now is part of the public API.)
    * Rename project and keyfile arguments to project_id and credentials.
      The project and keyfile arguments continue to be supported, but are
      considered deprecated.
    * Add Google::Auth::Credentials as supported argument value on constructor.

--- google-cloud-debugger/lib/google/cloud/debugger/middleware.rb
+++ google-cloud-debugger/lib/google/cloud/debugger/middleware.rb
@@ -116,7 +116,7 @@ module Google
         # Fallback to default configuration values if not defined already
         def init_default_config
           configuration.project_id ||= Cloud.configure.project_id ||
-                                       Debugger::Project.default_project
+                                       Debugger::Project.default_project_id
           configuration.keyfile ||= Cloud.configure.keyfile
 
           configuration.service_name ||= Debugger::Project.default_service_name
--- google-cloud-debugger/test/google/cloud/debugger_test.rb
+++ google-cloud-debugger/test/google/cloud/debugger_test.rb
@@ -76,7 +76,13 @@ describe Google::Cloud do
   end
 
   describe ".debugger" do
-    let(:default_credentials) { OpenStruct.new empty: true }
+    let(:default_credentials) do
+      creds = OpenStruct.new empty: true
+      def creds.is_a? target
+        target == Google::Auth::Credentials
+      end
+      creds
+    end
     let(:default_service_name) { "default-utest-service" }
     let(:default_service_version) { "vDefaultUTest" }
     let(:found_credentials) { "{}" }
@@ -148,7 +154,13 @@ describe Google::Cloud do
   end
 
   describe "Debugger.new" do
-    let(:default_credentials) { OpenStruct.new empty: true }
+    let(:default_credentials) do
+      creds = OpenStruct.new empty: true
+      def creds.is_a? target
+        target == Google::Auth::Credentials
+      end
+      creds
+    end
     let(:default_service_name) { "default-utest-service" }
     let(:default_service_version) { "vDefaultUTest" }
     let(:found_credentials) { "{}" }
@@ -174,7 +186,42 @@ describe Google::Cloud do
       end
     end
 
-    it "uses provided project_id, keyfile, service_name, and service_version" do
+    it "uses provided project_id, credentials, service_name, and service_version" do
+      stubbed_credentials = ->(keyfile, scope: nil) {
+        keyfile.must_equal "path/to/keyfile.json"
+        scope.must_be_nil
+        "debugger-credentials"
+      }
+      stubbed_service_name = "utest-service"
+      stubbed_service_version = "vUTest"
+      stubbed_service = ->(project, credentials, timeout: nil, client_config: nil) {
+        project.must_equal "project-id"
+        credentials.must_equal "debugger-credentials"
+        timeout.must_be :nil?
+        client_config.must_be :nil?
+        OpenStruct.new project: project
+      }
+
+      # Clear all environment variables
+      ENV.stub :[], nil do
+        File.stub :file?, true, ["path/to/keyfile.json"] do
+          File.stub :read, found_credentials, ["path/to/keyfile.json"] do
+            Google::Cloud::Debugger::Credentials.stub :new, stubbed_credentials do
+              Google::Cloud::Debugger::Service.stub :new, stubbed_service do
+                debugger = Google::Cloud::Debugger.new project_id: "project-id", credentials: "path/to/keyfile.json", service_name: stubbed_service_name, service_version: stubbed_service_version
+                debugger.must_be_kind_of Google::Cloud::Debugger::Project
+                debugger.project.must_equal "project-id"
+                debugger.agent.debuggee.service_name.must_equal stubbed_service_name
+                debugger.agent.debuggee.service_version.must_equal stubbed_service_version
+                debugger.service.must_be_kind_of OpenStruct
+              end
+            end
+          end
+        end
+      end
+    end
+
+    it "uses provided project (alias), keyfile (alias), service_name, and service_version" do
       stubbed_credentials = ->(keyfile, scope: nil) {
         keyfile.must_equal "path/to/keyfile.json"
         scope.must_be_nil
