aabbf3aa34719159d83dba63d439f0fcc7b7eea0
Mike Moore <mike@blowmage.com>
2017-11-13 05:24:28 -0700

    Add Credentials to Resource Manager
    * Add Resource Manager Credentials that inherits from Google::Auth::Credentials.
      (Previously Credentials was private, but now is part of the public API.)
    * Rename project and keyfile arguments to project_id and credentials.
      The project and keyfile arguments continue to be supported, but are
      considered deprecated.
    * Add Google::Auth::Credentials as supported argument value on constructor.

--- google-cloud-resource_manager/test/google/cloud/resource_manager_test.rb
+++ google-cloud-resource_manager/test/google/cloud/resource_manager_test.rb
@@ -20,7 +20,7 @@ describe Google::Cloud do
     it "calls out to Google::Cloud.resource_manager" do
       gcloud = Google::Cloud.new
       stubbed_resource_manager = ->(keyfile, scope: nil, retries: nil, timeout: nil) {
-        keyfile.must_equal nil
+        keyfile.must_be :nil?
         scope.must_be :nil?
         retries.must_be :nil?
         timeout.must_be :nil?
@@ -64,7 +64,13 @@ describe Google::Cloud do
   end
 
   describe ".resource_manager" do
-    let(:default_credentials) { OpenStruct.new empty: true }
+    let(:default_credentials) do
+      creds = OpenStruct.new empty: true
+      def creds.is_a? target
+        target == Google::Auth::Credentials
+      end
+      creds
+    end
     let(:found_credentials) { "{}" }
 
     it "gets defaults for project_id and keyfile" do
@@ -81,13 +87,13 @@ describe Google::Cloud do
     it "uses provided project_id and keyfile" do
       stubbed_credentials = ->(keyfile, scope: nil) {
         keyfile.must_equal "path/to/keyfile.json"
-        scope.must_equal nil
+        scope.must_be :nil?
         "resource_manager-credentials"
       }
       stubbed_service = ->(credentials, retries: nil, timeout: nil) {
         credentials.must_equal "resource_manager-credentials"
-        retries.must_equal nil
-        timeout.must_equal nil
+        retries.must_be :nil?
+        timeout.must_be :nil?
         OpenStruct.new
       }
 
@@ -109,7 +115,13 @@ describe Google::Cloud do
   end
 
   describe "ResourceManager.new" do
-    let(:default_credentials) { OpenStruct.new empty: true }
+    let(:default_credentials) do
+      creds = OpenStruct.new empty: true
+      def creds.is_a? target
+        target == Google::Auth::Credentials
+      end
+      creds
+    end
     let(:found_credentials) { "{}" }
 
     it "gets defaults for project_id and keyfile" do
@@ -123,16 +135,45 @@ describe Google::Cloud do
       end
     end
 
-    it "uses provided project_id and keyfile" do
+    it "uses provided credentials" do
       stubbed_credentials = ->(keyfile, scope: nil) {
         keyfile.must_equal "path/to/keyfile.json"
-        scope.must_equal nil
+        scope.must_be :nil?
         "resource_manager-credentials"
       }
       stubbed_service = ->(credentials, retries: nil, timeout: nil) {
         credentials.must_equal "resource_manager-credentials"
-        retries.must_equal nil
-        timeout.must_equal nil
+        retries.must_be :nil?
+        timeout.must_be :nil?
+        OpenStruct.new
+      }
+
+      # Clear all environment variables
+      ENV.stub :[], nil do
+        File.stub :file?, true, ["path/to/keyfile.json"] do
+          File.stub :read, found_credentials, ["path/to/keyfile.json"] do
+            Google::Cloud::ResourceManager::Credentials.stub :new, stubbed_credentials do
+              Google::Cloud::ResourceManager::Service.stub :new, stubbed_service do
+                resource_manager = Google::Cloud::ResourceManager.new credentials: "path/to/keyfile.json"
+                resource_manager.must_be_kind_of Google::Cloud::ResourceManager::Manager
+                resource_manager.service.must_be_kind_of OpenStruct
+              end
+            end
+          end
+        end
+      end
+    end
+
+    it "uses provided keyfile alias" do
+      stubbed_credentials = ->(keyfile, scope: nil) {
+        keyfile.must_equal "path/to/keyfile.json"
+        scope.must_be :nil?
+        "resource_manager-credentials"
+      }
+      stubbed_service = ->(credentials, retries: nil, timeout: nil) {
+        credentials.must_equal "resource_manager-credentials"
+        retries.must_be :nil?
+        timeout.must_be :nil?
         OpenStruct.new
       }
 
