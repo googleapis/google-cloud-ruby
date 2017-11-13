51ef092e10a8ab07792fd4f70ad765eb4b20e3ed
Mike Moore <mike@blowmage.com>
2017-11-13 05:32:11 -0700

    Add Credentials to Spanner
    * Add Spanner Credentials that inherits from Google::Auth::Credentials.
      (Previously Credentials was private, but now is part of the public API.)
    * Rename project and keyfile arguments to project_id and credentials.
      The project and keyfile arguments continue to be supported, but are
      considered deprecated.
    * Add Google::Auth::Credentials as supported argument value on constructor.

--- google-cloud-spanner/acceptance/spanner/client/snapshot_test.rb
+++ google-cloud-spanner/acceptance/spanner/client/snapshot_test.rb
@@ -305,13 +305,38 @@ describe "Spanner Client", :snapshot, :spanner do
   end
 
   def assert_accounts_equal expected, actual
-    expected[:account_id].must_equal actual[:account_id]
-    expected[:username].must_equal actual[:username]
-    expected[:reputation].must_equal actual[:reputation]
-    expected[:active].must_equal actual[:active]
+    if actual[:account_id].nil?
+      expected[:account_id].must_be :nil?
+    else
+      expected[:account_id].must_equal actual[:account_id]
+    end
+
+    if actual[:username].nil?
+      expected[:username].must_be :nil?
+    else
+      expected[:username].must_equal actual[:username]
+    end
+
+    if actual[:reputation].nil?
+      expected[:reputation].must_be :nil?
+    else
+      expected[:reputation].must_equal actual[:reputation]
+    end
+
+    if actual[:active].nil?
+      expected[:active].must_be :nil?
+    else
+      expected[:active].must_equal actual[:active]
+    end
+
     if expected[:avatar] && actual[:avatar]
       expected[:avatar].read.must_equal actual[:avatar].read
     end
-    expected[:friends].must_equal actual[:friends]
+
+    if actual[:friends].nil?
+      expected[:friends].must_be :nil?
+    else
+      expected[:friends].must_equal actual[:friends]
+    end
   end
 end
