12695334f12151f0814f36548626142fb5d2a91e
Mike Moore <mike@blowmage.com>
2017-11-13 05:21:44 -0700

    Add Credentials to Pub/Sub
    * Add Pub/Sub Credentials that inherits from Google::Auth::Credentials.
      (Previously Credentials was private, but now is part of the public API.)
    * Rename project and keyfile arguments to project_id and credentials.
      The project and keyfile arguments continue to be supported, but are
      considered deprecated.
    * Add Google::Auth::Credentials as supported argument value on constructor.

--- google-cloud-pubsub/lib/google/cloud/pubsub/credentials.rb
+++ google-cloud-pubsub/lib/google/cloud/pubsub/credentials.rb
@@ -13,18 +13,44 @@
 # limitations under the License.
 
 
-require "google/cloud/credentials"
+require "googleauth"
 
 module Google
   module Cloud
     module Pubsub
       ##
-      # @private Represents the OAuth 2.0 signing logic for Pub/Sub.
-      class Credentials < Google::Cloud::Credentials
+      # # Credentials
+      #
+      # Represents the authentication and authorization used to connect to the
+      # Pub/Sub API.
+      #
+      # @example
+      #   require "google/cloud/pubsub"
+      #
+      #   keyfile = "/path/to/keyfile.json"
+      #   creds = Google::Cloud::Pubsub::Credentials.new keyfile
+      #
+      #   pubsub = Google::Cloud::Pubsub.new(
+      #     project_id: "my-project",
+      #     credentials: creds
+      #   )
+      #
+      #   pubsub.project_id #=> "my-project"
+      #
+      class Credentials < Google::Auth::Credentials
         SCOPE = ["https://www.googleapis.com/auth/pubsub"]
-        PATH_ENV_VARS = %w(PUBSUB_KEYFILE GOOGLE_CLOUD_KEYFILE GCLOUD_KEYFILE)
-        JSON_ENV_VARS = %w(PUBSUB_KEYFILE_JSON GOOGLE_CLOUD_KEYFILE_JSON
+        PATH_ENV_VARS = %w(PUBSUB_CREDENTIALS
+                           PUBSUB_KEYFILE
+                           GOOGLE_CLOUD_CREDENTIALS
+                           GOOGLE_CLOUD_KEYFILE
+                           GCLOUD_KEYFILE)
+        JSON_ENV_VARS = %w(PUBSUB_CREDENTIALS_JSON
+                           PUBSUB_KEYFILE_JSON
+                           GOOGLE_CLOUD_CREDENTIALS_JSON
+                           GOOGLE_CLOUD_KEYFILE_JSON
                            GCLOUD_KEYFILE_JSON)
+        DEFAULT_PATHS = \
+          ["~/.config/gcloud/application_default_credentials.json"]
       end
     end
   end
