6c7d18b4d904c807e7b7a16e27a080375f8a27a7
Mike Moore <mike@blowmage.com>
2017-11-20 09:27:45 -0700

    Update service
    Remove host, channel, and chan_creds as we are using the credentials object now.

--- google-cloud-firestore/lib/google/cloud/firestore/service.rb
+++ google-cloud-firestore/lib/google/cloud/firestore/service.rb
@@ -25,44 +25,25 @@ module Google
       # @private Represents the gRPC Firestore service, including all the API
       # methods.
       class Service
-        attr_accessor :project, :credentials, :host, :timeout, :client_config
+        attr_accessor :project, :credentials, :timeout, :client_config
 
         ##
         # Creates a new Service instance.
-        def initialize project, credentials, host: nil, timeout: nil,
-                       client_config: nil
+        def initialize project, credentials, timeout: nil, client_config: nil
           @project = project
           @credentials = credentials
-          @host = host || V1beta1::FirestoreClient::SERVICE_ADDRESS
           @timeout = timeout
           @client_config = client_config || {}
-        end
-
-        def channel
-          require "grpc"
-          GRPC::Core::Channel.new host, nil, chan_creds
-        end
-
-        def chan_creds
-          return credentials if insecure?
-          require "grpc"
-          GRPC::Core::ChannelCredentials.new.compose \
-            GRPC::Core::CallCredentials.new credentials.client.updater_proc
         end
 
         def firestore
           @firestore ||= \
             V1beta1::FirestoreClient.new(
-              service_path: host,
-              channel: channel,
+              credentials: credentials,
               timeout: timeout,
               client_config: client_config,
               lib_name: "gccl",
               lib_version: Google::Cloud::Firestore::VERSION)
-        end
-
-        def insecure?
-          credentials == :this_channel_is_insecure
         end
 
         def list_documents parent, collection_id, token: nil
