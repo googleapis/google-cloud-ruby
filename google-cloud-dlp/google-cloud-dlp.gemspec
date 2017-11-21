5c7adeba1a1a3e6c23cf7a86b47aac9de58994fc
Google API Publisher <googleapis-publisher@google.com>
2017-11-21 23:37:39 +0000

    Ruby GAPIC: Dlp v2beta1

---
+++ google-cloud-dlp/google-cloud-dlp.gemspec
@@ -0,0 +1,30 @@
+# -*- ruby -*-
+# encoding: utf-8
+
+Gem::Specification.new do |gem|
+  gem.name          = "google-cloud-dlp"
+  gem.version       = "0.20.0"
+
+  gem.authors       = ["Google Inc"]
+  gem.email         = "googleapis-packages@google.com"
+  gem.description   = "google-cloud-dlp is the official library for DLP API."
+  gem.summary       = "API Client library for DLP API"
+  gem.homepage      = "https://github.com/googleapis/googleapis"
+  gem.license       = "Apache-2.0"
+
+  gem.platform      = Gem::Platform::RUBY
+
+  gem.files         = `git ls-files -- lib/*`.split("\n") +
+                      ["README.md", "LICENSE", ".yardopts"]
+  gem.require_paths = ["lib"]
+
+  gem.required_ruby_version = ">= 2.0.0"
+
+  gem.add_dependency "google-gax", "~> 0.10.1"
+  gem.add_dependency "googleapis-common-protos", "~> 1.3.1"
+  gem.add_dependency "googleauth", "~> 0.6.1"
+
+  gem.add_development_dependency "minitest", "~> 5.10"
+  gem.add_development_dependency "rubocop", "<= 0.35.1"
+  gem.add_development_dependency "simplecov", "~> 0.9"
+end
---
+++ google-cloud-dlp/lib/google/cloud/dlp/v2beta1/doc/google/type/timeofday.rb
@@ -0,0 +1,36 @@
+# Copyright 2017, Google LLC All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+module Google
+  module Type
+    # Represents a time of day. The date and time zone are either not significant
+    # or are specified elsewhere. An API may chose to allow leap seconds. Related
+    # types are {Google::Type::Date} and +google.protobuf.Timestamp+.
+    # @!attribute [rw] hours
+    #   @return [Integer]
+    #     Hours of day in 24 hour format. Should be from 0 to 23. An API may choose
+    #     to allow the value "24:00:00" for scenarios like business closing time.
+    # @!attribute [rw] minutes
+    #   @return [Integer]
+    #     Minutes of hour of day. Must be from 0 to 59.
+    # @!attribute [rw] seconds
+    #   @return [Integer]
+    #     Seconds of minutes of the time. Must normally be from 0 to 59. An API may
+    #     allow the value 60 if it allows leap-seconds.
+    # @!attribute [rw] nanos
+    #   @return [Integer]
+    #     Fractions of seconds in nanoseconds. Must be from 0 to 999,999,999.
+    class TimeOfDay; end
+  end
+end
