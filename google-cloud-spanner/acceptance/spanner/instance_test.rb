ea1324e4ffe127a918ed24f4f37557befefec4f2
Mike Moore <mike@blowmage.com>
2018-02-01 10:55:30 -0700

    Update Spanner Acceptance Test Setup
    We have a limited number of instances available for running tests.
    The Spanner team has asked that we reuse a instance instead of
    creating and deleting an instance for each test run.
    Create a shared instance and use a new database per test run.
    Remove the acceptance test that creates instances.

--- google-cloud-spanner/acceptance/spanner/instance_test.rb
+++ google-cloud-spanner/acceptance/spanner/instance_test.rb
@@ -15,46 +15,6 @@
 require "spanner_helper"
 
 describe "Spanner Instances", :spanner do
-  it "creates, updates, and deletes an instance" do
-    instance_id = "#{$spanner_prefix}-empty"
-    name = "#{$spanner_prefix} Empty"
-    config = "regional-us-central1"
-
-    spanner.instance(instance_id).must_be :nil?
-
-    job = spanner.create_instance instance_id, name: name, config: config, nodes: 1, labels: { env: :development }
-
-    job.must_be_kind_of Google::Cloud::Spanner::Instance::Job
-    job.wont_be :done?
-    job.wait_until_done!
-
-    job.must_be :done?
-    raise Google::Cloud::Error.from_error(job.error) if job.error?
-    instance = job.instance
-    instance.wont_be :nil?
-    instance.must_be_kind_of Google::Cloud::Spanner::Instance
-    instance.instance_id.must_equal instance_id
-    instance.name.must_equal name
-    instance.config.instance_config_id.must_equal config
-    instance.nodes.must_equal 1
-    map_to_hash(instance.labels).must_equal({ "env" => "development" })
-
-    spanner.instance(instance_id).wont_be :nil?
-
-    new_name = instance.name.reverse
-    instance.name = new_name
-    instance.nodes = 2
-    instance.labels["env"] = "production"
-    instance.save
-
-    instance.name.must_equal new_name
-    instance.nodes.must_equal 2
-    map_to_hash(instance.labels).must_equal({ "env" => "production" })
-
-    instance.delete
-    spanner.instance(instance_id).must_be :nil?
-  end
-
   it "lists and gets instances" do
     all_instances = spanner.instances.all.to_a
     all_instances.wont_be :empty?
@@ -64,14 +24,5 @@ describe "Spanner Instances", :spanner do
 
     first_instance = spanner.instance all_instances.first.instance_id
     first_instance.must_be_kind_of Google::Cloud::Spanner::Instance
-  end
-
-  def map_to_hash map
-    if map.respond_to? :to_h
-      map.to_h
-    else
-      # Enumerable doesn't have to_h on ruby 2.0...
-      Hash[map.to_a]
-    end
   end
 end
