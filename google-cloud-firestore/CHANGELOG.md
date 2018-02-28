04d0ee3ce5bc800174256a51730f7f33d612ec60
Mike Moore <mike@blowmage.com>
2018-02-27 18:10:57 -0700

    Release Firestore 0.21.0
    * Add Shared Configuration.

--- google-cloud-firestore/CHANGELOG.md
+++ google-cloud-firestore/CHANGELOG.md
@@ -1,5 +1,9 @@
 # Release History
 
+### 0.21.0 / 2018-02-27
+
+* Add Shared Configuration.
+
 ### 0.20.0 / 2018-01-10
 
 * First release
--- google-cloud-firestore/lib/google/cloud/firestore/version.rb
+++ google-cloud-firestore/lib/google/cloud/firestore/version.rb
@@ -16,7 +16,7 @@
 module Google
   module Cloud
     module Firestore
-      VERSION = "0.20.0".freeze
+      VERSION = "0.21.0".freeze
     end
   end
 end
