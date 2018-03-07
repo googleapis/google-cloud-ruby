6bfc260260afa0b37894145e2188d64ffbdc07b8
Ernest Landrito <landrito@google.com>
2018-03-07 00:23:47 -0800

    Generate Bigquery Data Transfer
    Command Log:
    
    ```
    set DEST ~/workspace/google-cloud-ruby
    git clone https://github.com/googleapis/googleapis.git
    cd googleapis
    artman --config google/cloud/bigquery/datatransfer/artman_bigquerydatatransfer.yaml generate ruby_gapic
    cp -rv ./artman-genfiles/ruby/google-cloud-ruby/google-cloud-bigquerydatatransfer $DEST
    ```

---
+++ google-cloud-bigquery-data_transfer/lib/google/cloud/bigquery/data_transfer.rb
@@ -0,0 +1,132 @@
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require "google/gax"
+require "pathname"
+
+module Google
+  module Cloud
+    module Bigquery
+      # rubocop:disable LineLength
+
+      ##
+      # # Ruby Client for BigQuery Data Transfer API ([Beta](https://github.com/GoogleCloudPlatform/google-cloud-ruby#versioning))
+      #
+      # [BigQuery Data Transfer API][Product Documentation]:
+      # Transfers data from partner SaaS applications to Google BigQuery on a
+      # scheduled, managed basis.
+      # - [Product Documentation][]
+      #
+      # ## Quick Start
+      # In order to use this library, you first need to go through the following
+      # steps:
+      #
+      # 1. [Select or create a Cloud Platform project.](https://console.cloud.google.com/project)
+      # 2. [Enable billing for your project.](https://cloud.google.com/billing/docs/how-to/modify-project#enable_billing_for_a_project)
+      # 3. [Enable the BigQuery Data Transfer API.](https://console.cloud.google.com/apis/api/bigquerydatatransfer)
+      # 4. [Setup Authentication.](https://googlecloudplatform.github.io/google-cloud-ruby/#/docs/google-cloud/master/guides/authentication)
+      #
+      # ### Preview
+      # #### DataTransferServiceClient
+      # ```rb
+      # require "google/cloud/bigquery/data_transfer"
+      #
+      # data_transfer_service_client = Google::Cloud::Bigquery::DataTransfer.new
+      # formatted_parent = Google::Cloud::Bigquery::DataTransfer::V1::DataTransferServiceClient.project_path(project_id)
+      #
+      # # Iterate over all results.
+      # data_transfer_service_client.list_data_sources(formatted_parent).each do |element|
+      #   # Process element.
+      # end
+      #
+      # # Or iterate over results one page at a time.
+      # data_transfer_service_client.list_data_sources(formatted_parent).each_page do |page|
+      #   # Process each page at a time.
+      #   page.each do |element|
+      #     # Process element.
+      #   end
+      # end
+      # ```
+      #
+      # ### Next Steps
+      # - Read the [BigQuery Data Transfer API Product documentation][Product Documentation]
+      #   to learn more about the product and see How-to Guides.
+      # - View this [repository's main README](https://github.com/GoogleCloudPlatform/google-cloud-ruby/blob/master/README.md)
+      #   to see the full list of Cloud APIs that we cover.
+      #
+      # [Product Documentation]: https://cloud.google.com/bigquerydatatransfer
+      #
+      #
+      module DataTransfer
+        # rubocop:enable LineLength
+
+        FILE_DIR = File.realdirpath(Pathname.new(__FILE__).join("..").join("data_transfer"))
+
+        AVAILABLE_VERSIONS = Dir["#{FILE_DIR}/*"]
+          .select { |file| File.directory?(file) }
+          .select { |dir| Google::Gax::VERSION_MATCHER.match(File.basename(dir)) }
+          .select { |dir| File.exist?(dir + ".rb") }
+          .map { |dir| File.basename(dir) }
+
+        ##
+        # The Google BigQuery Data Transfer Service API enables BigQuery users to
+        # configure the transfer of their data from other Google Products into BigQuery.
+        # This service contains methods that are end user exposed. It backs up the
+        # frontend.
+        #
+        # @param version [Symbol, String]
+        #   The major version of the service to be used. By default :v1
+        #   is used.
+        # @overload new(version:, credentials:, scopes:, client_config:, timeout:)
+        #   @param credentials [Google::Auth::Credentials, String, Hash, GRPC::Core::Channel, GRPC::Core::ChannelCredentials, Proc]
+        #     Provides the means for authenticating requests made by the client. This parameter can
+        #     be many types.
+        #     A `Google::Auth::Credentials` uses a the properties of its represented keyfile for
+        #     authenticating requests made by this client.
+        #     A `String` will be treated as the path to the keyfile to be used for the construction of
+        #     credentials for this client.
+        #     A `Hash` will be treated as the contents of a keyfile to be used for the construction of
+        #     credentials for this client.
+        #     A `GRPC::Core::Channel` will be used to make calls through.
+        #     A `GRPC::Core::ChannelCredentials` for the setting up the RPC client. The channel credentials
+        #     should already be composed with a `GRPC::Core::CallCredentials` object.
+        #     A `Proc` will be used as an updater_proc for the Grpc channel. The proc transforms the
+        #     metadata for requests, generally, to give OAuth credentials.
+        #   @param scopes [Array<String>]
+        #     The OAuth scopes for this service. This parameter is ignored if
+        #     an updater_proc is supplied.
+        #   @param client_config [Hash]
+        #     A Hash for call options for each method. See
+        #     Google::Gax#construct_settings for the structure of
+        #     this data. Falls back to the default config if not specified
+        #     or the specified config is missing data points.
+        #   @param timeout [Numeric]
+        #     The default timeout, in seconds, for calls made through this client.
+        def self.new(*args, version: :v1, **kwargs)
+          unless AVAILABLE_VERSIONS.include?(version.to_s.downcase)
+            raise "The version: #{version} is not available. The available versions " \
+              "are: [#{AVAILABLE_VERSIONS.join(", ")}]"
+          end
+
+          require "#{FILE_DIR}/#{version.to_s.downcase}"
+          version_module = Google::Cloud::Bigquery::DataTransfer
+            .constants
+            .select {|sym| sym.to_s.downcase == version.to_s.downcase}
+            .first
+          Google::Cloud::Bigquery::DataTransfer.const_get(version_module).new(*args, **kwargs)
+        end
+      end
+    end
+  end
+end
---
+++ google-cloud-bigquery-data_transfer/lib/google/cloud/bigquery/data_transfer/v1/doc/overview.rb
@@ -0,0 +1,79 @@
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+module Google
+  module Cloud
+    module Bigquery
+      # rubocop:disable LineLength
+
+      ##
+      # # Ruby Client for BigQuery Data Transfer API ([Beta](https://github.com/GoogleCloudPlatform/google-cloud-ruby#versioning))
+      #
+      # [BigQuery Data Transfer API][Product Documentation]:
+      # Transfers data from partner SaaS applications to Google BigQuery on a
+      # scheduled, managed basis.
+      # - [Product Documentation][]
+      #
+      # ## Quick Start
+      # In order to use this library, you first need to go through the following
+      # steps:
+      #
+      # 1. [Select or create a Cloud Platform project.](https://console.cloud.google.com/project)
+      # 2. [Enable billing for your project.](https://cloud.google.com/billing/docs/how-to/modify-project#enable_billing_for_a_project)
+      # 3. [Enable the BigQuery Data Transfer API.](https://console.cloud.google.com/apis/api/bigquerydatatransfer)
+      # 4. [Setup Authentication.](https://googlecloudplatform.github.io/google-cloud-ruby/#/docs/google-cloud/master/guides/authentication)
+      #
+      # ### Installation
+      # ```
+      # $ gem install google-cloud-bigquery-data_transfer
+      # ```
+      #
+      # ### Preview
+      # #### DataTransferServiceClient
+      # ```rb
+      # require "google/cloud/bigquery/data_transfer"
+      #
+      # data_transfer_service_client = Google::Cloud::Bigquery::DataTransfer.new
+      # formatted_parent = Google::Cloud::Bigquery::DataTransfer::V1::DataTransferServiceClient.project_path(project_id)
+      #
+      # # Iterate over all results.
+      # data_transfer_service_client.list_data_sources(formatted_parent).each do |element|
+      #   # Process element.
+      # end
+      #
+      # # Or iterate over results one page at a time.
+      # data_transfer_service_client.list_data_sources(formatted_parent).each_page do |page|
+      #   # Process each page at a time.
+      #   page.each do |element|
+      #     # Process element.
+      #   end
+      # end
+      # ```
+      #
+      # ### Next Steps
+      # - Read the [BigQuery Data Transfer API Product documentation][Product Documentation]
+      #   to learn more about the product and see How-to Guides.
+      # - View this [repository's main README](https://github.com/GoogleCloudPlatform/google-cloud-ruby/blob/master/README.md)
+      #   to see the full list of Cloud APIs that we cover.
+      #
+      # [Product Documentation]: https://cloud.google.com/bigquerydatatransfer
+      #
+      #
+      module DataTransfer
+        module V1
+        end
+      end
+    end
+  end
+end
