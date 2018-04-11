9bfb874e691cf88f6579630058508bbbabb05546
Mike Moore <mike@blowmage.com>
2018-04-11 12:49:42 -0600

    Fix tests for developer environments
    The Bigtable tests are failing on some developer systems because
    the tests expect no environment variables are set.
    Make the tests more resilient by clearing env vars in test setup.

--- google-cloud-bigtable/test/google/cloud/bigtable_test.rb
+++ google-cloud-bigtable/test/google/cloud/bigtable_test.rb
@@ -9,17 +9,19 @@ describe Google::Cloud do
       it "passes #{keyfile && "keyfile"} to Google::Cloud.bigtable to get #{client_class.name}" do
         gcloud = Google::Cloud.new("project-id", keyfile)
 
-        File.stub :file?, true, ["path/to/keyfile.json"] do
-          File.stub :read, found_credentials, ["path/to/keyfile.json"] do
-            Google::Cloud::Bigtable::Credentials.stub :default, default_credentials do
-              client = gcloud.bigtable(client_type: client_type, instance_id: instance_id)
-              assert_instance_of(client_class, client)
-              assert_equal("project-id", client.project_id)
-              assert_nil(client.options[:scopes])
-              assert_nil(client.options[:client_config])
-              assert_nil(client.options[:timeout])
+        ENV.stub :[], nil do
+          File.stub :file?, true, ["path/to/keyfile.json"] do
+            File.stub :read, found_credentials, ["path/to/keyfile.json"] do
+              Google::Cloud::Bigtable::Credentials.stub :default, default_credentials do
+                client = gcloud.bigtable(client_type: client_type, instance_id: instance_id)
+                assert_instance_of(client_class, client)
+                assert_equal("project-id", client.project_id)
+                assert_nil(client.options[:scopes])
+                assert_nil(client.options[:client_config])
+                assert_nil(client.options[:timeout])
 
-              yield self, client if block_given?
+                yield self, client if block_given?
+              end
             end
           end
         end
