86691946ccf63502bdf36c3830c25a052cfd206f
Chris Smith <quartzmo@gmail.com>
2018-05-23 16:19:53 -0600

    Update documentation guides for generated gRPC packages
    Add documentation for enabling gRPC logging.
    
    [closes #1907]

--- google-cloud-video_intelligence/lib/google/cloud/video_intelligence.rb
+++ google-cloud-video_intelligence/lib/google/cloud/video_intelligence.rb
@@ -73,6 +73,39 @@ module Google
     # operation.wait_until_done!
     # ```
     #
+    # ## Enabling Logging
+    #
+    # To enable logging for this library, set the logger for the underlying
+    # [gRPC](https://github.com/grpc/grpc/tree/master/src/ruby) library. The
+    # logger that you set may be a Ruby stdlib
+    # [`Logger`](https://ruby-doc.org/stdlib-2.5.0/libdoc/logger/rdoc/Logger.html)
+    # as shown below, or a
+    # [`Google::Cloud::Logging::Logger`](https://googlecloudplatform.github.io/google-cloud-ruby/#/docs/google-cloud-logging/latest/google/cloud/logging/logger)
+    # that will write logs to [Stackdriver
+    # Logging](https://cloud.google.com/logging/). See
+    # [grpc/logconfig.rb](https://github.com/grpc/grpc/blob/master/src/ruby/lib/grpc/logconfig.rb)
+    # and the gRPC
+    # [spec_helper.rb](https://github.com/grpc/grpc/blob/master/src/ruby/spec/spec_helper.rb)
+    # for additional information.
+    #
+    # Configuring a Ruby stdlib logger:
+    #
+    # ```ruby
+    # require "logger"
+    #
+    # module MyLogger
+    #   LOGGER = Logger.new $stderr, level: Logger::WARN
+    #   def logger
+    #     LOGGER
+    #   end
+    # end
+    #
+    # # Define a gRPC module-level logger method before grpc/logconfig.rb loads.
+    # module GRPC
+    #   extend MyLogger
+    # end
+    # ```
+    #
     # ### Next Steps
     # - Read the [Cloud Video Intelligence API Product documentation][Product Documentation]
     #   to learn more about the product and see How-to Guides.
