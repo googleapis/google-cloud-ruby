2806d0382284c5d8c97de48280b545acb66e8362
Chris Smith <quartzmo@gmail.com>
2018-05-23 15:41:40 -0600

    Update documentation guides for manual gRPC packages
    Add documentation for enabling gRPC logging.
    
    [refs #1907]

--- google-cloud-firestore/lib/google/cloud/firestore.rb
+++ google-cloud-firestore/lib/google/cloud/firestore.rb
@@ -40,6 +40,39 @@ module Google
     # options for connecting in the [Authentication
     # Guide](https://googlecloudplatform.github.io/google-cloud-ruby/#/docs/guides/authentication).
     #
+    # ## Enabling Logging
+    #
+    # To enable logging for this library, set the logger for the underlying
+    # [gRPC](https://github.com/grpc/grpc/tree/master/src/ruby) library. The
+    # logger that you set may be a Ruby stdlib
+    # [`Logger`](https://ruby-doc.org/stdlib-2.5.0/libdoc/logger/rdoc/Logger.html)
+    # as shown below, or a
+    # [`Google::Cloud::Logging::Logger`](https://googlecloudplatform.github.io/google-cloud-ruby/#/docs/google-cloud-logging/latest/google/cloud/logging/logger)
+    # that will write logs to [Stackdriver
+    # Logging](https://cloud.google.com/logging/). See
+    # [grpc/logconfig.rb](https://github.com/grpc/grpc/blob/master/src/ruby/lib/grpc/logconfig.rb)
+    # and the gRPC
+    # [spec_helper.rb](https://github.com/grpc/grpc/blob/master/src/ruby/spec/spec_helper.rb)
+    # for additional information.
+    #
+    # Configuring a Ruby stdlib logger:
+    #
+    # ```ruby
+    # require "logger"
+    #
+    # module MyLogger
+    #   LOGGER = Logger.new $stderr, level: Logger::WARN
+    #   def logger
+    #     LOGGER
+    #   end
+    # end
+    #
+    # # Define a gRPC module-level logger method before grpc/logconfig.rb loads.
+    # module GRPC
+    #   extend MyLogger
+    # end
+    # ```
+    #
     # ## Adding data
     #
     # Cloud Firestore stores data in Documents, which are stored in Collections.
--- google-cloud-pubsub/lib/google/cloud/pubsub.rb
+++ google-cloud-pubsub/lib/google/cloud/pubsub.rb
@@ -51,6 +51,39 @@ module Google
     # To learn more about Pub/Sub, read the [Google Cloud Pub/Sub Overview
     # ](https://cloud.google.com/pubsub/overview).
     #
+    # ## Enabling Logging
+    #
+    # To enable logging for this library, set the logger for the underlying
+    # [gRPC](https://github.com/grpc/grpc/tree/master/src/ruby) library. The
+    # logger that you set may be a Ruby stdlib
+    # [`Logger`](https://ruby-doc.org/stdlib-2.5.0/libdoc/logger/rdoc/Logger.html)
+    # as shown below, or a
+    # [`Google::Cloud::Logging::Logger`](https://googlecloudplatform.github.io/google-cloud-ruby/#/docs/google-cloud-logging/latest/google/cloud/logging/logger)
+    # that will write logs to [Stackdriver
+    # Logging](https://cloud.google.com/logging/). See
+    # [grpc/logconfig.rb](https://github.com/grpc/grpc/blob/master/src/ruby/lib/grpc/logconfig.rb)
+    # and the gRPC
+    # [spec_helper.rb](https://github.com/grpc/grpc/blob/master/src/ruby/spec/spec_helper.rb)
+    # for additional information.
+    #
+    # Configuring a Ruby stdlib logger:
+    #
+    # ```ruby
+    # require "logger"
+    #
+    # module MyLogger
+    #   LOGGER = Logger.new $stderr, level: Logger::WARN
+    #   def logger
+    #     LOGGER
+    #   end
+    # end
+    #
+    # # Define a gRPC module-level logger method before grpc/logconfig.rb loads.
+    # module GRPC
+    #   extend MyLogger
+    # end
+    # ```
+    #
     # ## Retrieving Topics
     #
     # A Topic is a named resource to which messages are sent by publishers.
