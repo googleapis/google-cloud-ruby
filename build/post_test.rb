4ed4cc87b1928238d0b28584a1898d54c095f211
Daniel Azuma <dazuma@google.com>
2018-06-12 16:24:31 -0700

    Update build scripts to use the smaller matrix of ruby versions

--- build/post_test.rb
+++ build/post_test.rb
@@ -1,8 +1,8 @@
 require "pty"
 
 commands = [
-  "rvm-exec 2.5.0 bundle update; rvm-exec 2.5.0 bundle exec rake circleci:post",
-  "rvm-exec 2.4.2 bundle update; rvm-exec 2.4.2 bundle exec rake test:coveralls"
+  "rvm-exec 2.5.1 bundle update; rvm-exec 2.5.1 bundle exec rake circleci:post",
+  "rvm-exec 2.4.4 bundle update; rvm-exec 2.4.4 bundle exec rake test:coveralls"
 ]
 
 node_index = Integer ENV["CIRCLE_NODE_INDEX"]
