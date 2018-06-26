60f820cc2d872dd4d2bb14e5830b398de9297161
TheRoyalTnetennba <paye@google.com>
2018-06-26 15:28:59 -0700

    add coments detailing manual steps to synth.py

--- google-cloud-tasks/synth.py
+++ google-cloud-tasks/synth.py
@@ -20,10 +20,23 @@ s.replace(
     "assert_not_nil",
     "refute_nil")
 
+# Adds grpc-google-iam-v1 to gemspec
 s.replace(
 	'google-cloud-tasks.gemspec',
 	'gem.add_dependency "google-gax", "~> 1.0"',
 	('gem.add_dependency "google-gax", "~> 1.0"' + "\n" + '  gem.add_dependency "grpc-google-iam-v1", "~> 0.6.9"')
 )
 
-# lib/google/cloud/tasks/ Move Credentials class under the V2beta2 module
+
+
+# Manual steps: 
+#     1) lib/google/cloud/tasks/ Move Credentials class under the V2beta2 module
+#     2) Add """
+# 			if Gem::Version.new(RUBY_VERSION) < Gem::Version.new("2.1")
+# 			  # WORKAROUND: builds are failing on Ruby 2.0.
+# 			  # We think this is because of a bug in Bundler 1.6.
+# 			  # Specify a viable version to allow the build to succeed.
+# 			  gem "jwt", "~> 1.5"
+# 			  gem "kramdown", "< 1.17.0" # Error in yard with 1.17.0
+# 			end
+# 			""" to gemfile
