62fafef51fb84cab08d937db9e8ba278b50c8beb
Daniel Azuma <dazuma@google.com>
2018-06-26 12:03:28 -0700

    Update synth scripts for vision and text-to-speech

--- google-cloud-text_to_speech/synth.py
+++ google-cloud-text_to_speech/synth.py
@@ -21,27 +21,13 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 
 gapic = gcp.GAPICGenerator()
-common = gcp.CommonTemplates()
 
-v1_library = gapic._generate_code(
-    'texttospeech', 'v1', 'ruby',
-    config_path='artman_texttospeech_v1.yaml',
-    artman_output_name='google-cloud-ruby/google-cloud-texttospeech')
+v1_library = gapic.ruby_library(
+    'texttospeech', 'v1',
+    artman_output_name='google-cloud-ruby/google-cloud-texttospeech'
+)
 
-s.copy(v1_library / "lib/google/cloud/text_to_speech/v1")
-s.copy(v1_library / "lib/google/cloud/text_to_speech/v1.rb")
-s.copy(v1_library / "lib/google/cloud/texttospeech/v1")
-s.copy(v1_library / "test/google/cloud/text_to_speech/v1")
-
-# Temporary until https://github.com/googleapis/gapic-generator/pull/2079
-# shows up in a build.
-s.replace(
-    "lib/google/cloud/text_to_speech/v1/text_to_speech_client.rb",
-    'require "google/cloud/text_to_speech/credentials"',
-    'require "google/cloud/text_to_speech/v1/credentials"')
-
-# https://github.com/googleapis/gapic-generator/issues/2080
-s.replace(
-    "test/google/cloud/text_to_speech/v1/text_to_speech_client_test.rb",
-    "assert_not_nil",
-    "refute_nil")
+s.copy(v1_library / 'lib/google/cloud/text_to_speech/v1')
+s.copy(v1_library / 'lib/google/cloud/text_to_speech/v1.rb')
+s.copy(v1_library / 'lib/google/cloud/texttospeech/v1')
+s.copy(v1_library / 'test/google/cloud/text_to_speech/v1')
