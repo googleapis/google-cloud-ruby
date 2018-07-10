94ff06c100563d7777a00793cb5ff00d12475f6c
TheRoyalTnetennba <paye@google.com>
2018-07-10 14:57:35 -0700

    update synth.py to copy Rakefile, etc

--- google-cloud-tasks/synth.py
+++ google-cloud-tasks/synth.py
@@ -7,11 +7,17 @@ logging.basicConfig(level=logging.DEBUG)
 
 gapic = gcp.GAPICGenerator()
 
-v1_library = gapic.ruby_library(
+v2_library = gapic.ruby_library(
     'tasks', 'v2beta2', artman_output_name='google-cloud-ruby/google-cloud-tasks',
     config_path='artman_cloudtasks.yaml'
 )
 
-s.copy(v1_library / 'lib')
-s.copy(v1_library / 'test')
-s.copy(v1_libary / 'README.md')
+# Copy everything but Gemfile, .gemspec, and Changelog.md
+s.copy(v2_library / 'lib')
+s.copy(v2_library / 'test')
+s.copy(v2_library / 'Rakefile')
+s.copy(v2_library / 'README.md')
+s.copy(v2_library / 'LICENSE')
+s.copy(v2_library / '.gitignore')
+s.copy(v2_library / '.rubocop.yml')
+s.copy(v2_library / '.yardopts')
