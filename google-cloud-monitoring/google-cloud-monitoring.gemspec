b4cea365e878a63852e0375989b7779fb83a00cd
Daniel Azuma <dazuma@google.com>
2018-08-06 11:21:44 -0700

    Expand fixes to cover all documentation files

--- google-cloud-monitoring/google-cloud-monitoring.gemspec
+++ google-cloud-monitoring/google-cloud-monitoring.gemspec
@@ -23,8 +23,8 @@ Gem::Specification.new do |gem|
   gem.add_dependency "google-gax", "~> 1.3"
 
   gem.add_development_dependency "minitest", "~> 5.10"
+  gem.add_development_dependency "redcarpet", "~> 3.0"
   gem.add_development_dependency "rubocop", "~> 0.50.0"
   gem.add_development_dependency "simplecov", "~> 0.9"
-  gem.add_development_dependency "redcarpet", "~> 3.0"
   gem.add_development_dependency "yard", "~> 0.9"
 end
--- google-cloud-monitoring/synth.py
+++ google-cloud-monitoring/synth.py
@@ -8,13 +8,23 @@ logging.basicConfig(level=logging.DEBUG)
 
 gapic = gcp.GAPICGenerator()
 
+# Temporary until we get Ruby-specific tools into synthtool
+def merge_gemspec(src, dest, path):
+    regex = re.compile(r'^\s+gem.version\s*=\s*"[\d\.]+"$', flags=re.MULTILINE)
+    match = regex.search(dest)
+    if match:
+        src = regex.sub(match.group(0), src, count=1)
+    regex = re.compile(r'^\s+gem.homepage\s*=\s*"[^"]+"$', flags=re.MULTILINE)
+    match = regex.search(dest)
+    if match:
+        src = regex.sub(match.group(0), src, count=1)
+    return src
+
 v3_library = gapic.ruby_library(
     'monitoring', 'v3',
     config_path='/google/monitoring/artman_monitoring.yaml',
     artman_output_name='google-cloud-ruby/google-cloud-monitoring'
 )
-
-# Copy everything but Gemfile, .gemspec, and Changelog.md
 s.copy(v3_library / 'acceptance')
 s.copy(v3_library / 'lib')
 s.copy(v3_library / 'test')
@@ -23,6 +33,7 @@ s.copy(v3_library / 'LICENSE')
 s.copy(v3_library / '.gitignore')
 s.copy(v3_library / '.rubocop.yml')
 s.copy(v3_library / '.yardopts')
+s.copy(v3_library / 'google-cloud-monitoring.gemspec', merge=merge_gemspec)
 
 # PERMANENT: Because lib/google-cloud-monitoring.rb is present and handwritten.
 s.replace(
@@ -48,7 +59,12 @@ s.replace(
 
 # https://github.com/googleapis/gapic-generator/issues/2195
 s.replace(
-    'README.md',
+    [
+      'README.md',
+      'lib/google/cloud/monitoring.rb',
+      'lib/google/cloud/monitoring/v3.rb',
+      'lib/google/cloud/monitoring/v3/doc/overview.rb'
+    ],
     '\\(https://console\\.cloud\\.google\\.com/apis/api/monitoring\\)',
     '(https://console.cloud.google.com/apis/library/monitoring.googleapis.com)')
 
@@ -57,20 +73,6 @@ s.replace(
     '.yardopts',
     '\n--markup markdown\n',
     '\n--markup markdown\n--markup-provider redcarpet\n')
-
-# Temporary until we get Ruby-specific tools into synthtool
-def merge_gemspec(src, dest, path):
-    regex = re.compile(r'^\s+gem.version\s*=\s*"[\d\.]+"$', flags=re.MULTILINE)
-    match = regex.search(dest)
-    if match:
-        src = regex.sub(match.group(0), src, count=1)
-    regex = re.compile(r'^\s+gem.homepage\s*=\s*"[^"]+"$', flags=re.MULTILINE)
-    match = regex.search(dest)
-    if match:
-        src = regex.sub(match.group(0), src, count=1)
-    return src
-
-s.copy(v3_library / 'google-cloud-monitoring.gemspec', merge=merge_gemspec)
 
 # https://github.com/googleapis/gapic-generator/issues/2194
 s.replace(
