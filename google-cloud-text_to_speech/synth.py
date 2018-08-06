6374aaa764c2180cafa91a79fcc25aa081b19578
Daniel Azuma <dazuma@gmail.com>
2018-08-06 08:25:58 -0700

    Update synth script for text_to_speech (#2225)

--- google-cloud-text_to_speech/synth.py
+++ google-cloud-text_to_speech/synth.py
@@ -17,6 +17,7 @@
 import synthtool as s
 import synthtool.gcp as gcp
 import logging
+import re
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -27,7 +28,63 @@ v1_library = gapic.ruby_library(
     artman_output_name='google-cloud-ruby/google-cloud-texttospeech'
 )
 
-s.copy(v1_library / 'lib/google/cloud/text_to_speech/v1')
-s.copy(v1_library / 'lib/google/cloud/text_to_speech/v1.rb')
-s.copy(v1_library / 'lib/google/cloud/texttospeech/v1')
-s.copy(v1_library / 'test/google/cloud/text_to_speech/v1')
+s.copy(v1_library / 'lib')
+s.copy(v1_library / 'test')
+s.copy(v1_library / 'README.md')
+s.copy(v1_library / 'LICENSE')
+s.copy(v1_library / '.gitignore')
+s.copy(v1_library / '.rubocop.yml')
+s.copy(v1_library / '.yardopts')
+
+# https://github.com/googleapis/gapic-generator/issues/2174
+s.replace(
+    'lib/google/cloud/text_to_speech.rb',
+    'File\.join\(dir, "\.rb"\)',
+    'dir + ".rb"')
+
+# https://github.com/googleapis/gapic-generator/issues/2182
+s.replace(
+    'lib/google/cloud/text_to_speech/v1/credentials.rb',
+    'TEXTTOSPEECH_KEYFILE\\n(\s+)TEXTTOSPEECH_CREDENTIALS\n',
+    'TEXTTOSPEECH_CREDENTIALS\\n\\1TEXTTOSPEECH_KEYFILE\n')
+s.replace(
+    'lib/google/cloud/text_to_speech/v1/credentials.rb',
+    'TEXTTOSPEECH_KEYFILE_JSON\\n(\s+)TEXTTOSPEECH_CREDENTIALS_JSON\n',
+    'TEXTTOSPEECH_CREDENTIALS_JSON\\n\\1TEXTTOSPEECH_KEYFILE_JSON\n')
+
+# https://github.com/googleapis/gapic-generator/issues/2194
+s.replace(
+    '.yardopts',
+    '\n--markup markdown\n\n',
+    '\n--markup markdown\n--markup-provider redcarpet\n\n')
+
+# https://github.com/googleapis/gapic-generator/issues/2195
+s.replace(
+    'README.md',
+    '\\(https://console\\.cloud\\.google\\.com/apis/api/texttospeech\\)',
+    '(https://console.cloud.google.com/apis/library/texttospeech.googleapis.com)')
+
+# Temporary until we get Ruby-specific tools into synthtool
+def merge_gemspec(src, dest, path):
+    regex = re.compile(r'^\s+gem.version\s*=\s*"[\d\.]+"$', flags=re.MULTILINE)
+    match = regex.search(dest)
+    if match:
+        src = regex.sub(match.group(0), src, count=1)
+    regex = re.compile(r'^\s+gem.homepage\s*=\s*"[^"]+"$', flags=re.MULTILINE)
+    match = regex.search(dest)
+    if match:
+        src = regex.sub(match.group(0), src, count=1)
+    return src
+
+s.copy(v1_library / 'google-cloud-text_to_speech.gemspec', merge=merge_gemspec)
+
+
+# https://github.com/googleapis/gapic-generator/issues/2194
+s.replace(
+    'google-cloud-text_to_speech.gemspec',
+    '\n  gem\\.add_development_dependency "minitest", "~> ([\\d\\.]+)"\n  gem\\.add_development_dependency "rubocop"',
+    '\n  gem.add_development_dependency "minitest", "~> \\1"\n  gem.add_development_dependency "redcarpet", "~> 3.0"\n  gem.add_development_dependency "rubocop"')
+s.replace(
+    'google-cloud-text_to_speech.gemspec',
+    '\n  gem\\.add_development_dependency "simplecov", "~> ([\\d\\.]+)"\nend',
+    '\n  gem.add_development_dependency "simplecov", "~> \\1"\n  gem.add_development_dependency "yard", "~> 0.9"\nend')
