7134f1658809acd9cc9d2a633077402505c0f281
Daniel Azuma <dazuma@gmail.com>
2018-08-07 13:49:51 -0700

    Create synth script for spanner (#2250)

--- google-cloud-spanner/lib/google/cloud/spanner/v1/doc/google/spanner/v1/spanner.rb
+++ google-cloud-spanner/lib/google/cloud/spanner/v1/doc/google/spanner/v1/spanner.rb
@@ -188,11 +188,17 @@ module Google
       # PartitionReadRequest.
       # @!attribute [rw] partition_size_bytes
       #   @return [Integer]
+      #     **Note:** This hint is currently ignored by PartitionQuery and
+      #     PartitionRead requests.
+      #
       #     The desired data size for each partition generated.  The default for this
       #     option is currently 1 GiB.  This is only a hint. The actual size of each
       #     partition may be smaller or larger than this size request.
       # @!attribute [rw] max_partitions
       #   @return [Integer]
+      #     **Note:** This hint is currently ignored by PartitionQuery and
+      #     PartitionRead requests.
+      #
       #     The desired maximum number of partitions to return.  For example, this may
       #     be set to the number of workers available.  The default for this option
       #     is currently 10,000. The maximum value is currently 200,000.  This is only
