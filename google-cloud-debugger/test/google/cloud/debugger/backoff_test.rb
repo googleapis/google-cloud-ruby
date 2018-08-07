45a3991fce5cf9987de8ef691aaaf28ea3473a85
Daniel Azuma <dazuma@gmail.com>
2018-08-07 13:58:20 -0700

    Fix intermittent debugger backoff test failures (#2261)

--- google-cloud-debugger/test/google/cloud/debugger/backoff_test.rb
+++ google-cloud-debugger/test/google/cloud/debugger/backoff_test.rb
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+require "helper"
 
 describe Google::Cloud::Debugger::Backoff do
   let(:backoff) { Google::Cloud::Debugger::Backoff.new }
