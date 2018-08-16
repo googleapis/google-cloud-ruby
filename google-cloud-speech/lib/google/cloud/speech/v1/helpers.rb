1c4378d241064e6523886b6124dbf101a4bb1ee8
Mike Moore <mike@blowmage.com>
2018-08-15 23:32:29 -0600

    Add get_operation support
    This was part of the previous hand-written API, but is missing from the
    generated GAPIC API. This functionality has not yet been generalized
    in the generated code, so add this with the same hooks used to support
    the streaming helper code.

--- google-cloud-speech/lib/google/cloud/speech/v1/helpers.rb
+++ google-cloud-speech/lib/google/cloud/speech/v1/helpers.rb
@@ -14,6 +14,7 @@
 
 require "google/cloud/speech/v1/speech_client"
 require "google/cloud/speech/v1/stream"
+require "uri"
 
 module Google
   module Cloud
@@ -85,6 +86,124 @@ module Google
                 @streaming_recognize.call(request_protos, options)
               end
             )
+          end
+
+          ##
+          # Gets the latest state of a long-running operation. Clients can use
+          # this method to poll the operation result at intervals as recommended
+          # by the API service.
+          #
+          # @param name [String]
+          #   The name of the operation resource.
+          # @param options [Google::Gax::CallOptions]
+          #   Overrides the default settings for this call, e.g, timeout,
+          #   retries, etc.
+          # @return [Google::Gax::Operation]
+          # @raise [Google::Gax::GaxError] if the RPC is aborted.
+          # @example
+          #   require "google/cloud/speech"
+          #
+          #   speech_client = Google::Cloud::Speech.new version: :v1
+          #
+          #   op = speech_client.get_operation "-"
+          #
+          #   # Process error operations.
+          #   log_error op.error if op.error?
+          #
+          #   if op.done?
+          #     # Process completed operations.
+          #     log_finished op.response, op.metadata
+          #   else
+          #     # Process pending operations.
+          #     log_pending op.name, op.metadata
+          #   end
+          #
+          def get_operation name, options: nil
+            proto_op = @operations_client.get_operation name, options: options
+            wrap_proto_op_with_gax_op proto_op, options
+          end
+
+          ##
+          # Lists operations that match the specified filter in the request. If
+          # the server doesn't support this method, it returns `UNIMPLEMENTED`.
+          #
+          # NOTE: the `name` binding below allows API services to override the
+          # binding to use different resource name schemes, such as
+          # `users/*/operations`.
+          #
+          # @param name [String]
+          #   The name of the operation collection.
+          # @param filter [String]
+          #   The standard list filter.
+          # @param page_size [Integer]
+          #   The maximum number of resources contained in the underlying API
+          #   response. If page streaming is performed per-resource, this
+          #   parameter does not affect the return value. If page streaming is
+          #   performed per-page, this determines the maximum number of
+          #   resources in a page.
+          # @param options [Google::Gax::CallOptions]
+          #   Overrides the default settings for this call, e.g, timeout,
+          #   retries, etc.
+          # @return [Google::Gax::PagedEnumerable<Google::Gax::Operation>]
+          #   An enumerable of Google::Gax::Operation instances. See
+          #   Google::Gax::PagedEnumerable documentation for other operations
+          #   such as per-page iteration or access to the response object.
+          # @raise [Google::Gax::GaxError] if the RPC is aborted.
+          # @example
+          #   require "google/cloud/speech"
+          #
+          #   speech_client = Google::Cloud::Speech.new version: :v1
+          #
+          #   # List all callbacks.
+          #   speech_client.list_operations "*", "" do |op|
+          #     # Process error operations.
+          #     log_error op.error if op.error?
+          #
+          #     if op.done?
+          #       # Process completed operations.
+          #       log_finished op.response, op.metadata
+          #     else
+          #       # Process pending operations.
+          #       log_pending op.name, op.metadata
+          #     end
+          #   end
+          #
+          def list_operations name, filter, page_size: nil, options: nil
+            paged_enum = @operations_client.list_operations \
+              name, filter, page_size: page_size, options: options
+            paged_enum.lazy.map do |proto_op|
+              wrap_proto_op_with_gax_op proto_op, options
+            end
+          end
+
+          protected
+
+          ##
+          # @private
+          # Lookup the Protobuf class from the type URL
+          def wrap_proto_op_with_gax_op proto_op, options
+            Google::Gax::Operation.new(
+              proto_op,
+              @operations_client,
+              get_type_class_from_proto_any(proto_op.response),
+              get_type_class_from_proto_any(proto_op.metadata),
+              call_options: options
+            )
+          end
+
+          ##
+          # @private
+          # Lookup the Protobuf class from the type URL
+          def get_type_class_from_proto_any proto_any
+            return Google::Protobuf::Any if proto_any.nil?
+
+            uri = URI.parse proto_any.type_url
+            type = uri.path.split("/".freeze).last
+            desc = Google::Protobuf::DescriptorPool.generated_pool.lookup type
+            return desc.msgclass if desc
+
+            # default is Any
+            Google::Protobuf::Any
           end
         end
       end
