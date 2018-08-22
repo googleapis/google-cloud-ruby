b5eca9548227668988e85d8a42d7639043d68c81
TheRoyalTnetennba <paye@google.com>
2018-08-22 11:11:55 -0700

    add rake task for generating kokoro build files

--- .kokoro/build.sh
+++ .kokoro/build.sh
@@ -10,6 +10,10 @@
 
 set -eo pipefail
 
+# Debug: show build environment
+env | grep KOKORO
+
+
 cd github/google-cloud-ruby/
 
 # Print out Ruby version
@@ -23,7 +27,14 @@ bundle update
 
 # CHANGED_DIRS is the list of top-level directories that changed. CHANGED_DIRS will be empty when run on master.
 # See https://github.com/GoogleCloudPlatform/google-cloud-python/blob/master/.kokoro/build.sh for alt implementation
-CHANGED_DIRS=$(git --no-pager diff --name-only HEAD $(git merge-base HEAD master) | grep "/" | cut -d/ -f1 | sort | uniq || true)
+CHANGED_DIRS="$(git --no-pager diff --name-only HEAD $(git merge-base HEAD master) | grep "/" | cut -d/ -f1 | sort | uniq || true)"
+
+GEMSPECS=($(git ls-files -- */*.gemspec))
+CHANGED_GEMS=()
+for i in "${CHANGED_DIRS[@]}"; do
+do
+   echo "$i"
+done
 
 # Capture failures
 EXIT_STATUS=0 # everything passed
@@ -31,15 +42,22 @@ function set_failed_status {
   EXIT_STATUS=1
 }
 
-
 # Setup service account credentials.
 export GOOGLE_APPLICATION_CREDENTIALS=${KOKORO_GFILE_DIR}/service-account.json
 
 case $JOB_TYPE in
-test)
+presubmit)
+  if [[ ! -n $(grep -x "$PACKAGE" ~/target_packages) ]]; then
+    echo "$PACKAGE was not modified, returning."
+    exit;
+  fi
+  cd $PACKAGE
+  bundle update
+  bundle exec rake ci:acceptance
   (bundle update && bundle exec rake circleci:build) || set_failed_status
   ;;
-post)
+continuous)
+  bundle exec rake ci:acceptance
   (bundle update && bundle exec rake circleci:post) || set_failed_status
   ;;
 release)
@@ -50,3 +68,13 @@ release)
 esac
 
 exit $EXIT_STATUS
+
+
+
+
+
+  if [[ ! -n $(grep -x google-cloud-trace $GEMSPECS) ]]; then
+    echo "was not modified, returning."
+  else
+    echo "it was modified"
+  fi
