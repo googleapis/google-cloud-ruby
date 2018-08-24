21fa73f37d32f2239f74f3da50ad8f4cdb1fb786
Mike Moore <mike@blowmage.com>
2018-08-24 13:11:11 -0600

    Set evaluation_time_limit in Evaluator tests (#2346)
    This extends a fix to the Debugger tests when running in the coverage task.
    We see frequent failures on CI because debugger tests fail during coverage.
    We think this is due to the increased overhead by the coverage reporting.
    We tried to extend this time limit before, but it was being reset by
    some of the other tests.
    Add the time limit extension to the Evaluator tests, which do not share the
    same test setup as most of the other Debugger tests.

--- google-cloud-debugger/test/google/cloud/debugger/breakpoint/evaluator/immutable_classes_test.rb
+++ google-cloud-debugger/test/google/cloud/debugger/breakpoint/evaluator/immutable_classes_test.rb
@@ -19,6 +19,14 @@ require_relative "expression_test_helper"
 describe Google::Cloud::Debugger::Breakpoint::Evaluator do
   let(:evaluator) { Google::Cloud::Debugger::Breakpoint::Evaluator }
 
+  before do
+    if ENV["GCLOUD_TEST_COVERAGE_DEBUGGER_TIMEOUT"]
+      # Have to set it here because configure gets reset by some tests.
+      eval_time_limit = Float ENV["GCLOUD_TEST_COVERAGE_DEBUGGER_TIMEOUT"]
+      Google::Cloud::Debugger.configure.evaluation_time_limit = eval_time_limit
+    end
+  end
+
   describe "Bignum" do
     it "allows #multiplication" do
       expression_must_equal "123123123123123123123 * 3",
