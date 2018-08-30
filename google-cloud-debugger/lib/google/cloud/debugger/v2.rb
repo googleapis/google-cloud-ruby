b88e46db548fcb230f63cc8150cfa526191eece1
dpebot <dpebot@google.com>
2018-08-30 03:35:35 -0700

    Re-generate library using google-cloud-debugger/synth.py

--- google-cloud-debugger/lib/google/cloud/debugger/v2.rb
+++ google-cloud-debugger/lib/google/cloud/debugger/v2.rb
@@ -37,6 +37,11 @@ module Google
       # 3. [Enable the Stackdriver Debugger API.](https://console.cloud.google.com/apis/library/clouddebugger.googleapis.com)
       # 4. [Setup Authentication.](https://googlecloudplatform.github.io/google-cloud-ruby/#/docs/google-cloud/master/guides/authentication)
       #
+      # ### Installation
+      # ```
+      # $ gem install google-cloud-debugger
+      # ```
+      #
       # ### Next Steps
       # - Read the [Stackdriver Debugger API Product documentation][Product Documentation]
       #   to learn more about the product and see How-to Guides.
