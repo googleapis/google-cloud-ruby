495fc62dce3032e9824efd6115636c0bbd25b319
Mike Moore <mike@blowmage.com>
2018-08-30 10:19:37 -0600

    Allow configuration of evaluation time limit
    Allow for a new default time limit to be set by environment variable.
    Also allow for the coverage task to set an even higher time limit.

--- google-cloud-debugger/test/helper.rb
+++ google-cloud-debugger/test/helper.rb
@@ -40,9 +40,9 @@ class MockDebugger < Minitest::Spec
     service
   }
   let(:debugger) {
-    if ENV["GCLOUD_TEST_COVERAGE_DEBUGGER_TIMEOUT"]
+    if ENV["GCLOUD_TEST_DEBUGGER_TIMEOUT"]
       # Have to set it here because configure gets reset by some tests.
-      eval_time_limit = Float ENV["GCLOUD_TEST_COVERAGE_DEBUGGER_TIMEOUT"]
+      eval_time_limit = Float ENV["GCLOUD_TEST_DEBUGGER_TIMEOUT"]
       Google::Cloud::Debugger.configure.evaluation_time_limit = eval_time_limit
     end
 
@@ -136,9 +136,9 @@ class EvaluatorSpec < Minitest::Spec
   let(:evaluator) { Google::Cloud::Debugger::Breakpoint::Evaluator }
 
   before do
-    if ENV["GCLOUD_TEST_COVERAGE_DEBUGGER_TIMEOUT"]
+    if ENV["GCLOUD_TEST_DEBUGGER_TIMEOUT"]
       # Have to set it here because configure gets reset by some tests.
-      eval_time_limit = Float ENV["GCLOUD_TEST_COVERAGE_DEBUGGER_TIMEOUT"]
+      eval_time_limit = Float ENV["GCLOUD_TEST_DEBUGGER_TIMEOUT"]
       Google::Cloud::Debugger.configure.evaluation_time_limit = eval_time_limit
     end
   end
