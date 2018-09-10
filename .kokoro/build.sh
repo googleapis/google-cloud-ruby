72331c585ca6bb8375a46e3c644ea91cfacb9353
Graham Paye <gpaye8@gmail.com>
2018-09-10 10:35:42 -0700

    fix kokoro continuous failure (#2416)

--- .kokoro/build.sh
+++ .kokoro/build.sh
@@ -41,7 +41,7 @@ if [ "$PACKAGE" = "post" ]; then
   rbenv global "2.5.1"
   (bundle update && bundle exec rake circleci:post) || set_failed_status
 elif [ "$JOB_TYPE" = "continuous" ]; then
-  git fetch --unshallow
+  git fetch --depth=10000
   for version in "${RUBY_VERSIONS[@]}"; do
     rbenv global "$version"
     (bundle update && bundle exec rake kokoro:continuous) || set_failed_status
