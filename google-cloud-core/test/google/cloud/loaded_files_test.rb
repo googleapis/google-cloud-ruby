7a95e706a02f01f28817c3d5b79fb2ce6a3586fb
Mike Moore <mike@blowmage.com>
2018-09-11 09:21:45 -0600

    Fix for auto loading files (#2425)
    The caller information can contain entries that are not files.
    This can cause an error when loading the gems.
    Ensure that all currently loaded files exist before calling realpath.
    
    [fixes #2423]

---
+++ google-cloud-core/test/google/cloud/loaded_files_test.rb
@@ -0,0 +1,37 @@
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+require "helper"
+
+describe Google::Cloud, :loaded_files do
+  let :loaded_files do
+    [
+      "lib/google/cloud.rb:123:in `require'",
+      "../google-cloud-core/lib/google/cloud/config.rb:123:in `require'",
+      File.realpath("lib/google/cloud/credentials.rb") + ":123:in `require'",
+      "-e:1:in `<main>'"
+    ]
+  end
+
+  it "gives real paths of files that exist" do
+    Google::Cloud.stub :caller, loaded_files do
+      Google::Cloud.loaded_files.must_equal [
+        File.realpath("lib/google/cloud.rb"),
+        File.realpath("lib/google/cloud/config.rb"),
+        File.realpath("lib/google/cloud/credentials.rb")
+      ]
+    end
+  end
+end
