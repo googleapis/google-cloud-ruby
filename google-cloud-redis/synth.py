9a760a1e3458e55d24bff5a1a110ce5ced6078a9
Daniel Azuma <dazuma@gmail.com>
2018-09-11 21:26:11 -0700

    Remove outdated overview.rb files and clean up synth a bit (#2429)

--- google-cloud-redis/synth.py
+++ google-cloud-redis/synth.py
@@ -1,5 +1,22 @@
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""This script is used to synthesize generated parts of this library."""
+
 import synthtool as s
 import synthtool.gcp as gcp
+import synthtool.languages.ruby as ruby
 import logging
 import re
 
@@ -7,18 +24,6 @@ import re
 logging.basicConfig(level=logging.DEBUG)
 
 gapic = gcp.GAPICGenerator()
-
-# Temporary until we get Ruby-specific tools into synthtool
-def merge_gemspec(src, dest, path):
-    regex = re.compile(r'^\s+gem.version\s*=\s*"[\d\.]+"$', flags=re.MULTILINE)
-    match = regex.search(dest)
-    if match:
-        src = regex.sub(match.group(0), src, count=1)
-    regex = re.compile(r'^\s+gem.homepage\s*=\s*"[^"]+"$', flags=re.MULTILINE)
-    match = regex.search(dest)
-    if match:
-        src = regex.sub(match.group(0), src, count=1)
-    return src
 
 v1beta1_library = gapic.ruby_library(
     'redis', 'v1beta1',
@@ -31,7 +36,7 @@ s.copy(v1beta1_library / 'README.md')
 s.copy(v1beta1_library / 'LICENSE')
 s.copy(v1beta1_library / '.gitignore')
 s.copy(v1beta1_library / '.yardopts')
-s.copy(v1beta1_library / 'google-cloud-redis.gemspec', merge=merge_gemspec)
+s.copy(v1beta1_library / 'google-cloud-redis.gemspec', merge=ruby.merge_gemspec)
 
 # https://github.com/googleapis/gapic-generator/issues/2196
 s.replace(
