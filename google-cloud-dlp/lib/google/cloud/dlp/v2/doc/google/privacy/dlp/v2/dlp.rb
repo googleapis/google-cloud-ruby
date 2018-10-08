bd70953c3950149f6aa2c13124cd6e355025af82
dpebot <dpebot@google.com>
2018-10-08 03:35:46 -0700

    Re-generate library using google-cloud-dlp/synth.py

--- google-cloud-dlp/lib/google/cloud/dlp/v2/doc/google/privacy/dlp/v2/dlp.rb
+++ google-cloud-dlp/lib/google/cloud/dlp/v2/doc/google/privacy/dlp/v2/dlp.rb
@@ -258,9 +258,15 @@ module Google
         # @!attribute [rw] container_name
         #   @return [String]
         #     Name of the container where the finding is located.
-        #     The top level name is the source file name or table name. Nested names
-        #     could be absent if the embedded object has no string identifier
-        #     (for an example an image contained within a document).
+        #     The top level name is the source file name or table name. Names of some
+        #     common storage containers are formatted as follows:
+        #
+        #     * BigQuery tables:  `<project_id>:<dataset_id>.<table_id>`
+        #     * Cloud Storage files: `gs://<bucket>/<path>`
+        #     * Datastore namespace: <namespace>
+        #
+        #     Nested names could be absent if the embedded object has no string
+        #     identifier (for an example an image contained within a document).
         # @!attribute [rw] record_location
         #   @return [Google::Privacy::Dlp::V2::RecordLocation]
         #     Location within a row or record of a database table.
@@ -2190,6 +2196,21 @@ module Google
         # @!attribute [rw] type
         #   @return [Google::Privacy::Dlp::V2::DlpJobType]
         #     The type of job. Defaults to `DlpJobType.INSPECT`
+        # @!attribute [rw] order_by
+        #   @return [String]
+        #     Optional comma separated list of fields to order by,
+        #     followed by `asc` or `desc` postfix. This list is case-insensitive,
+        #     default sorting order is ascending, redundant space characters are
+        #     insignificant.
+        #
+        #     Example: `name asc, end_time asc, create_time desc`
+        #
+        #     Supported fields are:
+        #
+        #     * `create_time`: corresponds to time the job was created.
+        #     * `end_time`: corresponds to time the job ended.
+        #     * `name`: corresponds to job's name.
+        #     * `state`: corresponds to `state`
         class ListDlpJobsRequest; end
 
         # The response message for listing DLP jobs.
