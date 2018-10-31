50edb0ffece6fbb1205fd94b9b3e45816d626b18
dpebot <dpebot@google.com>
2018-10-31 03:35:47 -0700

    [CHANGE ME] Re-generated google-cloud-logging to pick up changes in the API or client library generator.

--- google-cloud-logging/lib/google/cloud/logging/v2/doc/google/api/distribution.rb
+++ google-cloud-logging/lib/google/cloud/logging/v2/doc/google/api/distribution.rb
@@ -15,24 +15,25 @@
 
 module Google
   module Api
-    # Distribution contains summary statistics for a population of values and,
-    # optionally, a histogram representing the distribution of those values across
-    # a specified set of histogram buckets.
+    # `Distribution` contains summary statistics for a population of values. It
+    # optionally contains a histogram representing the distribution of those values
+    # across a set of buckets.
     #
     # The summary statistics are the count, mean, sum of the squared deviation from
     # the mean, the minimum, and the maximum of the set of population of values.
-    #
     # The histogram is based on a sequence of buckets and gives a count of values
-    # that fall into each bucket.  The boundaries of the buckets are given either
-    # explicitly or by specifying parameters for a method of computing them
-    # (buckets of fixed width or buckets of exponentially increasing width).
+    # that fall into each bucket. The boundaries of the buckets are given either
+    # explicitly or by formulas for buckets of fixed or exponentially increasing
+    # widths.
     #
     # Although it is not forbidden, it is generally a bad idea to include
     # non-finite values (infinities or NaNs) in the population of values, as this
     # will render the `mean` and `sum_of_squared_deviation` fields meaningless.
     # @!attribute [rw] count
     #   @return [Integer]
-    #     The number of values in the population. Must be non-negative.
+    #     The number of values in the population. Must be non-negative. This value
+    #     must equal the sum of the values in `bucket_counts` if a histogram is
+    #     provided.
     # @!attribute [rw] mean
     #   @return [Float]
     #     The arithmetic mean of the values in the population. If `count` is zero
@@ -40,7 +41,7 @@ module Google
     # @!attribute [rw] sum_of_squared_deviation
     #   @return [Float]
     #     The sum of squared deviations from the mean of the values in the
-    #     population.  For values x_i this is:
+    #     population. For values x_i this is:
     #
     #         Sum[i=1..n](https://cloud.google.com(x_i - mean)^2)
     #
@@ -54,21 +55,28 @@ module Google
     #     must not be present if the `count` is zero.
     # @!attribute [rw] bucket_options
     #   @return [Google::Api::Distribution::BucketOptions]
-    #     Defines the histogram bucket boundaries.
+    #     Defines the histogram bucket boundaries. If the distribution does not
+    #     contain a histogram, then omit this field.
     # @!attribute [rw] bucket_counts
     #   @return [Array<Integer>]
-    #     If `bucket_options` is given, then the sum of the values in `bucket_counts`
-    #     must equal the value in `count`.  If `bucket_options` is not given, no
-    #     `bucket_counts` fields may be given.
+    #     The number of values in each bucket of the histogram, as described in
+    #     `bucket_options`. If the distribution does not have a histogram, then omit
+    #     this field. If there is a histogram, then the sum of the values in
+    #     `bucket_counts` must equal the value in the `count` field of the
+    #     distribution.
     #
-    #     Bucket counts are given in order under the numbering scheme described
-    #     above (the underflow bucket has number 0; the finite buckets, if any,
-    #     have numbers 1 through N-2; the overflow bucket has number N-1).
+    #     If present, `bucket_counts` should contain N values, where N is the number
+    #     of buckets specified in `bucket_options`. If you supply fewer than N
+    #     values, the remaining values are assumed to be 0.
     #
-    #     The size of `bucket_counts` must be no greater than N as defined in
-    #     `bucket_options`.
-    #
-    #     Any suffix of trailing zero bucket_count fields may be omitted.
+    #     The order of the values in `bucket_counts` follows the bucket numbering
+    #     schemes described for the three bucket types. The first value must be the
+    #     count for the underflow bucket (number 0). The next N-2 values are the
+    #     counts for the finite buckets (number 1 through N-2). The N'th value in
+    #     `bucket_counts` is the count for the overflow bucket (number N-1).
+    # @!attribute [rw] exemplars
+    #   @return [Array<Google::Api::Distribution::Exemplar>]
+    #     Must be in increasing order of `value` field.
     class Distribution
       # The range of the population values.
       # @!attribute [rw] min
@@ -79,29 +87,21 @@ module Google
       #     The maximum of the population values.
       class Range; end
 
-      # A Distribution may optionally contain a histogram of the values in the
-      # population.  The histogram is given in `bucket_counts` as counts of values
-      # that fall into one of a sequence of non-overlapping buckets.  The sequence
-      # of buckets is described by `bucket_options`.
+      # `BucketOptions` describes the bucket boundaries used to create a histogram
+      # for the distribution. The buckets can be in a linear sequence, an
+      # exponential sequence, or each bucket can be specified explicitly.
+      # `BucketOptions` does not include the number of values in each bucket.
       #
-      # A bucket specifies an inclusive lower bound and exclusive upper bound for
-      # the values that are counted for that bucket.  The upper bound of a bucket
-      # is strictly greater than the lower bound.
-      #
-      # The sequence of N buckets for a Distribution consists of an underflow
-      # bucket (number 0), zero or more finite buckets (number 1 through N - 2) and
-      # an overflow bucket (number N - 1).  The buckets are contiguous:  the lower
-      # bound of bucket i (i > 0) is the same as the upper bound of bucket i - 1.
-      # The buckets span the whole range of finite values: lower bound of the
-      # underflow bucket is -infinity and the upper bound of the overflow bucket is
-      # +infinity.  The finite buckets are so-called because both bounds are
-      # finite.
-      #
-      # `BucketOptions` describes bucket boundaries in one of three ways.  Two
-      # describe the boundaries by giving parameters for a formula to generate
-      # boundaries and one gives the bucket boundaries explicitly.
-      #
-      # If `bucket_boundaries` is not given, then no `bucket_counts` may be given.
+      # A bucket has an inclusive lower bound and exclusive upper bound for the
+      # values that are counted for that bucket. The upper bound of a bucket must
+      # be strictly greater than the lower bound. The sequence of N buckets for a
+      # distribution consists of an underflow bucket (number 0), zero or more
+      # finite buckets (number 1 through N - 2) and an overflow bucket (number N -
+      # 1). The buckets are contiguous: the lower bound of bucket i (i > 0) is the
+      # same as the upper bound of bucket i - 1. The buckets span the whole range
+      # of finite values: lower bound of the underflow bucket is -infinity and the
+      # upper bound of the overflow bucket is +infinity. The finite buckets are
+      # so-called because both bounds are finite.
       # @!attribute [rw] linear_buckets
       #   @return [Google::Api::Distribution::BucketOptions::Linear]
       #     The linear bucket.
@@ -112,12 +112,12 @@ module Google
       #   @return [Google::Api::Distribution::BucketOptions::Explicit]
       #     The explicit buckets.
       class BucketOptions
-        # Specify a sequence of buckets that all have the same width (except
-        # overflow and underflow).  Each bucket represents a constant absolute
-        # uncertainty on the specific value in the bucket.
+        # Specifies a linear sequence of buckets that all have the same width
+        # (except overflow and underflow). Each bucket represents a constant
+        # absolute uncertainty on the specific value in the bucket.
         #
-        # Defines `num_finite_buckets + 2` (= N) buckets with these boundaries for
-        # bucket `i`:
+        # There are `num_finite_buckets + 2` (= N) buckets. Bucket `i` has the
+        # following boundaries:
         #
         #    Upper bound (0 <= i < N-1):     offset + (width * i).
         #    Lower bound (1 <= i < N):       offset + (width * (i - 1)).
@@ -132,12 +132,12 @@ module Google
         #     Lower bound of the first bucket.
         class Linear; end
 
-        # Specify a sequence of buckets that have a width that is proportional to
-        # the value of the lower bound.  Each bucket represents a constant relative
-        # uncertainty on a specific value in the bucket.
+        # Specifies an exponential sequence of buckets that have a width that is
+        # proportional to the value of the lower bound. Each bucket represents a
+        # constant relative uncertainty on a specific value in the bucket.
         #
-        # Defines `num_finite_buckets + 2` (= N) buckets with these boundaries for
-        # bucket i:
+        # There are `num_finite_buckets + 2` (= N) buckets. Bucket `i` has the
+        # following boundaries:
         #
         #    Upper bound (0 <= i < N-1):     scale * (growth_factor ^ i).
         #    Lower bound (1 <= i < N):       scale * (growth_factor ^ (i - 1)).
@@ -152,22 +152,49 @@ module Google
         #     Must be greater than 0.
         class Exponential; end
 
-        # A set of buckets with arbitrary widths.
+        # Specifies a set of buckets with arbitrary widths.
         #
-        # Defines `size(bounds) + 1` (= N) buckets with these boundaries for
-        # bucket i:
+        # There are `size(bounds) + 1` (= N) buckets. Bucket `i` has the following
+        # boundaries:
         #
         #    Upper bound (0 <= i < N-1):     bounds[i]
         #    Lower bound (1 <= i < N);       bounds[i - 1]
         #
-        # There must be at least one element in `bounds`.  If `bounds` has only one
-        # element, there are no finite buckets, and that single element is the
-        # common boundary of the overflow and underflow buckets.
+        # The `bounds` field must contain at least one element. If `bounds` has
+        # only one element, then there are no finite buckets, and that single
+        # element is the common boundary of the overflow and underflow buckets.
         # @!attribute [rw] bounds
         #   @return [Array<Float>]
         #     The values must be monotonically increasing.
         class Explicit; end
       end
+
+      # Exemplars are example points that may be used to annotate aggregated
+      # distribution values. They are metadata that gives information about a
+      # particular value added to a Distribution bucket, such as a trace ID that
+      # was active when a value was added. They may contain further information,
+      # such as a example values and timestamps, origin, etc.
+      # @!attribute [rw] value
+      #   @return [Float]
+      #     Value of the exemplar point. This value determines to which bucket the
+      #     exemplar belongs.
+      # @!attribute [rw] timestamp
+      #   @return [Google::Protobuf::Timestamp]
+      #     The observation (sampling) time of the above value.
+      # @!attribute [rw] attachments
+      #   @return [Array<Google::Protobuf::Any>]
+      #     Contextual information about the example value. Examples are:
+      #
+      #       Trace ID: type.googleapis.com/google.devtools.cloudtrace.v1.Trace
+      #
+      #       Literal string: type.googleapis.com/google.protobuf.StringValue
+      #
+      #       Labels dropped during aggregation:
+      #         type.googleapis.com/google.monitoring.v3.DroppedLabels
+      #
+      #     There may be only a single attachment of any given message type in a
+      #     single exemplar, and this is enforced by the system.
+      class Exemplar; end
     end
   end
 end
