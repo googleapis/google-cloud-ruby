4e7a0a9e09351ae28c4267193705701f5c395292
Chris Smith <quartzmo@gmail.com>
2018-11-13 14:21:14 -0700

    Add tags to Spanner acceptance tests
    [closes #2579]

--- google-cloud-spanner/acceptance/spanner/client/read_test.rb
+++ google-cloud-spanner/acceptance/spanner/client/read_test.rb
@@ -52,21 +52,37 @@ describe "Spanner Client", :read, :spanner do
   it "empty read works" do
     random_id = SecureRandom.int64
     db.delete table_name, random_id
+    # [START spanner_test_empty_read]
+    # [START spanner_test_single_key_dne_read]
     db.read(table_name, [:id], keys: random_id).rows.map(&:to_h).must_equal []
+    # [END spanner_test_empty_read]
+    # [END spanner_test_single_key_dne_read]
 
     db.delete table_name, 9997..9999
     db.read(table_name, [:id], keys: 9997..9999).rows.map(&:to_h).must_equal []
 
+    # [START spanner_test_empty_read_index]
+    # [START spanner_test_single_key_index_read]
     db.read(table_name, [:id, :bool], index: table_index, keys: [[false, 3]]).rows.map(&:to_h).must_equal []
+    # [END spanner_test_empty_read_index]
+    # [END spanner_test_single_key_index_read]
   end
 
   it "reads with a list of keys" do
+    # [START spanner_test_single_key_read]
+    # [START spanner_test_single_key_dne_index_read]
+    # [START spanner_test_read_set_point_keys]
+    # [START spanner_test_read_set_point_keys_index]
     db.read(table_name, [:id], keys: 1).rows.map(&:to_h).must_equal [{ id: 1 }]
+    # [END spanner_test_single_key_read]
+    # [END spanner_test_single_key_dne_index_read]
     db.read(table_name, [:id], keys: [1]).rows.map(&:to_h).must_equal [{ id: 1 }]
     db.read(table_name, [:id], keys: [3, 4, 5]).rows.map(&:to_h).must_equal [{ id: 3 }, { id: 4 }, { id: 5 }]
     db.read(table_name, [:id], keys: [3, 5, 7, 11]).rows.map(&:to_h).must_equal [{ id: 3 }, { id: 5 }, { id: 7 }, { id: 11 }]
 
     db.read(table_name, [:id], keys: [3, 5, 7, 11], limit: 2).rows.map(&:to_h).must_equal [{ id: 3 }, { id: 5 }]
+    # [END spanner_test_read_set_point_keys]
+    # [END spanner_test_read_set_point_keys_index]
   end
 
   it "reads with range key sets" do
@@ -81,6 +97,9 @@ describe "Spanner Client", :read, :spanner do
   end
 
   it "reads with range key sets and limit" do
+    # [START spanner_test_range_reads_single_key]
+    # [START spanner_test_read_with_limit]
+    # [START spanner_test_read_with_limit_index]
     db.read(table_name, [:id], keys: 3..9, limit: 2).rows.map(&:to_h).must_equal [{ id: 3 }, { id: 4 }]
     db.read(table_name, [:id], keys: 3...9, limit: 2).rows.map(&:to_h).must_equal [{ id: 3 }, { id: 4 }]
     db.read(table_name, [:id], keys: db.range(3, 9, exclude_begin: true), limit: 2).rows.map(&:to_h).must_equal [{ id: 4 }, { id: 5 }]
@@ -89,6 +108,9 @@ describe "Spanner Client", :read, :spanner do
     db.read(table_name, [:id], keys: db.range([3], [], exclude_begin: true), limit: 2).rows.map(&:to_h).must_equal [{ id: 4 }, { id: 5 }]
     db.read(table_name, [:id], keys: []..[9], limit: 2).rows.map(&:to_h).must_equal [{ id: 1 }, { id: 2 }]
     db.read(table_name, [:id], keys: []...[9], limit: 2).rows.map(&:to_h).must_equal [{ id: 1 }, { id: 2 }]
+    # [END spanner_test_range_reads_single_key]
+    # [END spanner_test_read_with_limit]
+    # [END spanner_test_read_with_limit_index]
   end
 
   it "reads from index with a list of composite keys" do
@@ -112,6 +134,7 @@ describe "Spanner Client", :read, :spanner do
   end
 
   it "reads from index with range key sets and limit" do
+    # [START spanner_test_range_reads_index]
     db.read(table_name, [:id, :bool], index: table_index, keys: [true, 3]..[true, 11], limit: 2).rows.map(&:to_h).must_equal [{ id: 3, bool: true }, { id: 5, bool: true }]
     db.read(table_name, [:id, :bool], index: table_index, keys: [true, 3]...[true, 11], limit: 2).rows.map(&:to_h).must_equal [{ id: 3, bool: true }, { id: 5, bool: true }]
     db.read(table_name, [:id, :bool], index: table_index, keys: db.range([true, 3], [true, 7], exclude_begin: true), limit: 2).rows.map(&:to_h).must_equal [{ id: 5, bool: true }, { id: 7, bool: true }]
@@ -120,5 +143,6 @@ describe "Spanner Client", :read, :spanner do
     db.read(table_name, [:id, :bool], index: table_index, keys: db.range([true, 5], [], exclude_begin: true), limit: 2).rows.map(&:to_h).must_equal [{ id: 7, bool: true }, { id: 11, bool: true }]
     db.read(table_name, [:id, :bool], index: table_index, keys: [true]..[true, 11], limit: 2).rows.map(&:to_h).must_equal [{ id: 3, bool: true }, { id: 5, bool: true }]
     db.read(table_name, [:id, :bool], index: table_index, keys: [true]...[true, 11], limit: 2).rows.map(&:to_h).must_equal [{ id: 3, bool: true }, { id: 5, bool: true }]
+    # [END spanner_test_range_reads_index]
   end
 end
