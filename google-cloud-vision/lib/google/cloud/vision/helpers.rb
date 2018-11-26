4f02cbd18b6bb1625dd37c97a6dffde467894bc3
TheRoyalTnetennba <paye@google.com>
2018-11-26 11:25:37 -0800

    add async

--- google-cloud-vision/lib/google/cloud/vision/helpers.rb
+++ google-cloud-vision/lib/google/cloud/vision/helpers.rb
@@ -10,8 +10,7 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-# limitations under the License.
-
+# # limitations under the License.
 
 require "uri"
 
@@ -25,25 +24,39 @@ module Google
           method_name = feature_type.to_s.downcase
           method_name += "_detection" unless method_name.include? "detection"
 
-          client.define_singleton_method(method_name.to_sym) do |images: [], image: nil, max_results: nil, options: nil, &blk|
+          client.define_singleton_method(method_name.to_sym) do |\
+            images: [], 
+            image: nil,
+            max_results: nil,
+            options: nil,
+            async: false,
+            output: nil,
+            mime_type: nil,
+            batch_size: 20,
+            destination: nil,
+            &blk
+          |
             feature = { type: feature_type }
             feature[:max_results] = max_results if max_results
             images << image if image
     
             formatted_images = images.map do |img|
+              formatted_image = {}
               if File.file? img
-                { content: File.binread(img) }
+                formatted_image = { content: File.binread(img) }
               elsif img =~ URI::DEFAULT_PARSER.make_regexp
                 if URI(img).scheme == "gs"
                   source = { gcs_image_uri: img }
-                  { source: source }
+                  formatted_image = { source: source }
                 else
                   source = { image_uri: img }
-                  { source: source }
+                  formatted_image = { source: source }
                 end
               else
                 raise TypeError.new("Image must be a filepath, file, url, or Google Cloud Storage url")
               end
+              formatted_image[:mime_type] = mime_type if mime_type
+              formatted_image
             end
     
             requests = formatted_images.map do |img|
@@ -52,8 +65,22 @@ module Google
                 features: [feature]
               }
             end
-    
-            batch_annotate_images requests, options, &blk
+
+            if async
+              requests.map! do |request|
+                {
+                  input_config: request[:image],
+                  features: request[:features],
+                  output_config: {
+                    gcs_destination: destination,
+                    batch_size: batch_size
+                  }
+                }
+              end
+              async_batch_annotate_files requests, options
+            else
+              batch_annotate_images requests, options, &blk
+            end
           end
         end
 
--- google-cloud-vision/test/google/cloud/vision/helpers_test.rb
+++ google-cloud-vision/test/google/cloud/vision/helpers_test.rb
@@ -49,21 +49,12 @@ describe Google::Cloud::Vision do
         method_name += "_detection" unless method_name.include? "detection"
         helper_hash[method_name] = feature_type
       end
+      helper_hash
     end
-    let(:image_variants) do 
-      {
-        "single image file": File.new("acceptance/data/face.jpg", "r"),
-        "list of image files": (0..1).map { File.new("acceptance/data/face.jpg", "r") },
-        "single image path": "acceptance/data/face.jpg",
-        "list of image paths": (0..1).map { "acceptance/data/face.jpg" },
-        "single image uri": "http://example.com/face.jpg",
-        "list of image uri's": (0..1).map { "http://example.com/face.jpg" },
-        "single gcs image uri": "gs://gapic-toolkit/President_Barack_Obama.jpg",
-        "list of gcs image uri's": (0..1).map { "gs://gapic-toolkit/President_Barack_Obama.jpg" }
-      }
-    end
+    let(:image_file) { File.new("acceptance/data/face.jpg", "r") }
+    let(:image_uri) { "http://example.com/face.jpg" }
+    let(:gcs_image_uri) { "gs://gapic-toolkit/President_Barack_Obama.jpg" }
     let(:mock_credentials) { Object.const_get("MockImageAnnotatorCredentials_#{version}").new("batch_annotate_images") }
-    # let(:feature_types)
   
     def image_object image
       return { content: File.binread(image_file) } if File.file? image
@@ -91,27 +82,71 @@ describe Google::Cloud::Vision do
       end
     end
 
-    describe Google::Cloud::Vision.const_get(version.capitalize)::ImageAnnotatorClient do
-      it "generates helper methods based on Feature.Types" do
-        helper_methods.each do |helper_method, feature_type|
-          describe helper_method do
-            it "formats images correctly" do
-              image_variants.each do |description, image|
-                it "correctly calls batch_annotate_images when given a #{description}" do
-      
-                  Google::Cloud::Vision.const_get(version.capitalize)::Credentials.stub(:default, mock_credentials) do
-                    client = Google::Cloud::Vision::ImageAnnotator.new(version: version.to_sym)
-                    stub = batch_annotate_stub image, feature_type
-                    client.stub(:batch_annotate_images, stub) do
-                      if image.is_a?(Array)
-                        client.public_send(helper_method, images: image)
-                      else
-                        client.public_send(helper_method, image: image)
-                      end
-                    end
-                  end
+    def async_annotate_stub image, feature_type, destination
+      input_config = image_object(image)
+      input_config[:mime_type] = "application/pdf"
+      expected_requests = [
+        {
+          input_config: input_config,
+          features: [{ type: feature_type }],
+          output_config: {
+            gcs_destination: destination,
+            batch_size: 20
+          }
+        }
+      ]
+      proc do |requests|
+        assert_equal(expected_requests, requests)
+      end
+    end
+
+    describe "ImageAnnotatorClient helper methods" do
+      helper_methods = {}
+      Google::Cloud::Vision.const_get(version.capitalize)::Feature::Type.constants.each do |feature_type|
+        next if feature_type == :TYPE_UNSPECIFIED
+        method_name = feature_type.to_s.downcase
+        method_name += "_detection" unless method_name.include? "detection"
+        helper_methods[method_name] = feature_type
+      end
+
+      helper_methods.each do |helper_method, feature_type|
+        {
+          "single image file": File.new("acceptance/data/face.jpg", "r"),
+          "list of image files": (0..1).map { File.new("acceptance/data/face.jpg", "r") },
+          "single image path": "acceptance/data/face.jpg",
+          "list of image paths": (0..1).map { "acceptance/data/face.jpg" },
+          "single image uri": "http://example.com/face.jpg",
+          "list of image uri's": (0..1).map { "http://example.com/face.jpg" },
+          "single gcs image uri": "gs://gapic-toolkit/President_Barack_Obama.jpg",
+          "list of gcs image uri's": (0..1).map { "gs://gapic-toolkit/President_Barack_Obama.jpg" }
+        }.each do |description, image|
+          it "#{helper_method} correctly calls batch_annotate_images when given a #{description}" do
+            Google::Cloud::Vision.const_get(version.capitalize)::Credentials.stub(:default, mock_credentials) do
+              client = Google::Cloud::Vision::ImageAnnotator.new(version: version.to_sym)
+              stub = batch_annotate_stub image, feature_type
+              client.stub(:batch_annotate_images, stub) do
+                if image.is_a?(Array)
+                  client.public_send(helper_method, images: image)
+                else
+                  client.public_send(helper_method, image: image)
                 end
               end
+            end
+          end
+        end
+
+        it "#{helper_method} correctly calls async_batch_annotate_files when async is true" do
+          Google::Cloud::Vision.const_get(version.capitalize)::Credentials.stub(:default, mock_credentials) do
+            client = Google::Cloud::Vision::ImageAnnotator.new(version: version.to_sym)
+            stub = async_annotate_stub gcs_image_uri, feature_type, "gs://my-bucket"
+            client.stub(:async_batch_annotate_files, stub) do
+              client.public_send(
+                helper_method, 
+                image: gcs_image_uri, 
+                destination: "gs://my-bucket",
+                async: true,
+                mime_type: "application/pdf"
+              )
             end
           end
         end
