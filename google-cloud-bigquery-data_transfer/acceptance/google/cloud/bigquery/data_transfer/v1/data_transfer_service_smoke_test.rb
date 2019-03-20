66ea6c7af8625e3c9f326fff8d038723252a9e4e
yoshi-automation <yoshi-automation@google.com>
2019-03-20 03:39:09 -0700

    [CHANGE ME] Re-generated google-cloud-bigquery-data_transfer to pick up changes in the API or client library generator.

--- google-cloud-bigquery-data_transfer/acceptance/google/cloud/bigquery/data_transfer/v1/data_transfer_service_smoke_test.rb
+++ google-cloud-bigquery-data_transfer/acceptance/google/cloud/bigquery/data_transfer/v1/data_transfer_service_smoke_test.rb
@@ -26,16 +26,16 @@ describe "DataTransferServiceSmokeTest v1" do
     end
     project_id = ENV["DATA_TRANSFER_TEST_PROJECT"].freeze
 
-    data_transfer_service_client = Google::Cloud::Bigquery::DataTransfer.new(version: :v1)
+    data_transfer_client = Google::Cloud::Bigquery::DataTransfer.new(version: :v1)
     formatted_parent = Google::Cloud::Bigquery::DataTransfer::V1::DataTransferServiceClient.project_path(project_id)
 
     # Iterate over all results.
-    data_transfer_service_client.list_data_sources(formatted_parent).each do |element|
+    data_transfer_client.list_data_sources(formatted_parent).each do |element|
       # Process element.
     end
 
     # Or iterate over results one page at a time.
-    data_transfer_service_client.list_data_sources(formatted_parent).each_page do |page|
+    data_transfer_client.list_data_sources(formatted_parent).each_page do |page|
       # Process each page at a time.
       page.each do |element|
         # Process element.
