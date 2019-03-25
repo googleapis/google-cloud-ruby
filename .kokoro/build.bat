c9bc25c82e47b30804ae6c0f77c73aa6ba8ecb4b
TheRoyalTnetennba <paye@google.com>
2019-03-25 14:28:50 -0700

    dockerize windows-builds

--- .kokoro/build.bat
+++ .kokoro/build.bat
@@ -1,8 +1,7 @@
 REM This file runs tests for merges, PRs, and nightlies.
 REM There are a few rules for what tests are run:
-REM  * PRs run all non-acceptance tests for every library.
-REM  * Merges run all non-acceptance tests for every library, and acceptance tests for all altered libraries.
-REM  * Nightlies run all acceptance tests for every library.
-REM Currently only runs tests on 2.5.1
+REM  * PRs run all non-acceptance tests for every library, against all rubies.
+REM  * Merges run all non-acceptance tests for every library, and acceptance tests for all altered libraries, against all rubies.
+REM  * Nightlies run all acceptance tests for every library, against all rubies.
 
 "C:\Program Files\Git\bin\bash.exe" github/google-cloud-ruby/.kokoro/windows.sh
---
+++ .kokoro/trampoline.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\Git\bin\bash.exe" github/google-cloud-ruby/.kokoro/windows-trampoline.sh
