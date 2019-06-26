31f301bd7ea6c37c07592043943e217b6325317b
Daniel Azuma <dazuma@google.com>
2019-06-26 07:19:11 -0700

    Ensure two blank lines after copyright block in container_analysis (#3527)

--- google-cloud-container_analysis/lib/google/cloud/container_analysis/v1.rb
+++ google-cloud-container_analysis/lib/google/cloud/container_analysis/v1.rb
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 require "google/cloud/container_analysis/v1/container_analysis_client"
 
 module Google
