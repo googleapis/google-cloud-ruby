50c14564e04a3d4b80ad4e351982409d458de258
TheRoyalTnetennba <paye@google.com>
2019-06-27 12:34:51 -0700

    add redirects for devsite

--- js/jsondoc-forwarder-init-0.1.0.js
+++ js/jsondoc-forwarder-init-0.1.0.js
@@ -3,4 +3,21 @@
 var jsondocTypes = {{ site.data.jsondoc_types | jsonify }};
 var forwarder = new JsondocForwarder(null, "/google-cloud-ruby/", null, jsondocTypes);
 var forwardUrl = forwarder.resolve(window.location.href);
-if (forwardUrl) window.location.href = forwardUrl;
+
+var currentUrl = forwardUrl ? forwardUrl : window.location.href;
+var wasRedirected = false;
+
+var redirectMap = {{ site.data.devsite_redirects | jsonify }};
+Object.keys(redirectMap).forEach(key => {
+    // Redirect to devsite for all gem docs.
+    // Leave for TOC until devsite has something similar;
+    if (currentUrl.indexOf(key) !== -1) {
+        var newURL = "https://googleapis.dev/ruby/" + redirectMap[key];
+        newURL += window.location.href.split(key).slice(-1)[0];
+        window.location.href = newURL;
+        wasRedirected = true;
+        return;
+    }
+});
+
+if (forwardUrl && !wasRedirected) window.location.href = forwardUrl;
