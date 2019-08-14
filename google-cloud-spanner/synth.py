b50c61e8f196a1f4c482a36951de8ab7a6a0c73f
Daniel Azuma <dazuma@google.com>
2019-08-14 08:17:38 -0700

    fix(spanner): Low-level admin clients now honor service_address and service_port
    [pr #3786]

--- google-cloud-spanner/synth.py
+++ google-cloud-spanner/synth.py
@@ -103,7 +103,10 @@ s.replace(
 
 # Support for service_address
 s.replace(
-    'lib/google/cloud/spanner/v*/*_client.rb',
+    [
+        'lib/google/cloud/spanner/v*/*_client.rb',
+        'lib/google/cloud/spanner/admin/*/v*/*_client.rb'
+    ],
     '\n(\\s+)#(\\s+)@param exception_transformer',
     '\n\\1#\\2@param service_address [String]\n' +
         '\\1#\\2  Override for the service hostname, or `nil` to leave as the default.\n' +
@@ -112,17 +115,26 @@ s.replace(
         '\\1#\\2@param exception_transformer'
 )
 s.replace(
-    'lib/google/cloud/spanner/v*/*_client.rb',
+    [
+        'lib/google/cloud/spanner/v*/*_client.rb',
+        'lib/google/cloud/spanner/admin/*/v*/*_client.rb'
+    ],
     '\n(\\s+)metadata: nil,\n\\s+exception_transformer: nil,\n',
     '\n\\1metadata: nil,\n\\1service_address: nil,\n\\1service_port: nil,\n\\1exception_transformer: nil,\n'
 )
 s.replace(
-    'lib/google/cloud/spanner/v*/*_client.rb',
+    [
+        'lib/google/cloud/spanner/v*/*_client.rb',
+        'lib/google/cloud/spanner/admin/*/v*/*_client.rb'
+    ],
     'service_path = self\\.class::SERVICE_ADDRESS',
     'service_path = service_address || self.class::SERVICE_ADDRESS'
 )
 s.replace(
-    'lib/google/cloud/spanner/v*/*_client.rb',
+    [
+        'lib/google/cloud/spanner/v*/*_client.rb',
+        'lib/google/cloud/spanner/admin/*/v*/*_client.rb'
+    ],
     'port = self\\.class::DEFAULT_SERVICE_PORT',
     'port = service_port || self.class::DEFAULT_SERVICE_PORT'
 )
