4d5a5d66495fee57eb76eef0871028bc9fbc5083
TheRoyalTnetennba <paye@google.com>
2019-08-19 16:25:14 -0700

    update variable set [ci skip]

--- .kokoro/trampoline.bat
+++ .kokoro/trampoline.bat
@@ -2,11 +2,12 @@ REM Run trampoline from gfile, so that the repos that copy this script can run
 
 CD github\\%REPO_DIR%
 
-git log --format=%%B -n 1 %KOKORO_GIT_COMMIT% > temp.txt
-SET /p COMMIT_MESSSAGE=<temp.txt
+FOR /F "tokens=*" %%g IN ('git log --format=%%B -n 1 %KOKORO_GIT_COMMIT%') do (
+    SET COMMIT_MESSAGE=%%g
+)
 
 IF "%JOB_TYPE%"=="presubmit"(
-    ECHO %COMMIT_MESSSAGE% | FIND /I "[ci skip]" > Nul && ( 
+    ECHO %COMMIT_MESSAGE% | FIND /I "[ci skip]" > Nul && ( 
         ECHO "[ci skip] found. Exiting"
     ) || (
         python %KOKORO_GFILE_DIR%\\trampoline_windows.py
