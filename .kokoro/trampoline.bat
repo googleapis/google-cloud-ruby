67c51773e4079f11b952dc818cd57f843ebede6f
TheRoyalTnetennba <paye@google.com>
2019-08-19 16:08:17 -0700

    adjust formatting [ci skip]

--- .kokoro/trampoline.bat
+++ .kokoro/trampoline.bat
@@ -2,11 +2,11 @@ REM Run trampoline from gfile, so that the repos that copy this script can run
 
 CD github\\%REPO_DIR%
 
-git log --format=%B -n 1 %KOKORO_GIT_COMMIT% > temp.txt
+git log --format=%%B -n 1 %KOKORO_GIT_COMMIT% > temp.txt
 SET /p COMMIT_MESSSAGE=<temp.txt
 
 IF "%JOB_TYPE%"=="presubmit"(
-    ECHO.%COMMIT_MESSSAGE%| FIND /I "[ci skip]">Nul && ( 
+    ECHO %COMMIT_MESSSAGE% | FIND /I "[ci skip]" > Nul && ( 
         ECHO "[ci skip] found. Exiting"
     ) || (
         python %KOKORO_GFILE_DIR%\\trampoline_windows.py
