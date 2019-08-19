6337c8109f5247d2f275e41f96a5dd10283dee79
TheRoyalTnetennba <paye@google.com>
2019-08-19 16:45:23 -0700

    wrap output in quotes [ci skip]

--- .kokoro/trampoline.bat
+++ .kokoro/trampoline.bat
@@ -3,7 +3,7 @@ REM Run trampoline from gfile, so that the repos that copy this script can run
 CD github\\%REPO_DIR%
 
 FOR /F "tokens=*" %%g IN ('git log --format^=%%B -n 1 %KOKORO_GIT_COMMIT%') do (
-    SET COMMIT_MESSAGE=%%g
+    SET COMMIT_MESSAGE="%%g"
 )
 
 IF "%JOB_TYPE%"=="presubmit"(
