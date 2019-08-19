89356175bdba6dfb3ad829a7a87863c5b41905d1
TheRoyalTnetennba <paye@google.com>
2019-08-19 15:50:05 -0700

    skip ci on windows too [ci skip]

--- .kokoro/trampoline.bat
+++ .kokoro/trampoline.bat
@@ -1,3 +1,16 @@
 REM Run trampoline from gfile, so that the repos that copy this script can run
 
-python %KOKORO_GFILE_DIR%\\trampoline_windows.py
+CD github\\%REPO_DIR%
+
+git log --format=%B -n 1 $KOKORO_GIT_COMMIT > temp.txt
+SET /p COMMIT_MESSSAGE=<temp.txt
+
+IF "%JOB_TYPE%"=="presubmit"(
+    ECHO.%COMMIT_MESSSAGE%| FIND /I "[ci skip]">Nul && ( 
+        ECHO "[ci skip] found. Exiting"
+    ) || (
+        python %KOKORO_GFILE_DIR%\\trampoline_windows.py
+    )
+) ELSE (
+    python %KOKORO_GFILE_DIR%\\trampoline_windows.py
+)
