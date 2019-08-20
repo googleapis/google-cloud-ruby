90aae43e0086d6a7cc8475346a517ded70d0c18a
TheRoyalTnetennba <paye@google.com>
2019-08-19 18:27:57 -0700

    windows please [ci skip]

--- .kokoro/trampoline.bat
+++ .kokoro/trampoline.bat
@@ -3,10 +3,10 @@ REM Run trampoline from gfile, so that the repos that copy this script can run
 CD github\\%REPO_DIR%
 
 FOR /F "tokens=*" %%g IN ('git log --format^=%%B -n 1 %KOKORO_GIT_COMMIT%') do (
-    SET COMMIT_MESSAGE="%%g"
+    SET "COMMIT_MESSAGE=%%g"
 )
 
-IF "%JOB_TYPE%"=="presubmit"(
+IF "%JOB_TYPE%"=="presubmit" (
     ECHO %COMMIT_MESSAGE% | FIND /I "[ci skip]" > Nul && ( 
         ECHO "[ci skip] found. Exiting"
     ) || (
