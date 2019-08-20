3bef3b7d1f8e819285fe9585853bc9307c7ed31a
TheRoyalTnetennba <paye@google.com>
2019-08-20 13:54:17 -0700

    temporarily disable [ci skip] check for windows

--- .kokoro/trampoline.bat
+++ .kokoro/trampoline.bat
@@ -2,16 +2,18 @@ REM Run trampoline from gfile, so that the repos that copy this script can run
 
 CD github\\%REPO_DIR%
 
-FOR /F "tokens=*" %%g IN ('git log --format^=%%B -n 1 %KOKORO_GIT_COMMIT%') do (
-    SET "COMMIT_MESSAGE=%%g"
-)
+REM FOR /F "tokens=*" %%g IN ('git log --format^=%%B -n 1 %KOKORO_GIT_COMMIT%') do (
+REM     SET "COMMIT_MESSAGE=%%g"
+REM )
 
-IF "%JOB_TYPE%"=="presubmit" (
-    ECHO %COMMIT_MESSAGE% | FIND /I "[ci skip]" > Nul && ( 
-        ECHO "[ci skip] found. Exiting"
-    ) || (
-        python %KOKORO_GFILE_DIR%\\trampoline_windows.py
-    )
-) ELSE (
-    python %KOKORO_GFILE_DIR%\\trampoline_windows.py
-)
+REM IF "%JOB_TYPE%"=="presubmit" (
+REM     ECHO %COMMIT_MESSAGE% | FIND /I "[ci skip]" > Nul && ( 
+REM         ECHO "[ci skip] found. Exiting"
+REM     ) || (
+REM         python %KOKORO_GFILE_DIR%\\trampoline_windows.py
+REM     )
+REM ) ELSE (
+REM     python %KOKORO_GFILE_DIR%\\trampoline_windows.py
+REM )
+
+python %KOKORO_GFILE_DIR%\\trampoline_windows.py
