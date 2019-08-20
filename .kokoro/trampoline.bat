80334df792b12ec30e42163c60e7c44806f136f4
Graham Paye <paye@google.com>
2019-08-20 11:55:05 -0700

    chore: skip presubmit tests when commit message contains "[ci skip]" (#3808)

--- .kokoro/trampoline.bat
+++ .kokoro/trampoline.bat
@@ -1,3 +1,17 @@
 REM Run trampoline from gfile, so that the repos that copy this script can run
 
-python %KOKORO_GFILE_DIR%\\trampoline_windows.py
+CD github\\%REPO_DIR%
+
+FOR /F "tokens=*" %%g IN ('git log --format^=%%B -n 1 %KOKORO_GIT_COMMIT%') do (
+    SET "COMMIT_MESSAGE=%%g"
+)
+
+IF "%JOB_TYPE%"=="presubmit" (
+    ECHO %COMMIT_MESSAGE% | FIND /I "[ci skip]" > Nul && ( 
+        ECHO "[ci skip] found. Exiting"
+    ) || (
+        python %KOKORO_GFILE_DIR%\\trampoline_windows.py
+    )
+) ELSE (
+    python %KOKORO_GFILE_DIR%\\trampoline_windows.py
+)
