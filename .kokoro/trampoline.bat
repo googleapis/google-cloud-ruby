5330ad201512b1305ccbf3d72bd4bd3d86713cbb
Graham Paye <paye@google.com>
2019-08-26 14:29:07 -0700

    chore: fix windows presubmit check (#3863)

--- .kokoro/trampoline.bat
+++ .kokoro/trampoline.bat
@@ -1,17 +1,3 @@
-REM Run trampoline from gfile, so that the repos that copy this script can run
+CD "github"
 
-CD github\\%REPO_DIR%
-
-FOR /F "tokens=*" %%g IN ('git log --format^=%%B -n 1 %KOKORO_GIT_COMMIT%') do (
-    SET "COMMIT_MESSAGE=%%g"
-)
-
-IF "%JOB_TYPE%"=="presubmit" (
-    ECHO %COMMIT_MESSAGE% | FIND /I "[ci skip]" > Nul && ( 
-        ECHO "[ci skip] found. Exiting"
-    ) || (
-        python %KOKORO_GFILE_DIR%\\trampoline_windows.py
-    )
-) ELSE (
-    python %KOKORO_GFILE_DIR%\\trampoline_windows.py
-)
+"C:\Program Files\Git\bin\bash.exe" %REPO_DIR%//.kokoro//trampoline.sh
