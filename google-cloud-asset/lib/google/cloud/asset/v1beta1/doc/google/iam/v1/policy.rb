f784f0799c353691e3621c6d2d4f79c78860d8da
yoshi-automation <yoshi-automation@google.com>
2019-08-27 03:37:43 -0700

    [CHANGE ME] Re-generated google-cloud-asset to pick up changes in the API or client library generator.

--- google-cloud-asset/lib/google/cloud/asset/v1beta1/doc/google/iam/v1/policy.rb
+++ google-cloud-asset/lib/google/cloud/asset/v1beta1/doc/google/iam/v1/policy.rb
@@ -63,7 +63,14 @@ module Google
       #     [IAM developer's guide](https://cloud.google.com/iam/docs).
       # @!attribute [rw] version
       #   @return [Integer]
-      #     Deprecated.
+      #     Specifies the format of the policy.
+      #
+      #     Valid values are 0, 1, and 3. Requests specifying an invalid value will be
+      #     rejected.
+      #
+      #     Policies with any conditional bindings must specify version 3. Policies
+      #     without any conditional bindings may specify any valid value or leave the
+      #     field unset.
       # @!attribute [rw] bindings
       #   @return [Array<Google::Iam::V1::Binding>]
       #     Associates a list of `members` to a `role`.
