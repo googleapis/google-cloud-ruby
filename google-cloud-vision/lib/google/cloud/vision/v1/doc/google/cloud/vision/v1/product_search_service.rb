bd409a3f424b0d678866b1f223f02aa1f3855188
yoshi-automation <yoshi-automation@google.com>
2019-09-26 03:44:41 -0700

    [CHANGE ME] Re-generated google-cloud-vision to pick up changes in the API or client library generator.

--- google-cloud-vision/lib/google/cloud/vision/v1/doc/google/cloud/vision/v1/product_search_service.rb
+++ google-cloud-vision/lib/google/cloud/vision/v1/doc/google/cloud/vision/v1/product_search_service.rb
@@ -37,9 +37,9 @@ module Google
         # @!attribute [rw] product_category
         #   @return [String]
         #     Immutable. The category for the product identified by the reference image. This should
-        #     be either "homegoods-v2", "apparel-v2", "toys-v2", "packagedgoods-v1", or
-        #     "general-v1" The legacy categories "homegoods", "apparel", and "toys" are
-        #     still supported, but these should not be used for new products.
+        #     be either "homegoods-v2", "apparel-v2", or "toys-v2". The legacy categories
+        #     "homegoods", "apparel", and "toys" are still supported, but these should
+        #     not be used for new products.
         # @!attribute [rw] product_labels
         #   @return [Array<Google::Cloud::Vision::V1::Product::KeyValue>]
         #     Key-value pairs that can be attached to a product. At query time,
