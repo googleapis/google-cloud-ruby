6ecd54abadcb74ade84e4290f1e7438e1dead2ea
yoshi-automation <yoshi-automation@google.com>
2019-10-02 03:42:57 -0700

    [CHANGE ME] Re-generated google-cloud-spanner to pick up changes in the API or client library generator.

--- google-cloud-spanner/lib/google/cloud/spanner/admin/database/v1/doc/google/iam/v1/policy.rb
+++ google-cloud-spanner/lib/google/cloud/spanner/admin/database/v1/doc/google/iam/v1/policy.rb
@@ -33,7 +33,7 @@ module Google
       #     {
       #       "bindings": [
       #         {
-      #           "role": "role/resourcemanager.organizationAdmin",
+      #           "role": "roles/resourcemanager.organizationAdmin",
       #           "members": [
       #             "user:mike@example.com",
       #             "group:admins@example.com",
