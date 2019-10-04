3398c9d0ed51479ee1a03e508c505736d03a5f3a
Chris Smith <quartzmo@gmail.com>
2019-10-04 14:23:21 -0600

    Update Bucket#post_object
    * Avoid encoding special variable name ${filename}
    
    [fixes #4047]

--- google-cloud-storage/test/google/cloud/storage/bucket_post_object_test.rb
+++ google-cloud-storage/test/google/cloud/storage/bucket_post_object_test.rb
@@ -19,7 +19,9 @@ describe Google::Cloud::Storage::Bucket, :post_object, :mock_storage do
   let(:bucket_gapi) { Google::Apis::StorageV1::Bucket.from_json random_bucket_hash(bucket_name).to_json }
   let(:bucket) { Google::Cloud::Storage::Bucket.from_gapi bucket_gapi, storage.service }
 
-  let(:file_path) { "file.ext" }
+  let(:file_path) { "a/b/c/{my_file}.ext" }
+  let(:file_path_encoded) { "a/b/c/%7Bmy_file%7D.ext" }
+  let(:file_path_special_variable) { "a/b/c/${filename}" }
 
   it "uses the credentials' issuer and signing_key to generate signed post objects" do
     Time.stub :now, Time.new(2012,1,1,0,0,0, "+00:00") do
@@ -46,6 +48,7 @@ describe Google::Cloud::Storage::Bucket, :post_object, :mock_storage do
       signed_post.url.must_equal Google::Cloud::Storage::GOOGLEAPIS_URL
       signed_post.fields[:GoogleAccessId].must_equal "native_client_email"
       signed_post.fields[:signature].must_equal Base64.strict_encode64("native-signature").delete("\n")
+      signed_post.fields[:key].must_equal [bucket_name, file_path_encoded].join("/")
 
       signing_key_mock.verify
     end
@@ -65,6 +68,28 @@ describe Google::Cloud::Storage::Bucket, :post_object, :mock_storage do
     signed_post.url.must_equal Google::Cloud::Storage::GOOGLEAPIS_URL
     signed_post.fields[:GoogleAccessId].must_equal "native_client_email"
     signed_post.fields[:signature].must_equal Base64.strict_encode64("native-signature").delete("\n")
+    signed_post.fields[:key].must_equal [bucket_name, file_path_encoded].join("/")
+
+    signing_key_mock.verify
+  end
+
+  it "gives a signature without URI encoding the special variable path ${filename}" do
+    # "You can also use the ${filename} variable if a user is providing a file name."
+    # https://cloud.google.com/storage/docs/xml-api/post-object
+    signing_key_mock = Minitest::Mock.new
+
+    json_policy = Base64.strict_encode64("{}").delete("\n")
+    signing_key_mock.expect :sign, "native-signature", [OpenSSL::Digest::SHA256, json_policy]
+    credentials.issuer = "native_client_email"
+    credentials.signing_key = signing_key_mock
+
+
+    signed_post = bucket.post_object file_path_special_variable
+
+    signed_post.url.must_equal Google::Cloud::Storage::GOOGLEAPIS_URL
+    signed_post.fields[:GoogleAccessId].must_equal "native_client_email"
+    signed_post.fields[:signature].must_equal Base64.strict_encode64("native-signature").delete("\n")
+    signed_post.fields[:key].must_equal [bucket_name, file_path_special_variable].join("/")
 
     signing_key_mock.verify
   end
