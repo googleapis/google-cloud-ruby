9b67b340dae79e1adcc3c6e0e7ce0e7c3e9bc67d
yoshi-automation <yoshi-automation@google.com>
2019-10-25 03:40:51 -0700

    [CHANGE ME] Re-generated google-cloud-os_login to pick up changes in the API or client library generator.

--- google-cloud-os_login/lib/google/cloud/oslogin/v1/oslogin_pb.rb
+++ google-cloud-os_login/lib/google/cloud/oslogin/v1/oslogin_pb.rb
@@ -16,7 +16,6 @@ Google::Protobuf::DescriptorPool.generated_pool.build do
     optional :name, :string, 1
     repeated :posix_accounts, :message, 2, "google.cloud.oslogin.common.PosixAccount"
     map :ssh_public_keys, :string, :message, 3, "google.cloud.oslogin.common.SshPublicKey"
-    optional :suspended, :bool, 4
   end
   add_message "google.cloud.oslogin.v1.DeletePosixAccountRequest" do
     optional :name, :string, 1
