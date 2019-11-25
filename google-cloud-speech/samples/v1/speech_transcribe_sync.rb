0e5bd5b7b83027a422498e59fd3b260a3ec75848
Rebecca Taylor <rebeccataylor@google.com>
2019-11-25 13:04:18 -0800

    Regenerate samples for review

---
+++ google-cloud-speech/samples/v1/speech_transcribe_sync.rb
@@ -0,0 +1,80 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# DO NOT EDIT! This is a generated sample ("Request",  "speech_transcribe_sync")
+
+# sample-metadata
+#   title: Transcribe Audio File (Local File)
+#   description: Transcribe a short audio file using synchronous speech recognition
+#   bundle exec ruby samples/v1/speech_transcribe_sync.rb [--local_file_path "resources/brooklyn_bridge.raw"]
+
+# Transcribe a short audio file using synchronous speech recognition
+
+def sample_recognize local_file_path = "resources/brooklyn_bridge.raw"
+  # [START speech_transcribe_sync]
+  # Import client library
+  require "google/cloud/speech"
+
+  # Instantiate a client
+  speech_client = Google::Cloud::Speech.new version: :v1
+
+  # TODO(developer): Uncomment these variables before running the sample.
+  # local_file_path = "resources/brooklyn_bridge.raw"
+
+  # The language of the supplied audio
+  language_code = "en-US"
+
+  # Sample rate in Hertz of the audio data sent
+  sample_rate_hertz = 16000
+
+  # Encoding of audio data sent. This sample sets this explicitly.
+  # This field is optional for FLAC and WAV audio formats.
+  encoding = :LINEAR16
+
+  config = {
+    language_code: language_code,
+    sample_rate_hertz: sample_rate_hertz,
+    encoding: encoding
+  }
+
+  content = File.binread local_file_path
+
+  audio = { content: content }
+
+  response = speech_client.recognize(config, audio)
+
+  response.results.each do |result|
+
+    # First alternative is the most probable result
+    alternative = result.alternatives[0]
+    puts "Transcript: #{alternative.transcript}"
+  end
+  # [END speech_transcribe_sync]
+end
+
+# Code below processes command-line arguments to execute this code sample.
+
+require "optparse"
+
+if $PROGRAM_NAME == __FILE__
+
+  local_file_path = "resources/brooklyn_bridge.raw"
+
+  ARGV.options do |opts|
+    opts.on("--local_file_path=val") { |val| local_file_path = val }
+    opts.parse!
+  end
+
+  sample_recognize(local_file_path)
+end
