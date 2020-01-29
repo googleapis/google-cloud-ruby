202b8905dba8f0e0e68896efac619687107354a5
yoshi-automation <yoshi-automation@google.com>
2020-01-29 03:47:11 -0800

    [CHANGE ME] Re-generated google-cloud-video_intelligence to pick up changes in the API or client library generator.

--- google-cloud-video_intelligence/lib/google/cloud/video_intelligence/v1p1beta1/doc/google/cloud/videointelligence/v1p1beta1/video_intelligence.rb
+++ google-cloud-video_intelligence/lib/google/cloud/video_intelligence/v1p1beta1/doc/google/cloud/videointelligence/v1p1beta1/video_intelligence.rb
@@ -24,10 +24,10 @@ module Google
         #     [Google Cloud Storage](https://cloud.google.com/storage/) URIs are
         #     supported, which must be specified in the following format:
         #     `gs://bucket-id/object-id` (other URI formats return
-        #     {Google::Rpc::Code::INVALID_ARGUMENT}). For
-        #     more information, see [Request URIs](https://cloud.google.com/storage/docs/reference-uris). A video
-        #     URI may include wildcards in `object-id`, and thus identify multiple
-        #     videos. Supported wildcards: '*' to match 0 or more characters;
+        #     {Google::Rpc::Code::INVALID_ARGUMENT}). For more information, see
+        #     [Request URIs](https://cloud.google.com/storage/docs/reference-uris).
+        #     A video URI may include wildcards in `object-id`, and thus identify
+        #     multiple videos. Supported wildcards: '*' to match 0 or more characters;
         #     '?' to match 1 character. If unset, the input video should be embedded
         #     in the request as `input_content`. If set, `input_content` should be unset.
         # @!attribute [rw] input_content
@@ -37,21 +37,21 @@ module Google
         #     If set, `input_uri` should be unset.
         # @!attribute [rw] features
         #   @return [Array<Google::Cloud::VideoIntelligence::V1p1beta1::Feature>]
-        #     Requested video annotation features.
+        #     Required. Requested video annotation features.
         # @!attribute [rw] video_context
         #   @return [Google::Cloud::VideoIntelligence::V1p1beta1::VideoContext]
         #     Additional video context and/or feature-specific parameters.
         # @!attribute [rw] output_uri
         #   @return [String]
-        #     Optional location where the output (in JSON format) should be stored.
+        #     Optional. Location where the output (in JSON format) should be stored.
         #     Currently, only [Google Cloud Storage](https://cloud.google.com/storage/)
         #     URIs are supported, which must be specified in the following format:
         #     `gs://bucket-id/object-id` (other URI formats return
-        #     {Google::Rpc::Code::INVALID_ARGUMENT}). For
-        #     more information, see [Request URIs](https://cloud.google.com/storage/docs/reference-uris).
+        #     {Google::Rpc::Code::INVALID_ARGUMENT}). For more information, see
+        #     [Request URIs](https://cloud.google.com/storage/docs/reference-uris).
         # @!attribute [rw] location_id
         #   @return [String]
-        #     Optional cloud region where annotation should take place. Supported cloud
+        #     Optional. Cloud region where annotation should take place. Supported cloud
         #     regions: `us-east1`, `us-west1`, `europe-west1`, `asia-east1`. If no region
         #     is specified, a region will be determined based on video file location.
         class AnnotateVideoRequest; end
@@ -258,30 +258,30 @@ module Google
         # Config for SPEECH_TRANSCRIPTION.
         # @!attribute [rw] language_code
         #   @return [String]
-        #     *Required* The language of the supplied audio as a
+        #     Required. *Required* The language of the supplied audio as a
         #     [BCP-47](https://www.rfc-editor.org/rfc/bcp/bcp47.txt) language tag.
         #     Example: "en-US".
         #     See [Language Support](https://cloud.google.com/speech/docs/languages)
         #     for a list of the currently supported language codes.
         # @!attribute [rw] max_alternatives
         #   @return [Integer]
-        #     *Optional* Maximum number of recognition hypotheses to be returned.
+        #     Optional. Maximum number of recognition hypotheses to be returned.
         #     Specifically, the maximum number of `SpeechRecognitionAlternative` messages
-        #     within each `SpeechRecognitionResult`. The server may return fewer than
+        #     within each `SpeechTranscription`. The server may return fewer than
         #     `max_alternatives`. Valid values are `0`-`30`. A value of `0` or `1` will
         #     return a maximum of one. If omitted, will return a maximum of one.
         # @!attribute [rw] filter_profanity
         #   @return [true, false]
-        #     *Optional* If set to `true`, the server will attempt to filter out
+        #     Optional. If set to `true`, the server will attempt to filter out
         #     profanities, replacing all but the initial character in each filtered word
         #     with asterisks, e.g. "f***". If set to `false` or omitted, profanities
         #     won't be filtered out.
         # @!attribute [rw] speech_contexts
         #   @return [Array<Google::Cloud::VideoIntelligence::V1p1beta1::SpeechContext>]
-        #     *Optional* A means to provide context to assist the speech recognition.
+        #     Optional. A means to provide context to assist the speech recognition.
         # @!attribute [rw] enable_automatic_punctuation
         #   @return [true, false]
-        #     *Optional* If 'true', adds punctuation to recognition result hypotheses.
+        #     Optional. If 'true', adds punctuation to recognition result hypotheses.
         #     This feature is only available in select languages. Setting this for
         #     requests in other languages has no effect at all. The default 'false' value
         #     does not add punctuation to result hypotheses. NOTE: "This is currently
@@ -289,7 +289,7 @@ module Google
         #     future this may be exclusively available as a premium feature."
         # @!attribute [rw] audio_tracks
         #   @return [Array<Integer>]
-        #     *Optional* For file formats, such as MXF or MKV, supporting multiple audio
+        #     Optional. For file formats, such as MXF or MKV, supporting multiple audio
         #     tracks, specify up to two tracks. Default: track 0.
         class SpeechTranscriptionConfig; end
 
@@ -297,7 +297,7 @@ module Google
         # in the results.
         # @!attribute [rw] phrases
         #   @return [Array<String>]
-        #     *Optional* A list of strings containing words and phrases "hints" so that
+        #     Optional. A list of strings containing words and phrases "hints" so that
         #     the speech recognition is more likely to recognize them. This can be used
         #     to improve the accuracy for specific words and phrases, for example, if
         #     specific commands are typically spoken by the user. This can also be used
@@ -308,10 +308,10 @@ module Google
         # A speech recognition result corresponding to a portion of the audio.
         # @!attribute [rw] alternatives
         #   @return [Array<Google::Cloud::VideoIntelligence::V1p1beta1::SpeechRecognitionAlternative>]
-        #     Output only. May contain one or more recognition hypotheses (up to the
-        #     maximum specified in `max_alternatives`).
-        #     These alternatives are ordered in terms of accuracy, with the top (first)
-        #     alternative being the most probable, as ranked by the recognizer.
+        #     May contain one or more recognition hypotheses (up to the maximum specified
+        #     in `max_alternatives`).  These alternatives are ordered in terms of
+        #     accuracy, with the top (first) alternative being the most probable, as
+        #     ranked by the recognizer.
         class SpeechTranscription; end
 
         # Alternative hypotheses (a.k.a. n-best list).
@@ -322,9 +322,9 @@ module Google
         #   @return [Float]
         #     Output only. The confidence estimate between 0.0 and 1.0. A higher number
         #     indicates an estimated greater likelihood that the recognized words are
-        #     correct. This field is typically provided only for the top hypothesis, and
-        #     only for `is_final=true` results. Clients should not rely on the
-        #     `confidence` field as it is not guaranteed to be accurate or consistent.
+        #     correct. This field is set only for the top alternative.
+        #     This field is not guaranteed to be accurate and users should not rely on it
+        #     to be always provided.
         #     The default of 0.0 is a sentinel value indicating `confidence` was not set.
         # @!attribute [rw] words
         #   @return [Array<Google::Cloud::VideoIntelligence::V1p1beta1::WordInfo>]
