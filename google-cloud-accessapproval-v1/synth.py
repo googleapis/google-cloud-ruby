60c59b3cc667f0d5fbbebeb8af0270134384fa1e
Viacheslav Rostovtsev <virost@google.com>
2020-02-21 19:23:34 +0000

    fix a typo

--- google-cloud-accessapproval-v1/synth.py
+++ google-cloud-accessapproval-v1/synth.py
@@ -30,7 +30,7 @@ library = gapic.ruby_library(
         "ruby-cloud-gem-name": "google-cloud-accessapproval-v1",
         "ruby-cloud-title": "Access Approval API V1",
         "ruby-cloud-summary": "An API for controlling access to data by Google personnel.",
-        "ruby-cloud-env-prefix": "ACCESSAPPROVAL:",
+        "ruby-cloud-env-prefix": "ACCESSAPPROVAL",
     }
 )
 
