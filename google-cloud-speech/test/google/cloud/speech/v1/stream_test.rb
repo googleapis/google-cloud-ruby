828319ec4b794ecfbe352940d5f5347db24d1877
Daniel Azuma <dazuma@google.com>
2020-02-25 14:21:29 -0800

    chore: require the simplecov library explicitly in test helpers

--- google-cloud-speech/test/google/cloud/speech/v1/stream_test.rb
+++ google-cloud-speech/test/google/cloud/speech/v1/stream_test.rb
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+require "simplecov"
 require "minitest/autorun"
 
 require "google/cloud/speech/v1"
