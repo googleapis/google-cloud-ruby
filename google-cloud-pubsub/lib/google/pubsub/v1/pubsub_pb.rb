3cf06fe9f31addba287f23d1834254eb1bd99095
yoshi-automation <yoshi-automation@google.com>
2020-03-05 03:43:01 -0800

    [CHANGE ME] Re-generated google-cloud-pubsub to pick up changes in the API or client library generator.

--- google-cloud-pubsub/lib/google/pubsub/v1/pubsub_pb.rb
+++ google-cloud-pubsub/lib/google/pubsub/v1/pubsub_pb.rb
@@ -4,6 +4,7 @@
 
 require 'google/protobuf'
 
+require 'google/api/annotations_pb'
 require 'google/api/client_pb'
 require 'google/api/field_behavior_pb'
 require 'google/api/resource_pb'
@@ -11,7 +12,6 @@ require 'google/protobuf/duration_pb'
 require 'google/protobuf/empty_pb'
 require 'google/protobuf/field_mask_pb'
 require 'google/protobuf/timestamp_pb'
-require 'google/api/annotations_pb'
 Google::Protobuf::DescriptorPool.generated_pool.build do
   add_message "google.pubsub.v1.MessageStoragePolicy" do
     repeated :allowed_persistence_regions, :string, 1
