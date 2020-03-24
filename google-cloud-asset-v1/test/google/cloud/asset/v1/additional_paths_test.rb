2e7c85977f3aecdbd6588b0a826e9a8b8545857b
Daniel Azuma <dazuma@google.com>
2020-03-24 16:49:23 -0700

    feat(asset-v1): add project path helper

---
+++ google-cloud-asset-v1/test/google/cloud/asset/v1/additional_paths_test.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require "simplecov"
+require "minitest/autorun"
+
+require "google/cloud/asset/v1"
+
+class AdditionalPathsTest < Minitest::Test
+  def test_project_path
+    path = Google::Cloud::Asset::V1::AssetService::Paths.project_path project: "my-project"
+    assert_equal "projects/my-project", path
+  end
+end
