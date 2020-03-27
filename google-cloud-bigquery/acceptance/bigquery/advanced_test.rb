ae8c3c26976f5e784f09b77a7bd93d9be67ab5d2
Chris Smith <quartzmo@gmail.com>
2020-03-27 17:41:01 -0600

    Indent heredoc in examples

--- google-cloud-bigquery/acceptance/bigquery/advanced_test.rb
+++ google-cloud-bigquery/acceptance/bigquery/advanced_test.rb
@@ -111,22 +111,22 @@ describe Google::Cloud::Bigquery, :advanced, :bigquery do
 
   it "executes SQL with multiple statements and creates child jobs with script_statistics" do
     multi_statement_sql = <<~SQL
-    -- Declare a variable to hold names as an array.
-    DECLARE top_names ARRAY<STRING>;
-    -- Build an array of the top 100 names from the year 2017.
-    SET top_names = (
-    SELECT ARRAY_AGG(name ORDER BY number DESC LIMIT 100)
-    FROM `bigquery-public-data.usa_names.usa_1910_current`
-    WHERE year = 2017
-    );
-    -- Which names appear as words in Shakespeare's plays?
-    SELECT
-    name AS shakespeare_name
-    FROM UNNEST(top_names) AS name
-    WHERE name IN (
-    SELECT word
-    FROM `bigquery-public-data.samples.shakespeare`
-    );
+      -- Declare a variable to hold names as an array.
+      DECLARE top_names ARRAY<STRING>;
+      -- Build an array of the top 100 names from the year 2017.
+      SET top_names = (
+      SELECT ARRAY_AGG(name ORDER BY number DESC LIMIT 100)
+      FROM `bigquery-public-data.usa_names.usa_1910_current`
+      WHERE year = 2017
+      );
+      -- Which names appear as words in Shakespeare's plays?
+      SELECT
+      name AS shakespeare_name
+      FROM UNNEST(top_names) AS name
+      WHERE name IN (
+      SELECT word
+      FROM `bigquery-public-data.samples.shakespeare`
+      );
     SQL
 
     job = bigquery.query_job multi_statement_sql
