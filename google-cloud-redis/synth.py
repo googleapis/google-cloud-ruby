3e854a4be04b94f0776b65d4f96a270485f7dd96
Daniel Azuma <dazuma@google.com>
2020-04-06 15:09:50 -0700

    feat(redis)!: Convert google-cloud-redis to a wrapper

--- google-cloud-redis/synth.py
+++ google-cloud-redis/synth.py
@@ -1,4 +1,4 @@
-# Copyright 2018 Google LLC
+# Copyright 2020 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,240 +18,22 @@ import synthtool as s
 import synthtool.gcp as gcp
 import synthtool.languages.ruby as ruby
 import logging
-import re
-
 
 logging.basicConfig(level=logging.DEBUG)
 
-gapic = gcp.GAPICGenerator()
-
-v1_library = gapic.ruby_library(
-    'redis', 'v1',
-    artman_output_name='google-cloud-ruby/google-cloud-redis'
-)
-s.copy(v1_library / 'lib/google/cloud/redis.rb')
-s.copy(v1_library / 'lib/google/cloud/redis/v1')
-s.copy(v1_library / 'lib/google/cloud/redis/v1.rb')
-s.copy(v1_library / 'test/google/cloud/redis/v1')
-s.copy(v1_library / 'README.md')
-s.copy(v1_library / 'LICENSE')
-s.copy(v1_library / '.gitignore')
-s.copy(v1_library / '.yardopts')
-s.copy(v1_library / 'google-cloud-redis.gemspec', merge=ruby.merge_gemspec)
-
-# Copy common templates
-templates = gcp.CommonTemplates().ruby_library()
-s.copy(templates)
-
-v1beta1_library = gapic.ruby_library(
-    'redis', 'v1beta1',
-    artman_output_name='google-cloud-ruby/google-cloud-redis'
-)
-s.copy(v1beta1_library / 'lib/google/cloud/redis/v1beta1')
-s.copy(v1beta1_library / 'lib/google/cloud/redis/v1beta1.rb')
-s.copy(v1beta1_library / 'test/google/cloud/redis/v1beta1')
-
-# Update gemspec to reflect Ruby 2.4
-ruby.update_gemspec('google-cloud-redis.gemspec')
-
-# Update README to reflect Ruby 2.4
-s.replace(
-    'README.md',
-    'Ruby 2.3',
-    'Ruby 2.4'
+gapic = gcp.GAPICMicrogenerator()
+library = gapic.ruby_library(
+    "redis", "v1",
+    generator_args={
+        "ruby-cloud-gem-name": "google-cloud-redis",
+        "ruby-cloud-title": "Google Cloud Memorystore for Redis",
+        "ruby-cloud-description": "Creates and manages Redis instances on the Google Cloud Platform.",
+        "ruby-cloud-env-prefix": "REDIS",
+        "ruby-cloud-wrapper-of": "v1:0.0;v1beta1:0.0",
+        "ruby-cloud-product-url": "https://cloud.google.com/memorystore/docs/redis",
+        "ruby-cloud-api-id": "redis",
+        "ruby-cloud-migration-version": "1.0",
+    }
 )
 
-# Support for service_address
-s.replace(
-    [
-        'lib/google/cloud/redis.rb',
-        'lib/google/cloud/redis/v*.rb',
-        'lib/google/cloud/redis/v*/*_client.rb'
-    ],
-    '\n(\\s+)#(\\s+)@param exception_transformer',
-    '\n\\1#\\2@param service_address [String]\n' +
-        '\\1#\\2  Override for the service hostname, or `nil` to leave as the default.\n' +
-        '\\1#\\2@param service_port [Integer]\n' +
-        '\\1#\\2  Override for the service port, or `nil` to leave as the default.\n' +
-        '\\1#\\2@param exception_transformer'
-)
-s.replace(
-    [
-        'lib/google/cloud/redis/v*.rb',
-        'lib/google/cloud/redis/v*/*_client.rb'
-    ],
-    '\n(\\s+)metadata: nil,\n\\s+exception_transformer: nil,\n',
-    '\n\\1metadata: nil,\n\\1service_address: nil,\n\\1service_port: nil,\n\\1exception_transformer: nil,\n'
-)
-s.replace(
-    [
-        'lib/google/cloud/redis/v*.rb',
-        'lib/google/cloud/redis/v*/*_client.rb'
-    ],
-    ',\n(\\s+)lib_name: lib_name,\n\\s+lib_version: lib_version',
-    ',\n\\1lib_name: lib_name,\n\\1service_address: service_address,\n\\1service_port: service_port,\n\\1lib_version: lib_version'
-)
-s.replace(
-    'lib/google/cloud/redis/v*/*_client.rb',
-    'service_path = self\\.class::SERVICE_ADDRESS',
-    'service_path = service_address || self.class::SERVICE_ADDRESS'
-)
-s.replace(
-    'lib/google/cloud/redis/v*/*_client.rb',
-    'port = self\\.class::DEFAULT_SERVICE_PORT',
-    'port = service_port || self.class::DEFAULT_SERVICE_PORT'
-)
-
-# Ensure simplecov is required
-s.replace(
-    'test/**/*.rb',
-    '\n\nrequire "minitest/autorun"\n',
-    '\n\nrequire "simplecov"\nrequire "minitest/autorun"\n'
-)
-
-# Remove legacy release level from documentation
-s.replace(
-    [
-        'README.md',
-        'lib/google/cloud/**/*.rb'
-    ],
-    '\\s+\\(\\[\\w+\\]\\(https://github\\.com/(googleapis|GoogleCloudPlatform)/google-cloud-ruby#versioning\\)\\)',
-    ''
-)
-
-# https://github.com/googleapis/gapic-generator/issues/2196
-s.replace(
-    [
-        'README.md',
-        'lib/google/cloud/redis.rb',
-        'lib/google/cloud/redis/v1.rb',
-        'lib/google/cloud/redis/v1beta1.rb'
-    ],
-    '\\[Product Documentation\\]: https://cloud\\.google\\.com/redis\n',
-    '[Product Documentation]: https://cloud.google.com/memorystore\n')
-
-# https://github.com/googleapis/gapic-generator/issues/2232
-s.replace(
-    [
-        'lib/google/cloud/redis/v1/cloud_redis_client.rb',
-        'lib/google/cloud/redis/v1beta1/cloud_redis_client.rb'
-    ],
-    '\n\n(\\s+)class OperationsClient < Google::Longrunning::OperationsClient',
-    '\n\n\\1# @private\n\\1class OperationsClient < Google::Longrunning::OperationsClient')
-
-# https://github.com/googleapis/gapic-generator/issues/2242
-def escape_braces(match):
-    expr = re.compile('^([^`]*(`[^`]*`[^`]*)*)([^`#\\$\\\\])\\{([\\w,]+)\\}')
-    content = match.group(0)
-    while True:
-        content, count = expr.subn('\\1\\3\\\\\\\\{\\4}', content)
-        if count == 0:
-            return content
-s.replace(
-    'lib/google/cloud/**/*.rb',
-    '\n(\\s+)#[^\n]*[^\n#\\$\\\\]\\{[\\w,]+\\}',
-    escape_braces)
-
-# https://github.com/googleapis/gapic-generator/issues/2243
-s.replace(
-    'lib/google/cloud/redis/*/*_client.rb',
-    '(\n\\s+class \\w+Client\n)(\\s+)(attr_reader :\\w+_stub)',
-    '\\1\\2# @private\n\\2\\3')
-
-# https://github.com/googleapis/gapic-generator/issues/2279
-s.replace(
-    'lib/**/*.rb',
-    '\\A(((#[^\n]*)?\n)*# (Copyright \\d+|Generated by the protocol buffer compiler)[^\n]+\n(#[^\n]*\n)*\n)([^\n])',
-    '\\1\n\\6')
-
-# https://github.com/googleapis/gapic-generator/issues/2323
-s.replace(
-    [
-        'lib/**/*.rb',
-        'README.md'
-    ],
-    'https://github\\.com/GoogleCloudPlatform/google-cloud-ruby',
-    'https://github.com/googleapis/google-cloud-ruby'
-)
-s.replace(
-    [
-        'lib/**/*.rb',
-        'README.md'
-    ],
-    'https://googlecloudplatform\\.github\\.io/google-cloud-ruby',
-    'https://googleapis.github.io/google-cloud-ruby'
-)
-
-s.replace(
-    'google-cloud-redis.gemspec',
-    'gem.add_dependency "google-gax", "~> 1\\.[\\d\\.]+"',
-    "\n".join([
-        'gem.add_dependency "google-gax", "~> 1.8"',
-        '  gem.add_dependency "googleapis-common-protos", ">= 1.3.9", "< 2.0"',
-        '  gem.add_dependency "googleapis-common-protos-types", ">= 1.0.4", "< 2.0"'
-    ])
-)
-
-s.replace(
-    'google-cloud-redis.gemspec',
-    '"README.md", "LICENSE"',
-    '"README.md", "AUTHENTICATION.md", "LICENSE"'
-)
-s.replace(
-    '.yardopts',
-    'README.md\n',
-    'README.md\nAUTHENTICATION.md\nLICENSE\n'
-)
-
-# https://github.com/googleapis/google-cloud-ruby/issues/3058
-s.replace(
-    'google-cloud-redis.gemspec',
-    '\nGem::Specification.new do',
-    'require File.expand_path("../lib/google/cloud/redis/version", __FILE__)\n\nGem::Specification.new do'
-)
-s.replace(
-    'google-cloud-redis.gemspec',
-    '(gem.version\s+=\s+).\d+.\d+.\d.*$',
-    '\\1Google::Cloud::Redis::VERSION'
-)
-for version in ['v1', 'v1beta1']:
-    s.replace(
-        f'lib/google/cloud/redis/{version}/*_client.rb',
-        f'(require \".*credentials\"\n)\n',
-        f'\\1require "google/cloud/redis/version"\n\n'
-    )
-    s.replace(
-        f'lib/google/cloud/redis/{version}/*_client.rb',
-        'Gem.loaded_specs\[.*\]\.version\.version',
-        'Google::Cloud::Redis::VERSION'
-    )
-
-# Fix links for devsite migration
-for file in ['lib/**/*.rb', '*.md']:
-    s.replace(
-        file,
-        'https://googleapis.github.io/google-cloud-ruby/#/docs/google-cloud-logging/latest/google/cloud/logging/logger',
-        'https://googleapis.dev/ruby/google-cloud-logging/latest'
-    )
-s.replace(
-    '*.md',
-    'https://googleapis.github.io/google-cloud-ruby/#/docs/.*/authentication',
-    'https://googleapis.dev/ruby/google-cloud-redis/latest/file.AUTHENTICATION.html'
-)
-s.replace(
-    'lib/**/*.rb',
-    'https://googleapis.github.io/google-cloud-ruby/#/docs/.*/authentication',
-    'https://googleapis.dev/ruby/google-cloud-redis/latest/file.AUTHENTICATION.html'
-)
-s.replace(
-    'README.md',
-    'github.io/google-cloud-ruby/#/docs/google-cloud-redis/latest/.*$',
-    'dev/ruby/google-cloud-redis/latest'
-)
-
-# http://redis.io/topics/config is 404ing
-s.replace(
-    'lib/**/cloud_redis.rb',
-    'http://redis.io/topics/config',
-    'https://redis.io/topics/config'
-)
+s.copy(library, merge=ruby.global_merge)
