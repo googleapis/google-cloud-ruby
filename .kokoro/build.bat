4a7ea8159934a944f5db20666aead4348aa03fd9
TheRoyalTnetennba <paye@google.com>
2020-04-09 11:00:20 -0600

    fix batch syntax

--- .kokoro/build.bat
+++ .kokoro/build.bat
@@ -13,12 +13,17 @@ REM Neither Powershell's Copy-Item nor xcopy correctly copy the symlinks.
 REM So we clone/checkout the repo ourselves rather than relying on Kokoro.
 SET "run_kokoro=ridk enable && bundle update && bundle exec rake kokoro:%JOB_TYPE%"
 
-IF "%JOB_TYPE%"=="presubmit" OR IF "%JOB_TYPE%"=="samples_presubmit" (
+IF "%JOB_TYPE%"=="presubmit" (
     SET clone_command="`git clone #{ENV['KOKORO_GITHUB_PULL_REQUEST_URL'].split('/pull')[0]}.git`"
     SET "git_commands=git fetch origin pull/%KOKORO_GITHUB_PULL_REQUEST_NUMBER%/merge:pull_branch && git checkout pull_branch"
 ) ELSE (
-    SET clone_command="`git clone #{ENV['KOKORO_GITHUB_COMMIT_URL'].split('/commit')[0]}.git`"
-    SET "git_commands=git fetch --depth=10000 && git checkout %KOKORO_GIT_COMMIT%"
+    IF "%JOB_TYPE%"=="samples_presubmit" (
+        SET clone_command="`git clone #{ENV['KOKORO_GITHUB_PULL_REQUEST_URL'].split('/pull')[0]}.git`"
+        SET "git_commands=git fetch origin pull/%KOKORO_GITHUB_PULL_REQUEST_NUMBER%/merge:pull_branch && git checkout pull_branch"  
+    ) ELSE (
+        SET clone_command="`git clone #{ENV['KOKORO_GITHUB_COMMIT_URL'].split('/commit')[0]}.git`"
+        SET "git_commands=git fetch --depth=10000 && git checkout %KOKORO_GIT_COMMIT%"
+    )
 )
 
 ruby -e %clone_command% && CD %REPO_DIR% && %git_commands% && %run_kokoro% && SET /A ERROR_CODE=0
