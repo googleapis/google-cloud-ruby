fdad1e982410570728ef4ad58f709f57ed68d513
yoshi-automation <yoshi-automation@google.com>
2020-04-12 03:48:00 -0700

    [CHANGE ME] Re-generated google-cloud-dataproc-v1beta2 to pick up changes in the API or client library generator.

--- google-cloud-dataproc-v1beta2/lib/google/cloud/dataproc/v1beta2/workflow_template_service/paths.rb
+++ google-cloud-dataproc-v1beta2/lib/google/cloud/dataproc/v1beta2/workflow_template_service/paths.rb
@@ -25,6 +25,40 @@ module Google
           # Path helper methods for the WorkflowTemplateService API.
           module Paths
             ##
+            # Create a fully-qualified Location resource string.
+            #
+            # The resource will be in the following format:
+            #
+            # `projects/{project}/locations/{location}`
+            #
+            # @param project [String]
+            # @param location [String]
+            #
+            # @return [String]
+            def location_path project:, location:
+              raise ArgumentError, "project cannot contain /" if project.to_s.include? "/"
+
+              "projects/#{project}/locations/#{location}"
+            end
+
+            ##
+            # Create a fully-qualified Region resource string.
+            #
+            # The resource will be in the following format:
+            #
+            # `projects/{project}/regions/{region}`
+            #
+            # @param project [String]
+            # @param region [String]
+            #
+            # @return [String]
+            def region_path project:, region:
+              raise ArgumentError, "project cannot contain /" if project.to_s.include? "/"
+
+              "projects/#{project}/regions/#{region}"
+            end
+
+            ##
             # Create a fully-qualified WorkflowTemplate resource string.
             #
             # @overload workflow_template_path(project:, region:, workflow_template:)
