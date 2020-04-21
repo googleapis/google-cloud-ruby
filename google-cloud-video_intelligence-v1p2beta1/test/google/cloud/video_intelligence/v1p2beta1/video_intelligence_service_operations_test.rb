26ce5b1a26a06c0755c0e7875d27c2b290deac7f
yoshi-automation <yoshi-automation@google.com>
2020-04-21 04:03:31 -0700

    [CHANGE ME] Re-generated google-cloud-video_intelligence-v1p2beta1 to pick up changes in the API or client library generator.

--- google-cloud-video_intelligence-v1p2beta1/test/google/cloud/video_intelligence/v1p2beta1/video_intelligence_service_operations_test.rb
+++ google-cloud-video_intelligence-v1p2beta1/test/google/cloud/video_intelligence/v1p2beta1/video_intelligence_service_operations_test.rb
@@ -293,4 +293,22 @@ class Google::Cloud::VideoIntelligence::V1p2beta1::VideoIntelligenceService::Ope
       assert_equal 5, cancel_operation_client_stub.call_rpc_count
     end
   end
+
+  def test_configure
+    grpc_channel = GRPC::Core::Channel.new "localhost:8888", nil, :this_channel_is_insecure
+
+    client = block_config = config = nil
+    Gapic::ServiceStub.stub :new, nil do
+      client = Google::Cloud::VideoIntelligence::V1p2beta1::VideoIntelligenceService::Operations.new do |config|
+        config.credentials = grpc_channel
+      end
+    end
+
+    config = client.configure do |c|
+      block_config = c
+    end
+
+    assert_same block_config, config
+    assert_kind_of Google::Cloud::VideoIntelligence::V1p2beta1::VideoIntelligenceService::Operations::Configuration, config
+  end
 end
