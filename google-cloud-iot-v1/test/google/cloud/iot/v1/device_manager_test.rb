a4ad3e97fb7ab0460766aeff5a7ec7369eb9792b
yoshi-automation <yoshi-automation@google.com>
2020-04-21 03:48:27 -0700

    [CHANGE ME] Re-generated google-cloud-iot-v1 to pick up changes in the API or client library generator.

--- google-cloud-iot-v1/test/google/cloud/iot/v1/device_manager_test.rb
+++ google-cloud-iot-v1/test/google/cloud/iot/v1/device_manager_test.rb
@@ -1209,4 +1209,22 @@ class Google::Cloud::Iot::V1::DeviceManager::ClientTest < Minitest::Test
       assert_equal 5, unbind_device_from_gateway_client_stub.call_rpc_count
     end
   end
+
+  def test_configure
+    grpc_channel = GRPC::Core::Channel.new "localhost:8888", nil, :this_channel_is_insecure
+
+    client = block_config = config = nil
+    Gapic::ServiceStub.stub :new, nil do
+      client = Google::Cloud::Iot::V1::DeviceManager::Client.new do |config|
+        config.credentials = grpc_channel
+      end
+    end
+
+    config = client.configure do |c|
+      block_config = c
+    end
+
+    assert_same block_config, config
+    assert_kind_of Google::Cloud::Iot::V1::DeviceManager::Client::Configuration, config
+  end
 end
