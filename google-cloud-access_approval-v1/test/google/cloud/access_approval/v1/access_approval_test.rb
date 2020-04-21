c4294119d7b71aa28bcbccef088a77671bcf6989
yoshi-automation <yoshi-automation@google.com>
2020-04-21 03:39:15 -0700

    [CHANGE ME] Re-generated google-cloud-access_approval-v1 to pick up changes in the API or client library generator.

--- google-cloud-access_approval-v1/test/google/cloud/access_approval/v1/access_approval_test.rb
+++ google-cloud-access_approval-v1/test/google/cloud/access_approval/v1/access_approval_test.rb
@@ -466,4 +466,22 @@ class Google::Cloud::AccessApproval::V1::AccessApproval::ClientTest < Minitest::
       assert_equal 5, delete_access_approval_settings_client_stub.call_rpc_count
     end
   end
+
+  def test_configure
+    grpc_channel = GRPC::Core::Channel.new "localhost:8888", nil, :this_channel_is_insecure
+
+    client = block_config = config = nil
+    Gapic::ServiceStub.stub :new, nil do
+      client = Google::Cloud::AccessApproval::V1::AccessApproval::Client.new do |config|
+        config.credentials = grpc_channel
+      end
+    end
+
+    config = client.configure do |c|
+      block_config = c
+    end
+
+    assert_same block_config, config
+    assert_kind_of Google::Cloud::AccessApproval::V1::AccessApproval::Client::Configuration, config
+  end
 end
