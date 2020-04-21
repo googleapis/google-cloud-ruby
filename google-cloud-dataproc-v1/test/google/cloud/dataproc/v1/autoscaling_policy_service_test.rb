50ce804167d58bb2d64bb0eb037eaf68c7056e64
yoshi-automation <yoshi-automation@google.com>
2020-04-21 03:45:29 -0700

    [CHANGE ME] Re-generated google-cloud-dataproc-v1 to pick up changes in the API or client library generator.

--- google-cloud-dataproc-v1/test/google/cloud/dataproc/v1/autoscaling_policy_service_test.rb
+++ google-cloud-dataproc-v1/test/google/cloud/dataproc/v1/autoscaling_policy_service_test.rb
@@ -346,4 +346,22 @@ class Google::Cloud::Dataproc::V1::AutoscalingPolicyService::ClientTest < Minite
       assert_equal 5, delete_autoscaling_policy_client_stub.call_rpc_count
     end
   end
+
+  def test_configure
+    grpc_channel = GRPC::Core::Channel.new "localhost:8888", nil, :this_channel_is_insecure
+
+    client = block_config = config = nil
+    Gapic::ServiceStub.stub :new, nil do
+      client = Google::Cloud::Dataproc::V1::AutoscalingPolicyService::Client.new do |config|
+        config.credentials = grpc_channel
+      end
+    end
+
+    config = client.configure do |c|
+      block_config = c
+    end
+
+    assert_same block_config, config
+    assert_kind_of Google::Cloud::Dataproc::V1::AutoscalingPolicyService::Client::Configuration, config
+  end
 end
