c22e4bd49c5b12e636332945034db5d9a20ecea4
yoshi-automation <yoshi-automation@google.com>
2020-04-21 03:56:55 -0700

    [CHANGE ME] Re-generated google-cloud-security_center-v1p1beta1 to pick up changes in the API or client library generator.

--- google-cloud-security_center-v1p1beta1/test/google/cloud/security_center/v1p1beta1/security_center_test.rb
+++ google-cloud-security_center-v1p1beta1/test/google/cloud/security_center/v1p1beta1/security_center_test.rb
@@ -1514,4 +1514,35 @@ class Google::Cloud::SecurityCenter::V1p1beta1::SecurityCenter::ClientTest < Min
       assert_equal 5, update_security_marks_client_stub.call_rpc_count
     end
   end
+
+  def test_configure
+    grpc_channel = GRPC::Core::Channel.new "localhost:8888", nil, :this_channel_is_insecure
+
+    client = block_config = config = nil
+    Gapic::ServiceStub.stub :new, nil do
+      client = Google::Cloud::SecurityCenter::V1p1beta1::SecurityCenter::Client.new do |config|
+        config.credentials = grpc_channel
+      end
+    end
+
+    config = client.configure do |c|
+      block_config = c
+    end
+
+    assert_same block_config, config
+    assert_kind_of Google::Cloud::SecurityCenter::V1p1beta1::SecurityCenter::Client::Configuration, config
+  end
+
+  def test_operations_client
+    grpc_channel = GRPC::Core::Channel.new "localhost:8888", nil, :this_channel_is_insecure
+
+    client = nil
+    Gapic::ServiceStub.stub :new, nil do
+      client = Google::Cloud::SecurityCenter::V1p1beta1::SecurityCenter::Client.new do |config|
+        config.credentials = grpc_channel
+      end
+    end
+
+    assert_kind_of Google::Cloud::SecurityCenter::V1p1beta1::SecurityCenter::Operations, client.operations_client
+  end
 end
