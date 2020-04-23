a2289b2b2c6d001a5e1b682327700b11b695d134
yoshi-automation <yoshi-automation@google.com>
2020-04-23 03:40:04 -0700

    [CHANGE ME] Re-generated google-cloud-automl-v1 to pick up changes in the API or client library generator.

--- google-cloud-automl-v1/test/google/cloud/automl/v1/prediction_service_test.rb
+++ google-cloud-automl-v1/test/google/cloud/automl/v1/prediction_service_test.rb
@@ -176,4 +176,35 @@ class Google::Cloud::AutoML::V1::PredictionService::ClientTest < Minitest::Test
       assert_equal 5, batch_predict_client_stub.call_rpc_count
     end
   end
+
+  def test_configure
+    grpc_channel = GRPC::Core::Channel.new "localhost:8888", nil, :this_channel_is_insecure
+
+    client = block_config = config = nil
+    Gapic::ServiceStub.stub :new, nil do
+      client = Google::Cloud::AutoML::V1::PredictionService::Client.new do |config|
+        config.credentials = grpc_channel
+      end
+    end
+
+    config = client.configure do |c|
+      block_config = c
+    end
+
+    assert_same block_config, config
+    assert_kind_of Google::Cloud::AutoML::V1::PredictionService::Client::Configuration, config
+  end
+
+  def test_operations_client
+    grpc_channel = GRPC::Core::Channel.new "localhost:8888", nil, :this_channel_is_insecure
+
+    client = nil
+    Gapic::ServiceStub.stub :new, nil do
+      client = Google::Cloud::AutoML::V1::PredictionService::Client.new do |config|
+        config.credentials = grpc_channel
+      end
+    end
+
+    assert_kind_of Google::Cloud::AutoML::V1::PredictionService::Operations, client.operations_client
+  end
 end
