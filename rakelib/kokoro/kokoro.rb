235314f5fe4c4ab3e55f54c1b23c94ca189b1faf
Daniel Azuma <dazuma@google.com>
2020-04-24 09:04:25 -0700

    chore: Kick the link checker can again

--- rakelib/kokoro/kokoro.rb
+++ rakelib/kokoro/kokoro.rb
@@ -38,7 +38,7 @@ class Kokoro < Command
         run "bundle exec rake ci", 1800
         # TODO: Remove date requirement
         require "date"
-        next unless Date.today > Date.new(2020, 4, 23)
+        next unless Date.today > Date.new(2020, 4, 30)
         local_docs_test if should_link_check? gem || (
           autorelease_pending? && @should_release
         )
