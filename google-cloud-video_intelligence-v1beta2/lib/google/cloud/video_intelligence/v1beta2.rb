f58fb5aa02a2463e84c69b7272939691b16df210
yoshi-automation <yoshi-automation@google.com>
2020-04-29 04:05:11 -0700

    [CHANGE ME] Re-generated google-cloud-video_intelligence-v1beta2 to pick up changes in the API or client library generator.

--- google-cloud-video_intelligence-v1beta2/lib/google/cloud/video_intelligence/v1beta2.rb
+++ google-cloud-video_intelligence-v1beta2/lib/google/cloud/video_intelligence/v1beta2.rb
@@ -26,7 +26,7 @@ module Google
       # To load this package, including all its services, and instantiate a client:
       #
       #     require "google/cloud/video_intelligence/v1beta2"
-      #     client = Google::Cloud::VideoIntelligence::V1beta2::VideoIntelligenceService::Client.new
+      #     client = ::Google::Cloud::VideoIntelligence::V1beta2::VideoIntelligenceService::Client.new
       #
       module V1beta2
       end
