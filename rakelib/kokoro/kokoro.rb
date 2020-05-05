742a1381a771839b24ed15ebc2e3a29a99859212
Daniel Azuma <dazuma@google.com>
2020-05-05 08:19:01 -0700

    chore: Delay local_docs_test until May 11

--- rakelib/kokoro/kokoro.rb
+++ rakelib/kokoro/kokoro.rb
@@ -43,7 +43,7 @@ class Kokoro < Command
         run "bundle exec rake ci", 1800
         # TODO: Remove date requirement
         require "date"
-        next unless Date.today > Date.new(2020, 4, 30)
+        next unless Date.today > Date.new(2020, 5, 11)
         local_docs_test if should_link_check? gem || (
           autorelease_pending? && @should_release
         )
