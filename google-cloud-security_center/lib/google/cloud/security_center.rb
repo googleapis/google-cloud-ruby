cf66ca6427299c3251ae7b10a1e9ab081277cda8
yoshi-automation <yoshi-automation@google.com>
2020-05-19 10:32:12 -0700

    changes without context
            autosynth cannot find the source of changes triggered by earlier changes in this
            repository, or by version upgrades to tools such as linters.

--- google-cloud-security_center/lib/google/cloud/security_center.rb
+++ google-cloud-security_center/lib/google/cloud/security_center.rb
@@ -29,13 +29,16 @@ require "google/cloud/config"
 
 # Set the default configuration
 ::Google::Cloud.configure.add_config! :security_center do |config|
-  config.add_field! :credentials,  nil, match: [::String, ::Hash, ::Google::Auth::Credentials]
-  config.add_field! :lib_name,     nil, match: ::String
-  config.add_field! :lib_version,  nil, match: ::String
-  config.add_field! :interceptors, nil, match: ::Array
-  config.add_field! :timeout,      nil, match: ::Numeric
-  config.add_field! :metadata,     nil, match: ::Hash
-  config.add_field! :retry_policy, nil, match: [::Hash, ::Proc]
+  config.add_field! :endpoint,      "securitycenter.googleapis.com", match: ::String
+  config.add_field! :credentials,   nil, match: [::String, ::Hash, ::Google::Auth::Credentials]
+  config.add_field! :scope,         nil, match: [::Array, ::String]
+  config.add_field! :lib_name,      nil, match: ::String
+  config.add_field! :lib_version,   nil, match: ::String
+  config.add_field! :interceptors,  nil, match: ::Array
+  config.add_field! :timeout,       nil, match: ::Numeric
+  config.add_field! :metadata,      nil, match: ::Hash
+  config.add_field! :retry_policy,  nil, match: [::Hash, ::Proc]
+  config.add_field! :quota_project, nil, match: ::String
 end
 
 module Google
@@ -107,3 +110,6 @@ module Google
     end
   end
 end
+
+helper_path = ::File.join __dir__, "security_center", "helpers.rb"
+require "google/cloud/security_center/helpers" if ::File.file? helper_path
