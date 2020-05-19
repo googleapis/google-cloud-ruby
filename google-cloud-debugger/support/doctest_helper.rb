97c5d9d77e1f6f4a7ef01b3e75ebb769d9bc7cbe
Daniel Azuma <dazuma@google.com>
2020-05-18 17:34:39 -0700

    Update a bunch of gemfiles and doctest helpers

--- google-cloud-debugger/support/doctest_helper.rb
+++ google-cloud-debugger/support/doctest_helper.rb
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+require "minitest/focus"
 
 require "google/cloud/debugger"
 require "minitest/rg"
