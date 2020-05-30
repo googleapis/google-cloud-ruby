5c8328bce4a6fd93600109dbed260fdbe32c8396
yoshi-automation <yoshi-automation@google.com>
2020-05-30 03:46:15 -0700

    Drop the experimental tag on the Subscription filter and retry policy fields Clarify some retry policy comments
    PiperOrigin-RevId: 313812131
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Fri May 29 10:59:34 2020 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: fb21971becb6c0441efc855dabc371825732872c
    Source-Link: https://github.com/googleapis/googleapis/commit/fb21971becb6c0441efc855dabc371825732872c

--- google-cloud-pubsub/lib/google/cloud/pubsub/v1/doc/google/pubsub/v1/pubsub.rb
+++ google-cloud-pubsub/lib/google/cloud/pubsub/v1/doc/google/pubsub/v1/pubsub.rb
@@ -311,9 +311,6 @@ module Google
       #     then only `PubsubMessage`s whose `attributes` field matches the filter are
       #     delivered on this subscription. If empty, then no messages are filtered
       #     out.
-      #     <b>EXPERIMENTAL:</b> This feature is part of a closed alpha release. This
-      #     API might be changed in backward-incompatible ways and is not recommended
-      #     for production use. It is not subject to any SLA or deprecation policy.
       # @!attribute [rw] dead_letter_policy
       #   @return [Google::Cloud::PubSub::V1::DeadLetterPolicy]
       #     A policy that specifies the conditions for dead lettering messages in
@@ -326,16 +323,13 @@ module Google
       #     permission to Acknowledge() messages on this subscription.
       # @!attribute [rw] retry_policy
       #   @return [Google::Cloud::PubSub::V1::RetryPolicy]
-      #     A policy that specifies how Cloud Pub/Sub retries message delivery for this
+      #     A policy that specifies how Pub/Sub retries message delivery for this
       #     subscription.
       #
       #     If not set, the default retry policy is applied. This generally implies
       #     that messages will be retried as soon as possible for healthy subscribers.
       #     RetryPolicy will be triggered on NACKs or acknowledgement deadline
       #     exceeded events for a given message.
-      #     <b>EXPERIMENTAL:</b> This API might be changed in backward-incompatible
-      #     ways and is not recommended for production use. It is not subject to any
-      #     SLA or deprecation policy.
       class Subscription; end
 
       # A policy that specifies how Cloud Pub/Sub retries message delivery.
