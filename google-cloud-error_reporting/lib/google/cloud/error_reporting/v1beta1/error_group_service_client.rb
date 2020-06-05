1abfbad2ee9202a4b76c531fa9c8e160408b4c60
yoshi-automation <yoshi-automation@google.com>
2020-06-05 03:50:47 -0700

    docs: fix several broken links in the docs.
    PiperOrigin-RevId: 314787285
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Thu Jun 4 13:00:20 2020 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: 8f41c697c67d357665ce9afac18793c8f80ac73c
    Source-Link: https://github.com/googleapis/googleapis/commit/8f41c697c67d357665ce9afac18793c8f80ac73c

--- google-cloud-error_reporting/lib/google/cloud/error_reporting/v1beta1/error_group_service_client.rb
+++ google-cloud-error_reporting/lib/google/cloud/error_reporting/v1beta1/error_group_service_client.rb
@@ -245,14 +245,12 @@ module Google
           # Get the specified group.
           #
           # @param group_name [String]
-          #   Required. The group resource name. Written as
-          #   <code>projects/<var>projectID</var>/groups/<var>group_name</var></code>.
-          #   Call
-          #   <a href="/error-reporting/reference/rest/v1beta1/projects.groupStats/list">
-          #   <code>groupStats.list</code></a> to return a list of groups belonging to
-          #   this project.
+          #   The group resource name. Written as
+          #   `projects/{projectID}/groups/{group_name}`. Call
+          #   [`groupStats.list`](https://cloud.google.com/error-reporting/reference/rest/v1beta1/projects.groupStats/list)
+          #   to return a list of groups belonging to this project.
           #
-          #   Example: <code>projects/my-project-123/groups/my-group</code>
+          #   Example: `projects/my-project-123/groups/my-group`
           # @param options [Google::Gax::CallOptions]
           #   Overrides the default settings for this call, e.g, timeout,
           #   retries, etc.
