5a43f4b1812f6797847a2873cdbfd5e3b071d849
yoshi-automation <yoshi-automation@google.com>
2020-06-11 03:49:42 -0700

    feat: Add Resource to the v1 NotificationMessage, which contains resource-related fields associated to the Finding notification. docs: Adjusted wording from Cloud SCC to Security Command Center, and updated filtering docs for List APIs.
    PiperOrigin-RevId: 315715539
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Wed Jun 10 10:20:15 2020 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: 3e7056405a83fcbdca86c829d32264b8102fc893
    Source-Link: https://github.com/googleapis/googleapis/commit/3e7056405a83fcbdca86c829d32264b8102fc893

--- google-cloud-security_center-v1/proto_docs/google/cloud/securitycenter/v1/source.rb
+++ google-cloud-security_center-v1/proto_docs/google/cloud/securitycenter/v1/source.rb
@@ -21,7 +21,7 @@ module Google
   module Cloud
     module SecurityCenter
       module V1
-        # Cloud Security Command Center's (Cloud SCC) finding source. A finding source
+        # Security Command Center finding source. A finding source
         # is an entity or a mechanism that can produce a finding. A source is like a
         # container of findings that come from the same scanner, logger, monitor, and
         # other tools.
