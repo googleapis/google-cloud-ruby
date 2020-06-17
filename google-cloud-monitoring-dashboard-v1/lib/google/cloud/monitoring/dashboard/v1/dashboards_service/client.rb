fdac8a3bede84288d1cfaada27c41fd741257755
yoshi-automation <yoshi-automation@google.com>
2020-06-17 03:55:30 -0700

    changes without context
            autosynth cannot find the source of changes triggered by earlier changes in this
            repository, or by version upgrades to tools such as linters.

--- google-cloud-monitoring-dashboard-v1/lib/google/cloud/monitoring/dashboard/v1/dashboards_service/client.rb
+++ google-cloud-monitoring-dashboard-v1/lib/google/cloud/monitoring/dashboard/v1/dashboards_service/client.rb
@@ -67,6 +67,20 @@ module Google
                                   end
                   default_config = Client::Configuration.new parent_config
 
+                  default_config.timeout = 30.0
+                  default_config.retry_policy = {
+                    initial_delay: 1.0,
+                    max_delay:     10.0,
+                    multiplier:    1.3,
+                    retry_codes:   ["UNAVAILABLE", "UNKNOWN"]
+                  }
+
+                  default_config.rpcs.create_dashboard.timeout = 30.0
+
+                  default_config.rpcs.delete_dashboard.timeout = 30.0
+
+                  default_config.rpcs.update_dashboard.timeout = 30.0
+
                   default_config
                 end
                 yield @configure if block_given?
