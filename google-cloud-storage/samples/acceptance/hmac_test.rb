0e3cc44cc9f803bcf4a6c00d1349aa481200e503
Chris Smith <quartzmo@gmail.com>
2020-06-24 17:31:12 -0600

    Convert hmac_test.rb to mocks

--- google-cloud-storage/samples/acceptance/hmac_test.rb
+++ google-cloud-storage/samples/acceptance/hmac_test.rb
@@ -15,25 +15,54 @@
 require_relative "helper"
 require_relative "../hmac.rb"
 
+# HMAC key tests are mocked due to the very limited quota of keys per project.
 describe "HMAC Snippets" do
-  let(:storage_client)        { Google::Cloud::Storage.new }
-  let(:project_id)            { storage_client.project }
-  let(:service_account_email) { "#{project_id}@appspot.gserviceaccount.com" }
+  let(:project) { "test" }
+  let(:credentials) { OpenStruct.new(client: OpenStruct.new(updater_proc: proc {})) }
+  let(:storage) { Google::Cloud::Storage::Project.new Google::Cloud::Storage::Service.new(project, credentials) }
 
-  let :hmac_key do
-    hmac_key = storage_client.create_hmac_key service_account_email
-    hmac_key
+  let(:access_id) { "my-access-id" }
+  let(:service_account_email) { "my_service_account@gs-project-accounts.iam.gserviceaccount.com" }
+  let :hmac_key_metadata_gapi do
+    Google::Apis::StorageV1::HmacKeyMetadata.new(
+      access_id:             access_id,
+      etag:                  "123456",
+      id:                    "test/#{access_id}",
+      project_id:            "test",
+      self_link:             "https://www.googleapis.com/storage/v1/b/bucket-name/keys/#{access_id}",
+      service_account_email: service_account_email,
+      state:                 "ACTIVE",
+      time_created:          DateTime.now,
+      updated:               DateTime.now
+    )
+  end
+  let :hmac_keys_metadata_gapi do
+    Google::Apis::StorageV1::HmacKeysMetadata.new(
+      items: [hmac_key_metadata_gapi, hmac_key_metadata_gapi, hmac_key_metadata_gapi]
+    )
+  end
+  let :hmac_key_gapi do
+    Google::Apis::StorageV1::HmacKey.new(
+      secret:   "0123456789012345678901234567890123456789",
+      metadata: hmac_key_metadata_gapi
+    )
   end
 
-  after do
-    delete_hmac_key_helper hmac_key
-  end
+  it "list_hmac_keys" do
+    mock = Minitest::Mock.new
+    mock.expect :list_project_hmac_keys,
+                hmac_keys_metadata_gapi,
+                [
+                  project,
+                  max_results:           nil,
+                  page_token:            nil,
+                  service_account_email: nil,
+                  show_deleted_keys:     nil,
+                  user_project:          nil
+                ]
+    storage.service.mocked_service = mock
 
-  describe "list_hmac_keys" do
-    it "lists the hmac keys for a GCP project" do
-      skip "hmac"
-      access_id = hmac_key.access_id
-
+    Google::Cloud::Storage.stub :new, storage do
       out, _err = capture_io do
         list_hmac_keys
       end
@@ -41,76 +70,92 @@ describe "HMAC Snippets" do
       assert_includes out, "Service Account Email: #{service_account_email}"
       assert_includes out, "Access ID: #{access_id}"
     end
+    mock.verify
   end
 
-  describe "create_hmac_key" do
-    it "creates an hmac key" do
-      skip "hmac"
+  it "create_hmac_key" do
+    mock = Minitest::Mock.new
+    mock.expect :create_project_hmac_key, hmac_key_gapi, ["test", service_account_email, user_project: nil]
+    storage.service.mocked_service = mock
+
+    Google::Cloud::Storage.stub :new, storage do
       out, _err = capture_io do
         create_hmac_key service_account_email: service_account_email
       end
-      match = out.match(/Access ID:\s+(.*)\n/)
-      access_id = match[1]
-      # Raises error if no hmac_key found
-      delete_hmac_key_helper storage_client.hmac_key(access_id)
 
+      assert_match(/Access ID:\s+#{access_id}/, out)
       assert_match(/Service Account Email:\s+#{service_account_email}/, out)
     end
+    mock.verify
   end
 
-  describe "get_hmac_key" do
-    it "gets an hmac key from an access_id" do
-      skip "hmac"
-      access_id = hmac_key.access_id
+  it "get_hmac_key" do
+    mock = Minitest::Mock.new
+    mock.expect :get_project_hmac_key, hmac_key_metadata_gapi, [project, access_id, user_project: nil]
+    storage.service.mocked_service = mock
 
+    Google::Cloud::Storage.stub :new, storage do
       out, _err = capture_io do
         get_hmac_key access_id: access_id
       end
 
       assert_match(/Service Account Email:\s+#{service_account_email}/, out)
     end
+    mock.verify
   end
 
-  describe "activate_hmac_key" do
-    it "activates an hmac key from an access_id" do
-      skip "hmac"
-      hmac_key.inactive!
+  it "activate_hmac_key" do
+    mock = Minitest::Mock.new
+    inactive_gapi = hmac_key_metadata_gapi.dup
+    inactive_gapi.state = "INACTIVE"
+    mock.expect :get_project_hmac_key, inactive_gapi, [project, access_id, user_project: nil]
+    # Expect any HmacKeyMetadata rather than `hmac_key_metadata_gapi` due to mock matching error.
+    update_args = [project, access_id, Google::Apis::StorageV1::HmacKeyMetadata, { user_project: nil }]
+    mock.expect :update_project_hmac_key, hmac_key_metadata_gapi, update_args
+    storage.service.mocked_service = mock
 
+    Google::Cloud::Storage.stub :new, storage do
       out, _err = capture_io do
-        activate_hmac_key access_id: hmac_key.access_id
+        activate_hmac_key access_id: access_id
       end
 
       assert_match(/Service Account Email:\s+#{service_account_email}/, out)
-      hmac_key.refresh!
-      assert hmac_key.active?
     end
+    mock.verify
   end
 
-  describe "deactivate_hmac_key" do
-    it "deactivates an hmac key from an access_id" do
-      skip "hmac"
+  it "deactivate_hmac_key" do
+    mock = Minitest::Mock.new
+    mock.expect :get_project_hmac_key, hmac_key_metadata_gapi, [project, access_id, user_project: nil]
+    inactive_gapi = hmac_key_metadata_gapi.dup
+    inactive_gapi.state = "INACTIVE"
+    # Expect any HmacKeyMetadata rather than `inactive_gapi` due to mock matching error.
+    update_args = [project, access_id, Google::Apis::StorageV1::HmacKeyMetadata, { user_project: nil }]
+    mock.expect :update_project_hmac_key, inactive_gapi, update_args
+    storage.service.mocked_service = mock
+
+    Google::Cloud::Storage.stub :new, storage do
       out, _err = capture_io do
-        deactivate_hmac_key access_id: hmac_key.access_id
+        deactivate_hmac_key access_id: access_id
       end
 
       assert_match(/Service Account Email:\s+#{service_account_email}/, out)
-      hmac_key.refresh!
-      assert hmac_key.inactive?
     end
+    mock.verify
   end
 
-  describe "delete_hmac_key" do
-    it "deletes an hmac key from an access_id" do
-      skip "hmac"
-      hmac_key.inactive!
-      access_id = hmac_key.access_id
+  it "delete_hmac_key" do
+    mock = Minitest::Mock.new
+    mock.expect :get_project_hmac_key, hmac_key_metadata_gapi, [project, access_id, user_project: nil]
+    mock.expect :delete_project_hmac_key, hmac_key_metadata_gapi, [project, access_id, { user_project: nil }]
+    mock.expect :get_project_hmac_key, hmac_key_metadata_gapi, [project, access_id, { user_project: nil }]
+    storage.service.mocked_service = mock
 
+    Google::Cloud::Storage.stub :new, storage do
       assert_output "The key is deleted, though it may still appear in Client#hmac_keys results.\n" do
         delete_hmac_key access_id: access_id
       end
-
-      hmac_key.refresh!
-      assert hmac_key.deleted?
     end
+    mock.verify
   end
 end
