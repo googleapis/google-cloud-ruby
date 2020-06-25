f9e55803ae5d4a9e5f5d329488b1f3501abc47d5
Chris Smith <quartzmo@gmail.com>
2020-06-25 09:11:07 -0600

    Consolidate more isolated tests in buckets_test.rb

--- google-cloud-storage/samples/acceptance/buckets_test.rb
+++ google-cloud-storage/samples/acceptance/buckets_test.rb
@@ -26,17 +26,12 @@ describe "Buckets Snippets" do
     create_bucket_helper "ruby_storage_sample_#{SecureRandom.hex}"
   end
 
-  let :secondary_bucket do
-    create_bucket_helper "ruby_storage_sample_#{SecureRandom.hex}_secondary"
-  end
-
   after do
     delete_bucket_helper bucket.name
-    delete_bucket_helper secondary_bucket.name
   end
 
   describe "bucket lifecycle" do
-    it "create_bucket, list_bucket_details, delete_bucket" do
+    it "create_bucket, create_bucket_class_location, list_buckets, list_bucket_details, delete_bucket" do
       # create_bucket
       bucket_name = "ruby_storage_sample_#{SecureRandom.hex}"
       refute storage_client.bucket bucket_name
@@ -49,55 +44,49 @@ describe "Buckets Snippets" do
 
       refute_nil storage_client.bucket bucket_name
 
-      # list_bucket_details
-      out, _err = capture_io do
-        list_bucket_details bucket_name: bucket.name
-      end
+      # create_bucket_class_location
 
-      assert_includes out, bucket.name
-
-      # delete_bucket
-      assert_output "Deleted bucket: #{bucket.name}\n" do
-        delete_bucket bucket_name: bucket.name
-      end
-
-      refute storage_client.bucket bucket.name
-    end
-  end
-
-  describe "create_bucket_class_location" do
-    it "creates a storage bucket with a given class and location" do
-      bucket_name = "ruby_storage_sample_#{SecureRandom.hex}"
+      secondary_bucket_name = "ruby_storage_sample_#{SecureRandom.hex}"
       location = "US"
       storage_class = "STANDARD"
-      refute storage_client.bucket bucket_name
+      refute storage_client.bucket secondary_bucket_name
 
       retry_resource_exhaustion do
-        assert_output "Created bucket #{bucket_name} in #{location} with #{storage_class} class\n" do
-          create_bucket_class_location bucket_name:   bucket_name,
+        assert_output "Created bucket #{secondary_bucket_name} in #{location} with #{storage_class} class\n" do
+          create_bucket_class_location bucket_name:   secondary_bucket_name,
                                        location:      location,
                                        storage_class: storage_class
         end
       end
 
-      refute_nil storage_client.bucket bucket_name
+      refute_nil storage_client.bucket secondary_bucket_name
       assert_equal storage_client.bucket(bucket_name).location, location
       assert_equal storage_client.bucket(bucket_name).storage_class, storage_class
-      delete_bucket_helper bucket_name
-    end
-  end
 
-  describe "list_buckets" do
-    it "puts the buckets for a GCP project" do
-      bucket
-      secondary_bucket
-
+      # list_buckets
       out, _err = capture_io do
         list_buckets
       end
 
-      assert_includes out, bucket.name
-      assert_includes out, secondary_bucket.name
+      assert_includes out, bucket_name
+      assert_includes out, secondary_bucket_name
+
+      # list_bucket_details
+      out, _err = capture_io do
+        list_bucket_details bucket_name: bucket_name
+      end
+
+      assert_includes out, bucket_name
+
+      # delete_bucket
+      assert_output "Deleted bucket: #{bucket_name}\n" do
+        delete_bucket bucket_name: bucket_name
+      end
+
+      refute storage_client.bucket bucket_name
+
+
+      delete_bucket_helper bucket_name
     end
   end
 
@@ -228,8 +217,6 @@ describe "Buckets Snippets" do
       assert_equal bucket.retention_period, retention_period
 
       # get_retention_policy
-      bucket.retention_period = retention_period
-
       out, _err = capture_io do
         get_retention_policy bucket_name: bucket.name
       end
@@ -237,10 +224,7 @@ describe "Buckets Snippets" do
       assert_includes out, "period: #{retention_period}\n"
 
       # remove_retention_policy
-      bucket.retention_period = retention_period
-      secondary_bucket.retention_period = retention_period
-      secondary_bucket.lock_retention_policy!
-
+      assert_equal bucket.retention_period, retention_period
       assert_output "Retention policy for #{bucket.name} has been removed.\n" do
         remove_retention_policy bucket_name: bucket.name
       end
@@ -248,14 +232,6 @@ describe "Buckets Snippets" do
       bucket.refresh!
       refute bucket.retention_period
 
-      assert_output "Policy is locked and retention policy can't be removed.\n" do
-        remove_retention_policy bucket_name: secondary_bucket.name
-      end
-    end
-  end
-
-  describe "lock_retention_policy" do
-    it "locks the retention policy for a given bucket" do
       # lock_retention_policy
       bucket.retention_period = retention_period
       out, _err = capture_io do
@@ -265,6 +241,11 @@ describe "Buckets Snippets" do
       assert_includes out, "Retention policy for #{bucket.name} is now locked."
       bucket.refresh!
       assert bucket.retention_policy_locked?
+
+      # remove_retention_policy
+      assert_output "Policy is locked and retention policy can't be removed.\n" do
+        remove_retention_policy bucket_name: bucket.name
+      end
     end
   end
 
