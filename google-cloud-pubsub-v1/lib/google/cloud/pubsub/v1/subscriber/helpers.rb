d6b6685b6936213cedf94f7e6408650023c9218a
Chris Smith <quartzmo@gmail.com>
2020-07-20 15:18:21 -0600

    Add publisher/helpers.rb and subscriber/helpers.rb
    * Add grpc.max_send_message_length to Config#channel_args
    * Add grpc.max_receive_message_length to Config#channel_args
    * Add grpc.keepalive_time_ms to Config#channel_args
    
    refs: #1513
    refs: #7050

---
+++ google-cloud-pubsub-v1/lib/google/cloud/pubsub/v1/subscriber/helpers.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+module Google
+  module Cloud
+    module PubSub
+      module V1
+        module Subscriber
+          ##
+          # Additional configuration for the client.
+          # The max_send_message_length and max_receive_message_length settings were originally added in
+          # https://github.com/googleapis/google-cloud-ruby/pull/1513.
+          #
+          class Client
+            configure do |config|
+              config.channel_args ||= {}
+              config.channel_args["grpc.max_send_message_length"] = -1
+              config.channel_args["grpc.max_receive_message_length"] = -1
+              config.channel_args["grpc.keepalive_time_ms"] = 300_000
+            end
+          end
+        end
+      end
+    end
+  end
+end
