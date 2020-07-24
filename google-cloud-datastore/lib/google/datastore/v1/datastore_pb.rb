d861c54b6846996951ef1d54978063317be59c90
yoshi-automation <yoshi-automation@google.com>
2020-07-24 03:43:40 -0700

    changes without context
            autosynth cannot find the source of changes triggered by earlier changes in this
            repository, or by version upgrades to tools such as linters.

--- google-cloud-datastore/lib/google/datastore/v1/datastore_pb.rb
+++ google-cloud-datastore/lib/google/datastore/v1/datastore_pb.rb
@@ -10,138 +10,142 @@ require 'google/api/field_behavior_pb'
 require 'google/datastore/v1/entity_pb'
 require 'google/datastore/v1/query_pb'
 Google::Protobuf::DescriptorPool.generated_pool.build do
-  add_message "google.datastore.v1.LookupRequest" do
-    optional :project_id, :string, 8
-    optional :read_options, :message, 1, "google.datastore.v1.ReadOptions"
-    repeated :keys, :message, 3, "google.datastore.v1.Key"
-  end
-  add_message "google.datastore.v1.LookupResponse" do
-    repeated :found, :message, 1, "google.datastore.v1.EntityResult"
-    repeated :missing, :message, 2, "google.datastore.v1.EntityResult"
-    repeated :deferred, :message, 3, "google.datastore.v1.Key"
-  end
-  add_message "google.datastore.v1.RunQueryRequest" do
-    optional :project_id, :string, 8
-    optional :partition_id, :message, 2, "google.datastore.v1.PartitionId"
-    optional :read_options, :message, 1, "google.datastore.v1.ReadOptions"
-    oneof :query_type do
-      optional :query, :message, 3, "google.datastore.v1.Query"
-      optional :gql_query, :message, 7, "google.datastore.v1.GqlQuery"
+  add_file("google/datastore/v1/datastore.proto", :syntax => :proto3) do
+    add_message "google.datastore.v1.LookupRequest" do
+      optional :project_id, :string, 8
+      optional :read_options, :message, 1, "google.datastore.v1.ReadOptions"
+      repeated :keys, :message, 3, "google.datastore.v1.Key"
     end
-  end
-  add_message "google.datastore.v1.RunQueryResponse" do
-    optional :batch, :message, 1, "google.datastore.v1.QueryResultBatch"
-    optional :query, :message, 2, "google.datastore.v1.Query"
-  end
-  add_message "google.datastore.v1.BeginTransactionRequest" do
-    optional :project_id, :string, 8
-    optional :transaction_options, :message, 10, "google.datastore.v1.TransactionOptions"
-  end
-  add_message "google.datastore.v1.BeginTransactionResponse" do
-    optional :transaction, :bytes, 1
-  end
-  add_message "google.datastore.v1.RollbackRequest" do
-    optional :project_id, :string, 8
-    optional :transaction, :bytes, 1
-  end
-  add_message "google.datastore.v1.RollbackResponse" do
-  end
-  add_message "google.datastore.v1.CommitRequest" do
-    optional :project_id, :string, 8
-    optional :mode, :enum, 5, "google.datastore.v1.CommitRequest.Mode"
-    repeated :mutations, :message, 6, "google.datastore.v1.Mutation"
-    oneof :transaction_selector do
+    add_message "google.datastore.v1.LookupResponse" do
+      repeated :found, :message, 1, "google.datastore.v1.EntityResult"
+      repeated :missing, :message, 2, "google.datastore.v1.EntityResult"
+      repeated :deferred, :message, 3, "google.datastore.v1.Key"
+    end
+    add_message "google.datastore.v1.RunQueryRequest" do
+      optional :project_id, :string, 8
+      optional :partition_id, :message, 2, "google.datastore.v1.PartitionId"
+      optional :read_options, :message, 1, "google.datastore.v1.ReadOptions"
+      oneof :query_type do
+        optional :query, :message, 3, "google.datastore.v1.Query"
+        optional :gql_query, :message, 7, "google.datastore.v1.GqlQuery"
+      end
+    end
+    add_message "google.datastore.v1.RunQueryResponse" do
+      optional :batch, :message, 1, "google.datastore.v1.QueryResultBatch"
+      optional :query, :message, 2, "google.datastore.v1.Query"
+    end
+    add_message "google.datastore.v1.BeginTransactionRequest" do
+      optional :project_id, :string, 8
+      optional :transaction_options, :message, 10, "google.datastore.v1.TransactionOptions"
+    end
+    add_message "google.datastore.v1.BeginTransactionResponse" do
       optional :transaction, :bytes, 1
     end
-  end
-  add_enum "google.datastore.v1.CommitRequest.Mode" do
-    value :MODE_UNSPECIFIED, 0
-    value :TRANSACTIONAL, 1
-    value :NON_TRANSACTIONAL, 2
-  end
-  add_message "google.datastore.v1.CommitResponse" do
-    repeated :mutation_results, :message, 3, "google.datastore.v1.MutationResult"
-    optional :index_updates, :int32, 4
-  end
-  add_message "google.datastore.v1.AllocateIdsRequest" do
-    optional :project_id, :string, 8
-    repeated :keys, :message, 1, "google.datastore.v1.Key"
-  end
-  add_message "google.datastore.v1.AllocateIdsResponse" do
-    repeated :keys, :message, 1, "google.datastore.v1.Key"
-  end
-  add_message "google.datastore.v1.ReserveIdsRequest" do
-    optional :project_id, :string, 8
-    optional :database_id, :string, 9
-    repeated :keys, :message, 1, "google.datastore.v1.Key"
-  end
-  add_message "google.datastore.v1.ReserveIdsResponse" do
-  end
-  add_message "google.datastore.v1.Mutation" do
-    oneof :operation do
-      optional :insert, :message, 4, "google.datastore.v1.Entity"
-      optional :update, :message, 5, "google.datastore.v1.Entity"
-      optional :upsert, :message, 6, "google.datastore.v1.Entity"
-      optional :delete, :message, 7, "google.datastore.v1.Key"
+    add_message "google.datastore.v1.RollbackRequest" do
+      optional :project_id, :string, 8
+      optional :transaction, :bytes, 1
     end
-    oneof :conflict_detection_strategy do
-      optional :base_version, :int64, 8
+    add_message "google.datastore.v1.RollbackResponse" do
     end
-  end
-  add_message "google.datastore.v1.MutationResult" do
-    optional :key, :message, 3, "google.datastore.v1.Key"
-    optional :version, :int64, 4
-    optional :conflict_detected, :bool, 5
-  end
-  add_message "google.datastore.v1.ReadOptions" do
-    oneof :consistency_type do
-      optional :read_consistency, :enum, 1, "google.datastore.v1.ReadOptions.ReadConsistency"
-      optional :transaction, :bytes, 2
+    add_message "google.datastore.v1.CommitRequest" do
+      optional :project_id, :string, 8
+      optional :mode, :enum, 5, "google.datastore.v1.CommitRequest.Mode"
+      repeated :mutations, :message, 6, "google.datastore.v1.Mutation"
+      oneof :transaction_selector do
+        optional :transaction, :bytes, 1
+      end
     end
-  end
-  add_enum "google.datastore.v1.ReadOptions.ReadConsistency" do
-    value :READ_CONSISTENCY_UNSPECIFIED, 0
-    value :STRONG, 1
-    value :EVENTUAL, 2
-  end
-  add_message "google.datastore.v1.TransactionOptions" do
-    oneof :mode do
-      optional :read_write, :message, 1, "google.datastore.v1.TransactionOptions.ReadWrite"
-      optional :read_only, :message, 2, "google.datastore.v1.TransactionOptions.ReadOnly"
+    add_enum "google.datastore.v1.CommitRequest.Mode" do
+      value :MODE_UNSPECIFIED, 0
+      value :TRANSACTIONAL, 1
+      value :NON_TRANSACTIONAL, 2
     end
-  end
-  add_message "google.datastore.v1.TransactionOptions.ReadWrite" do
-    optional :previous_transaction, :bytes, 1
-  end
-  add_message "google.datastore.v1.TransactionOptions.ReadOnly" do
+    add_message "google.datastore.v1.CommitResponse" do
+      repeated :mutation_results, :message, 3, "google.datastore.v1.MutationResult"
+      optional :index_updates, :int32, 4
+    end
+    add_message "google.datastore.v1.AllocateIdsRequest" do
+      optional :project_id, :string, 8
+      repeated :keys, :message, 1, "google.datastore.v1.Key"
+    end
+    add_message "google.datastore.v1.AllocateIdsResponse" do
+      repeated :keys, :message, 1, "google.datastore.v1.Key"
+    end
+    add_message "google.datastore.v1.ReserveIdsRequest" do
+      optional :project_id, :string, 8
+      optional :database_id, :string, 9
+      repeated :keys, :message, 1, "google.datastore.v1.Key"
+    end
+    add_message "google.datastore.v1.ReserveIdsResponse" do
+    end
+    add_message "google.datastore.v1.Mutation" do
+      oneof :operation do
+        optional :insert, :message, 4, "google.datastore.v1.Entity"
+        optional :update, :message, 5, "google.datastore.v1.Entity"
+        optional :upsert, :message, 6, "google.datastore.v1.Entity"
+        optional :delete, :message, 7, "google.datastore.v1.Key"
+      end
+      oneof :conflict_detection_strategy do
+        optional :base_version, :int64, 8
+      end
+    end
+    add_message "google.datastore.v1.MutationResult" do
+      optional :key, :message, 3, "google.datastore.v1.Key"
+      optional :version, :int64, 4
+      optional :conflict_detected, :bool, 5
+    end
+    add_message "google.datastore.v1.ReadOptions" do
+      oneof :consistency_type do
+        optional :read_consistency, :enum, 1, "google.datastore.v1.ReadOptions.ReadConsistency"
+        optional :transaction, :bytes, 2
+      end
+    end
+    add_enum "google.datastore.v1.ReadOptions.ReadConsistency" do
+      value :READ_CONSISTENCY_UNSPECIFIED, 0
+      value :STRONG, 1
+      value :EVENTUAL, 2
+    end
+    add_message "google.datastore.v1.TransactionOptions" do
+      oneof :mode do
+        optional :read_write, :message, 1, "google.datastore.v1.TransactionOptions.ReadWrite"
+        optional :read_only, :message, 2, "google.datastore.v1.TransactionOptions.ReadOnly"
+      end
+    end
+    add_message "google.datastore.v1.TransactionOptions.ReadWrite" do
+      optional :previous_transaction, :bytes, 1
+    end
+    add_message "google.datastore.v1.TransactionOptions.ReadOnly" do
+    end
   end
 end
 
 module Google
-  module Datastore
+  module Cloud
+    module Datastore
+      module V1
+        LookupRequest = ::Google::Protobuf::DescriptorPool.generated_pool.lookup("google.datastore.v1.LookupRequest").msgclass
+        LookupResponse = ::Google::Protobuf::DescriptorPool.generated_pool.lookup("google.datastore.v1.LookupResponse").msgclass
+        RunQueryRequest = ::Google::Protobuf::DescriptorPool.generated_pool.lookup("google.datastore.v1.RunQueryRequest").msgclass
+        RunQueryResponse = ::Google::Protobuf::DescriptorPool.generated_pool.lookup("google.datastore.v1.RunQueryResponse").msgclass
+        BeginTransactionRequest = ::Google::Protobuf::DescriptorPool.generated_pool.lookup("google.datastore.v1.BeginTransactionRequest").msgclass
+        BeginTransactionResponse = ::Google::Protobuf::DescriptorPool.generated_pool.lookup("google.datastore.v1.BeginTransactionResponse").msgclass
+        RollbackRequest = ::Google::Protobuf::DescriptorPool.generated_pool.lookup("google.datastore.v1.RollbackRequest").msgclass
+        RollbackResponse = ::Google::Protobuf::DescriptorPool.generated_pool.lookup("google.datastore.v1.RollbackResponse").msgclass
+        CommitRequest = ::Google::Protobuf::DescriptorPool.generated_pool.lookup("google.datastore.v1.CommitRequest").msgclass
+        CommitRequest::Mode = ::Google::Protobuf::DescriptorPool.generated_pool.lookup("google.datastore.v1.CommitRequest.Mode").enummodule
+        CommitResponse = ::Google::Protobuf::DescriptorPool.generated_pool.lookup("google.datastore.v1.CommitResponse").msgclass
+        AllocateIdsRequest = ::Google::Protobuf::DescriptorPool.generated_pool.lookup("google.datastore.v1.AllocateIdsRequest").msgclass
+        AllocateIdsResponse = ::Google::Protobuf::DescriptorPool.generated_pool.lookup("google.datastore.v1.AllocateIdsResponse").msgclass
+        ReserveIdsRequest = ::Google::Protobuf::DescriptorPool.generated_pool.lookup("google.datastore.v1.ReserveIdsRequest").msgclass
+        ReserveIdsResponse = ::Google::Protobuf::DescriptorPool.generated_pool.lookup("google.datastore.v1.ReserveIdsResponse").msgclass
+        Mutation = ::Google::Protobuf::DescriptorPool.generated_pool.lookup("google.datastore.v1.Mutation").msgclass
+        MutationResult = ::Google::Protobuf::DescriptorPool.generated_pool.lookup("google.datastore.v1.MutationResult").msgclass
+        ReadOptions = ::Google::Protobuf::DescriptorPool.generated_pool.lookup("google.datastore.v1.ReadOptions").msgclass
+        ReadOptions::ReadConsistency = ::Google::Protobuf::DescriptorPool.generated_pool.lookup("google.datastore.v1.ReadOptions.ReadConsistency").enummodule
+        TransactionOptions = ::Google::Protobuf::DescriptorPool.generated_pool.lookup("google.datastore.v1.TransactionOptions").msgclass
+        TransactionOptions::ReadWrite = ::Google::Protobuf::DescriptorPool.generated_pool.lookup("google.datastore.v1.TransactionOptions.ReadWrite").msgclass
+        TransactionOptions::ReadOnly = ::Google::Protobuf::DescriptorPool.generated_pool.lookup("google.datastore.v1.TransactionOptions.ReadOnly").msgclass
+      end
+    end
   end
-end
-module Google::Datastore::V1
-  LookupRequest = Google::Protobuf::DescriptorPool.generated_pool.lookup("google.datastore.v1.LookupRequest").msgclass
-  LookupResponse = Google::Protobuf::DescriptorPool.generated_pool.lookup("google.datastore.v1.LookupResponse").msgclass
-  RunQueryRequest = Google::Protobuf::DescriptorPool.generated_pool.lookup("google.datastore.v1.RunQueryRequest").msgclass
-  RunQueryResponse = Google::Protobuf::DescriptorPool.generated_pool.lookup("google.datastore.v1.RunQueryResponse").msgclass
-  BeginTransactionRequest = Google::Protobuf::DescriptorPool.generated_pool.lookup("google.datastore.v1.BeginTransactionRequest").msgclass
-  BeginTransactionResponse = Google::Protobuf::DescriptorPool.generated_pool.lookup("google.datastore.v1.BeginTransactionResponse").msgclass
-  RollbackRequest = Google::Protobuf::DescriptorPool.generated_pool.lookup("google.datastore.v1.RollbackRequest").msgclass
-  RollbackResponse = Google::Protobuf::DescriptorPool.generated_pool.lookup("google.datastore.v1.RollbackResponse").msgclass
-  CommitRequest = Google::Protobuf::DescriptorPool.generated_pool.lookup("google.datastore.v1.CommitRequest").msgclass
-  CommitRequest::Mode = Google::Protobuf::DescriptorPool.generated_pool.lookup("google.datastore.v1.CommitRequest.Mode").enummodule
-  CommitResponse = Google::Protobuf::DescriptorPool.generated_pool.lookup("google.datastore.v1.CommitResponse").msgclass
-  AllocateIdsRequest = Google::Protobuf::DescriptorPool.generated_pool.lookup("google.datastore.v1.AllocateIdsRequest").msgclass
-  AllocateIdsResponse = Google::Protobuf::DescriptorPool.generated_pool.lookup("google.datastore.v1.AllocateIdsResponse").msgclass
-  ReserveIdsRequest = Google::Protobuf::DescriptorPool.generated_pool.lookup("google.datastore.v1.ReserveIdsRequest").msgclass
-  ReserveIdsResponse = Google::Protobuf::DescriptorPool.generated_pool.lookup("google.datastore.v1.ReserveIdsResponse").msgclass
-  Mutation = Google::Protobuf::DescriptorPool.generated_pool.lookup("google.datastore.v1.Mutation").msgclass
-  MutationResult = Google::Protobuf::DescriptorPool.generated_pool.lookup("google.datastore.v1.MutationResult").msgclass
-  ReadOptions = Google::Protobuf::DescriptorPool.generated_pool.lookup("google.datastore.v1.ReadOptions").msgclass
-  ReadOptions::ReadConsistency = Google::Protobuf::DescriptorPool.generated_pool.lookup("google.datastore.v1.ReadOptions.ReadConsistency").enummodule
-  TransactionOptions = Google::Protobuf::DescriptorPool.generated_pool.lookup("google.datastore.v1.TransactionOptions").msgclass
-  TransactionOptions::ReadWrite = Google::Protobuf::DescriptorPool.generated_pool.lookup("google.datastore.v1.TransactionOptions.ReadWrite").msgclass
-  TransactionOptions::ReadOnly = Google::Protobuf::DescriptorPool.generated_pool.lookup("google.datastore.v1.TransactionOptions.ReadOnly").msgclass
 end
--- google-cloud-datastore/lib/google/datastore/v1/entity_pb.rb
+++ google-cloud-datastore/lib/google/datastore/v1/entity_pb.rb
@@ -9,56 +9,60 @@ require 'google/protobuf/struct_pb'
 require 'google/protobuf/timestamp_pb'
 require 'google/type/latlng_pb'
 Google::Protobuf::DescriptorPool.generated_pool.build do
-  add_message "google.datastore.v1.PartitionId" do
-    optional :project_id, :string, 2
-    optional :namespace_id, :string, 4
-  end
-  add_message "google.datastore.v1.Key" do
-    optional :partition_id, :message, 1, "google.datastore.v1.PartitionId"
-    repeated :path, :message, 2, "google.datastore.v1.Key.PathElement"
-  end
-  add_message "google.datastore.v1.Key.PathElement" do
-    optional :kind, :string, 1
-    oneof :id_type do
-      optional :id, :int64, 2
-      optional :name, :string, 3
+  add_file("google/datastore/v1/entity.proto", :syntax => :proto3) do
+    add_message "google.datastore.v1.PartitionId" do
+      optional :project_id, :string, 2
+      optional :namespace_id, :string, 4
     end
-  end
-  add_message "google.datastore.v1.ArrayValue" do
-    repeated :values, :message, 1, "google.datastore.v1.Value"
-  end
-  add_message "google.datastore.v1.Value" do
-    optional :meaning, :int32, 14
-    optional :exclude_from_indexes, :bool, 19
-    oneof :value_type do
-      optional :null_value, :enum, 11, "google.protobuf.NullValue"
-      optional :boolean_value, :bool, 1
-      optional :integer_value, :int64, 2
-      optional :double_value, :double, 3
-      optional :timestamp_value, :message, 10, "google.protobuf.Timestamp"
-      optional :key_value, :message, 5, "google.datastore.v1.Key"
-      optional :string_value, :string, 17
-      optional :blob_value, :bytes, 18
-      optional :geo_point_value, :message, 8, "google.type.LatLng"
-      optional :entity_value, :message, 6, "google.datastore.v1.Entity"
-      optional :array_value, :message, 9, "google.datastore.v1.ArrayValue"
+    add_message "google.datastore.v1.Key" do
+      optional :partition_id, :message, 1, "google.datastore.v1.PartitionId"
+      repeated :path, :message, 2, "google.datastore.v1.Key.PathElement"
     end
-  end
-  add_message "google.datastore.v1.Entity" do
-    optional :key, :message, 1, "google.datastore.v1.Key"
-    map :properties, :string, :message, 3, "google.datastore.v1.Value"
+    add_message "google.datastore.v1.Key.PathElement" do
+      optional :kind, :string, 1
+      oneof :id_type do
+        optional :id, :int64, 2
+        optional :name, :string, 3
+      end
+    end
+    add_message "google.datastore.v1.ArrayValue" do
+      repeated :values, :message, 1, "google.datastore.v1.Value"
+    end
+    add_message "google.datastore.v1.Value" do
+      optional :meaning, :int32, 14
+      optional :exclude_from_indexes, :bool, 19
+      oneof :value_type do
+        optional :null_value, :enum, 11, "google.protobuf.NullValue"
+        optional :boolean_value, :bool, 1
+        optional :integer_value, :int64, 2
+        optional :double_value, :double, 3
+        optional :timestamp_value, :message, 10, "google.protobuf.Timestamp"
+        optional :key_value, :message, 5, "google.datastore.v1.Key"
+        optional :string_value, :string, 17
+        optional :blob_value, :bytes, 18
+        optional :geo_point_value, :message, 8, "google.type.LatLng"
+        optional :entity_value, :message, 6, "google.datastore.v1.Entity"
+        optional :array_value, :message, 9, "google.datastore.v1.ArrayValue"
+      end
+    end
+    add_message "google.datastore.v1.Entity" do
+      optional :key, :message, 1, "google.datastore.v1.Key"
+      map :properties, :string, :message, 3, "google.datastore.v1.Value"
+    end
   end
 end
 
 module Google
-  module Datastore
+  module Cloud
+    module Datastore
+      module V1
+        PartitionId = ::Google::Protobuf::DescriptorPool.generated_pool.lookup("google.datastore.v1.PartitionId").msgclass
+        Key = ::Google::Protobuf::DescriptorPool.generated_pool.lookup("google.datastore.v1.Key").msgclass
+        Key::PathElement = ::Google::Protobuf::DescriptorPool.generated_pool.lookup("google.datastore.v1.Key.PathElement").msgclass
+        ArrayValue = ::Google::Protobuf::DescriptorPool.generated_pool.lookup("google.datastore.v1.ArrayValue").msgclass
+        Value = ::Google::Protobuf::DescriptorPool.generated_pool.lookup("google.datastore.v1.Value").msgclass
+        Entity = ::Google::Protobuf::DescriptorPool.generated_pool.lookup("google.datastore.v1.Entity").msgclass
+      end
+    end
   end
-end
-module Google::Datastore::V1
-  PartitionId = Google::Protobuf::DescriptorPool.generated_pool.lookup("google.datastore.v1.PartitionId").msgclass
-  Key = Google::Protobuf::DescriptorPool.generated_pool.lookup("google.datastore.v1.Key").msgclass
-  Key::PathElement = Google::Protobuf::DescriptorPool.generated_pool.lookup("google.datastore.v1.Key.PathElement").msgclass
-  ArrayValue = Google::Protobuf::DescriptorPool.generated_pool.lookup("google.datastore.v1.ArrayValue").msgclass
-  Value = Google::Protobuf::DescriptorPool.generated_pool.lookup("google.datastore.v1.Value").msgclass
-  Entity = Google::Protobuf::DescriptorPool.generated_pool.lookup("google.datastore.v1.Entity").msgclass
 end
