2dbc8ad63d1f6347c570f684b5bfd752bf4b8255
Chris Smith <quartzmo@gmail.com>
2020-08-05 12:29:40 -0600

    chore: Initial microgen of Datastore Admin V1

---
+++ google-cloud-datastore-admin-v1/synth.py
@@ -0,0 +1,44 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""This script is used to synthesize generated parts of this library."""
+
+import synthtool as s
+import synthtool.gcp as gcp
+import synthtool.languages.ruby as ruby
+import logging
+
+
+logging.basicConfig(level=logging.DEBUG)
+
+gapic = gcp.GAPICMicrogenerator()
+library = gapic.ruby_library(
+    "datastore/admin", "v1",
+    proto_path="google/datastore/admin/v1",
+    extra_proto_files=[
+        "google/cloud/common_resources.proto",
+    ],
+    generator_args={
+        "ruby-cloud-gem-name": "google-cloud-datastore-admin-v1",
+        "ruby-cloud-title": "Firestore in Datastore mode Admin V1",
+        "ruby-cloud-description": "Firestore in Datastore mode is a NoSQL document database built for automatic scaling, high performance, and ease of application development.",
+        "ruby-cloud-env-prefix": "DATASTORE",
+        "ruby-cloud-grpc-service-config": "google/datastore/admin/v1/datastore_admin_grpc_service_config.json",
+        "ruby-cloud-product-url": "https://cloud.google.com/datastore",
+        "ruby-cloud-api-id": "datastore.googleapis.com",
+        "ruby-cloud-api-shortname": "datastore",
+    }
+)
+
+s.copy(library, merge=ruby.global_merge)
