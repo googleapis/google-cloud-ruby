844b4c0586403ab6f8c87a6730b3e600bc04a8eb
yoshi-automation <yoshi-automation@google.com>
2020-08-05 11:54:25 -0700

    changes without context
            autosynth cannot find the source of changes triggered by earlier changes in this
            repository, or by version upgrades to tools such as linters.

--- google-cloud-web_risk-v1beta1/lib/google/cloud/web_risk/v1beta1/web_risk_service/client.rb
+++ google-cloud-web_risk-v1beta1/lib/google/cloud/web_risk/v1beta1/web_risk_service/client.rb
@@ -69,7 +69,7 @@ module Google
                   initial_delay: 0.1,
                   max_delay:     60.0,
                   multiplier:    1.3,
-                  retry_codes:   ["DEADLINE_EXCEEDED", "UNAVAILABLE"]
+                  retry_codes:   [4, 14]
                 }
 
                 default_config.rpcs.search_uris.timeout = 600.0
@@ -77,7 +77,7 @@ module Google
                   initial_delay: 0.1,
                   max_delay:     60.0,
                   multiplier:    1.3,
-                  retry_codes:   ["DEADLINE_EXCEEDED", "UNAVAILABLE"]
+                  retry_codes:   [4, 14]
                 }
 
                 default_config.rpcs.search_hashes.timeout = 600.0
@@ -85,7 +85,7 @@ module Google
                   initial_delay: 0.1,
                   max_delay:     60.0,
                   multiplier:    1.3,
-                  retry_codes:   ["DEADLINE_EXCEEDED", "UNAVAILABLE"]
+                  retry_codes:   [4, 14]
                 }
 
                 default_config
