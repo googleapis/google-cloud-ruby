d8e1e3f3d683c3286d2a7e9f04102a3d58c473a9
yoshi-automation <yoshi-automation@google.com>
2020-09-17 04:01:11 -0700

    changes without context
            autosynth cannot find the source of changes triggered by earlier changes in this
            repository, or by version upgrades to tools such as linters.

--- google-cloud-firestore-v1/lib/google/firestore/v1/firestore_services_pb.rb
+++ google-cloud-firestore-v1/lib/google/firestore/v1/firestore_services_pb.rb
@@ -43,36 +43,36 @@ module Google
             self.service_name = 'google.firestore.v1.Firestore'
 
             # Gets a single document.
-            rpc :GetDocument, Google::Cloud::Firestore::V1::GetDocumentRequest, Google::Cloud::Firestore::V1::Document
+            rpc :GetDocument, ::Google::Cloud::Firestore::V1::GetDocumentRequest, ::Google::Cloud::Firestore::V1::Document
             # Lists documents.
-            rpc :ListDocuments, Google::Cloud::Firestore::V1::ListDocumentsRequest, Google::Cloud::Firestore::V1::ListDocumentsResponse
+            rpc :ListDocuments, ::Google::Cloud::Firestore::V1::ListDocumentsRequest, ::Google::Cloud::Firestore::V1::ListDocumentsResponse
             # Updates or inserts a document.
-            rpc :UpdateDocument, Google::Cloud::Firestore::V1::UpdateDocumentRequest, Google::Cloud::Firestore::V1::Document
+            rpc :UpdateDocument, ::Google::Cloud::Firestore::V1::UpdateDocumentRequest, ::Google::Cloud::Firestore::V1::Document
             # Deletes a document.
-            rpc :DeleteDocument, Google::Cloud::Firestore::V1::DeleteDocumentRequest, Google::Protobuf::Empty
+            rpc :DeleteDocument, ::Google::Cloud::Firestore::V1::DeleteDocumentRequest, ::Google::Protobuf::Empty
             # Gets multiple documents.
             #
             # Documents returned by this method are not guaranteed to be returned in the
             # same order that they were requested.
-            rpc :BatchGetDocuments, Google::Cloud::Firestore::V1::BatchGetDocumentsRequest, stream(Google::Cloud::Firestore::V1::BatchGetDocumentsResponse)
+            rpc :BatchGetDocuments, ::Google::Cloud::Firestore::V1::BatchGetDocumentsRequest, stream(::Google::Cloud::Firestore::V1::BatchGetDocumentsResponse)
             # Starts a new transaction.
-            rpc :BeginTransaction, Google::Cloud::Firestore::V1::BeginTransactionRequest, Google::Cloud::Firestore::V1::BeginTransactionResponse
+            rpc :BeginTransaction, ::Google::Cloud::Firestore::V1::BeginTransactionRequest, ::Google::Cloud::Firestore::V1::BeginTransactionResponse
             # Commits a transaction, while optionally updating documents.
-            rpc :Commit, Google::Cloud::Firestore::V1::CommitRequest, Google::Cloud::Firestore::V1::CommitResponse
+            rpc :Commit, ::Google::Cloud::Firestore::V1::CommitRequest, ::Google::Cloud::Firestore::V1::CommitResponse
             # Rolls back a transaction.
-            rpc :Rollback, Google::Cloud::Firestore::V1::RollbackRequest, Google::Protobuf::Empty
+            rpc :Rollback, ::Google::Cloud::Firestore::V1::RollbackRequest, ::Google::Protobuf::Empty
             # Runs a query.
-            rpc :RunQuery, Google::Cloud::Firestore::V1::RunQueryRequest, stream(Google::Cloud::Firestore::V1::RunQueryResponse)
+            rpc :RunQuery, ::Google::Cloud::Firestore::V1::RunQueryRequest, stream(::Google::Cloud::Firestore::V1::RunQueryResponse)
             # Partitions a query by returning partition cursors that can be used to run
             # the query in parallel. The returned partition cursors are split points that
             # can be used by RunQuery as starting/end points for the query results.
-            rpc :PartitionQuery, Google::Cloud::Firestore::V1::PartitionQueryRequest, Google::Cloud::Firestore::V1::PartitionQueryResponse
+            rpc :PartitionQuery, ::Google::Cloud::Firestore::V1::PartitionQueryRequest, ::Google::Cloud::Firestore::V1::PartitionQueryResponse
             # Streams batches of document updates and deletes, in order.
-            rpc :Write, stream(Google::Cloud::Firestore::V1::WriteRequest), stream(Google::Cloud::Firestore::V1::WriteResponse)
+            rpc :Write, stream(::Google::Cloud::Firestore::V1::WriteRequest), stream(::Google::Cloud::Firestore::V1::WriteResponse)
             # Listens to changes.
-            rpc :Listen, stream(Google::Cloud::Firestore::V1::ListenRequest), stream(Google::Cloud::Firestore::V1::ListenResponse)
+            rpc :Listen, stream(::Google::Cloud::Firestore::V1::ListenRequest), stream(::Google::Cloud::Firestore::V1::ListenResponse)
             # Lists all the collection IDs underneath a document.
-            rpc :ListCollectionIds, Google::Cloud::Firestore::V1::ListCollectionIdsRequest, Google::Cloud::Firestore::V1::ListCollectionIdsResponse
+            rpc :ListCollectionIds, ::Google::Cloud::Firestore::V1::ListCollectionIdsRequest, ::Google::Cloud::Firestore::V1::ListCollectionIdsResponse
             # Applies a batch of write operations.
             #
             # The BatchWrite method does not apply the write operations atomically
@@ -82,9 +82,9 @@ module Google
             #
             # If you require an atomically applied set of writes, use
             # [Commit][google.firestore.v1.Firestore.Commit] instead.
-            rpc :BatchWrite, Google::Cloud::Firestore::V1::BatchWriteRequest, Google::Cloud::Firestore::V1::BatchWriteResponse
+            rpc :BatchWrite, ::Google::Cloud::Firestore::V1::BatchWriteRequest, ::Google::Cloud::Firestore::V1::BatchWriteResponse
             # Creates a new document.
-            rpc :CreateDocument, Google::Cloud::Firestore::V1::CreateDocumentRequest, Google::Cloud::Firestore::V1::Document
+            rpc :CreateDocument, ::Google::Cloud::Firestore::V1::CreateDocumentRequest, ::Google::Cloud::Firestore::V1::Document
           end
 
           Stub = Service.rpc_stub_class
