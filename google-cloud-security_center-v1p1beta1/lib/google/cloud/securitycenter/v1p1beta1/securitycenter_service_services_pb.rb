906b69d0d51a8f26a82f859f303ee3ac1ac06538
yoshi-automation <yoshi-automation@google.com>
2020-09-17 04:22:14 -0700

    changes without context
            autosynth cannot find the source of changes triggered by earlier changes in this
            repository, or by version upgrades to tools such as linters.

--- google-cloud-security_center-v1p1beta1/lib/google/cloud/securitycenter/v1p1beta1/securitycenter_service_services_pb.rb
+++ google-cloud-security_center-v1p1beta1/lib/google/cloud/securitycenter/v1p1beta1/securitycenter_service_services_pb.rb
@@ -34,67 +34,67 @@ module Google
             self.service_name = 'google.cloud.securitycenter.v1p1beta1.SecurityCenter'
 
             # Creates a source.
-            rpc :CreateSource, Google::Cloud::SecurityCenter::V1p1beta1::CreateSourceRequest, Google::Cloud::SecurityCenter::V1p1beta1::Source
+            rpc :CreateSource, ::Google::Cloud::SecurityCenter::V1p1beta1::CreateSourceRequest, ::Google::Cloud::SecurityCenter::V1p1beta1::Source
             # Creates a finding. The corresponding source must exist for finding
             # creation to succeed.
-            rpc :CreateFinding, Google::Cloud::SecurityCenter::V1p1beta1::CreateFindingRequest, Google::Cloud::SecurityCenter::V1p1beta1::Finding
+            rpc :CreateFinding, ::Google::Cloud::SecurityCenter::V1p1beta1::CreateFindingRequest, ::Google::Cloud::SecurityCenter::V1p1beta1::Finding
             # Creates a notification config.
-            rpc :CreateNotificationConfig, Google::Cloud::SecurityCenter::V1p1beta1::CreateNotificationConfigRequest, Google::Cloud::SecurityCenter::V1p1beta1::NotificationConfig
+            rpc :CreateNotificationConfig, ::Google::Cloud::SecurityCenter::V1p1beta1::CreateNotificationConfigRequest, ::Google::Cloud::SecurityCenter::V1p1beta1::NotificationConfig
             # Deletes a notification config.
-            rpc :DeleteNotificationConfig, Google::Cloud::SecurityCenter::V1p1beta1::DeleteNotificationConfigRequest, Google::Protobuf::Empty
+            rpc :DeleteNotificationConfig, ::Google::Cloud::SecurityCenter::V1p1beta1::DeleteNotificationConfigRequest, ::Google::Protobuf::Empty
             # Gets the access control policy on the specified Source.
-            rpc :GetIamPolicy, Google::Iam::V1::GetIamPolicyRequest, Google::Iam::V1::Policy
+            rpc :GetIamPolicy, ::Google::Iam::V1::GetIamPolicyRequest, ::Google::Iam::V1::Policy
             # Gets a notification config.
-            rpc :GetNotificationConfig, Google::Cloud::SecurityCenter::V1p1beta1::GetNotificationConfigRequest, Google::Cloud::SecurityCenter::V1p1beta1::NotificationConfig
+            rpc :GetNotificationConfig, ::Google::Cloud::SecurityCenter::V1p1beta1::GetNotificationConfigRequest, ::Google::Cloud::SecurityCenter::V1p1beta1::NotificationConfig
             # Gets the settings for an organization.
-            rpc :GetOrganizationSettings, Google::Cloud::SecurityCenter::V1p1beta1::GetOrganizationSettingsRequest, Google::Cloud::SecurityCenter::V1p1beta1::OrganizationSettings
+            rpc :GetOrganizationSettings, ::Google::Cloud::SecurityCenter::V1p1beta1::GetOrganizationSettingsRequest, ::Google::Cloud::SecurityCenter::V1p1beta1::OrganizationSettings
             # Gets a source.
-            rpc :GetSource, Google::Cloud::SecurityCenter::V1p1beta1::GetSourceRequest, Google::Cloud::SecurityCenter::V1p1beta1::Source
+            rpc :GetSource, ::Google::Cloud::SecurityCenter::V1p1beta1::GetSourceRequest, ::Google::Cloud::SecurityCenter::V1p1beta1::Source
             # Filters an organization's assets and  groups them by their specified
             # properties.
-            rpc :GroupAssets, Google::Cloud::SecurityCenter::V1p1beta1::GroupAssetsRequest, Google::Cloud::SecurityCenter::V1p1beta1::GroupAssetsResponse
+            rpc :GroupAssets, ::Google::Cloud::SecurityCenter::V1p1beta1::GroupAssetsRequest, ::Google::Cloud::SecurityCenter::V1p1beta1::GroupAssetsResponse
             # Filters an organization or source's findings and  groups them by their
             # specified properties.
             #
             # To group across all sources provide a `-` as the source id.
             # Example: /v1p1beta1/organizations/{organization_id}/sources/-/findings
-            rpc :GroupFindings, Google::Cloud::SecurityCenter::V1p1beta1::GroupFindingsRequest, Google::Cloud::SecurityCenter::V1p1beta1::GroupFindingsResponse
+            rpc :GroupFindings, ::Google::Cloud::SecurityCenter::V1p1beta1::GroupFindingsRequest, ::Google::Cloud::SecurityCenter::V1p1beta1::GroupFindingsResponse
             # Lists an organization's assets.
-            rpc :ListAssets, Google::Cloud::SecurityCenter::V1p1beta1::ListAssetsRequest, Google::Cloud::SecurityCenter::V1p1beta1::ListAssetsResponse
+            rpc :ListAssets, ::Google::Cloud::SecurityCenter::V1p1beta1::ListAssetsRequest, ::Google::Cloud::SecurityCenter::V1p1beta1::ListAssetsResponse
             # Lists an organization or source's findings.
             #
             # To list across all sources provide a `-` as the source id.
             # Example: /v1p1beta1/organizations/{organization_id}/sources/-/findings
-            rpc :ListFindings, Google::Cloud::SecurityCenter::V1p1beta1::ListFindingsRequest, Google::Cloud::SecurityCenter::V1p1beta1::ListFindingsResponse
+            rpc :ListFindings, ::Google::Cloud::SecurityCenter::V1p1beta1::ListFindingsRequest, ::Google::Cloud::SecurityCenter::V1p1beta1::ListFindingsResponse
             # Lists notification configs.
-            rpc :ListNotificationConfigs, Google::Cloud::SecurityCenter::V1p1beta1::ListNotificationConfigsRequest, Google::Cloud::SecurityCenter::V1p1beta1::ListNotificationConfigsResponse
+            rpc :ListNotificationConfigs, ::Google::Cloud::SecurityCenter::V1p1beta1::ListNotificationConfigsRequest, ::Google::Cloud::SecurityCenter::V1p1beta1::ListNotificationConfigsResponse
             # Lists all sources belonging to an organization.
-            rpc :ListSources, Google::Cloud::SecurityCenter::V1p1beta1::ListSourcesRequest, Google::Cloud::SecurityCenter::V1p1beta1::ListSourcesResponse
+            rpc :ListSources, ::Google::Cloud::SecurityCenter::V1p1beta1::ListSourcesRequest, ::Google::Cloud::SecurityCenter::V1p1beta1::ListSourcesResponse
             # Runs asset discovery. The discovery is tracked with a long-running
             # operation.
             #
             # This API can only be called with limited frequency for an organization. If
             # it is called too frequently the caller will receive a TOO_MANY_REQUESTS
             # error.
-            rpc :RunAssetDiscovery, Google::Cloud::SecurityCenter::V1p1beta1::RunAssetDiscoveryRequest, Google::Longrunning::Operation
+            rpc :RunAssetDiscovery, ::Google::Cloud::SecurityCenter::V1p1beta1::RunAssetDiscoveryRequest, ::Google::Longrunning::Operation
             # Updates the state of a finding.
-            rpc :SetFindingState, Google::Cloud::SecurityCenter::V1p1beta1::SetFindingStateRequest, Google::Cloud::SecurityCenter::V1p1beta1::Finding
+            rpc :SetFindingState, ::Google::Cloud::SecurityCenter::V1p1beta1::SetFindingStateRequest, ::Google::Cloud::SecurityCenter::V1p1beta1::Finding
             # Sets the access control policy on the specified Source.
-            rpc :SetIamPolicy, Google::Iam::V1::SetIamPolicyRequest, Google::Iam::V1::Policy
+            rpc :SetIamPolicy, ::Google::Iam::V1::SetIamPolicyRequest, ::Google::Iam::V1::Policy
             # Returns the permissions that a caller has on the specified source.
-            rpc :TestIamPermissions, Google::Iam::V1::TestIamPermissionsRequest, Google::Iam::V1::TestIamPermissionsResponse
+            rpc :TestIamPermissions, ::Google::Iam::V1::TestIamPermissionsRequest, ::Google::Iam::V1::TestIamPermissionsResponse
             # Creates or updates a finding. The corresponding source must exist for a
             # finding creation to succeed.
-            rpc :UpdateFinding, Google::Cloud::SecurityCenter::V1p1beta1::UpdateFindingRequest, Google::Cloud::SecurityCenter::V1p1beta1::Finding
+            rpc :UpdateFinding, ::Google::Cloud::SecurityCenter::V1p1beta1::UpdateFindingRequest, ::Google::Cloud::SecurityCenter::V1p1beta1::Finding
             # Updates a notification config. The following update
             # fields are allowed: description, pubsub_topic, streaming_config.filter
-            rpc :UpdateNotificationConfig, Google::Cloud::SecurityCenter::V1p1beta1::UpdateNotificationConfigRequest, Google::Cloud::SecurityCenter::V1p1beta1::NotificationConfig
+            rpc :UpdateNotificationConfig, ::Google::Cloud::SecurityCenter::V1p1beta1::UpdateNotificationConfigRequest, ::Google::Cloud::SecurityCenter::V1p1beta1::NotificationConfig
             # Updates an organization's settings.
-            rpc :UpdateOrganizationSettings, Google::Cloud::SecurityCenter::V1p1beta1::UpdateOrganizationSettingsRequest, Google::Cloud::SecurityCenter::V1p1beta1::OrganizationSettings
+            rpc :UpdateOrganizationSettings, ::Google::Cloud::SecurityCenter::V1p1beta1::UpdateOrganizationSettingsRequest, ::Google::Cloud::SecurityCenter::V1p1beta1::OrganizationSettings
             # Updates a source.
-            rpc :UpdateSource, Google::Cloud::SecurityCenter::V1p1beta1::UpdateSourceRequest, Google::Cloud::SecurityCenter::V1p1beta1::Source
+            rpc :UpdateSource, ::Google::Cloud::SecurityCenter::V1p1beta1::UpdateSourceRequest, ::Google::Cloud::SecurityCenter::V1p1beta1::Source
             # Updates security marks.
-            rpc :UpdateSecurityMarks, Google::Cloud::SecurityCenter::V1p1beta1::UpdateSecurityMarksRequest, Google::Cloud::SecurityCenter::V1p1beta1::SecurityMarks
+            rpc :UpdateSecurityMarks, ::Google::Cloud::SecurityCenter::V1p1beta1::UpdateSecurityMarksRequest, ::Google::Cloud::SecurityCenter::V1p1beta1::SecurityMarks
           end
 
           Stub = Service.rpc_stub_class
