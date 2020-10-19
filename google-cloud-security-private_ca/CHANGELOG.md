b28e06508bb698839bec5decd154defb786c67c4
Daniel Azuma <dazuma@google.com>
2020-10-19 16:48:35 +0000

    Release google-cloud-security-private_ca 0.1.0
    Initial release.

--- google-cloud-security-private_ca/CHANGELOG.md
+++ google-cloud-security-private_ca/CHANGELOG.md
@@ -1,2 +1,6 @@
 # Release History
 
+### 0.1.0 / 2020-10-19
+
+Initial release.
+
--- google-cloud-security-private_ca/lib/google/cloud/security/private_ca/version.rb
+++ google-cloud-security-private_ca/lib/google/cloud/security/private_ca/version.rb
@@ -21,7 +21,7 @@ module Google
   module Cloud
     module Security
       module PrivateCA
-        VERSION = "0.0.1"
+        VERSION = "0.1.0"
       end
     end
   end
