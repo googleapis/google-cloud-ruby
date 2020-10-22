e881e2795d3343b8c9b2384f949bd4cea25b228a
Chris Smith <quartzmo@gmail.com>
2020-10-22 14:30:44 -0600

    docs(pubsub): Update deprecated attribute name limit to max_outstanding_messages
    closes:  #7939
    pr: #8051

--- google-cloud-pubsub/test/google/cloud/pubsub/subscriber_test.rb
+++ google-cloud-pubsub/test/google/cloud/pubsub/subscriber_test.rb
@@ -19,23 +19,58 @@ describe Google::Cloud::PubSub::Subscriber, :mock_pubsub do
   let(:subscription_name) { "subscription-name-goes-here" }
   let(:deadline) { 120 }
   let(:streams) { 8 }
-  let(:inventory) { 2000 }
+  let(:max_outstanding_messages) { 2000 }
   let(:callback_threads) { 16 }
   let(:push_threads) { 8 }
-  let(:subscriber) { Google::Cloud::PubSub::Subscriber.new subscription_name, callback, deadline: deadline, streams: streams, inventory: inventory, threads: { callback: callback_threads, push: push_threads}, service: pubsub.service }
+  let :subscriber do
+    Google::Cloud::PubSub::Subscriber.new(
+      subscription_name,
+      callback,
+      deadline: deadline,
+      streams: streams,
+      inventory: max_outstanding_messages,
+      threads: {
+        callback: callback_threads,
+        push: push_threads
+      },
+      service: pubsub.service
+    )
+  end
 
-  it "knows itself" do
+  it "knows its defaults" do
+    subscriber = Google::Cloud::PubSub::Subscriber.new(
+      subscription_name,
+      callback,
+      service: pubsub.service
+    )
+    _(subscriber).must_be_kind_of Google::Cloud::PubSub::Subscriber
+    _(subscriber.deadline).must_equal 60
+    _(subscriber.streams).must_equal 2
+    _(subscriber.inventory).must_equal 1000 # deprecated Use #max_outstanding_messages.
+    _(subscriber.inventory_limit).must_equal 1000 # deprecated Use #max_outstanding_messages.
+    _(subscriber.max_outstanding_messages).must_equal 1000
+    _(subscriber.inventory_bytesize).must_equal 100_000_000 # deprecated Use #max_outstanding_bytes.
+    _(subscriber.max_outstanding_bytes).must_equal 100_000_000
+    _(subscriber.inventory_extension).must_equal 3600 # deprecated Use #max_total_lease_duration.
+    _(subscriber.max_total_lease_duration).must_equal 3600
+    _(subscriber.max_duration_per_lease_extension).must_equal 0
+    _(subscriber.stream_inventory).must_equal({limit: 500, bytesize: 50000000, max_duration_per_lease_extension: 0, extension: 3600})
+    _(subscriber.callback_threads).must_equal 8
+    _(subscriber.push_threads).must_equal 4
+  end
+
+  it "knows its given attributes and retains defaults" do
     _(subscriber).must_be_kind_of Google::Cloud::PubSub::Subscriber
     _(subscriber.callback).must_equal callback
     _(subscriber.subscription_name).must_equal subscription_name
     _(subscriber.deadline).must_equal deadline
     _(subscriber.streams).must_equal streams
-    _(subscriber.inventory).must_equal inventory
-    _(subscriber.inventory_limit).must_equal inventory
-    _(subscriber.max_outstanding_messages).must_equal inventory
-    _(subscriber.inventory_bytesize).must_equal 100_000_000
+    _(subscriber.inventory).must_equal max_outstanding_messages # deprecated Use #max_outstanding_messages.
+    _(subscriber.inventory_limit).must_equal max_outstanding_messages # deprecated Use #max_outstanding_messages.
+    _(subscriber.max_outstanding_messages).must_equal max_outstanding_messages
+    _(subscriber.inventory_bytesize).must_equal 100_000_000 # deprecated Use #max_outstanding_bytes.
     _(subscriber.max_outstanding_bytes).must_equal 100_000_000
-    _(subscriber.inventory_extension).must_equal 3600
+    _(subscriber.inventory_extension).must_equal 3600 # deprecated Use #max_total_lease_duration.
     _(subscriber.max_total_lease_duration).must_equal 3600
     _(subscriber.max_duration_per_lease_extension).must_equal 0
     _(subscriber.stream_inventory).must_equal({limit: 250, bytesize: 12500000, max_duration_per_lease_extension: 0, extension: 3600})
