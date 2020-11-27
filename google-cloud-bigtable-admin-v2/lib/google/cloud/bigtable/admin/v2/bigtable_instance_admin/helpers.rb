1060529a24de9ea609b4d65fe0ee579c9b66f9d2
Chris Smith <quartzmo@gmail.com>
2020-11-27 14:09:21 -0700

    fix(bigtable-admin-v2): Update keepalive configuration
    refs: #8172

---
+++ google-cloud-bigtable-admin-v2/lib/google/cloud/bigtable/admin/v2/bigtable_instance_admin/helpers.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+Google::Cloud::Bigtable::Admin::V2::BigtableInstanceAdmin::Client.configure do |config|
+  config.channel_args ||= {}
+  config.channel_args["grpc.keepalive_time_ms"] = 30_000 # Sets 30s as Google Frontends allows keepalive pings at 30s
+  config.channel_args["grpc.keepalive_timeout_ms"] = 10_000 # Conservative timeout at 10s
+end
