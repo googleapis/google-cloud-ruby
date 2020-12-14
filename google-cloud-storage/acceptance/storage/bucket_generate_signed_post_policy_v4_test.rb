d0e9a27eb10707a1ceba690422b4feac8b41e9a2
Chris Smith <quartzmo@gmail.com>
2020-12-14 12:01:50 -0700

    fix(storage): Fix support for #generate_signed_post_policy_v4 conditions
    closes: #8138
    pr: #8373

--- google-cloud-storage/acceptance/storage/bucket_generate_signed_post_policy_v4_test.rb
+++ google-cloud-storage/acceptance/storage/bucket_generate_signed_post_policy_v4_test.rb
@@ -26,6 +26,8 @@ describe Google::Cloud::Storage::Bucket, :generate_signed_post_policy_v4, :stora
   let(:uri) { URI.parse Google::Cloud::Storage::GOOGLEAPIS_URL }
   let(:data_file) { "logo.jpg" }
   let(:data) { File.expand_path("../data/#{data_file}", __dir__) }
+  let(:data_csv_file) { "example.csv" } # < 1 KB
+  let(:data_csv) { File.expand_path("../data/#{data_csv_file}", __dir__) }
 
   it "generates a signed post object v4 simple" do
     post_object = bucket.generate_signed_post_policy_v4 "test-object", expires: 10
@@ -226,6 +228,47 @@ describe Google::Cloud::Storage::Bucket, :generate_signed_post_policy_v4, :stora
       tmpfile.binmode
       downloaded = file.download tmpfile
       _(File.read(downloaded.path, mode: "rb")).must_equal File.read(data, mode: "rb")
+    end
+  end
+
+  it "generates a signed post object v4 with conditions" do
+    conditions = [["starts-with", "$key", ""]]
+    post_object = bucket.generate_signed_post_policy_v4 "${filename}", expires: 10, conditions: conditions
+
+    _(post_object.fields.keys.sort).must_equal [
+      "key",
+      "policy",
+      "x-goog-algorithm",
+      "x-goog-credential",
+      "x-goog-date",
+      "x-goog-signature"
+    ]
+
+    http = Net::HTTP.new uri.host, uri.port
+    http.use_ssl = true
+    request = Net::HTTP::Post.new post_object.url
+
+    form_data = [
+      ["file", File.open(data_csv)],
+      ["key", post_object.fields["key"]],
+      ["policy", post_object.fields["policy"]],
+      ["x-goog-algorithm", post_object.fields["x-goog-algorithm"]],
+      ["x-goog-credential", post_object.fields["x-goog-credential"]],
+      ["x-goog-date", post_object.fields["x-goog-date"]],
+      ["x-goog-signature", post_object.fields["x-goog-signature"]],
+    ]
+
+    request.set_form form_data, "multipart/form-data"
+
+    response = http.request request
+    puts response.body
+    _(response.code).must_equal "204"
+    file = bucket.file("example.csv")
+    _(file).wont_be :nil?
+    Tempfile.open ["example", ".csv"] do |tmpfile|
+      tmpfile.binmode
+      downloaded = file.download tmpfile
+      _(File.read(downloaded.path)).must_equal File.read(data_csv)
     end
   end
 
