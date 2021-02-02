e8644478bb1a9c7b15b7e8d1ac6602e48e0e4738
yoshi-automation <yoshi-automation@google.com>
2021-02-01 16:31:43 -0800

    changes without context
            autosynth cannot find the source of changes triggered by earlier changes in this
            repository, or by version upgrades to tools such as linters.

--- google-cloud-vision-v1p3beta1/proto_docs/google/type/latlng.rb
+++ google-cloud-vision-v1p3beta1/proto_docs/google/type/latlng.rb
@@ -19,8 +19,8 @@
 
 module Google
   module Type
-    # An object representing a latitude/longitude pair. This is expressed as a pair
-    # of doubles representing degrees latitude and degrees longitude. Unless
+    # An object that represents a latitude/longitude pair. This is expressed as a
+    # pair of doubles to represent degrees latitude and degrees longitude. Unless
     # specified otherwise, this must conform to the
     # <a href="http://www.unoosa.org/pdf/icg/2012/template/WGS_84.pdf">WGS84
     # standard</a>. Values must be within normalized ranges.
