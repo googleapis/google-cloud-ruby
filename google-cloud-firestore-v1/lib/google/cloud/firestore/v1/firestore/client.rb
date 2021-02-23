5a43b640a06195d2d4f6af7479ee4ef4bcdee9c4
yoshi-automation <yoshi-automation@google.com>
2021-02-22 20:30:13 -0800

    Merge pull request #624 from vchudnov-g/drift-metadata
    Add schema for providing GAPIC metadata
    
    Source-Author: Victor Chudnovsky <vchudnov@google.com>
    Source-Date: Mon Oct 19 14:48:15 2020 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: 34c5a5c5132a2c5ef46f599550a1195e07f01f80
    Source-Link: https://github.com/googleapis/googleapis/commit/34c5a5c5132a2c5ef46f599550a1195e07f01f80

--- google-cloud-firestore-v1/lib/google/cloud/firestore/v1/firestore/client.rb
+++ google-cloud-firestore-v1/lib/google/cloud/firestore/v1/firestore/client.rb
@@ -142,12 +142,12 @@ module Google
                   retry_codes:   [14, 13, 4]
                 }
 
-                default_config.rpcs.partition_query.timeout = 60.0
+                default_config.rpcs.partition_query.timeout = 300.0
                 default_config.rpcs.partition_query.retry_policy = {
                   initial_delay: 0.1,
                   max_delay:     60.0,
                   multiplier:    1.3,
-                  retry_codes:   [14]
+                  retry_codes:   [14, 13, 4]
                 }
 
                 default_config.rpcs.write.timeout = 86_400.0
