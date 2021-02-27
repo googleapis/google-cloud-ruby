e88cb98240ddfb82fe0bfbbff7af634622f22de5
yoshi-automation <yoshi-automation@google.com>
2021-02-27 14:40:44 -0800

    changes without context
            autosynth cannot find the source of changes triggered by earlier changes in this
            repository, or by version upgrades to tools such as linters.

--- google-cloud-scheduler-v1/lib/google/cloud/scheduler/v1/cloud_scheduler/client.rb
+++ google-cloud-scheduler-v1/lib/google/cloud/scheduler/v1/cloud_scheduler/client.rb
@@ -61,7 +61,7 @@ module Google
                 parent_config = while namespace.any?
                                   parent_name = namespace.join "::"
                                   parent_const = const_get parent_name
-                                  break parent_const.configure if parent_const&.respond_to? :configure
+                                  break parent_const.configure if parent_const.respond_to? :configure
                                   namespace.pop
                                 end
                 default_config = Client::Configuration.new parent_config
@@ -69,17 +69,17 @@ module Google
                 default_config.rpcs.list_jobs.timeout = 600.0
                 default_config.rpcs.list_jobs.retry_policy = {
                   initial_delay: 0.1,
-                  max_delay:     60.0,
-                  multiplier:    1.3,
-                  retry_codes:   [4, 14]
+              max_delay: 60.0,
+              multiplier: 1.3,
+              retry_codes: [4, 14]
                 }
 
                 default_config.rpcs.get_job.timeout = 600.0
                 default_config.rpcs.get_job.retry_policy = {
                   initial_delay: 0.1,
-                  max_delay:     60.0,
-                  multiplier:    1.3,
-                  retry_codes:   [4, 14]
+              max_delay: 60.0,
+              multiplier: 1.3,
+              retry_codes: [4, 14]
                 }
 
                 default_config.rpcs.create_job.timeout = 600.0
@@ -89,9 +89,9 @@ module Google
                 default_config.rpcs.delete_job.timeout = 600.0
                 default_config.rpcs.delete_job.retry_policy = {
                   initial_delay: 0.1,
-                  max_delay:     60.0,
-                  multiplier:    1.3,
-                  retry_codes:   [4, 14]
+              max_delay: 60.0,
+              multiplier: 1.3,
+              retry_codes: [4, 14]
                 }
 
                 default_config.rpcs.pause_job.timeout = 600.0
@@ -166,7 +166,7 @@ module Google
               enable_self_signed_jwt = @config.scope == Client.configure.scope &&
                                        @config.endpoint == Client.configure.endpoint &&
                                        !@config.endpoint.split(".").first.include?("-")
-              credentials ||= Credentials.default scope:                  @config.scope,
+              credentials ||= Credentials.default scope: @config.scope,
                                                   enable_self_signed_jwt: enable_self_signed_jwt
               if credentials.is_a?(String) || credentials.is_a?(Hash)
                 credentials = Credentials.new credentials, scope: @config.scope
@@ -861,7 +861,7 @@ module Google
               config_attr :scope,         nil, ::String, ::Array, nil
               config_attr :lib_name,      nil, ::String, nil
               config_attr :lib_version,   nil, ::String, nil
-              config_attr(:channel_args,  { "grpc.service_config_disable_resolution"=>1 }, ::Hash, nil)
+              config_attr(:channel_args,  { "grpc.service_config_disable_resolution" => 1 }, ::Hash, nil)
               config_attr :interceptors,  nil, ::Array, nil
               config_attr :timeout,       nil, ::Numeric, nil
               config_attr :metadata,      nil, ::Hash, nil
@@ -882,7 +882,7 @@ module Google
               def rpcs
                 @rpcs ||= begin
                   parent_rpcs = nil
-                  parent_rpcs = @parent_config.rpcs if defined?(@parent_config) && @parent_config&.respond_to?(:rpcs)
+                  parent_rpcs = @parent_config.rpcs if defined?(@parent_config) && @parent_config.respond_to?(:rpcs)
                   Rpcs.new parent_rpcs
                 end
               end
@@ -948,21 +948,21 @@ module Google
 
                 # @private
                 def initialize parent_rpcs = nil
-                  list_jobs_config = parent_rpcs&.list_jobs if parent_rpcs&.respond_to? :list_jobs
+                  list_jobs_config = parent_rpcs.list_jobs if parent_rpcs.respond_to? :list_jobs
                   @list_jobs = ::Gapic::Config::Method.new list_jobs_config
-                  get_job_config = parent_rpcs&.get_job if parent_rpcs&.respond_to? :get_job
+                  get_job_config = parent_rpcs.get_job if parent_rpcs.respond_to? :get_job
                   @get_job = ::Gapic::Config::Method.new get_job_config
-                  create_job_config = parent_rpcs&.create_job if parent_rpcs&.respond_to? :create_job
+                  create_job_config = parent_rpcs.create_job if parent_rpcs.respond_to? :create_job
                   @create_job = ::Gapic::Config::Method.new create_job_config
-                  update_job_config = parent_rpcs&.update_job if parent_rpcs&.respond_to? :update_job
+                  update_job_config = parent_rpcs.update_job if parent_rpcs.respond_to? :update_job
                   @update_job = ::Gapic::Config::Method.new update_job_config
-                  delete_job_config = parent_rpcs&.delete_job if parent_rpcs&.respond_to? :delete_job
+                  delete_job_config = parent_rpcs.delete_job if parent_rpcs.respond_to? :delete_job
                   @delete_job = ::Gapic::Config::Method.new delete_job_config
-                  pause_job_config = parent_rpcs&.pause_job if parent_rpcs&.respond_to? :pause_job
+                  pause_job_config = parent_rpcs.pause_job if parent_rpcs.respond_to? :pause_job
                   @pause_job = ::Gapic::Config::Method.new pause_job_config
-                  resume_job_config = parent_rpcs&.resume_job if parent_rpcs&.respond_to? :resume_job
+                  resume_job_config = parent_rpcs.resume_job if parent_rpcs.respond_to? :resume_job
                   @resume_job = ::Gapic::Config::Method.new resume_job_config
-                  run_job_config = parent_rpcs&.run_job if parent_rpcs&.respond_to? :run_job
+                  run_job_config = parent_rpcs.run_job if parent_rpcs.respond_to? :run_job
                   @run_job = ::Gapic::Config::Method.new run_job_config
 
                   yield self if block_given?
