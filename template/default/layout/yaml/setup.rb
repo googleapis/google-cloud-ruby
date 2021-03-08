615e885d6a67d870bf7aeae961bed2fb37b67841
Graham Paye <paye@google.com>
2021-03-08 12:00:23 -0700

    move template logic to setup.rb

--- template/default/layout/yaml/setup.rb
+++ template/default/layout/yaml/setup.rb
@@ -3,23 +3,42 @@ def init
   @methods = @object.children.select { |child| child.type == :method }
   @constants = @object.children.select { |child| child.type == :constant }
   @references = @object.children.reject { |child| [:method, :constant].include? child.type }
-  @object_text = ERB.new(File.read"#{__dir__}/_object.erb").result(binding)
+  @object_text = ERB.new(File.read"#{__dir__}/_object.erb").result binding
 
   @method_text = @methods.map { |method|
-    ERB.new(File.read"#{__dir__}/_method.erb").result_with_hash :method => method
+    @method = method
+    ERB.new(File.read"#{__dir__}/_method.erb").result binding
   }.join("\n")
 
   @constant_text = @constants.map { |constant|
-    ERB.new(File.read"#{__dir__}/_constant.erb").result_with_hash :constant => constant
+    @constant = constant
+    ERB.new(File.read"#{__dir__}/_constant.erb").result binding
   }.join("\n")
 
   if @references.empty?
     @reference_text = "[]"
   else
-    @reference_text = @references.map { |reference|
-      ERB.new(File.read "#{__dir__}/_reference.erb").result_with_hash :reference => reference
+    @reference_text = "\n" + @references.map { |reference|
+      @reference = reference
+      ERB.new(File.read "#{__dir__}/_reference.erb").result binding
     }.join("\n")
   end
 
   sections :layout
+end
+
+def children
+  if !@object.children || @object.children.empty?
+    "[]"
+  else
+    out = "\n"
+    out += @object.children.map { |child|
+      "      - #{child.path}"
+    }.join("\n")
+    out
+  end
+end
+
+def docstring obj
+  obj.docstring.to_str.chomp.sub("--- ", "").sub("|-\n", "").gsub '"', "'"
 end
