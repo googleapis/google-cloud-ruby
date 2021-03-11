a43e510a50f7f656c277a340b8b6fb1224934f62
Chris Smith <quartzmo@gmail.com>
2021-03-11 10:35:19 -0700

    Update Gemfile

--- google-cloud-bigtable/samples/acceptance/helper.rb
+++ google-cloud-bigtable/samples/acceptance/helper.rb
@@ -1,4 +1,4 @@
-# Copyright 2020 Google LLC
+# Copyright 2021 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,7 +16,6 @@ require "minitest/autorun"
 require "minitest/focus"
 require "minitest/hooks/default"
 require "google/cloud/bigtable"
-require "google/cloud/bigtable/admin"
 require "securerandom"
 
 $bigtable = Google::Cloud.new.bigtable
