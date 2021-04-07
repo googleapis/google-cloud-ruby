eea373e55e240be7ad209cf2acad812ee70d8146
yoshi-automation <yoshi-automation@google.com>
2021-04-07 03:47:16 -0700

    fix: add Java options to Binaryauthorization protos
    Committer: @miraleung
    PiperOrigin-RevId: 367156876
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Tue Apr 6 23:06:52 2021 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: 7e91915c247a5c84ab5fcdf11ce2bdf7404f2fcc
    Source-Link: https://github.com/googleapis/googleapis/commit/7e91915c247a5c84ab5fcdf11ce2bdf7404f2fcc

--- google-cloud-binary_authorization-v1beta1/lib/google/cloud/binaryauthorization/v1beta1/service_services_pb.rb
+++ google-cloud-binary_authorization-v1beta1/lib/google/cloud/binaryauthorization/v1beta1/service_services_pb.rb
@@ -42,36 +42,50 @@ module Google
             self.unmarshal_class_method = :decode
             self.service_name = 'google.cloud.binaryauthorization.v1beta1.BinauthzManagementServiceV1Beta1'
 
-            # A [policy][google.cloud.binaryauthorization.v1beta1.Policy] specifies the [attestors][google.cloud.binaryauthorization.v1beta1.Attestor] that must attest to
-            # a container image, before the project is allowed to deploy that
+            # A [policy][google.cloud.binaryauthorization.v1beta1.Policy] specifies the
+            # [attestors][google.cloud.binaryauthorization.v1beta1.Attestor] that must
+            # attest to a container image, before the project is allowed to deploy that
             # image. There is at most one policy per project. All image admission
             # requests are permitted if a project has no policy.
             #
-            # Gets the [policy][google.cloud.binaryauthorization.v1beta1.Policy] for this project. Returns a default
-            # [policy][google.cloud.binaryauthorization.v1beta1.Policy] if the project does not have one.
+            # Gets the [policy][google.cloud.binaryauthorization.v1beta1.Policy] for this
+            # project. Returns a default
+            # [policy][google.cloud.binaryauthorization.v1beta1.Policy] if the project
+            # does not have one.
             rpc :GetPolicy, ::Google::Cloud::BinaryAuthorization::V1beta1::GetPolicyRequest, ::Google::Cloud::BinaryAuthorization::V1beta1::Policy
-            # Creates or updates a project's [policy][google.cloud.binaryauthorization.v1beta1.Policy], and returns a copy of the
-            # new [policy][google.cloud.binaryauthorization.v1beta1.Policy]. A policy is always updated as a whole, to avoid race
-            # conditions with concurrent policy enforcement (or management!)
-            # requests. Returns NOT_FOUND if the project does not exist, INVALID_ARGUMENT
-            # if the request is malformed.
+            # Creates or updates a project's
+            # [policy][google.cloud.binaryauthorization.v1beta1.Policy], and returns a
+            # copy of the new [policy][google.cloud.binaryauthorization.v1beta1.Policy].
+            # A policy is always updated as a whole, to avoid race conditions with
+            # concurrent policy enforcement (or management!) requests. Returns NOT_FOUND
+            # if the project does not exist, INVALID_ARGUMENT if the request is
+            # malformed.
             rpc :UpdatePolicy, ::Google::Cloud::BinaryAuthorization::V1beta1::UpdatePolicyRequest, ::Google::Cloud::BinaryAuthorization::V1beta1::Policy
-            # Creates an [attestor][google.cloud.binaryauthorization.v1beta1.Attestor], and returns a copy of the new
-            # [attestor][google.cloud.binaryauthorization.v1beta1.Attestor]. Returns NOT_FOUND if the project does not exist,
-            # INVALID_ARGUMENT if the request is malformed, ALREADY_EXISTS if the
-            # [attestor][google.cloud.binaryauthorization.v1beta1.Attestor] already exists.
+            # Creates an [attestor][google.cloud.binaryauthorization.v1beta1.Attestor],
+            # and returns a copy of the new
+            # [attestor][google.cloud.binaryauthorization.v1beta1.Attestor]. Returns
+            # NOT_FOUND if the project does not exist, INVALID_ARGUMENT if the request is
+            # malformed, ALREADY_EXISTS if the
+            # [attestor][google.cloud.binaryauthorization.v1beta1.Attestor] already
+            # exists.
             rpc :CreateAttestor, ::Google::Cloud::BinaryAuthorization::V1beta1::CreateAttestorRequest, ::Google::Cloud::BinaryAuthorization::V1beta1::Attestor
             # Gets an [attestor][google.cloud.binaryauthorization.v1beta1.Attestor].
-            # Returns NOT_FOUND if the [attestor][google.cloud.binaryauthorization.v1beta1.Attestor] does not exist.
+            # Returns NOT_FOUND if the
+            # [attestor][google.cloud.binaryauthorization.v1beta1.Attestor] does not
+            # exist.
             rpc :GetAttestor, ::Google::Cloud::BinaryAuthorization::V1beta1::GetAttestorRequest, ::Google::Cloud::BinaryAuthorization::V1beta1::Attestor
             # Updates an [attestor][google.cloud.binaryauthorization.v1beta1.Attestor].
-            # Returns NOT_FOUND if the [attestor][google.cloud.binaryauthorization.v1beta1.Attestor] does not exist.
+            # Returns NOT_FOUND if the
+            # [attestor][google.cloud.binaryauthorization.v1beta1.Attestor] does not
+            # exist.
             rpc :UpdateAttestor, ::Google::Cloud::BinaryAuthorization::V1beta1::UpdateAttestorRequest, ::Google::Cloud::BinaryAuthorization::V1beta1::Attestor
             # Lists [attestors][google.cloud.binaryauthorization.v1beta1.Attestor].
             # Returns INVALID_ARGUMENT if the project does not exist.
             rpc :ListAttestors, ::Google::Cloud::BinaryAuthorization::V1beta1::ListAttestorsRequest, ::Google::Cloud::BinaryAuthorization::V1beta1::ListAttestorsResponse
-            # Deletes an [attestor][google.cloud.binaryauthorization.v1beta1.Attestor]. Returns NOT_FOUND if the
-            # [attestor][google.cloud.binaryauthorization.v1beta1.Attestor] does not exist.
+            # Deletes an [attestor][google.cloud.binaryauthorization.v1beta1.Attestor].
+            # Returns NOT_FOUND if the
+            # [attestor][google.cloud.binaryauthorization.v1beta1.Attestor] does not
+            # exist.
             rpc :DeleteAttestor, ::Google::Cloud::BinaryAuthorization::V1beta1::DeleteAttestorRequest, ::Google::Protobuf::Empty
           end
 
