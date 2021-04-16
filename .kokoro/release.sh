29e62691dbceab5b6ec3fce025476ac9ad597a27
Daniel Azuma <dazuma@google.com>
2021-04-16 21:15:23 +0000

    chore: Update release scripts

---
+++ .kokoro/release.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -eo pipefail
+
+# Install gems in the user directory because the default install directory
+# is in a read-only location.
+export GEM_HOME=$HOME/.gem
+export PATH=$GEM_HOME/bin:$PATH
+
+python3 -m pip install git+https://github.com/googleapis/releasetool
+python3 -m pip install gcp-docuploader
+gem install --no-document toys
+
+python3 -m releasetool publish-reporter-script > /tmp/publisher-script; source /tmp/publisher-script
+toys kokoro release < /dev/null
