8c3d962b445bcbe7fa877a4f1bff003f813a28c1
yoshi-automation <yoshi-automation@google.com>
2021-05-08 03:40:42 -0700

    changes without context
            autosynth cannot find the source of changes triggered by earlier changes in this
            repository, or by version upgrades to tools such as linters.

--- google-analytics-data-v1alpha/lib/google/analytics/data/v1alpha/analytics_data_api_services_pb.rb
+++ google-analytics-data-v1alpha/lib/google/analytics/data/v1alpha/analytics_data_api_services_pb.rb
@@ -27,7 +27,7 @@ module Google
           # Google Analytics reporting data service.
           class Service
 
-            include GRPC::GenericService
+            include ::GRPC::GenericService
 
             self.marshal_class_method = :encode
             self.unmarshal_class_method = :decode
