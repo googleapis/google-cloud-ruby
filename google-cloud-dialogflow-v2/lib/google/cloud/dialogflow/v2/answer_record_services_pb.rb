a6592310b12986ae422ab9b509c645401dd60587
yoshi-automation <yoshi-automation@google.com>
2021-05-08 04:02:45 -0700

    changes without context
            autosynth cannot find the source of changes triggered by earlier changes in this
            repository, or by version upgrades to tools such as linters.

--- google-cloud-dialogflow-v2/lib/google/cloud/dialogflow/v2/answer_record_services_pb.rb
+++ google-cloud-dialogflow-v2/lib/google/cloud/dialogflow/v2/answer_record_services_pb.rb
@@ -27,7 +27,7 @@ module Google
           # Service for managing [AnswerRecords][google.cloud.dialogflow.v2.AnswerRecord].
           class Service
 
-            include GRPC::GenericService
+            include ::GRPC::GenericService
 
             self.marshal_class_method = :encode
             self.unmarshal_class_method = :decode
