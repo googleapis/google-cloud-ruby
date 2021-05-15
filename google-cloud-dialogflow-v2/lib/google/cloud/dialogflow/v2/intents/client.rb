db3bb3c81b77a173b85a5aa3472b9fd0af6fd45a
yoshi-automation <yoshi-automation@google.com>
2021-05-15 03:52:36 -0700

    feat: added location-aware HTTP path binding for ListIntents
    PiperOrigin-RevId: 373895026
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Fri May 14 17:14:27 2021 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: 0d68bbb80a7620b69aff5ab0b497c8a396e73558
    Source-Link: https://github.com/googleapis/googleapis/commit/0d68bbb80a7620b69aff5ab0b497c8a396e73558

--- google-cloud-dialogflow-v2/lib/google/cloud/dialogflow/v2/intents/client.rb
+++ google-cloud-dialogflow-v2/lib/google/cloud/dialogflow/v2/intents/client.rb
@@ -190,7 +190,15 @@ module Google
             #
             #   @param parent [::String]
             #     Required. The agent to list all intents from.
-            #     Format: `projects/<Project ID>/agent`.
+            #     Format: `projects/<Project ID>/agent` or `projects/<Project
+            #     ID>/locations/<Location ID>/agent`.
+            #
+            #     Alternatively, you can specify the environment to list intents for.
+            #     Format: `projects/<Project ID>/agent/environments/<Environment ID>`
+            #     or `projects/<Project ID>/locations/<Location
+            #     ID>/agent/environments/<Environment ID>`.
+            #     Note: training phrases of the intents will not be returned for non-draft
+            #     environment.
             #   @param language_code [::String]
             #     Optional. The language used to access language-specific data.
             #     If not specified, the agent's default language is used.
