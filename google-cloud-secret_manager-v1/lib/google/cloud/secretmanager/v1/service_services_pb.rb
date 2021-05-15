0eb9aea329414e0a6bbf5996b23e09075090b63c
yoshi-automation <yoshi-automation@google.com>
2021-05-15 04:10:48 -0700

    feat: Etags in Secret Manager
    Users can now use etags for optimistic concurrency control when modifying Secret or SecretVersion.
    
    PiperOrigin-RevId: 373836373
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Fri May 14 11:59:47 2021 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: bff807490f09712527b1af607160a4f7ae48702e
    Source-Link: https://github.com/googleapis/googleapis/commit/bff807490f09712527b1af607160a4f7ae48702e

--- google-cloud-secret_manager-v1/lib/google/cloud/secretmanager/v1/service_services_pb.rb
+++ google-cloud-secret_manager-v1/lib/google/cloud/secretmanager/v1/service_services_pb.rb
@@ -57,13 +57,13 @@ module Google
             rpc :ListSecretVersions, ::Google::Cloud::SecretManager::V1::ListSecretVersionsRequest, ::Google::Cloud::SecretManager::V1::ListSecretVersionsResponse
             # Gets metadata for a [SecretVersion][google.cloud.secretmanager.v1.SecretVersion].
             #
-            # `projects/*/secrets/*/versions/latest` is an alias to the `latest`
-            # [SecretVersion][google.cloud.secretmanager.v1.SecretVersion].
+            # `projects/*/secrets/*/versions/latest` is an alias to the most recently
+            # created [SecretVersion][google.cloud.secretmanager.v1.SecretVersion].
             rpc :GetSecretVersion, ::Google::Cloud::SecretManager::V1::GetSecretVersionRequest, ::Google::Cloud::SecretManager::V1::SecretVersion
             # Accesses a [SecretVersion][google.cloud.secretmanager.v1.SecretVersion]. This call returns the secret data.
             #
-            # `projects/*/secrets/*/versions/latest` is an alias to the `latest`
-            # [SecretVersion][google.cloud.secretmanager.v1.SecretVersion].
+            # `projects/*/secrets/*/versions/latest` is an alias to the most recently
+            # created [SecretVersion][google.cloud.secretmanager.v1.SecretVersion].
             rpc :AccessSecretVersion, ::Google::Cloud::SecretManager::V1::AccessSecretVersionRequest, ::Google::Cloud::SecretManager::V1::AccessSecretVersionResponse
             # Disables a [SecretVersion][google.cloud.secretmanager.v1.SecretVersion].
             #
