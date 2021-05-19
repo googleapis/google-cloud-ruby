1951095c41a47b75cd06970045d38a442c7d4dfb
yoshi-automation <yoshi-automation@google.com>
2021-05-18 17:10:15 -0700

    changes without context
            autosynth cannot find the source of changes triggered by earlier changes in this
            repository, or by version upgrades to tools such as linters.

--- google-cloud-speech-v1p1beta1/lib/google/cloud/speech/v1p1beta1/speech/client.rb
+++ google-cloud-speech-v1p1beta1/lib/google/cloud/speech/v1p1beta1/speech/client.rb
@@ -68,9 +68,9 @@ module Google
                 default_config.rpcs.recognize.timeout = 5000.0
                 default_config.rpcs.recognize.retry_policy = {
                   initial_delay: 0.1,
-              max_delay: 60.0,
-              multiplier: 1.3,
-              retry_codes: [4, 14]
+                  max_delay: 60.0,
+                  multiplier: 1.3,
+                  retry_codes: [4, 14]
                 }
 
                 default_config.rpcs.long_running_recognize.timeout = 5000.0
@@ -78,9 +78,9 @@ module Google
                 default_config.rpcs.streaming_recognize.timeout = 5000.0
                 default_config.rpcs.streaming_recognize.retry_policy = {
                   initial_delay: 0.1,
-              max_delay: 60.0,
-              multiplier: 1.3,
-              retry_codes: [4, 14]
+                  max_delay: 60.0,
+                  multiplier: 1.3,
+                  retry_codes: [4, 14]
                 }
 
                 default_config
