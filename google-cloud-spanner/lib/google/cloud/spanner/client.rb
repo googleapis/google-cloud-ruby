f62746d20c974b4907049858d8dabc1b775c8d87
Knut Olav Løite <koloite@gmail.com>
2021-06-03 21:42:03 +0200

    fix: extract binary retry info from error
    Retry info that is returned by Cloud Spanner is binary encoded in the metadata
    of an error. This was not correctly decoded by the Spanner client.
    
    Fixes #11655

--- google-cloud-spanner/lib/google/cloud/spanner/client.rb
+++ google-cloud-spanner/lib/google/cloud/spanner/client.rb
@@ -11,6 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+# frozen_string_literal: true
 
 
 require "google/cloud/spanner/errors"
@@ -1804,11 +1806,10 @@ module Google
         # GRPC::Aborted
         def delay_from_aborted err
           return nil if err.nil?
-          if err.respond_to?(:metadata) && err.metadata["retryDelay"]
-            # a correct metadata will look like this:
-            # "{\"retryDelay\":{\"seconds\":60}}"
-            seconds = err.metadata["retryDelay"]["seconds"].to_i
-            nanos = err.metadata["retryDelay"]["nanos"].to_i
+          if err.respond_to?(:metadata) && err.metadata["google.rpc.retryinfo-bin"]
+            retry_info = Google::Rpc::RetryInfo.decode(err.metadata["google.rpc.retryinfo-bin"])
+            seconds = retry_info["retry_delay"].seconds
+            nanos = retry_info["retry_delay"].nanos
             return seconds if nanos.zero?
             return seconds + (nanos / 1_000_000_000.0)
           end
