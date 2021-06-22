d29a95ec1daaa77b85f261eab380a02ef3bf5d71
Daniel Azuma <dazuma@google.com>
2021-06-22 15:49:16 -0700

    chore: Fix synth for service_control and api_gateway

--- google-cloud-api_gateway/synth.py
+++ google-cloud-api_gateway/synth.py
@@ -29,7 +29,7 @@ library = gapic.ruby_library(
         "ruby-cloud-title": "API Gateway",
         "ruby-cloud-description": "API Gateway enables you to provide secure access to your backend services through a well-defined REST API that is consistent across all of your services, regardless of the service implementation. Clients consume your REST APIS to implement standalone apps for a mobile device or tablet, through apps running in a browser, or through any other type of app that can make a request to an HTTP endpoint.",
         "ruby-cloud-env-prefix": "API_GATEWAY",
-        "ruby-cloud-wrapper-of": "v1:0.0",
+        "ruby-cloud-wrapper-of": "v1:0.1",
         "ruby-cloud-product-url": "https://cloud.google.com/api-gateway/",
         "ruby-cloud-api-id": "apigateway.googleapis.com",
         "ruby-cloud-api-shortname": "apigateway",
