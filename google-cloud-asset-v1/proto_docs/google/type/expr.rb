7321af66cdc149c58784d0d95bc0a85cfb85b839
Daniel Azuma <dazuma@google.com>
2021-07-07 20:36:54 +0000

    chore(asset-v1): Undo bazel migration for asset v1 because it is missing files

--- google-cloud-asset-v1/proto_docs/google/type/expr.rb
+++ google-cloud-asset-v1/proto_docs/google/type/expr.rb
@@ -19,53 +19,30 @@
 
 module Google
   module Type
-    # Represents a textual expression in the Common Expression Language (CEL)
-    # syntax. CEL is a C-like expression language. The syntax and semantics of CEL
-    # are documented at https://github.com/google/cel-spec.
+    # Represents an expression text. Example:
     #
-    # Example (Comparison):
-    #
-    #     title: "Summary size limit"
-    #     description: "Determines if a summary is less than 100 chars"
-    #     expression: "document.summary.size() < 100"
-    #
-    # Example (Equality):
-    #
-    #     title: "Requestor is owner"
-    #     description: "Determines if requestor is the document owner"
-    #     expression: "document.owner == request.auth.claims.email"
-    #
-    # Example (Logic):
-    #
-    #     title: "Public documents"
-    #     description: "Determine whether the document should be publicly visible"
-    #     expression: "document.type != 'private' && document.type != 'internal'"
-    #
-    # Example (Data Manipulation):
-    #
-    #     title: "Notification string"
-    #     description: "Create a notification string with a timestamp."
-    #     expression: "'New message received at ' + string(document.create_time)"
-    #
-    # The exact variables and functions that may be referenced within an expression
-    # are determined by the service that evaluates it. See the service
-    # documentation for additional information.
+    #     title: "User account presence"
+    #     description: "Determines whether the request has a user account"
+    #     expression: "size(request.user) > 0"
     # @!attribute [rw] expression
     #   @return [::String]
-    #     Textual representation of an expression in Common Expression Language
-    #     syntax.
+    #     Textual representation of an expression in
+    #     Common Expression Language syntax.
+    #
+    #     The application context of the containing message determines which
+    #     well-known feature set of CEL is supported.
     # @!attribute [rw] title
     #   @return [::String]
-    #     Optional. Title for the expression, i.e. a short string describing
+    #     An optional title for the expression, i.e. a short string describing
     #     its purpose. This can be used e.g. in UIs which allow to enter the
     #     expression.
     # @!attribute [rw] description
     #   @return [::String]
-    #     Optional. Description of the expression. This is a longer text which
+    #     An optional description of the expression. This is a longer text which
     #     describes the expression, e.g. when hovered over it in a UI.
     # @!attribute [rw] location
     #   @return [::String]
-    #     Optional. String indicating the location of the expression for error
+    #     An optional string indicating the location of the expression for error
     #     reporting, e.g. a file name and a position in the file.
     class Expr
       include ::Google::Protobuf::MessageExts
