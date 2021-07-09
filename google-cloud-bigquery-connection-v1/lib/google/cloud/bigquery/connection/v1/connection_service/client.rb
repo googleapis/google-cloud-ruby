3f682b3ac564b6064424d158bb7d0640f3bc5858
yoshi-automation <yoshi-automation@google.com>
2021-07-09 06:36:10 +0000

    chore(ruby): Use latest microgenerator for Bazel GAPIC generation
    PiperOrigin-RevId: 374926739
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Thu May 20 12:30:04 2021 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: a13ed109cbe531b2e6bf078f0eec37f548c657db
    Source-Link: https://github.com/googleapis/googleapis/commit/a13ed109cbe531b2e6bf078f0eec37f548c657db

--- google-cloud-bigquery-connection-v1/lib/google/cloud/bigquery/connection/v1/connection_service/client.rb
+++ google-cloud-bigquery-connection-v1/lib/google/cloud/bigquery/connection/v1/connection_service/client.rb
@@ -71,17 +71,17 @@ module Google
                   default_config.rpcs.get_connection.timeout = 60.0
                   default_config.rpcs.get_connection.retry_policy = {
                     initial_delay: 0.1,
-                max_delay: 60.0,
-                multiplier: 1.3,
-                retry_codes: [4, 14]
+                    max_delay: 60.0,
+                    multiplier: 1.3,
+                    retry_codes: [4, 14]
                   }
 
                   default_config.rpcs.list_connections.timeout = 60.0
                   default_config.rpcs.list_connections.retry_policy = {
                     initial_delay: 0.1,
-                max_delay: 60.0,
-                multiplier: 1.3,
-                retry_codes: [4, 14]
+                    max_delay: 60.0,
+                    multiplier: 1.3,
+                    retry_codes: [4, 14]
                   }
 
                   default_config.rpcs.update_connection.timeout = 60.0
@@ -89,9 +89,9 @@ module Google
                   default_config.rpcs.delete_connection.timeout = 60.0
                   default_config.rpcs.delete_connection.retry_policy = {
                     initial_delay: 0.1,
-                max_delay: 60.0,
-                multiplier: 1.3,
-                retry_codes: [4, 14]
+                    max_delay: 60.0,
+                    multiplier: 1.3,
+                    retry_codes: [4, 14]
                   }
 
                   default_config.rpcs.get_iam_policy.timeout = 60.0
