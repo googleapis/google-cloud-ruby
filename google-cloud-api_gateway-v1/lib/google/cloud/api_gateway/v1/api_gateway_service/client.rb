3193bf99c3e58d5fc0afac4d13a2a198db9c77db
yoshi-automation <yoshi-automation@google.com>
2021-07-09 00:47:16 +0000

    chore(ruby): Switch Bazel jobs to use the Ruby gapic-generator 0.9.0
    PiperOrigin-RevId: 382291678
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Wed Jun 30 06:36:55 2021 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: c5954ef056f94a94930cf34322eb771b98a86fe5
    Source-Link: https://github.com/googleapis/googleapis/commit/c5954ef056f94a94930cf34322eb771b98a86fe5

--- google-cloud-api_gateway-v1/lib/google/cloud/api_gateway/v1/api_gateway_service/client.rb
+++ google-cloud-api_gateway-v1/lib/google/cloud/api_gateway/v1/api_gateway_service/client.rb
@@ -67,74 +67,47 @@ module Google
 
                 default_config.rpcs.create_gateway.timeout = 60.0
                 default_config.rpcs.create_gateway.retry_policy = {
-                  initial_delay: 1.0,
-                  max_delay: 60.0,
-                  multiplier: 2,
-                  retry_codes: [2, 14]
+                  initial_delay: 1.0, max_delay: 60.0, multiplier: 2, retry_codes: [2, 14]
                 }
 
                 default_config.rpcs.update_gateway.timeout = 60.0
                 default_config.rpcs.update_gateway.retry_policy = {
-                  initial_delay: 1.0,
-                  max_delay: 60.0,
-                  multiplier: 2,
-                  retry_codes: [2, 14]
+                  initial_delay: 1.0, max_delay: 60.0, multiplier: 2, retry_codes: [2, 14]
                 }
 
                 default_config.rpcs.delete_gateway.timeout = 60.0
                 default_config.rpcs.delete_gateway.retry_policy = {
-                  initial_delay: 1.0,
-                  max_delay: 60.0,
-                  multiplier: 2,
-                  retry_codes: [2, 14]
+                  initial_delay: 1.0, max_delay: 60.0, multiplier: 2, retry_codes: [2, 14]
                 }
 
                 default_config.rpcs.create_api.timeout = 60.0
                 default_config.rpcs.create_api.retry_policy = {
-                  initial_delay: 1.0,
-                  max_delay: 60.0,
-                  multiplier: 2,
-                  retry_codes: [2, 14]
+                  initial_delay: 1.0, max_delay: 60.0, multiplier: 2, retry_codes: [2, 14]
                 }
 
                 default_config.rpcs.update_api.timeout = 60.0
                 default_config.rpcs.update_api.retry_policy = {
-                  initial_delay: 1.0,
-                  max_delay: 60.0,
-                  multiplier: 2,
-                  retry_codes: [2, 14]
+                  initial_delay: 1.0, max_delay: 60.0, multiplier: 2, retry_codes: [2, 14]
                 }
 
                 default_config.rpcs.delete_api.timeout = 60.0
                 default_config.rpcs.delete_api.retry_policy = {
-                  initial_delay: 1.0,
-                  max_delay: 60.0,
-                  multiplier: 2,
-                  retry_codes: [2, 14]
+                  initial_delay: 1.0, max_delay: 60.0, multiplier: 2, retry_codes: [2, 14]
                 }
 
                 default_config.rpcs.create_api_config.timeout = 60.0
                 default_config.rpcs.create_api_config.retry_policy = {
-                  initial_delay: 1.0,
-                  max_delay: 60.0,
-                  multiplier: 2,
-                  retry_codes: [2, 14]
+                  initial_delay: 1.0, max_delay: 60.0, multiplier: 2, retry_codes: [2, 14]
                 }
 
                 default_config.rpcs.update_api_config.timeout = 60.0
                 default_config.rpcs.update_api_config.retry_policy = {
-                  initial_delay: 1.0,
-                  max_delay: 60.0,
-                  multiplier: 2,
-                  retry_codes: [2, 14]
+                  initial_delay: 1.0, max_delay: 60.0, multiplier: 2, retry_codes: [2, 14]
                 }
 
                 default_config.rpcs.delete_api_config.timeout = 60.0
                 default_config.rpcs.delete_api_config.retry_policy = {
-                  initial_delay: 1.0,
-                  max_delay: 60.0,
-                  multiplier: 2,
-                  retry_codes: [2, 14]
+                  initial_delay: 1.0, max_delay: 60.0, multiplier: 2, retry_codes: [2, 14]
                 }
 
                 default_config
@@ -205,7 +178,7 @@ module Google
                                        !@config.endpoint.split(".").first.include?("-")
               credentials ||= Credentials.default scope: @config.scope,
                                                   enable_self_signed_jwt: enable_self_signed_jwt
-              if credentials.is_a?(String) || credentials.is_a?(Hash)
+              if credentials.is_a?(::String) || credentials.is_a?(::Hash)
                 credentials = Credentials.new credentials, scope: @config.scope
               end
               @quota_project_id = @config.quota_project
