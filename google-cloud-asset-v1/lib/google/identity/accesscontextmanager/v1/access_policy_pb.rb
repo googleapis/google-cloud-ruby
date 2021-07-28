19a817877caf18f705245645842bb9473a19591d
yoshi-automation <yoshi-automation@google.com>
2021-07-28 03:50:38 -0700

    chore: publish Access Context Manager API v1
    PiperOrigin-RevId: 387216202
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Tue Jul 27 15:58:26 2021 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: 0b261def1cb4c61a9ddbb8bf14b103ce6add1bce
    Source-Link: https://github.com/googleapis/googleapis/commit/0b261def1cb4c61a9ddbb8bf14b103ce6add1bce

--- google-cloud-asset-v1/lib/google/identity/accesscontextmanager/v1/access_policy_pb.rb
+++ google-cloud-asset-v1/lib/google/identity/accesscontextmanager/v1/access_policy_pb.rb
@@ -3,6 +3,7 @@
 
 require 'google/protobuf'
 
+require 'google/api/resource_pb'
 require 'google/protobuf/timestamp_pb'
 require 'google/api/annotations_pb'
 Google::Protobuf::DescriptorPool.generated_pool.build do
