e0d37f9a4e46a683649644ae478118a4d0580562
yoshi-automation <yoshi-automation@google.com>
2021-08-10 10:52:56 +0000

    [CHANGE ME] Re-generated google-cloud-dataqna-v1alpha to pick up changes in the API or client library generator.

--- google-cloud-dataqna-v1alpha/lib/google/cloud/dataqna/v1alpha/auto_suggestion_service/client.rb
+++ google-cloud-dataqna-v1alpha/lib/google/cloud/dataqna/v1alpha/auto_suggestion_service/client.rb
@@ -106,13 +106,12 @@ module Google
             # See {::Google::Cloud::DataQnA::V1alpha::AutoSuggestionService::Client::Configuration}
             # for a description of the configuration fields.
             #
-            # ## Example
+            # @example
             #
-            # To modify the configuration for all AutoSuggestionService clients:
-            #
-            #     ::Google::Cloud::DataQnA::V1alpha::AutoSuggestionService::Client.configure do |config|
-            #       config.timeout = 10.0
-            #     end
+            #   # Modify the configuration for all AutoSuggestionService clients
+            #   ::Google::Cloud::DataQnA::V1alpha::AutoSuggestionService::Client.configure do |config|
+            #     config.timeout = 10.0
+            #   end
             #
             # @yield [config] Configure the Client client.
             # @yieldparam config [Client::Configuration]
@@ -161,19 +160,15 @@ module Google
             ##
             # Create a new AutoSuggestionService client object.
             #
-            # ## Examples
+            # @example
             #
-            # To create a new AutoSuggestionService client with the default
-            # configuration:
+            #   # Create a client using the default configuration
+            #   client = ::Google::Cloud::DataQnA::V1alpha::AutoSuggestionService::Client.new
             #
-            #     client = ::Google::Cloud::DataQnA::V1alpha::AutoSuggestionService::Client.new
-            #
-            # To create a new AutoSuggestionService client with a custom
-            # configuration:
-            #
-            #     client = ::Google::Cloud::DataQnA::V1alpha::AutoSuggestionService::Client.new do |config|
-            #       config.timeout = 10.0
-            #     end
+            #   # Create a client using a custom configuration
+            #   client = ::Google::Cloud::DataQnA::V1alpha::AutoSuggestionService::Client.new do |config|
+            #     config.timeout = 10.0
+            #   end
             #
             # @yield [config] Configure the AutoSuggestionService client.
             # @yieldparam config [Client::Configuration]
@@ -193,10 +188,9 @@ module Google
 
               # Create credentials
               credentials = @config.credentials
-              # Use self-signed JWT if the scope and endpoint are unchanged from default,
+              # Use self-signed JWT if the endpoint is unchanged from default,
               # but only if the default endpoint does not have a region prefix.
-              enable_self_signed_jwt = @config.scope == Client.configure.scope &&
-                                       @config.endpoint == Client.configure.endpoint &&
+              enable_self_signed_jwt = @config.endpoint == Client.configure.endpoint &&
                                        !@config.endpoint.split(".").first.include?("-")
               credentials ||= Credentials.default scope: @config.scope,
                                                   enable_self_signed_jwt: enable_self_signed_jwt
@@ -289,7 +283,9 @@ module Google
               options.apply_defaults timeout:      @config.rpcs.suggest_queries.timeout,
                                      metadata:     metadata,
                                      retry_policy: @config.rpcs.suggest_queries.retry_policy
-              options.apply_defaults metadata:     @config.metadata,
+
+              options.apply_defaults timeout:      @config.timeout,
+                                     metadata:     @config.metadata,
                                      retry_policy: @config.retry_policy
 
               @auto_suggestion_service_stub.call_rpc :suggest_queries, request, options: options do |response, operation|
@@ -313,22 +309,21 @@ module Google
             # Configuration can be applied globally to all clients, or to a single client
             # on construction.
             #
-            # # Examples
+            # @example
             #
-            # To modify the global config, setting the timeout for suggest_queries
-            # to 20 seconds, and all remaining timeouts to 10 seconds:
+            #   # Modify the global config, setting the timeout for
+            #   # suggest_queries to 20 seconds,
+            #   # and all remaining timeouts to 10 seconds.
+            #   ::Google::Cloud::DataQnA::V1alpha::AutoSuggestionService::Client.configure do |config|
+            #     config.timeout = 10.0
+            #     config.rpcs.suggest_queries.timeout = 20.0
+            #   end
             #
-            #     ::Google::Cloud::DataQnA::V1alpha::AutoSuggestionService::Client.configure do |config|
-            #       config.timeout = 10.0
-            #       config.rpcs.suggest_queries.timeout = 20.0
-            #     end
-            #
-            # To apply the above configuration only to a new client:
-            #
-            #     client = ::Google::Cloud::DataQnA::V1alpha::AutoSuggestionService::Client.new do |config|
-            #       config.timeout = 10.0
-            #       config.rpcs.suggest_queries.timeout = 20.0
-            #     end
+            #   # Apply the above configuration only to a new client.
+            #   client = ::Google::Cloud::DataQnA::V1alpha::AutoSuggestionService::Client.new do |config|
+            #     config.timeout = 10.0
+            #     config.rpcs.suggest_queries.timeout = 20.0
+            #   end
             #
             # @!attribute [rw] endpoint
             #   The hostname or hostname:port of the service endpoint.
