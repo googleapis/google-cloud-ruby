cd773d8ff594bcfaabf269b8ec7a90e314a50231
yoshi-automation <yoshi-automation@google.com>
2021-08-10 10:57:37 +0000

    [CHANGE ME] Re-generated google-cloud-gaming-v1 to pick up changes in the API or client library generator.

--- google-cloud-gaming-v1/lib/google/cloud/gaming/v1/game_server_configs_service/client.rb
+++ google-cloud-gaming-v1/lib/google/cloud/gaming/v1/game_server_configs_service/client.rb
@@ -41,13 +41,12 @@ module Google
             # See {::Google::Cloud::Gaming::V1::GameServerConfigsService::Client::Configuration}
             # for a description of the configuration fields.
             #
-            # ## Example
+            # @example
             #
-            # To modify the configuration for all GameServerConfigsService clients:
-            #
-            #     ::Google::Cloud::Gaming::V1::GameServerConfigsService::Client.configure do |config|
-            #       config.timeout = 10.0
-            #     end
+            #   # Modify the configuration for all GameServerConfigsService clients
+            #   ::Google::Cloud::Gaming::V1::GameServerConfigsService::Client.configure do |config|
+            #     config.timeout = 10.0
+            #   end
             #
             # @yield [config] Configure the Client client.
             # @yieldparam config [Client::Configuration]
@@ -108,19 +107,15 @@ module Google
             ##
             # Create a new GameServerConfigsService client object.
             #
-            # ## Examples
+            # @example
             #
-            # To create a new GameServerConfigsService client with the default
-            # configuration:
+            #   # Create a client using the default configuration
+            #   client = ::Google::Cloud::Gaming::V1::GameServerConfigsService::Client.new
             #
-            #     client = ::Google::Cloud::Gaming::V1::GameServerConfigsService::Client.new
-            #
-            # To create a new GameServerConfigsService client with a custom
-            # configuration:
-            #
-            #     client = ::Google::Cloud::Gaming::V1::GameServerConfigsService::Client.new do |config|
-            #       config.timeout = 10.0
-            #     end
+            #   # Create a client using a custom configuration
+            #   client = ::Google::Cloud::Gaming::V1::GameServerConfigsService::Client.new do |config|
+            #     config.timeout = 10.0
+            #   end
             #
             # @yield [config] Configure the GameServerConfigsService client.
             # @yieldparam config [Client::Configuration]
@@ -140,10 +135,9 @@ module Google
 
               # Create credentials
               credentials = @config.credentials
-              # Use self-signed JWT if the scope and endpoint are unchanged from default,
+              # Use self-signed JWT if the endpoint is unchanged from default,
               # but only if the default endpoint does not have a region prefix.
-              enable_self_signed_jwt = @config.scope == Client.configure.scope &&
-                                       @config.endpoint == Client.configure.endpoint &&
+              enable_self_signed_jwt = @config.endpoint == Client.configure.endpoint &&
                                        !@config.endpoint.split(".").first.include?("-")
               credentials ||= Credentials.default scope: @config.scope,
                                                   enable_self_signed_jwt: enable_self_signed_jwt
@@ -247,7 +241,9 @@ module Google
               options.apply_defaults timeout:      @config.rpcs.list_game_server_configs.timeout,
                                      metadata:     metadata,
                                      retry_policy: @config.rpcs.list_game_server_configs.retry_policy
-              options.apply_defaults metadata:     @config.metadata,
+
+              options.apply_defaults timeout:      @config.timeout,
+                                     metadata:     @config.metadata,
                                      retry_policy: @config.retry_policy
 
               @game_server_configs_service_stub.call_rpc :list_game_server_configs, request, options: options do |response, operation|
@@ -316,7 +312,9 @@ module Google
               options.apply_defaults timeout:      @config.rpcs.get_game_server_config.timeout,
                                      metadata:     metadata,
                                      retry_policy: @config.rpcs.get_game_server_config.retry_policy
-              options.apply_defaults metadata:     @config.metadata,
+
+              options.apply_defaults timeout:      @config.timeout,
+                                     metadata:     @config.metadata,
                                      retry_policy: @config.retry_policy
 
               @game_server_configs_service_stub.call_rpc :get_game_server_config, request, options: options do |response, operation|
@@ -390,7 +388,9 @@ module Google
               options.apply_defaults timeout:      @config.rpcs.create_game_server_config.timeout,
                                      metadata:     metadata,
                                      retry_policy: @config.rpcs.create_game_server_config.retry_policy
-              options.apply_defaults metadata:     @config.metadata,
+
+              options.apply_defaults timeout:      @config.timeout,
+                                     metadata:     @config.metadata,
                                      retry_policy: @config.retry_policy
 
               @game_server_configs_service_stub.call_rpc :create_game_server_config, request, options: options do |response, operation|
@@ -460,7 +460,9 @@ module Google
               options.apply_defaults timeout:      @config.rpcs.delete_game_server_config.timeout,
                                      metadata:     metadata,
                                      retry_policy: @config.rpcs.delete_game_server_config.retry_policy
-              options.apply_defaults metadata:     @config.metadata,
+
+              options.apply_defaults timeout:      @config.timeout,
+                                     metadata:     @config.metadata,
                                      retry_policy: @config.retry_policy
 
               @game_server_configs_service_stub.call_rpc :delete_game_server_config, request, options: options do |response, operation|
@@ -485,22 +487,21 @@ module Google
             # Configuration can be applied globally to all clients, or to a single client
             # on construction.
             #
-            # # Examples
+            # @example
             #
-            # To modify the global config, setting the timeout for list_game_server_configs
-            # to 20 seconds, and all remaining timeouts to 10 seconds:
+            #   # Modify the global config, setting the timeout for
+            #   # list_game_server_configs to 20 seconds,
+            #   # and all remaining timeouts to 10 seconds.
+            #   ::Google::Cloud::Gaming::V1::GameServerConfigsService::Client.configure do |config|
+            #     config.timeout = 10.0
+            #     config.rpcs.list_game_server_configs.timeout = 20.0
+            #   end
             #
-            #     ::Google::Cloud::Gaming::V1::GameServerConfigsService::Client.configure do |config|
-            #       config.timeout = 10.0
-            #       config.rpcs.list_game_server_configs.timeout = 20.0
-            #     end
-            #
-            # To apply the above configuration only to a new client:
-            #
-            #     client = ::Google::Cloud::Gaming::V1::GameServerConfigsService::Client.new do |config|
-            #       config.timeout = 10.0
-            #       config.rpcs.list_game_server_configs.timeout = 20.0
-            #     end
+            #   # Apply the above configuration only to a new client.
+            #   client = ::Google::Cloud::Gaming::V1::GameServerConfigsService::Client.new do |config|
+            #     config.timeout = 10.0
+            #     config.rpcs.list_game_server_configs.timeout = 20.0
+            #   end
             #
             # @!attribute [rw] endpoint
             #   The hostname or hostname:port of the service endpoint.
