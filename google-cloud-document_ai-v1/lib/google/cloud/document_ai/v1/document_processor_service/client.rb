3f6c1be26119758a1ae36f0a0e9c51afdeb053e2
yoshi-automation <yoshi-automation@google.com>
2021-08-10 10:55:01 +0000

    [CHANGE ME] Re-generated google-cloud-document_ai-v1 to pick up changes in the API or client library generator.

--- google-cloud-document_ai-v1/lib/google/cloud/document_ai/v1/document_processor_service/client.rb
+++ google-cloud-document_ai-v1/lib/google/cloud/document_ai/v1/document_processor_service/client.rb
@@ -44,13 +44,12 @@ module Google
             # See {::Google::Cloud::DocumentAI::V1::DocumentProcessorService::Client::Configuration}
             # for a description of the configuration fields.
             #
-            # ## Example
+            # @example
             #
-            # To modify the configuration for all DocumentProcessorService clients:
-            #
-            #     ::Google::Cloud::DocumentAI::V1::DocumentProcessorService::Client.configure do |config|
-            #       config.timeout = 10.0
-            #     end
+            #   # Modify the configuration for all DocumentProcessorService clients
+            #   ::Google::Cloud::DocumentAI::V1::DocumentProcessorService::Client.configure do |config|
+            #     config.timeout = 10.0
+            #   end
             #
             # @yield [config] Configure the Client client.
             # @yieldparam config [Client::Configuration]
@@ -112,19 +111,15 @@ module Google
             ##
             # Create a new DocumentProcessorService client object.
             #
-            # ## Examples
+            # @example
             #
-            # To create a new DocumentProcessorService client with the default
-            # configuration:
+            #   # Create a client using the default configuration
+            #   client = ::Google::Cloud::DocumentAI::V1::DocumentProcessorService::Client.new
             #
-            #     client = ::Google::Cloud::DocumentAI::V1::DocumentProcessorService::Client.new
-            #
-            # To create a new DocumentProcessorService client with a custom
-            # configuration:
-            #
-            #     client = ::Google::Cloud::DocumentAI::V1::DocumentProcessorService::Client.new do |config|
-            #       config.timeout = 10.0
-            #     end
+            #   # Create a client using a custom configuration
+            #   client = ::Google::Cloud::DocumentAI::V1::DocumentProcessorService::Client.new do |config|
+            #     config.timeout = 10.0
+            #   end
             #
             # @yield [config] Configure the DocumentProcessorService client.
             # @yieldparam config [Client::Configuration]
@@ -144,10 +139,9 @@ module Google
 
               # Create credentials
               credentials = @config.credentials
-              # Use self-signed JWT if the scope and endpoint are unchanged from default,
+              # Use self-signed JWT if the endpoint is unchanged from default,
               # but only if the default endpoint does not have a region prefix.
-              enable_self_signed_jwt = @config.scope == Client.configure.scope &&
-                                       @config.endpoint == Client.configure.endpoint &&
+              enable_self_signed_jwt = @config.endpoint == Client.configure.endpoint &&
                                        !@config.endpoint.split(".").first.include?("-")
               credentials ||= Credentials.default scope: @config.scope,
                                                   enable_self_signed_jwt: enable_self_signed_jwt
@@ -242,7 +236,9 @@ module Google
               options.apply_defaults timeout:      @config.rpcs.process_document.timeout,
                                      metadata:     metadata,
                                      retry_policy: @config.rpcs.process_document.retry_policy
-              options.apply_defaults metadata:     @config.metadata,
+
+              options.apply_defaults timeout:      @config.timeout,
+                                     metadata:     @config.metadata,
                                      retry_policy: @config.retry_policy
 
               @document_processor_service_stub.call_rpc :process_document, request, options: options do |response, operation|
@@ -316,7 +312,9 @@ module Google
               options.apply_defaults timeout:      @config.rpcs.batch_process_documents.timeout,
                                      metadata:     metadata,
                                      retry_policy: @config.rpcs.batch_process_documents.retry_policy
-              options.apply_defaults metadata:     @config.metadata,
+
+              options.apply_defaults timeout:      @config.timeout,
+                                     metadata:     @config.metadata,
                                      retry_policy: @config.retry_policy
 
               @document_processor_service_stub.call_rpc :batch_process_documents, request, options: options do |response, operation|
@@ -391,7 +389,9 @@ module Google
               options.apply_defaults timeout:      @config.rpcs.review_document.timeout,
                                      metadata:     metadata,
                                      retry_policy: @config.rpcs.review_document.retry_policy
-              options.apply_defaults metadata:     @config.metadata,
+
+              options.apply_defaults timeout:      @config.timeout,
+                                     metadata:     @config.metadata,
                                      retry_policy: @config.retry_policy
 
               @document_processor_service_stub.call_rpc :review_document, request, options: options do |response, operation|
@@ -416,22 +416,21 @@ module Google
             # Configuration can be applied globally to all clients, or to a single client
             # on construction.
             #
-            # # Examples
+            # @example
             #
-            # To modify the global config, setting the timeout for process_document
-            # to 20 seconds, and all remaining timeouts to 10 seconds:
+            #   # Modify the global config, setting the timeout for
+            #   # process_document to 20 seconds,
+            #   # and all remaining timeouts to 10 seconds.
+            #   ::Google::Cloud::DocumentAI::V1::DocumentProcessorService::Client.configure do |config|
+            #     config.timeout = 10.0
+            #     config.rpcs.process_document.timeout = 20.0
+            #   end
             #
-            #     ::Google::Cloud::DocumentAI::V1::DocumentProcessorService::Client.configure do |config|
-            #       config.timeout = 10.0
-            #       config.rpcs.process_document.timeout = 20.0
-            #     end
-            #
-            # To apply the above configuration only to a new client:
-            #
-            #     client = ::Google::Cloud::DocumentAI::V1::DocumentProcessorService::Client.new do |config|
-            #       config.timeout = 10.0
-            #       config.rpcs.process_document.timeout = 20.0
-            #     end
+            #   # Apply the above configuration only to a new client.
+            #   client = ::Google::Cloud::DocumentAI::V1::DocumentProcessorService::Client.new do |config|
+            #     config.timeout = 10.0
+            #     config.rpcs.process_document.timeout = 20.0
+            #   end
             #
             # @!attribute [rw] endpoint
             #   The hostname or hostname:port of the service endpoint.
