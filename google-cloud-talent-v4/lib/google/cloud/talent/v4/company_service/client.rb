450b056700d565316fa5f90a88bf2f91cb98b0f5
yoshi-automation <yoshi-automation@google.com>
2021-08-10 11:16:01 +0000

    [CHANGE ME] Re-generated google-cloud-talent-v4 to pick up changes in the API or client library generator.

--- google-cloud-talent-v4/lib/google/cloud/talent/v4/company_service/client.rb
+++ google-cloud-talent-v4/lib/google/cloud/talent/v4/company_service/client.rb
@@ -41,13 +41,12 @@ module Google
             # See {::Google::Cloud::Talent::V4::CompanyService::Client::Configuration}
             # for a description of the configuration fields.
             #
-            # ## Example
+            # @example
             #
-            # To modify the configuration for all CompanyService clients:
-            #
-            #     ::Google::Cloud::Talent::V4::CompanyService::Client.configure do |config|
-            #       config.timeout = 10.0
-            #     end
+            #   # Modify the configuration for all CompanyService clients
+            #   ::Google::Cloud::Talent::V4::CompanyService::Client.configure do |config|
+            #     config.timeout = 10.0
+            #   end
             #
             # @yield [config] Configure the Client client.
             # @yieldparam config [Client::Configuration]
@@ -113,19 +112,15 @@ module Google
             ##
             # Create a new CompanyService client object.
             #
-            # ## Examples
+            # @example
             #
-            # To create a new CompanyService client with the default
-            # configuration:
+            #   # Create a client using the default configuration
+            #   client = ::Google::Cloud::Talent::V4::CompanyService::Client.new
             #
-            #     client = ::Google::Cloud::Talent::V4::CompanyService::Client.new
-            #
-            # To create a new CompanyService client with a custom
-            # configuration:
-            #
-            #     client = ::Google::Cloud::Talent::V4::CompanyService::Client.new do |config|
-            #       config.timeout = 10.0
-            #     end
+            #   # Create a client using a custom configuration
+            #   client = ::Google::Cloud::Talent::V4::CompanyService::Client.new do |config|
+            #     config.timeout = 10.0
+            #   end
             #
             # @yield [config] Configure the CompanyService client.
             # @yieldparam config [Client::Configuration]
@@ -145,10 +140,9 @@ module Google
 
               # Create credentials
               credentials = @config.credentials
-              # Use self-signed JWT if the scope and endpoint are unchanged from default,
+              # Use self-signed JWT if the endpoint is unchanged from default,
               # but only if the default endpoint does not have a region prefix.
-              enable_self_signed_jwt = @config.scope == Client.configure.scope &&
-                                       @config.endpoint == Client.configure.endpoint &&
+              enable_self_signed_jwt = @config.endpoint == Client.configure.endpoint &&
                                        !@config.endpoint.split(".").first.include?("-")
               credentials ||= Credentials.default scope: @config.scope,
                                                   enable_self_signed_jwt: enable_self_signed_jwt
@@ -229,7 +223,9 @@ module Google
               options.apply_defaults timeout:      @config.rpcs.create_company.timeout,
                                      metadata:     metadata,
                                      retry_policy: @config.rpcs.create_company.retry_policy
-              options.apply_defaults metadata:     @config.metadata,
+
+              options.apply_defaults timeout:      @config.timeout,
+                                     metadata:     @config.metadata,
                                      retry_policy: @config.retry_policy
 
               @company_service_stub.call_rpc :create_company, request, options: options do |response, operation|
@@ -299,7 +295,9 @@ module Google
               options.apply_defaults timeout:      @config.rpcs.get_company.timeout,
                                      metadata:     metadata,
                                      retry_policy: @config.rpcs.get_company.retry_policy
-              options.apply_defaults metadata:     @config.metadata,
+
+              options.apply_defaults timeout:      @config.timeout,
+                                     metadata:     @config.metadata,
                                      retry_policy: @config.retry_policy
 
               @company_service_stub.call_rpc :get_company, request, options: options do |response, operation|
@@ -373,7 +371,9 @@ module Google
               options.apply_defaults timeout:      @config.rpcs.update_company.timeout,
                                      metadata:     metadata,
                                      retry_policy: @config.rpcs.update_company.retry_policy
-              options.apply_defaults metadata:     @config.metadata,
+
+              options.apply_defaults timeout:      @config.timeout,
+                                     metadata:     @config.metadata,
                                      retry_policy: @config.retry_policy
 
               @company_service_stub.call_rpc :update_company, request, options: options do |response, operation|
@@ -444,7 +444,9 @@ module Google
               options.apply_defaults timeout:      @config.rpcs.delete_company.timeout,
                                      metadata:     metadata,
                                      retry_policy: @config.rpcs.delete_company.retry_policy
-              options.apply_defaults metadata:     @config.metadata,
+
+              options.apply_defaults timeout:      @config.timeout,
+                                     metadata:     @config.metadata,
                                      retry_policy: @config.retry_policy
 
               @company_service_stub.call_rpc :delete_company, request, options: options do |response, operation|
@@ -525,7 +527,9 @@ module Google
               options.apply_defaults timeout:      @config.rpcs.list_companies.timeout,
                                      metadata:     metadata,
                                      retry_policy: @config.rpcs.list_companies.retry_policy
-              options.apply_defaults metadata:     @config.metadata,
+
+              options.apply_defaults timeout:      @config.timeout,
+                                     metadata:     @config.metadata,
                                      retry_policy: @config.retry_policy
 
               @company_service_stub.call_rpc :list_companies, request, options: options do |response, operation|
@@ -550,22 +554,21 @@ module Google
             # Configuration can be applied globally to all clients, or to a single client
             # on construction.
             #
-            # # Examples
+            # @example
             #
-            # To modify the global config, setting the timeout for create_company
-            # to 20 seconds, and all remaining timeouts to 10 seconds:
+            #   # Modify the global config, setting the timeout for
+            #   # create_company to 20 seconds,
+            #   # and all remaining timeouts to 10 seconds.
+            #   ::Google::Cloud::Talent::V4::CompanyService::Client.configure do |config|
+            #     config.timeout = 10.0
+            #     config.rpcs.create_company.timeout = 20.0
+            #   end
             #
-            #     ::Google::Cloud::Talent::V4::CompanyService::Client.configure do |config|
-            #       config.timeout = 10.0
-            #       config.rpcs.create_company.timeout = 20.0
-            #     end
-            #
-            # To apply the above configuration only to a new client:
-            #
-            #     client = ::Google::Cloud::Talent::V4::CompanyService::Client.new do |config|
-            #       config.timeout = 10.0
-            #       config.rpcs.create_company.timeout = 20.0
-            #     end
+            #   # Apply the above configuration only to a new client.
+            #   client = ::Google::Cloud::Talent::V4::CompanyService::Client.new do |config|
+            #     config.timeout = 10.0
+            #     config.rpcs.create_company.timeout = 20.0
+            #   end
             #
             # @!attribute [rw] endpoint
             #   The hostname or hostname:port of the service endpoint.
