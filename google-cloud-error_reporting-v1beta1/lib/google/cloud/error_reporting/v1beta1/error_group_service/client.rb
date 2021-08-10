cae133295bfc80034b4857c1bf936baafffe3053
yoshi-automation <yoshi-automation@google.com>
2021-08-10 10:55:59 +0000

    [CHANGE ME] Re-generated google-cloud-error_reporting-v1beta1 to pick up changes in the API or client library generator.

--- google-cloud-error_reporting-v1beta1/lib/google/cloud/error_reporting/v1beta1/error_group_service/client.rb
+++ google-cloud-error_reporting-v1beta1/lib/google/cloud/error_reporting/v1beta1/error_group_service/client.rb
@@ -41,13 +41,12 @@ module Google
             # See {::Google::Cloud::ErrorReporting::V1beta1::ErrorGroupService::Client::Configuration}
             # for a description of the configuration fields.
             #
-            # ## Example
+            # @example
             #
-            # To modify the configuration for all ErrorGroupService clients:
-            #
-            #     ::Google::Cloud::ErrorReporting::V1beta1::ErrorGroupService::Client.configure do |config|
-            #       config.timeout = 10.0
-            #     end
+            #   # Modify the configuration for all ErrorGroupService clients
+            #   ::Google::Cloud::ErrorReporting::V1beta1::ErrorGroupService::Client.configure do |config|
+            #     config.timeout = 10.0
+            #   end
             #
             # @yield [config] Configure the Client client.
             # @yieldparam config [Client::Configuration]
@@ -99,19 +98,15 @@ module Google
             ##
             # Create a new ErrorGroupService client object.
             #
-            # ## Examples
+            # @example
             #
-            # To create a new ErrorGroupService client with the default
-            # configuration:
+            #   # Create a client using the default configuration
+            #   client = ::Google::Cloud::ErrorReporting::V1beta1::ErrorGroupService::Client.new
             #
-            #     client = ::Google::Cloud::ErrorReporting::V1beta1::ErrorGroupService::Client.new
-            #
-            # To create a new ErrorGroupService client with a custom
-            # configuration:
-            #
-            #     client = ::Google::Cloud::ErrorReporting::V1beta1::ErrorGroupService::Client.new do |config|
-            #       config.timeout = 10.0
-            #     end
+            #   # Create a client using a custom configuration
+            #   client = ::Google::Cloud::ErrorReporting::V1beta1::ErrorGroupService::Client.new do |config|
+            #     config.timeout = 10.0
+            #   end
             #
             # @yield [config] Configure the ErrorGroupService client.
             # @yieldparam config [Client::Configuration]
@@ -131,10 +126,9 @@ module Google
 
               # Create credentials
               credentials = @config.credentials
-              # Use self-signed JWT if the scope and endpoint are unchanged from default,
+              # Use self-signed JWT if the endpoint is unchanged from default,
               # but only if the default endpoint does not have a region prefix.
-              enable_self_signed_jwt = @config.scope == Client.configure.scope &&
-                                       @config.endpoint == Client.configure.endpoint &&
+              enable_self_signed_jwt = @config.endpoint == Client.configure.endpoint &&
                                        !@config.endpoint.split(".").first.include?("-")
               credentials ||= Credentials.default scope: @config.scope,
                                                   enable_self_signed_jwt: enable_self_signed_jwt
@@ -215,7 +209,9 @@ module Google
               options.apply_defaults timeout:      @config.rpcs.get_group.timeout,
                                      metadata:     metadata,
                                      retry_policy: @config.rpcs.get_group.retry_policy
-              options.apply_defaults metadata:     @config.metadata,
+
+              options.apply_defaults timeout:      @config.timeout,
+                                     metadata:     @config.metadata,
                                      retry_policy: @config.retry_policy
 
               @error_group_service_stub.call_rpc :get_group, request, options: options do |response, operation|
@@ -282,7 +278,9 @@ module Google
               options.apply_defaults timeout:      @config.rpcs.update_group.timeout,
                                      metadata:     metadata,
                                      retry_policy: @config.rpcs.update_group.retry_policy
-              options.apply_defaults metadata:     @config.metadata,
+
+              options.apply_defaults timeout:      @config.timeout,
+                                     metadata:     @config.metadata,
                                      retry_policy: @config.retry_policy
 
               @error_group_service_stub.call_rpc :update_group, request, options: options do |response, operation|
@@ -306,22 +304,21 @@ module Google
             # Configuration can be applied globally to all clients, or to a single client
             # on construction.
             #
-            # # Examples
+            # @example
             #
-            # To modify the global config, setting the timeout for get_group
-            # to 20 seconds, and all remaining timeouts to 10 seconds:
+            #   # Modify the global config, setting the timeout for
+            #   # get_group to 20 seconds,
+            #   # and all remaining timeouts to 10 seconds.
+            #   ::Google::Cloud::ErrorReporting::V1beta1::ErrorGroupService::Client.configure do |config|
+            #     config.timeout = 10.0
+            #     config.rpcs.get_group.timeout = 20.0
+            #   end
             #
-            #     ::Google::Cloud::ErrorReporting::V1beta1::ErrorGroupService::Client.configure do |config|
-            #       config.timeout = 10.0
-            #       config.rpcs.get_group.timeout = 20.0
-            #     end
-            #
-            # To apply the above configuration only to a new client:
-            #
-            #     client = ::Google::Cloud::ErrorReporting::V1beta1::ErrorGroupService::Client.new do |config|
-            #       config.timeout = 10.0
-            #       config.rpcs.get_group.timeout = 20.0
-            #     end
+            #   # Apply the above configuration only to a new client.
+            #   client = ::Google::Cloud::ErrorReporting::V1beta1::ErrorGroupService::Client.new do |config|
+            #     config.timeout = 10.0
+            #     config.rpcs.get_group.timeout = 20.0
+            #   end
             #
             # @!attribute [rw] endpoint
             #   The hostname or hostname:port of the service endpoint.
