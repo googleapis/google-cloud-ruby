2a6e5d36913c0354e6adc47facd62c05a8e89df8
yoshi-automation <yoshi-automation@google.com>
2021-08-10 10:51:54 +0000

    [CHANGE ME] Re-generated google-cloud-dataflow-v1beta3 to pick up changes in the API or client library generator.

--- google-cloud-dataflow-v1beta3/lib/google/cloud/dataflow/v1beta3/messages/client.rb
+++ google-cloud-dataflow-v1beta3/lib/google/cloud/dataflow/v1beta3/messages/client.rb
@@ -40,13 +40,12 @@ module Google
             # See {::Google::Cloud::Dataflow::V1beta3::Messages::Client::Configuration}
             # for a description of the configuration fields.
             #
-            # ## Example
+            # @example
             #
-            # To modify the configuration for all Messages clients:
-            #
-            #     ::Google::Cloud::Dataflow::V1beta3::Messages::Client.configure do |config|
-            #       config.timeout = 10.0
-            #     end
+            #   # Modify the configuration for all Messages clients
+            #   ::Google::Cloud::Dataflow::V1beta3::Messages::Client.configure do |config|
+            #     config.timeout = 10.0
+            #   end
             #
             # @yield [config] Configure the Client client.
             # @yieldparam config [Client::Configuration]
@@ -95,19 +94,15 @@ module Google
             ##
             # Create a new Messages client object.
             #
-            # ## Examples
+            # @example
             #
-            # To create a new Messages client with the default
-            # configuration:
+            #   # Create a client using the default configuration
+            #   client = ::Google::Cloud::Dataflow::V1beta3::Messages::Client.new
             #
-            #     client = ::Google::Cloud::Dataflow::V1beta3::Messages::Client.new
-            #
-            # To create a new Messages client with a custom
-            # configuration:
-            #
-            #     client = ::Google::Cloud::Dataflow::V1beta3::Messages::Client.new do |config|
-            #       config.timeout = 10.0
-            #     end
+            #   # Create a client using a custom configuration
+            #   client = ::Google::Cloud::Dataflow::V1beta3::Messages::Client.new do |config|
+            #     config.timeout = 10.0
+            #   end
             #
             # @yield [config] Configure the Messages client.
             # @yieldparam config [Client::Configuration]
@@ -127,10 +122,9 @@ module Google
 
               # Create credentials
               credentials = @config.credentials
-              # Use self-signed JWT if the scope and endpoint are unchanged from default,
+              # Use self-signed JWT if the endpoint is unchanged from default,
               # but only if the default endpoint does not have a region prefix.
-              enable_self_signed_jwt = @config.scope == Client.configure.scope &&
-                                       @config.endpoint == Client.configure.endpoint &&
+              enable_self_signed_jwt = @config.endpoint == Client.configure.endpoint &&
                                        !@config.endpoint.split(".").first.include?("-")
               credentials ||= Credentials.default scope: @config.scope,
                                                   enable_self_signed_jwt: enable_self_signed_jwt
@@ -228,7 +222,9 @@ module Google
               options.apply_defaults timeout:      @config.rpcs.list_job_messages.timeout,
                                      metadata:     metadata,
                                      retry_policy: @config.rpcs.list_job_messages.retry_policy
-              options.apply_defaults metadata:     @config.metadata,
+
+              options.apply_defaults timeout:      @config.timeout,
+                                     metadata:     @config.metadata,
                                      retry_policy: @config.retry_policy
 
               @messages_stub.call_rpc :list_job_messages, request, options: options do |response, operation|
@@ -253,22 +249,21 @@ module Google
             # Configuration can be applied globally to all clients, or to a single client
             # on construction.
             #
-            # # Examples
+            # @example
             #
-            # To modify the global config, setting the timeout for list_job_messages
-            # to 20 seconds, and all remaining timeouts to 10 seconds:
+            #   # Modify the global config, setting the timeout for
+            #   # list_job_messages to 20 seconds,
+            #   # and all remaining timeouts to 10 seconds.
+            #   ::Google::Cloud::Dataflow::V1beta3::Messages::Client.configure do |config|
+            #     config.timeout = 10.0
+            #     config.rpcs.list_job_messages.timeout = 20.0
+            #   end
             #
-            #     ::Google::Cloud::Dataflow::V1beta3::Messages::Client.configure do |config|
-            #       config.timeout = 10.0
-            #       config.rpcs.list_job_messages.timeout = 20.0
-            #     end
-            #
-            # To apply the above configuration only to a new client:
-            #
-            #     client = ::Google::Cloud::Dataflow::V1beta3::Messages::Client.new do |config|
-            #       config.timeout = 10.0
-            #       config.rpcs.list_job_messages.timeout = 20.0
-            #     end
+            #   # Apply the above configuration only to a new client.
+            #   client = ::Google::Cloud::Dataflow::V1beta3::Messages::Client.new do |config|
+            #     config.timeout = 10.0
+            #     config.rpcs.list_job_messages.timeout = 20.0
+            #   end
             #
             # @!attribute [rw] endpoint
             #   The hostname or hostname:port of the service endpoint.
