0e196dc3fbb61a214e425911e727980834852ddb
yoshi-automation <yoshi-automation@google.com>
2021-08-10 11:10:13 +0000

    [CHANGE ME] Re-generated google-cloud-scheduler-v1beta1 to pick up changes in the API or client library generator.

--- google-cloud-scheduler-v1beta1/lib/google/cloud/scheduler/v1beta1/cloud_scheduler/client.rb
+++ google-cloud-scheduler-v1beta1/lib/google/cloud/scheduler/v1beta1/cloud_scheduler/client.rb
@@ -42,13 +42,12 @@ module Google
             # See {::Google::Cloud::Scheduler::V1beta1::CloudScheduler::Client::Configuration}
             # for a description of the configuration fields.
             #
-            # ## Example
+            # @example
             #
-            # To modify the configuration for all CloudScheduler clients:
-            #
-            #     ::Google::Cloud::Scheduler::V1beta1::CloudScheduler::Client.configure do |config|
-            #       config.timeout = 10.0
-            #     end
+            #   # Modify the configuration for all CloudScheduler clients
+            #   ::Google::Cloud::Scheduler::V1beta1::CloudScheduler::Client.configure do |config|
+            #     config.timeout = 10.0
+            #   end
             #
             # @yield [config] Configure the Client client.
             # @yieldparam config [Client::Configuration]
@@ -126,19 +125,15 @@ module Google
             ##
             # Create a new CloudScheduler client object.
             #
-            # ## Examples
+            # @example
             #
-            # To create a new CloudScheduler client with the default
-            # configuration:
+            #   # Create a client using the default configuration
+            #   client = ::Google::Cloud::Scheduler::V1beta1::CloudScheduler::Client.new
             #
-            #     client = ::Google::Cloud::Scheduler::V1beta1::CloudScheduler::Client.new
-            #
-            # To create a new CloudScheduler client with a custom
-            # configuration:
-            #
-            #     client = ::Google::Cloud::Scheduler::V1beta1::CloudScheduler::Client.new do |config|
-            #       config.timeout = 10.0
-            #     end
+            #   # Create a client using a custom configuration
+            #   client = ::Google::Cloud::Scheduler::V1beta1::CloudScheduler::Client.new do |config|
+            #     config.timeout = 10.0
+            #   end
             #
             # @yield [config] Configure the CloudScheduler client.
             # @yieldparam config [Client::Configuration]
@@ -158,10 +153,9 @@ module Google
 
               # Create credentials
               credentials = @config.credentials
-              # Use self-signed JWT if the scope and endpoint are unchanged from default,
+              # Use self-signed JWT if the endpoint is unchanged from default,
               # but only if the default endpoint does not have a region prefix.
-              enable_self_signed_jwt = @config.scope == Client.configure.scope &&
-                                       @config.endpoint == Client.configure.endpoint &&
+              enable_self_signed_jwt = @config.endpoint == Client.configure.endpoint &&
                                        !@config.endpoint.split(".").first.include?("-")
               credentials ||= Credentials.default scope: @config.scope,
                                                   enable_self_signed_jwt: enable_self_signed_jwt
@@ -253,7 +247,9 @@ module Google
               options.apply_defaults timeout:      @config.rpcs.list_jobs.timeout,
                                      metadata:     metadata,
                                      retry_policy: @config.rpcs.list_jobs.retry_policy
-              options.apply_defaults metadata:     @config.metadata,
+
+              options.apply_defaults timeout:      @config.timeout,
+                                     metadata:     @config.metadata,
                                      retry_policy: @config.retry_policy
 
               @cloud_scheduler_stub.call_rpc :list_jobs, request, options: options do |response, operation|
@@ -321,7 +317,9 @@ module Google
               options.apply_defaults timeout:      @config.rpcs.get_job.timeout,
                                      metadata:     metadata,
                                      retry_policy: @config.rpcs.get_job.retry_policy
-              options.apply_defaults metadata:     @config.metadata,
+
+              options.apply_defaults timeout:      @config.timeout,
+                                     metadata:     @config.metadata,
                                      retry_policy: @config.retry_policy
 
               @cloud_scheduler_stub.call_rpc :get_job, request, options: options do |response, operation|
@@ -394,7 +392,9 @@ module Google
               options.apply_defaults timeout:      @config.rpcs.create_job.timeout,
                                      metadata:     metadata,
                                      retry_policy: @config.rpcs.create_job.retry_policy
-              options.apply_defaults metadata:     @config.metadata,
+
+              options.apply_defaults timeout:      @config.timeout,
+                                     metadata:     @config.metadata,
                                      retry_policy: @config.retry_policy
 
               @cloud_scheduler_stub.call_rpc :create_job, request, options: options do |response, operation|
@@ -473,7 +473,9 @@ module Google
               options.apply_defaults timeout:      @config.rpcs.update_job.timeout,
                                      metadata:     metadata,
                                      retry_policy: @config.rpcs.update_job.retry_policy
-              options.apply_defaults metadata:     @config.metadata,
+
+              options.apply_defaults timeout:      @config.timeout,
+                                     metadata:     @config.metadata,
                                      retry_policy: @config.retry_policy
 
               @cloud_scheduler_stub.call_rpc :update_job, request, options: options do |response, operation|
@@ -540,7 +542,9 @@ module Google
               options.apply_defaults timeout:      @config.rpcs.delete_job.timeout,
                                      metadata:     metadata,
                                      retry_policy: @config.rpcs.delete_job.retry_policy
-              options.apply_defaults metadata:     @config.metadata,
+
+              options.apply_defaults timeout:      @config.timeout,
+                                     metadata:     @config.metadata,
                                      retry_policy: @config.retry_policy
 
               @cloud_scheduler_stub.call_rpc :delete_job, request, options: options do |response, operation|
@@ -613,7 +617,9 @@ module Google
               options.apply_defaults timeout:      @config.rpcs.pause_job.timeout,
                                      metadata:     metadata,
                                      retry_policy: @config.rpcs.pause_job.retry_policy
-              options.apply_defaults metadata:     @config.metadata,
+
+              options.apply_defaults timeout:      @config.timeout,
+                                     metadata:     @config.metadata,
                                      retry_policy: @config.retry_policy
 
               @cloud_scheduler_stub.call_rpc :pause_job, request, options: options do |response, operation|
@@ -685,7 +691,9 @@ module Google
               options.apply_defaults timeout:      @config.rpcs.resume_job.timeout,
                                      metadata:     metadata,
                                      retry_policy: @config.rpcs.resume_job.retry_policy
-              options.apply_defaults metadata:     @config.metadata,
+
+              options.apply_defaults timeout:      @config.timeout,
+                                     metadata:     @config.metadata,
                                      retry_policy: @config.retry_policy
 
               @cloud_scheduler_stub.call_rpc :resume_job, request, options: options do |response, operation|
@@ -755,7 +763,9 @@ module Google
               options.apply_defaults timeout:      @config.rpcs.run_job.timeout,
                                      metadata:     metadata,
                                      retry_policy: @config.rpcs.run_job.retry_policy
-              options.apply_defaults metadata:     @config.metadata,
+
+              options.apply_defaults timeout:      @config.timeout,
+                                     metadata:     @config.metadata,
                                      retry_policy: @config.retry_policy
 
               @cloud_scheduler_stub.call_rpc :run_job, request, options: options do |response, operation|
@@ -779,22 +789,21 @@ module Google
             # Configuration can be applied globally to all clients, or to a single client
             # on construction.
             #
-            # # Examples
+            # @example
             #
-            # To modify the global config, setting the timeout for list_jobs
-            # to 20 seconds, and all remaining timeouts to 10 seconds:
+            #   # Modify the global config, setting the timeout for
+            #   # list_jobs to 20 seconds,
+            #   # and all remaining timeouts to 10 seconds.
+            #   ::Google::Cloud::Scheduler::V1beta1::CloudScheduler::Client.configure do |config|
+            #     config.timeout = 10.0
+            #     config.rpcs.list_jobs.timeout = 20.0
+            #   end
             #
-            #     ::Google::Cloud::Scheduler::V1beta1::CloudScheduler::Client.configure do |config|
-            #       config.timeout = 10.0
-            #       config.rpcs.list_jobs.timeout = 20.0
-            #     end
-            #
-            # To apply the above configuration only to a new client:
-            #
-            #     client = ::Google::Cloud::Scheduler::V1beta1::CloudScheduler::Client.new do |config|
-            #       config.timeout = 10.0
-            #       config.rpcs.list_jobs.timeout = 20.0
-            #     end
+            #   # Apply the above configuration only to a new client.
+            #   client = ::Google::Cloud::Scheduler::V1beta1::CloudScheduler::Client.new do |config|
+            #     config.timeout = 10.0
+            #     config.rpcs.list_jobs.timeout = 20.0
+            #   end
             #
             # @!attribute [rw] endpoint
             #   The hostname or hostname:port of the service endpoint.
