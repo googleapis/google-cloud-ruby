089909549d450b3ebf1814299a05a745bfe728a7
yoshi-automation <yoshi-automation@google.com>
2021-08-10 10:57:53 +0000

    [CHANGE ME] Re-generated google-cloud-gke_connect-gateway-v1beta1 to pick up changes in the API or client library generator.

--- google-cloud-gke_connect-gateway-v1beta1/lib/google/cloud/gke_connect/gateway/v1beta1/gateway_service/client.rb
+++ google-cloud-gke_connect-gateway-v1beta1/lib/google/cloud/gke_connect/gateway/v1beta1/gateway_service/client.rb
@@ -44,13 +44,12 @@ module Google
               # See {::Google::Cloud::GkeConnect::Gateway::V1beta1::GatewayService::Client::Configuration}
               # for a description of the configuration fields.
               #
-              # ## Example
+              # @example
               #
-              # To modify the configuration for all GatewayService clients:
-              #
-              #     ::Google::Cloud::GkeConnect::Gateway::V1beta1::GatewayService::Client.configure do |config|
-              #       config.timeout = 10.0
-              #     end
+              #   # Modify the configuration for all GatewayService clients
+              #   ::Google::Cloud::GkeConnect::Gateway::V1beta1::GatewayService::Client.configure do |config|
+              #     config.timeout = 10.0
+              #   end
               #
               # @yield [config] Configure the Client client.
               # @yieldparam config [Client::Configuration]
@@ -97,19 +96,15 @@ module Google
               ##
               # Create a new GatewayService client object.
               #
-              # ## Examples
+              # @example
               #
-              # To create a new GatewayService client with the default
-              # configuration:
+              #   # Create a client using the default configuration
+              #   client = ::Google::Cloud::GkeConnect::Gateway::V1beta1::GatewayService::Client.new
               #
-              #     client = ::Google::Cloud::GkeConnect::Gateway::V1beta1::GatewayService::Client.new
-              #
-              # To create a new GatewayService client with a custom
-              # configuration:
-              #
-              #     client = ::Google::Cloud::GkeConnect::Gateway::V1beta1::GatewayService::Client.new do |config|
-              #       config.timeout = 10.0
-              #     end
+              #   # Create a client using a custom configuration
+              #   client = ::Google::Cloud::GkeConnect::Gateway::V1beta1::GatewayService::Client.new do |config|
+              #     config.timeout = 10.0
+              #   end
               #
               # @yield [config] Configure the GatewayService client.
               # @yieldparam config [Client::Configuration]
@@ -129,10 +124,9 @@ module Google
 
                 # Create credentials
                 credentials = @config.credentials
-                # Use self-signed JWT if the scope and endpoint are unchanged from default,
+                # Use self-signed JWT if the endpoint is unchanged from default,
                 # but only if the default endpoint does not have a region prefix.
-                enable_self_signed_jwt = @config.scope == Client.configure.scope &&
-                                         @config.endpoint == Client.configure.endpoint &&
+                enable_self_signed_jwt = @config.endpoint == Client.configure.endpoint &&
                                          !@config.endpoint.split(".").first.include?("-")
                 credentials ||= Credentials.default scope: @config.scope,
                                                     enable_self_signed_jwt: enable_self_signed_jwt
@@ -207,7 +201,9 @@ module Google
                 options.apply_defaults timeout:      @config.rpcs.get_resource.timeout,
                                        metadata:     metadata,
                                        retry_policy: @config.rpcs.get_resource.retry_policy
-                options.apply_defaults metadata:     @config.metadata,
+
+                options.apply_defaults timeout:      @config.timeout,
+                                       metadata:     @config.metadata,
                                        retry_policy: @config.retry_policy
 
                 @gateway_service_stub.call_rpc :get_resource, request, options: options do |response, operation|
@@ -272,7 +268,9 @@ module Google
                 options.apply_defaults timeout:      @config.rpcs.post_resource.timeout,
                                        metadata:     metadata,
                                        retry_policy: @config.rpcs.post_resource.retry_policy
-                options.apply_defaults metadata:     @config.metadata,
+
+                options.apply_defaults timeout:      @config.timeout,
+                                       metadata:     @config.metadata,
                                        retry_policy: @config.retry_policy
 
                 @gateway_service_stub.call_rpc :post_resource, request, options: options do |response, operation|
@@ -337,7 +335,9 @@ module Google
                 options.apply_defaults timeout:      @config.rpcs.delete_resource.timeout,
                                        metadata:     metadata,
                                        retry_policy: @config.rpcs.delete_resource.retry_policy
-                options.apply_defaults metadata:     @config.metadata,
+
+                options.apply_defaults timeout:      @config.timeout,
+                                       metadata:     @config.metadata,
                                        retry_policy: @config.retry_policy
 
                 @gateway_service_stub.call_rpc :delete_resource, request, options: options do |response, operation|
@@ -402,7 +402,9 @@ module Google
                 options.apply_defaults timeout:      @config.rpcs.put_resource.timeout,
                                        metadata:     metadata,
                                        retry_policy: @config.rpcs.put_resource.retry_policy
-                options.apply_defaults metadata:     @config.metadata,
+
+                options.apply_defaults timeout:      @config.timeout,
+                                       metadata:     @config.metadata,
                                        retry_policy: @config.retry_policy
 
                 @gateway_service_stub.call_rpc :put_resource, request, options: options do |response, operation|
@@ -467,7 +469,9 @@ module Google
                 options.apply_defaults timeout:      @config.rpcs.patch_resource.timeout,
                                        metadata:     metadata,
                                        retry_policy: @config.rpcs.patch_resource.retry_policy
-                options.apply_defaults metadata:     @config.metadata,
+
+                options.apply_defaults timeout:      @config.timeout,
+                                       metadata:     @config.metadata,
                                        retry_policy: @config.retry_policy
 
                 @gateway_service_stub.call_rpc :patch_resource, request, options: options do |response, operation|
@@ -491,22 +495,21 @@ module Google
               # Configuration can be applied globally to all clients, or to a single client
               # on construction.
               #
-              # # Examples
+              # @example
               #
-              # To modify the global config, setting the timeout for get_resource
-              # to 20 seconds, and all remaining timeouts to 10 seconds:
+              #   # Modify the global config, setting the timeout for
+              #   # get_resource to 20 seconds,
+              #   # and all remaining timeouts to 10 seconds.
+              #   ::Google::Cloud::GkeConnect::Gateway::V1beta1::GatewayService::Client.configure do |config|
+              #     config.timeout = 10.0
+              #     config.rpcs.get_resource.timeout = 20.0
+              #   end
               #
-              #     ::Google::Cloud::GkeConnect::Gateway::V1beta1::GatewayService::Client.configure do |config|
-              #       config.timeout = 10.0
-              #       config.rpcs.get_resource.timeout = 20.0
-              #     end
-              #
-              # To apply the above configuration only to a new client:
-              #
-              #     client = ::Google::Cloud::GkeConnect::Gateway::V1beta1::GatewayService::Client.new do |config|
-              #       config.timeout = 10.0
-              #       config.rpcs.get_resource.timeout = 20.0
-              #     end
+              #   # Apply the above configuration only to a new client.
+              #   client = ::Google::Cloud::GkeConnect::Gateway::V1beta1::GatewayService::Client.new do |config|
+              #     config.timeout = 10.0
+              #     config.rpcs.get_resource.timeout = 20.0
+              #   end
               #
               # @!attribute [rw] endpoint
               #   The hostname or hostname:port of the service endpoint.
