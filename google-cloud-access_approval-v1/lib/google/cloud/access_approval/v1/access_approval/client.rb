507ab6731812650d91c83e2ee9a68454f8950202
yoshi-automation <yoshi-automation@google.com>
2021-08-10 10:38:12 +0000

    [CHANGE ME] Re-generated google-cloud-access_approval-v1 to pick up changes in the API or client library generator.

--- google-cloud-access_approval-v1/lib/google/cloud/access_approval/v1/access_approval/client.rb
+++ google-cloud-access_approval-v1/lib/google/cloud/access_approval/v1/access_approval/client.rb
@@ -71,13 +71,12 @@ module Google
             # See {::Google::Cloud::AccessApproval::V1::AccessApproval::Client::Configuration}
             # for a description of the configuration fields.
             #
-            # ## Example
+            # @example
             #
-            # To modify the configuration for all AccessApproval clients:
-            #
-            #     ::Google::Cloud::AccessApproval::V1::AccessApproval::Client.configure do |config|
-            #       config.timeout = 10.0
-            #     end
+            #   # Modify the configuration for all AccessApproval clients
+            #   ::Google::Cloud::AccessApproval::V1::AccessApproval::Client.configure do |config|
+            #     config.timeout = 10.0
+            #   end
             #
             # @yield [config] Configure the Client client.
             # @yieldparam config [Client::Configuration]
@@ -147,19 +146,15 @@ module Google
             ##
             # Create a new AccessApproval client object.
             #
-            # ## Examples
+            # @example
             #
-            # To create a new AccessApproval client with the default
-            # configuration:
+            #   # Create a client using the default configuration
+            #   client = ::Google::Cloud::AccessApproval::V1::AccessApproval::Client.new
             #
-            #     client = ::Google::Cloud::AccessApproval::V1::AccessApproval::Client.new
-            #
-            # To create a new AccessApproval client with a custom
-            # configuration:
-            #
-            #     client = ::Google::Cloud::AccessApproval::V1::AccessApproval::Client.new do |config|
-            #       config.timeout = 10.0
-            #     end
+            #   # Create a client using a custom configuration
+            #   client = ::Google::Cloud::AccessApproval::V1::AccessApproval::Client.new do |config|
+            #     config.timeout = 10.0
+            #   end
             #
             # @yield [config] Configure the AccessApproval client.
             # @yieldparam config [Client::Configuration]
@@ -179,10 +174,9 @@ module Google
 
               # Create credentials
               credentials = @config.credentials
-              # Use self-signed JWT if the scope and endpoint are unchanged from default,
+              # Use self-signed JWT if the endpoint is unchanged from default,
               # but only if the default endpoint does not have a region prefix.
-              enable_self_signed_jwt = @config.scope == Client.configure.scope &&
-                                       @config.endpoint == Client.configure.endpoint &&
+              enable_self_signed_jwt = @config.endpoint == Client.configure.endpoint &&
                                        !@config.endpoint.split(".").first.include?("-")
               credentials ||= Credentials.default scope: @config.scope,
                                                   enable_self_signed_jwt: enable_self_signed_jwt
@@ -274,7 +268,9 @@ module Google
               options.apply_defaults timeout:      @config.rpcs.list_approval_requests.timeout,
                                      metadata:     metadata,
                                      retry_policy: @config.rpcs.list_approval_requests.retry_policy
-              options.apply_defaults metadata:     @config.metadata,
+
+              options.apply_defaults timeout:      @config.timeout,
+                                     metadata:     @config.metadata,
                                      retry_policy: @config.retry_policy
 
               @access_approval_stub.call_rpc :list_approval_requests, request, options: options do |response, operation|
@@ -341,7 +337,9 @@ module Google
               options.apply_defaults timeout:      @config.rpcs.get_approval_request.timeout,
                                      metadata:     metadata,
                                      retry_policy: @config.rpcs.get_approval_request.retry_policy
-              options.apply_defaults metadata:     @config.metadata,
+
+              options.apply_defaults timeout:      @config.timeout,
+                                     metadata:     @config.metadata,
                                      retry_policy: @config.retry_policy
 
               @access_approval_stub.call_rpc :get_approval_request, request, options: options do |response, operation|
@@ -412,7 +410,9 @@ module Google
               options.apply_defaults timeout:      @config.rpcs.approve_approval_request.timeout,
                                      metadata:     metadata,
                                      retry_policy: @config.rpcs.approve_approval_request.retry_policy
-              options.apply_defaults metadata:     @config.metadata,
+
+              options.apply_defaults timeout:      @config.timeout,
+                                     metadata:     @config.metadata,
                                      retry_policy: @config.retry_policy
 
               @access_approval_stub.call_rpc :approve_approval_request, request, options: options do |response, operation|
@@ -487,7 +487,9 @@ module Google
               options.apply_defaults timeout:      @config.rpcs.dismiss_approval_request.timeout,
                                      metadata:     metadata,
                                      retry_policy: @config.rpcs.dismiss_approval_request.retry_policy
-              options.apply_defaults metadata:     @config.metadata,
+
+              options.apply_defaults timeout:      @config.timeout,
+                                     metadata:     @config.metadata,
                                      retry_policy: @config.retry_policy
 
               @access_approval_stub.call_rpc :dismiss_approval_request, request, options: options do |response, operation|
@@ -553,7 +555,9 @@ module Google
               options.apply_defaults timeout:      @config.rpcs.get_access_approval_settings.timeout,
                                      metadata:     metadata,
                                      retry_policy: @config.rpcs.get_access_approval_settings.retry_policy
-              options.apply_defaults metadata:     @config.metadata,
+
+              options.apply_defaults timeout:      @config.timeout,
+                                     metadata:     @config.metadata,
                                      retry_policy: @config.retry_policy
 
               @access_approval_stub.call_rpc :get_access_approval_settings, request, options: options do |response, operation|
@@ -631,7 +635,9 @@ module Google
               options.apply_defaults timeout:      @config.rpcs.update_access_approval_settings.timeout,
                                      metadata:     metadata,
                                      retry_policy: @config.rpcs.update_access_approval_settings.retry_policy
-              options.apply_defaults metadata:     @config.metadata,
+
+              options.apply_defaults timeout:      @config.timeout,
+                                     metadata:     @config.metadata,
                                      retry_policy: @config.retry_policy
 
               @access_approval_stub.call_rpc :update_access_approval_settings, request, options: options do |response, operation|
@@ -702,7 +708,9 @@ module Google
               options.apply_defaults timeout:      @config.rpcs.delete_access_approval_settings.timeout,
                                      metadata:     metadata,
                                      retry_policy: @config.rpcs.delete_access_approval_settings.retry_policy
-              options.apply_defaults metadata:     @config.metadata,
+
+              options.apply_defaults timeout:      @config.timeout,
+                                     metadata:     @config.metadata,
                                      retry_policy: @config.retry_policy
 
               @access_approval_stub.call_rpc :delete_access_approval_settings, request, options: options do |response, operation|
@@ -726,22 +734,21 @@ module Google
             # Configuration can be applied globally to all clients, or to a single client
             # on construction.
             #
-            # # Examples
+            # @example
             #
-            # To modify the global config, setting the timeout for list_approval_requests
-            # to 20 seconds, and all remaining timeouts to 10 seconds:
+            #   # Modify the global config, setting the timeout for
+            #   # list_approval_requests to 20 seconds,
+            #   # and all remaining timeouts to 10 seconds.
+            #   ::Google::Cloud::AccessApproval::V1::AccessApproval::Client.configure do |config|
+            #     config.timeout = 10.0
+            #     config.rpcs.list_approval_requests.timeout = 20.0
+            #   end
             #
-            #     ::Google::Cloud::AccessApproval::V1::AccessApproval::Client.configure do |config|
-            #       config.timeout = 10.0
-            #       config.rpcs.list_approval_requests.timeout = 20.0
-            #     end
-            #
-            # To apply the above configuration only to a new client:
-            #
-            #     client = ::Google::Cloud::AccessApproval::V1::AccessApproval::Client.new do |config|
-            #       config.timeout = 10.0
-            #       config.rpcs.list_approval_requests.timeout = 20.0
-            #     end
+            #   # Apply the above configuration only to a new client.
+            #   client = ::Google::Cloud::AccessApproval::V1::AccessApproval::Client.new do |config|
+            #     config.timeout = 10.0
+            #     config.rpcs.list_approval_requests.timeout = 20.0
+            #   end
             #
             # @!attribute [rw] endpoint
             #   The hostname or hostname:port of the service endpoint.
