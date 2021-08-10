0f086e99c05da1a12d53a3fa0765c2ce8dbe5f71
yoshi-automation <yoshi-automation@google.com>
2021-08-10 10:52:38 +0000

    [CHANGE ME] Re-generated google-cloud-dataproc-v1beta2 to pick up changes in the API or client library generator.

--- google-cloud-dataproc-v1beta2/lib/google/cloud/dataproc/v1beta2/workflow_templates_services_pb.rb
+++ google-cloud-dataproc-v1beta2/lib/google/cloud/dataproc/v1beta2/workflow_templates_services_pb.rb
@@ -54,7 +54,7 @@ module Google
             # clusters to be deleted.
             #
             # The [Operation.metadata][google.longrunning.Operation.metadata] will be
-            # [WorkflowMetadata](https://cloud.google.com/dataproc/docs/reference/rpc/google.cloud.dataproc.v1beta2#workflowmetadata).
+            # WorkflowMetadata.
             # Also see [Using
             # WorkflowMetadata](https://cloud.google.com/dataproc/docs/concepts/workflows/debugging#using_workflowmetadata).
             #
