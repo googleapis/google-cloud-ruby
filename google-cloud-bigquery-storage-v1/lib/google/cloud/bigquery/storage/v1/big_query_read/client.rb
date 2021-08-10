94509e5e8dd771ec332b9fe7e08af2360aa7a331
yoshi-automation <yoshi-automation@google.com>
2021-08-10 10:41:46 +0000

    [CHANGE ME] Re-generated google-cloud-bigquery-storage-v1 to pick up changes in the API or client library generator.

--- google-cloud-bigquery-storage-v1/lib/google/cloud/bigquery/storage/v1/big_query_read/client.rb
+++ google-cloud-bigquery-storage-v1/lib/google/cloud/bigquery/storage/v1/big_query_read/client.rb
@@ -44,13 +44,12 @@ module Google
               # See {::Google::Cloud::Bigquery::Storage::V1::BigQueryRead::Client::Configuration}
               # for a description of the configuration fields.
               #
-              # ## Example
+              # @example
               #
-              # To modify the configuration for all BigQueryRead clients:
-              #
-              #     ::Google::Cloud::Bigquery::Storage::V1::BigQueryRead::Client.configure do |config|
-              #       config.timeout = 10.0
-              #     end
+              #   # Modify the configuration for all BigQueryRead clients
+              #   ::Google::Cloud::Bigquery::Storage::V1::BigQueryRead::Client.configure do |config|
+              #     config.timeout = 10.0
+              #   end
               #
               # @yield [config] Configure the Client client.
               # @yieldparam config [Client::Configuration]
@@ -112,19 +111,15 @@ module Google
               ##
               # Create a new BigQueryRead client object.
               #
-              # ## Examples
+              # @example
               #
-              # To create a new BigQueryRead client with the default
-              # configuration:
+              #   # Create a client using the default configuration
+              #   client = ::Google::Cloud::Bigquery::Storage::V1::BigQueryRead::Client.new
               #
-              #     client = ::Google::Cloud::Bigquery::Storage::V1::BigQueryRead::Client.new
-              #
-              # To create a new BigQueryRead client with a custom
-              # configuration:
-              #
-              #     client = ::Google::Cloud::Bigquery::Storage::V1::BigQueryRead::Client.new do |config|
-              #       config.timeout = 10.0
-              #     end
+              #   # Create a client using a custom configuration
+              #   client = ::Google::Cloud::Bigquery::Storage::V1::BigQueryRead::Client.new do |config|
+              #     config.timeout = 10.0
+              #   end
               #
               # @yield [config] Configure the BigQueryRead client.
               # @yieldparam config [Client::Configuration]
@@ -144,10 +139,9 @@ module Google
 
                 # Create credentials
                 credentials = @config.credentials
-                # Use self-signed JWT if the scope and endpoint are unchanged from default,
+                # Use self-signed JWT if the endpoint is unchanged from default,
                 # but only if the default endpoint does not have a region prefix.
-                enable_self_signed_jwt = @config.scope == Client.configure.scope &&
-                                         @config.endpoint == Client.configure.endpoint &&
+                enable_self_signed_jwt = @config.endpoint == Client.configure.endpoint &&
                                          !@config.endpoint.split(".").first.include?("-")
                 credentials ||= Credentials.default scope: @config.scope,
                                                     enable_self_signed_jwt: enable_self_signed_jwt
@@ -253,7 +247,9 @@ module Google
                 options.apply_defaults timeout:      @config.rpcs.create_read_session.timeout,
                                        metadata:     metadata,
                                        retry_policy: @config.rpcs.create_read_session.retry_policy
-                options.apply_defaults metadata:     @config.metadata,
+
+                options.apply_defaults timeout:      @config.timeout,
+                                       metadata:     @config.metadata,
                                        retry_policy: @config.retry_policy
 
                 @big_query_read_stub.call_rpc :create_read_session, request, options: options do |response, operation|
@@ -329,7 +325,9 @@ module Google
                 options.apply_defaults timeout:      @config.rpcs.read_rows.timeout,
                                        metadata:     metadata,
                                        retry_policy: @config.rpcs.read_rows.retry_policy
-                options.apply_defaults metadata:     @config.metadata,
+
+                options.apply_defaults timeout:      @config.timeout,
+                                       metadata:     @config.metadata,
                                        retry_policy: @config.retry_policy
 
                 @big_query_read_stub.call_rpc :read_rows, request, options: options do |response, operation|
@@ -414,7 +412,9 @@ module Google
                 options.apply_defaults timeout:      @config.rpcs.split_read_stream.timeout,
                                        metadata:     metadata,
                                        retry_policy: @config.rpcs.split_read_stream.retry_policy
-                options.apply_defaults metadata:     @config.metadata,
+
+                options.apply_defaults timeout:      @config.timeout,
+                                       metadata:     @config.metadata,
                                        retry_policy: @config.retry_policy
 
                 @big_query_read_stub.call_rpc :split_read_stream, request, options: options do |response, operation|
@@ -438,22 +438,21 @@ module Google
               # Configuration can be applied globally to all clients, or to a single client
               # on construction.
               #
-              # # Examples
+              # @example
               #
-              # To modify the global config, setting the timeout for create_read_session
-              # to 20 seconds, and all remaining timeouts to 10 seconds:
+              #   # Modify the global config, setting the timeout for
+              #   # create_read_session to 20 seconds,
+              #   # and all remaining timeouts to 10 seconds.
+              #   ::Google::Cloud::Bigquery::Storage::V1::BigQueryRead::Client.configure do |config|
+              #     config.timeout = 10.0
+              #     config.rpcs.create_read_session.timeout = 20.0
+              #   end
               #
-              #     ::Google::Cloud::Bigquery::Storage::V1::BigQueryRead::Client.configure do |config|
-              #       config.timeout = 10.0
-              #       config.rpcs.create_read_session.timeout = 20.0
-              #     end
-              #
-              # To apply the above configuration only to a new client:
-              #
-              #     client = ::Google::Cloud::Bigquery::Storage::V1::BigQueryRead::Client.new do |config|
-              #       config.timeout = 10.0
-              #       config.rpcs.create_read_session.timeout = 20.0
-              #     end
+              #   # Apply the above configuration only to a new client.
+              #   client = ::Google::Cloud::Bigquery::Storage::V1::BigQueryRead::Client.new do |config|
+              #     config.timeout = 10.0
+              #     config.rpcs.create_read_session.timeout = 20.0
+              #   end
               #
               # @!attribute [rw] endpoint
               #   The hostname or hostname:port of the service endpoint.
