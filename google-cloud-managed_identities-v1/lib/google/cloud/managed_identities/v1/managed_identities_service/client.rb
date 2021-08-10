3396f4d45388e152a5452102f13b6094f791286b
yoshi-automation <yoshi-automation@google.com>
2021-08-10 11:00:48 +0000

    [CHANGE ME] Re-generated google-cloud-managed_identities-v1 to pick up changes in the API or client library generator.

--- google-cloud-managed_identities-v1/lib/google/cloud/managed_identities/v1/managed_identities_service/client.rb
+++ google-cloud-managed_identities-v1/lib/google/cloud/managed_identities/v1/managed_identities_service/client.rb
@@ -72,13 +72,12 @@ module Google
             # See {::Google::Cloud::ManagedIdentities::V1::ManagedIdentitiesService::Client::Configuration}
             # for a description of the configuration fields.
             #
-            # ## Example
+            # @example
             #
-            # To modify the configuration for all ManagedIdentitiesService clients:
-            #
-            #     ::Google::Cloud::ManagedIdentities::V1::ManagedIdentitiesService::Client.configure do |config|
-            #       config.timeout = 10.0
-            #     end
+            #   # Modify the configuration for all ManagedIdentitiesService clients
+            #   ::Google::Cloud::ManagedIdentities::V1::ManagedIdentitiesService::Client.configure do |config|
+            #     config.timeout = 10.0
+            #   end
             #
             # @yield [config] Configure the Client client.
             # @yieldparam config [Client::Configuration]
@@ -145,19 +144,15 @@ module Google
             ##
             # Create a new ManagedIdentitiesService client object.
             #
-            # ## Examples
+            # @example
             #
-            # To create a new ManagedIdentitiesService client with the default
-            # configuration:
+            #   # Create a client using the default configuration
+            #   client = ::Google::Cloud::ManagedIdentities::V1::ManagedIdentitiesService::Client.new
             #
-            #     client = ::Google::Cloud::ManagedIdentities::V1::ManagedIdentitiesService::Client.new
-            #
-            # To create a new ManagedIdentitiesService client with a custom
-            # configuration:
-            #
-            #     client = ::Google::Cloud::ManagedIdentities::V1::ManagedIdentitiesService::Client.new do |config|
-            #       config.timeout = 10.0
-            #     end
+            #   # Create a client using a custom configuration
+            #   client = ::Google::Cloud::ManagedIdentities::V1::ManagedIdentitiesService::Client.new do |config|
+            #     config.timeout = 10.0
+            #   end
             #
             # @yield [config] Configure the ManagedIdentitiesService client.
             # @yieldparam config [Client::Configuration]
@@ -177,10 +172,9 @@ module Google
 
               # Create credentials
               credentials = @config.credentials
-              # Use self-signed JWT if the scope and endpoint are unchanged from default,
+              # Use self-signed JWT if the endpoint is unchanged from default,
               # but only if the default endpoint does not have a region prefix.
-              enable_self_signed_jwt = @config.scope == Client.configure.scope &&
-                                       @config.endpoint == Client.configure.endpoint &&
+              enable_self_signed_jwt = @config.endpoint == Client.configure.endpoint &&
                                        !@config.endpoint.split(".").first.include?("-")
               credentials ||= Credentials.default scope: @config.scope,
                                                   enable_self_signed_jwt: enable_self_signed_jwt
@@ -284,7 +278,9 @@ module Google
               options.apply_defaults timeout:      @config.rpcs.create_microsoft_ad_domain.timeout,
                                      metadata:     metadata,
                                      retry_policy: @config.rpcs.create_microsoft_ad_domain.retry_policy
-              options.apply_defaults metadata:     @config.metadata,
+
+              options.apply_defaults timeout:      @config.timeout,
+                                     metadata:     @config.metadata,
                                      retry_policy: @config.retry_policy
 
               @managed_identities_service_stub.call_rpc :create_microsoft_ad_domain, request, options: options do |response, operation|
@@ -352,7 +348,9 @@ module Google
               options.apply_defaults timeout:      @config.rpcs.reset_admin_password.timeout,
                                      metadata:     metadata,
                                      retry_policy: @config.rpcs.reset_admin_password.retry_policy
-              options.apply_defaults metadata:     @config.metadata,
+
+              options.apply_defaults timeout:      @config.timeout,
+                                     metadata:     @config.metadata,
                                      retry_policy: @config.retry_policy
 
               @managed_identities_service_stub.call_rpc :reset_admin_password, request, options: options do |response, operation|
@@ -437,7 +435,9 @@ module Google
               options.apply_defaults timeout:      @config.rpcs.list_domains.timeout,
                                      metadata:     metadata,
                                      retry_policy: @config.rpcs.list_domains.retry_policy
-              options.apply_defaults metadata:     @config.metadata,
+
+              options.apply_defaults timeout:      @config.timeout,
+                                     metadata:     @config.metadata,
                                      retry_policy: @config.retry_policy
 
               @managed_identities_service_stub.call_rpc :list_domains, request, options: options do |response, operation|
@@ -505,7 +505,9 @@ module Google
               options.apply_defaults timeout:      @config.rpcs.get_domain.timeout,
                                      metadata:     metadata,
                                      retry_policy: @config.rpcs.get_domain.retry_policy
-              options.apply_defaults metadata:     @config.metadata,
+
+              options.apply_defaults timeout:      @config.timeout,
+                                     metadata:     @config.metadata,
                                      retry_policy: @config.retry_policy
 
               @managed_identities_service_stub.call_rpc :get_domain, request, options: options do |response, operation|
@@ -579,7 +581,9 @@ module Google
               options.apply_defaults timeout:      @config.rpcs.update_domain.timeout,
                                      metadata:     metadata,
                                      retry_policy: @config.rpcs.update_domain.retry_policy
-              options.apply_defaults metadata:     @config.metadata,
+
+              options.apply_defaults timeout:      @config.timeout,
+                                     metadata:     @config.metadata,
                                      retry_policy: @config.retry_policy
 
               @managed_identities_service_stub.call_rpc :update_domain, request, options: options do |response, operation|
@@ -647,7 +651,9 @@ module Google
               options.apply_defaults timeout:      @config.rpcs.delete_domain.timeout,
                                      metadata:     metadata,
                                      retry_policy: @config.rpcs.delete_domain.retry_policy
-              options.apply_defaults metadata:     @config.metadata,
+
+              options.apply_defaults timeout:      @config.timeout,
+                                     metadata:     @config.metadata,
                                      retry_policy: @config.retry_policy
 
               @managed_identities_service_stub.call_rpc :delete_domain, request, options: options do |response, operation|
@@ -717,7 +723,9 @@ module Google
               options.apply_defaults timeout:      @config.rpcs.attach_trust.timeout,
                                      metadata:     metadata,
                                      retry_policy: @config.rpcs.attach_trust.retry_policy
-              options.apply_defaults metadata:     @config.metadata,
+
+              options.apply_defaults timeout:      @config.timeout,
+                                     metadata:     @config.metadata,
                                      retry_policy: @config.retry_policy
 
               @managed_identities_service_stub.call_rpc :attach_trust, request, options: options do |response, operation|
@@ -791,7 +799,9 @@ module Google
               options.apply_defaults timeout:      @config.rpcs.reconfigure_trust.timeout,
                                      metadata:     metadata,
                                      retry_policy: @config.rpcs.reconfigure_trust.retry_policy
-              options.apply_defaults metadata:     @config.metadata,
+
+              options.apply_defaults timeout:      @config.timeout,
+                                     metadata:     @config.metadata,
                                      retry_policy: @config.retry_policy
 
               @managed_identities_service_stub.call_rpc :reconfigure_trust, request, options: options do |response, operation|
@@ -861,7 +871,9 @@ module Google
               options.apply_defaults timeout:      @config.rpcs.detach_trust.timeout,
                                      metadata:     metadata,
                                      retry_policy: @config.rpcs.detach_trust.retry_policy
-              options.apply_defaults metadata:     @config.metadata,
+
+              options.apply_defaults timeout:      @config.timeout,
+                                     metadata:     @config.metadata,
                                      retry_policy: @config.retry_policy
 
               @managed_identities_service_stub.call_rpc :detach_trust, request, options: options do |response, operation|
@@ -932,7 +944,9 @@ module Google
               options.apply_defaults timeout:      @config.rpcs.validate_trust.timeout,
                                      metadata:     metadata,
                                      retry_policy: @config.rpcs.validate_trust.retry_policy
-              options.apply_defaults metadata:     @config.metadata,
+
+              options.apply_defaults timeout:      @config.timeout,
+                                     metadata:     @config.metadata,
                                      retry_policy: @config.retry_policy
 
               @managed_identities_service_stub.call_rpc :validate_trust, request, options: options do |response, operation|
@@ -957,22 +971,21 @@ module Google
             # Configuration can be applied globally to all clients, or to a single client
             # on construction.
             #
-            # # Examples
+            # @example
             #
-            # To modify the global config, setting the timeout for create_microsoft_ad_domain
-            # to 20 seconds, and all remaining timeouts to 10 seconds:
+            #   # Modify the global config, setting the timeout for
+            #   # create_microsoft_ad_domain to 20 seconds,
+            #   # and all remaining timeouts to 10 seconds.
+            #   ::Google::Cloud::ManagedIdentities::V1::ManagedIdentitiesService::Client.configure do |config|
+            #     config.timeout = 10.0
+            #     config.rpcs.create_microsoft_ad_domain.timeout = 20.0
+            #   end
             #
-            #     ::Google::Cloud::ManagedIdentities::V1::ManagedIdentitiesService::Client.configure do |config|
-            #       config.timeout = 10.0
-            #       config.rpcs.create_microsoft_ad_domain.timeout = 20.0
-            #     end
-            #
-            # To apply the above configuration only to a new client:
-            #
-            #     client = ::Google::Cloud::ManagedIdentities::V1::ManagedIdentitiesService::Client.new do |config|
-            #       config.timeout = 10.0
-            #       config.rpcs.create_microsoft_ad_domain.timeout = 20.0
-            #     end
+            #   # Apply the above configuration only to a new client.
+            #   client = ::Google::Cloud::ManagedIdentities::V1::ManagedIdentitiesService::Client.new do |config|
+            #     config.timeout = 10.0
+            #     config.rpcs.create_microsoft_ad_domain.timeout = 20.0
+            #   end
             #
             # @!attribute [rw] endpoint
             #   The hostname or hostname:port of the service endpoint.
