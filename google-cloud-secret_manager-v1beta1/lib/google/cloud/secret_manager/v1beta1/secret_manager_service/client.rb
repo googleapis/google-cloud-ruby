b37015bbc87c1c37fb3100be1703610390443357
yoshi-automation <yoshi-automation@google.com>
2021-08-10 11:10:50 +0000

    [CHANGE ME] Re-generated google-cloud-secret_manager-v1beta1 to pick up changes in the API or client library generator.

--- google-cloud-secret_manager-v1beta1/lib/google/cloud/secret_manager/v1beta1/secret_manager_service/client.rb
+++ google-cloud-secret_manager-v1beta1/lib/google/cloud/secret_manager/v1beta1/secret_manager_service/client.rb
@@ -47,13 +47,12 @@ module Google
             # See {::Google::Cloud::SecretManager::V1beta1::SecretManagerService::Client::Configuration}
             # for a description of the configuration fields.
             #
-            # ## Example
+            # @example
             #
-            # To modify the configuration for all SecretManagerService clients:
-            #
-            #     ::Google::Cloud::SecretManager::V1beta1::SecretManagerService::Client.configure do |config|
-            #       config.timeout = 10.0
-            #     end
+            #   # Modify the configuration for all SecretManagerService clients
+            #   ::Google::Cloud::SecretManager::V1beta1::SecretManagerService::Client.configure do |config|
+            #     config.timeout = 10.0
+            #   end
             #
             # @yield [config] Configure the Client client.
             # @yieldparam config [Client::Configuration]
@@ -133,19 +132,15 @@ module Google
             ##
             # Create a new SecretManagerService client object.
             #
-            # ## Examples
+            # @example
             #
-            # To create a new SecretManagerService client with the default
-            # configuration:
+            #   # Create a client using the default configuration
+            #   client = ::Google::Cloud::SecretManager::V1beta1::SecretManagerService::Client.new
             #
-            #     client = ::Google::Cloud::SecretManager::V1beta1::SecretManagerService::Client.new
-            #
-            # To create a new SecretManagerService client with a custom
-            # configuration:
-            #
-            #     client = ::Google::Cloud::SecretManager::V1beta1::SecretManagerService::Client.new do |config|
-            #       config.timeout = 10.0
-            #     end
+            #   # Create a client using a custom configuration
+            #   client = ::Google::Cloud::SecretManager::V1beta1::SecretManagerService::Client.new do |config|
+            #     config.timeout = 10.0
+            #   end
             #
             # @yield [config] Configure the SecretManagerService client.
             # @yieldparam config [Client::Configuration]
@@ -165,10 +160,9 @@ module Google
 
               # Create credentials
               credentials = @config.credentials
-              # Use self-signed JWT if the scope and endpoint are unchanged from default,
+              # Use self-signed JWT if the endpoint is unchanged from default,
               # but only if the default endpoint does not have a region prefix.
-              enable_self_signed_jwt = @config.scope == Client.configure.scope &&
-                                       @config.endpoint == Client.configure.endpoint &&
+              enable_self_signed_jwt = @config.endpoint == Client.configure.endpoint &&
                                        !@config.endpoint.split(".").first.include?("-")
               credentials ||= Credentials.default scope: @config.scope,
                                                   enable_self_signed_jwt: enable_self_signed_jwt
@@ -252,7 +246,9 @@ module Google
               options.apply_defaults timeout:      @config.rpcs.list_secrets.timeout,
                                      metadata:     metadata,
                                      retry_policy: @config.rpcs.list_secrets.retry_policy
-              options.apply_defaults metadata:     @config.metadata,
+
+              options.apply_defaults timeout:      @config.timeout,
+                                     metadata:     @config.metadata,
                                      retry_policy: @config.retry_policy
 
               @secret_manager_service_stub.call_rpc :list_secrets, request, options: options do |response, operation|
@@ -328,7 +324,9 @@ module Google
               options.apply_defaults timeout:      @config.rpcs.create_secret.timeout,
                                      metadata:     metadata,
                                      retry_policy: @config.rpcs.create_secret.retry_policy
-              options.apply_defaults metadata:     @config.metadata,
+
+              options.apply_defaults timeout:      @config.timeout,
+                                     metadata:     @config.metadata,
                                      retry_policy: @config.retry_policy
 
               @secret_manager_service_stub.call_rpc :create_secret, request, options: options do |response, operation|
@@ -398,7 +396,9 @@ module Google
               options.apply_defaults timeout:      @config.rpcs.add_secret_version.timeout,
                                      metadata:     metadata,
                                      retry_policy: @config.rpcs.add_secret_version.retry_policy
-              options.apply_defaults metadata:     @config.metadata,
+
+              options.apply_defaults timeout:      @config.timeout,
+                                     metadata:     @config.metadata,
                                      retry_policy: @config.retry_policy
 
               @secret_manager_service_stub.call_rpc :add_secret_version, request, options: options do |response, operation|
@@ -464,7 +464,9 @@ module Google
               options.apply_defaults timeout:      @config.rpcs.get_secret.timeout,
                                      metadata:     metadata,
                                      retry_policy: @config.rpcs.get_secret.retry_policy
-              options.apply_defaults metadata:     @config.metadata,
+
+              options.apply_defaults timeout:      @config.timeout,
+                                     metadata:     @config.metadata,
                                      retry_policy: @config.retry_policy
 
               @secret_manager_service_stub.call_rpc :get_secret, request, options: options do |response, operation|
@@ -532,7 +534,9 @@ module Google
               options.apply_defaults timeout:      @config.rpcs.update_secret.timeout,
                                      metadata:     metadata,
                                      retry_policy: @config.rpcs.update_secret.retry_policy
-              options.apply_defaults metadata:     @config.metadata,
+
+              options.apply_defaults timeout:      @config.timeout,
+                                     metadata:     @config.metadata,
                                      retry_policy: @config.retry_policy
 
               @secret_manager_service_stub.call_rpc :update_secret, request, options: options do |response, operation|
@@ -599,7 +603,9 @@ module Google
               options.apply_defaults timeout:      @config.rpcs.delete_secret.timeout,
                                      metadata:     metadata,
                                      retry_policy: @config.rpcs.delete_secret.retry_policy
-              options.apply_defaults metadata:     @config.metadata,
+
+              options.apply_defaults timeout:      @config.timeout,
+                                     metadata:     @config.metadata,
                                      retry_policy: @config.retry_policy
 
               @secret_manager_service_stub.call_rpc :delete_secret, request, options: options do |response, operation|
@@ -675,7 +681,9 @@ module Google
               options.apply_defaults timeout:      @config.rpcs.list_secret_versions.timeout,
                                      metadata:     metadata,
                                      retry_policy: @config.rpcs.list_secret_versions.retry_policy
-              options.apply_defaults metadata:     @config.metadata,
+
+              options.apply_defaults timeout:      @config.timeout,
+                                     metadata:     @config.metadata,
                                      retry_policy: @config.retry_policy
 
               @secret_manager_service_stub.call_rpc :list_secret_versions, request, options: options do |response, operation|
@@ -748,7 +756,9 @@ module Google
               options.apply_defaults timeout:      @config.rpcs.get_secret_version.timeout,
                                      metadata:     metadata,
                                      retry_policy: @config.rpcs.get_secret_version.retry_policy
-              options.apply_defaults metadata:     @config.metadata,
+
+              options.apply_defaults timeout:      @config.timeout,
+                                     metadata:     @config.metadata,
                                      retry_policy: @config.retry_policy
 
               @secret_manager_service_stub.call_rpc :get_secret_version, request, options: options do |response, operation|
@@ -818,7 +828,9 @@ module Google
               options.apply_defaults timeout:      @config.rpcs.access_secret_version.timeout,
                                      metadata:     metadata,
                                      retry_policy: @config.rpcs.access_secret_version.retry_policy
-              options.apply_defaults metadata:     @config.metadata,
+
+              options.apply_defaults timeout:      @config.timeout,
+                                     metadata:     @config.metadata,
                                      retry_policy: @config.retry_policy
 
               @secret_manager_service_stub.call_rpc :access_secret_version, request, options: options do |response, operation|
@@ -888,7 +900,9 @@ module Google
               options.apply_defaults timeout:      @config.rpcs.disable_secret_version.timeout,
                                      metadata:     metadata,
                                      retry_policy: @config.rpcs.disable_secret_version.retry_policy
-              options.apply_defaults metadata:     @config.metadata,
+
+              options.apply_defaults timeout:      @config.timeout,
+                                     metadata:     @config.metadata,
                                      retry_policy: @config.retry_policy
 
               @secret_manager_service_stub.call_rpc :disable_secret_version, request, options: options do |response, operation|
@@ -958,7 +972,9 @@ module Google
               options.apply_defaults timeout:      @config.rpcs.enable_secret_version.timeout,
                                      metadata:     metadata,
                                      retry_policy: @config.rpcs.enable_secret_version.retry_policy
-              options.apply_defaults metadata:     @config.metadata,
+
+              options.apply_defaults timeout:      @config.timeout,
+                                     metadata:     @config.metadata,
                                      retry_policy: @config.retry_policy
 
               @secret_manager_service_stub.call_rpc :enable_secret_version, request, options: options do |response, operation|
@@ -1029,7 +1045,9 @@ module Google
               options.apply_defaults timeout:      @config.rpcs.destroy_secret_version.timeout,
                                      metadata:     metadata,
                                      retry_policy: @config.rpcs.destroy_secret_version.retry_policy
-              options.apply_defaults metadata:     @config.metadata,
+
+              options.apply_defaults timeout:      @config.timeout,
+                                     metadata:     @config.metadata,
                                      retry_policy: @config.retry_policy
 
               @secret_manager_service_stub.call_rpc :destroy_secret_version, request, options: options do |response, operation|
@@ -1105,7 +1123,9 @@ module Google
               options.apply_defaults timeout:      @config.rpcs.set_iam_policy.timeout,
                                      metadata:     metadata,
                                      retry_policy: @config.rpcs.set_iam_policy.retry_policy
-              options.apply_defaults metadata:     @config.metadata,
+
+              options.apply_defaults timeout:      @config.timeout,
+                                     metadata:     @config.metadata,
                                      retry_policy: @config.retry_policy
 
               @secret_manager_service_stub.call_rpc :set_iam_policy, request, options: options do |response, operation|
@@ -1176,7 +1196,9 @@ module Google
               options.apply_defaults timeout:      @config.rpcs.get_iam_policy.timeout,
                                      metadata:     metadata,
                                      retry_policy: @config.rpcs.get_iam_policy.retry_policy
-              options.apply_defaults metadata:     @config.metadata,
+
+              options.apply_defaults timeout:      @config.timeout,
+                                     metadata:     @config.metadata,
                                      retry_policy: @config.retry_policy
 
               @secret_manager_service_stub.call_rpc :get_iam_policy, request, options: options do |response, operation|
@@ -1254,7 +1276,9 @@ module Google
               options.apply_defaults timeout:      @config.rpcs.test_iam_permissions.timeout,
                                      metadata:     metadata,
                                      retry_policy: @config.rpcs.test_iam_permissions.retry_policy
-              options.apply_defaults metadata:     @config.metadata,
+
+              options.apply_defaults timeout:      @config.timeout,
+                                     metadata:     @config.metadata,
                                      retry_policy: @config.retry_policy
 
               @secret_manager_service_stub.call_rpc :test_iam_permissions, request, options: options do |response, operation|
@@ -1278,22 +1302,21 @@ module Google
             # Configuration can be applied globally to all clients, or to a single client
             # on construction.
             #
-            # # Examples
+            # @example
             #
-            # To modify the global config, setting the timeout for list_secrets
-            # to 20 seconds, and all remaining timeouts to 10 seconds:
+            #   # Modify the global config, setting the timeout for
+            #   # list_secrets to 20 seconds,
+            #   # and all remaining timeouts to 10 seconds.
+            #   ::Google::Cloud::SecretManager::V1beta1::SecretManagerService::Client.configure do |config|
+            #     config.timeout = 10.0
+            #     config.rpcs.list_secrets.timeout = 20.0
+            #   end
             #
-            #     ::Google::Cloud::SecretManager::V1beta1::SecretManagerService::Client.configure do |config|
-            #       config.timeout = 10.0
-            #       config.rpcs.list_secrets.timeout = 20.0
-            #     end
-            #
-            # To apply the above configuration only to a new client:
-            #
-            #     client = ::Google::Cloud::SecretManager::V1beta1::SecretManagerService::Client.new do |config|
-            #       config.timeout = 10.0
-            #       config.rpcs.list_secrets.timeout = 20.0
-            #     end
+            #   # Apply the above configuration only to a new client.
+            #   client = ::Google::Cloud::SecretManager::V1beta1::SecretManagerService::Client.new do |config|
+            #     config.timeout = 10.0
+            #     config.rpcs.list_secrets.timeout = 20.0
+            #   end
             #
             # @!attribute [rw] endpoint
             #   The hostname or hostname:port of the service endpoint.
