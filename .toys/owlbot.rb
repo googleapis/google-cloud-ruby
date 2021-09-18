54a916eb39f1445a3ab142a28e3a1cf93b0bcef4
Daniel Azuma <dazuma@google.com>
2021-09-18 15:29:10 -0700

    chore: OwlBot tool can run multiple gems

--- .toys/owlbot.rb
+++ .toys/owlbot.rb
@@ -1,10 +1,25 @@
-desc "Run OwlBot on the given library"
+desc "Runs OwlBot for one or more gems."
 
-optional_arg :gem_name do
-  desc "The gem for which to run owlbot. Optional if run from within a gem subdirectory."
+long_desc \
+  "Runs OwlBot for one or more gems.",
+  "",
+  "Gems are chosen as follows:",
+  "* If one or more gem names are provided on the command line, they are selected.",
+  "* Otherwise, if the --all flag is given, all gems covered by OwlBot are selected.",
+  "* Otherwise, if run from a gem subdirectory, that gem is selected.",
+  "* Otherwise, an error is raised."
+
+remaining_args :gem_names do
+  desc "The gems for which to run owlbot."
+end
+flag :all do
+  desc "Run OwlBot on all gems in this repo."
 end
 flag :postprocessor_tag, "--postprocessor-tag=TAG", default: "latest" do
   desc "The tag for the Ruby postprocessor image. Defaults to 'latest'."
+end
+flag :owlbot_cli_tag, "--owlbot-cli-tag=TAG", default: "latest" do
+  desc "The tag for the OwlBot CLI image. Defaults to 'latest'."
 end
 flag :pull do
   desc "Pull the latest images before running"
@@ -12,62 +27,143 @@ end
 flag :git_remote, "--remote NAME" do
   desc "The name of the git remote to use as the pull request head. If omitted, does not open a pull request."
 end
-flag :branch_name, "--branch NAME" do
-  desc "The name of the branch to use if opening a pull request. Defaults to owlbot/GEM-NAME."
-end
 
 OWLBOT_CONFIG_FILE_NAME = ".OwlBot.yaml"
+OWLBOT_CLI_IMAGE = "gcr.io/cloud-devrel-public-resources/owlbot-cli"
+POSTPROCESSOR_IMAGE = "gcr.io/cloud-devrel-public-resources/owlbot-ruby"
 
 include :exec, e: true
 include :terminal
 
 def run
+  require "psych"
+  require "fileutils"
   require "pull_request_generator"
   extend PullRequestGenerator
-  choose_gem
-  set :branch_name, "owlbot/#{gem_name}" if branch_name.nil?
+
+  gems = choose_gems
   Dir.chdir context_directory
-  result = generate_pull_request gem_name: gem_name,
-                                 git_remote: git_remote,
-                                 branch_name: branch_name,
-                                 commit_message: "[CHANGE ME] Manual run of OwlBot for #{gem_name}" do
-    run_owlbot
-  end
-  case result
-  when :opened
-    puts "Created pull request", :bold
-  when :unchanged
-    puts "No pull request created because nothing changed", :bold
-  when :disabled
-    puts "Results left in the local directory", :bold
-  end
+  sources = collect_sources gems
+
+  pull_images
+  run_owlbot sources
+  verify_staging gems
+  results = process_gems gems
+  final_output results
 end
 
-def choose_gem
+def choose_gems
+  gems = gem_names
+  if gems.empty?
+    gems =
+      if all
+        Dir.glob("*/#{OWLBOT_CONFIG_FILE_NAME}").map { |path| File.dirname path }
+      else
+        Array(gem_from_subdirectory)
+      end
+    gems.delete_if do |name|
+      !File.file? File.join(context_directory, name, "#{name}.gemspec")
+    end
+  end
+  error "You must specify at least one gem name" if gems.empty?
+  logger.info "Gems: #{gems}"
+  gems
+end
+
+def gem_from_subdirectory
   curwd = Dir.getwd
-  if context_directory == curwd
-    error "gem name is required unless run from a gem directory" unless gem_name
-  elsif !curwd.start_with? context_directory
-    error "unexpected directory"
-  elsif !gem_name
-    diff = curwd.sub "#{context_directory}/", ""
-    set :gem_name, diff.split("/").first
-  end
-  gem_dir = File.join context_directory, gem_name
-  config_path = File.join gem_dir, OWLBOT_CONFIG_FILE_NAME
-  error "no owlbot config #{config_path}" unless File.file? config_path
-  gem_name
+  return nil if context_directory == curwd
+  error "unexpected current directory #{curwd}" unless curwd.start_with? context_directory
+  curwd.sub("#{context_directory}/", "").split("/").first
 end
 
-def run_owlbot
-  owlbot_image = "gcr.io/cloud-devrel-public-resources/owlbot-cli:latest"
-  postprocessor_image = "gcr.io/cloud-devrel-public-resources/owlbot-ruby:#{postprocessor_tag}"
-  if pull
-    exec ["docker", "pull", owlbot_image]
-    exec ["docker", "pull", postprocessor_image]
+def pull_images
+  return unless pull
+  exec ["docker", "pull", "#{OWLBOT_CLI_IMAGE}:#{owlbot_cli_tag}"]
+  exec ["docker", "pull", "#{POSTPROCESSOR_IMAGE}:#{owlbot_cli_tag}"]
+end
+
+def collect_sources gems
+  sources = {}
+  gems.each do |name|
+    config_path = File.join name, OWLBOT_CONFIG_FILE_NAME
+    error "Gem #{name} has no #{OWLBOT_CONFIG_FILE_NAME}" unless File.file? config_path
+    config = Psych.load_file config_path
+    deep_copy_regexes = config["deep-copy-regex"]
+    error "Expected exactly one deep-copy-regex for gem #{name}" unless deep_copy_regexes.size == 1
+    deep_copy_regex = deep_copy_regexes.first
+    error "Wrong dest deep-copy-regex for gem #{name}" unless deep_copy_regex["dest"] == "/owl-bot-staging/#{name}/$1"
+    error "Source missing in deep-copy-regex for gem #{name}" unless deep_copy_regex["source"]
+    sources[name] = deep_copy_regex["source"]
   end
-  docker_run owlbot_image, "copy-code", "--config-file", File.join(gem_name, OWLBOT_CONFIG_FILE_NAME)
-  docker_run postprocessor_image
+  sources
+end
+
+def run_owlbot sources
+  FileUtils.mkdir_p "tmp"
+  temp_config = File.join "tmp", OWLBOT_CONFIG_FILE_NAME
+  FileUtils.rm_f temp_config
+  combined_deep_copy_regex = sources.map do |name, source|
+    {
+      "source" => source,
+      "dest" => "/owl-bot-staging/#{name}/$1"
+    }
+  end
+  combined_config = {"deep-copy-regex" => combined_deep_copy_regex}
+  File.open temp_config, "w" do |file|
+    file.puts Psych.dump combined_config
+  end
+  docker_run "#{OWLBOT_CLI_IMAGE}:#{owlbot_cli_tag}", "copy-code", "--config-file", temp_config
+end
+
+def verify_staging gems
+  gems.each do |name|
+    staging_dir = File.join "owl-bot-staging", name
+    error "Gem #{name} did not output a staging directory" unless File.directory? staging_dir
+    error "Gem #{name} staging directory is empty" if Dir.children(staging_dir).empty?
+  end
+end
+
+def process_gems gems
+  results = {}
+  gems.each do |name|
+    timestamp = Time.now.utc.strftime("%Y%m%d-%H%M%S")
+    branch_name = "owlbot/#{name}-#{timestamp}"
+    result = generate_pull_request gem_name: name,
+                                   git_remote: git_remote,
+                                   commit_message: "[CHANGE ME] Catch-up run of OwlBot for #{name}" do
+      docker_run "#{POSTPROCESSOR_IMAGE}:#{owlbot_cli_tag}", "--gem", name
+    end
+    case result
+    when :opened
+      puts "#{name}: Created pull request", :bold
+    when :unchanged
+      puts "#{name}: No pull request created because nothing changed", :bold
+    when :disabled
+      puts "#{name}: Results left in the local directory", :bold
+    else
+      puts "#{name}: Unknown result #{result.inspect}", :bold
+    end
+    results[name] = result
+  end
+  results
+end
+
+def final_output results
+  puts
+  puts "Final results:", :bold
+  results.each do |name, result|
+    case result
+    when :opened
+      puts "#{name}: Created pull request"
+    when :unchanged
+      puts "#{name}: No pull request created because nothing changed"
+    when :disabled
+      puts "#{name}: Results left in the local directory"
+    else
+      puts "#{name}: Unknown result #{result.inspect}"
+    end
+  end
 end
 
 def docker_run *args
