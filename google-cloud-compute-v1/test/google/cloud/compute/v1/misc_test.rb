72f49311102490530f519ea1ff700c9ef23d9779
Viacheslav Rostovtsev <58152857+viacheslav-rostovtsev@users.noreply.github.com>
2021-10-07 13:36:04 -0700

    Update misc_test.rb

--- google-cloud-compute-v1/test/google/cloud/compute/v1/misc_test.rb
+++ google-cloud-compute-v1/test/google/cloud/compute/v1/misc_test.rb
@@ -12,9 +12,17 @@ class MiscTest < Minitest::Test
     deprecation_status_json = Google::Cloud::Compute::V1::DeprecationStatus.new(
       deleted: "foo",
       state: :OBSOLETE).to_json
-
+       
     ds_decoded = ::Google::Cloud::Compute::V1::DeprecationStatus.decode_json deprecation_status_json, ignore_unknown_fields: true
     assert_equal :OBSOLETE, ds_decoded.state
+    
+    # DeprecationStatus.state is a enum with the following values specified:
+    # UNDEFINED_STATE = 0;
+    # ACTIVE = 314733318;
+    # DELETED = 120962041;
+    # DEPRECATED = 463360435;
+    # OBSOLETE = 66532761;
+    # (see https://raw.githubusercontent.com/googleapis/googleapis-discovery/master/google/cloud/compute/v1/compute.proto)
 
     unknown_enum_json = deprecation_status_json.gsub "OBSOLETE", "THIS_VALUE_DOES_NOT_EXIST"
     ds_decoded = ::Google::Cloud::Compute::V1::DeprecationStatus.decode_json unknown_enum_json, ignore_unknown_fields: true
