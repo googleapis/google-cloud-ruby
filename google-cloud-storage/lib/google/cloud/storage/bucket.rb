b05f5e1c348b3072902a578feea379b0cf13510f
Neha Bajaj <bajajnehaa@google.com>
2021-10-25 21:29:32 +0530

    Deprecating method public_access_prevention_unspecified?

--- google-cloud-storage/lib/google/cloud/storage/bucket.rb
+++ google-cloud-storage/lib/google/cloud/storage/bucket.rb
@@ -1011,6 +1011,14 @@ module Google
         end
 
         ##
+        # @deprecated Use {#public_access_prevention_inherited?s} instead.
+        #
+        def public_access_prevention_unspecified?
+          return false unless @gapi.iam_configuration&.public_access_prevention
+          @gapi.iam_configuration.public_access_prevention.to_s == "unspecified"
+        end
+
+        ##
         # Whether the value for Public Access Prevention in the bucket's IAM configuration is `inherited`. The default
         # is `false`. This value can be modified by calling {Bucket#public_access_prevention=}.
         #
@@ -1032,7 +1040,6 @@ module Google
           return false unless @gapi.iam_configuration&.public_access_prevention
           @gapi.iam_configuration.public_access_prevention.to_s == "inherited"
         end
-        alias public_access_prevention_unspecified? public_access_prevention_inherited?
 
         ##
         # Updates the bucket with changes made in the given block in a single
