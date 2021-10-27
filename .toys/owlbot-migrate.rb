67200884e8e69a949173c7c34eb0260ea16af639
Daniel Azuma <dazuma@google.com>
2021-10-27 23:02:39 +0000

    chore: Support building from a piper client with the owlbot script

--- .toys/owlbot-migrate.rb
+++ .toys/owlbot-migrate.rb
@@ -19,6 +19,9 @@ end
 flag :branch_name, "--branch=NAME" do
   desc "The branch name"
 end
+flag :source_repo, "--source-repo=PATH" do
+  desc "Path to the googleapis-gen source repo"
+end
 
 OWLBOT_CONFIG_FILE_NAME = ".OwlBot.yaml"
 SYNTH_CONFIG_FILE_NAME = "synth.py"
@@ -72,10 +75,6 @@ def choose_gems
       error "No gemspec found for #{name}"
     end
     content = File.read File.join(context_directory, name, SYNTH_CONFIG_FILE_NAME)
-    unless content.include? "gapic = gcp.GAPICBazel()\n"
-      next if auto_gems
-      error "Synth script for #{name} doesn't seem to invoke bazel"
-    end
     proto_path = proto_path_from_synth_content name, content
     unless proto_path
       next if auto_gems
@@ -88,7 +87,7 @@ end
 
 def proto_path_from_synth_content name, content
   match = %r{proto_path="([^"]+)/v\d\w*"}.match content
-  return match[1] if match
+  return match[1].sub %r{/v\d\w*$}, "" if match
   match = %r{gapic\.ruby_library\(\s*"([^"]+)",}.match content
   return "google/cloud/#{match[1]}" if match
   nil
@@ -125,6 +124,7 @@ end
 
 def run_owlbot names
   cmd = ["owlbot"]
+  cmd << "--source-repo" << source_repo if source_repo
   cmd << "--pull" if pull
   if verbosity < 0
     cmd << "-#{'q' * (-verbosity)}"
