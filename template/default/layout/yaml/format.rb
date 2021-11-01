d28269be59d0c9e3877c0f80f3c6216a2d954f80
TheRoyalTnetennba <paye@google.com>
2021-11-01 11:00:10 -0700

    clean up and comment

--- template/default/layout/yaml/format.rb
+++ template/default/layout/yaml/format.rb
@@ -59,8 +59,24 @@ class Formatter
   end
 end
 
-def markdown str
-  renderer = Redcarpet::Render::HTML.new(render_options = {})
+# TODO: create version of Redcarpet::Render::HTML
+# where only code blocking does anything something like
+# class OnlyCode < Redcarpet::Render::HTML
+#   def block_code code, language
+#     super code, language
+#   end
+  
+#   def codespan(code)
+#     super code
+#   end
+
+#   def block_quote(quote)
+#     nil
+#   end
+#   ...
+# end
+
+def markdown str, renderer
   redcarpet = Redcarpet::Markdown.new renderer
   str = redcarpet.render str
   while str.end_with? "\n"
@@ -81,30 +97,28 @@ end
 def pre_format str
   str = str.to_s
   # Parse markdown prior to running resolve_links, which checks for HTML codeblocks
-  # if @method && @method.path.to_s == "Google::Cloud::PubSub::Subscription#remove_dead_letter_policy"
-  #   require "pry"
-  #   binding.pry
-  # end
-  str = markdown str
-  # str = YARD::Templates::Helpers::HtmlHelper.resolve_links str
+  # use our above defined renderer that only handles code:
+  # renderer = OnlyCode.new(render_options = {})
+  # str = markdown str, renderer
+
+  # now that codeblocks have been identifed, parse_links
   str = Formatter.new(@object, @options).parse_links str
-  # YARD turns {} style links into [] markdown style links. Re-parsing markdown to get HTML links.
-  # str = markdown str
+
+  # now that the codeblocks have been formatted, and the links have been identified, demote remaining headers
+  str = demote_headers str
+
+  # YARD turns {} style links into [] markdown style links. Re-parsing markdown to get HTML links and other formatting
+  renderer = Redcarpet::Render::HTML.new(render_options = {})
+  str = markdown str, renderer
+
   str = escapes str
-  # str = codeblock_space str
-  # str = str.gsub("\\\\{", "{").gsub "\\{", "{"
-  # str = str.gsub("\\\\}", "}").gsub "\\}", "}"
-  # str = codeblock_backtick str
-  # str = demote_headers str
   str = fix_googleapis_links str
-  str = fix_object_links str
-  # str = Formatter.resolve_links str
+
+  # I don't think this is still necessary, but leaving it in case I'm missing something
+  # str = fix_object_links str
   
-  # str = normalize_links str
   str
 end
-
-
 
 def unparagraph str
   if str.start_with?("<p>") && str.end_with?("</p>")
@@ -114,41 +128,41 @@ def unparagraph str
 end
 
 def demote_headers str, min = 0
-  # out = ""
-  # match_list = []
-  # i = 0
-  # while i < str.size
-  #   if str[(i - 1)..-1] =~ /\A(#+\s\w+)/
-  #     match_list << [Regexp.last_match, i - 1]
-  #     i += Regexp.last_match[0].size
-  #   else
-  #     i += 1
-  #   end
-  # end
-  # return str if match_list.empty?
+  out = ""
+  match_list = []
+  i = 0
+  while i < str.size
+    if str[(i - 1)..-1] =~ /\A(#+\s\w+)/
+      match_list << [Regexp.last_match, i - 1]
+      i += Regexp.last_match[0].size
+    else
+      i += 1
+    end
+  end
+  return str if match_list.empty?
 
-  # prev = 0
-  # match_list.each do |entry|
-  #   match = entry[0][1]
-  #   i = entry[1]
-  #   out += str[prev...i]
+  prev = 0
+  match_list.each do |entry|
+    match = entry[0][1]
+    i = entry[1]
+    out += str[prev...i]
 
-  #   if in_codeblock? str, match, i
-  #     out += match
-  #     prev = i + match.size
-  #   else
-  #     new_header = "#" + match
-  #     while new_header.split(" ").first.size < min
-  #       new_header = "#" + new_header 
-  #     end
-  #     out += new_header
-  #     prev = i + match.size
-  #   end
-  # end
+    if in_codeblock? str, match, i
+      out += match
+      prev = i + match.size
+    else
+      new_header = "#" + match
+      while new_header.split(" ").first.size < min
+        new_header = "#" + new_header 
+      end
+      out += new_header
+      prev = i + match.size
+    end
+  end
 
-  # out += str[prev..-1]
+  out += str[prev..-1]
 
-  # out
+  out
   str
 end
 
@@ -183,44 +197,6 @@ def fix_object_links str
     str = str.gsub old_link, new_link
   end
   str
-end
-
-
-def normalize_links str
-  out = ""
-  match_list = []
-  (0..str.length).each do |i|
-    if str[(i - 1)..i] != "\\" && (str[i..-1] =~ /\A\{([\w\.\:\#\d]*)\}/ || str[i..-1] =~ /\A\{([\w\.\:\#\d]*)\s([\w\.\:\#\d\s]*)\}/)
-      match_list << [Regexp.last_match, i] 
-    end
-  end
-  return str if match_list.empty?
-
-  prev = 0
-  match_list.each do |entry|
-    obj = @object.path
-    match = entry[0][0]
-    url = entry[0][1] || ""
-    text = entry[0][2] || ""
-    i = entry[1]
-    out += str[prev...i]
-    if url.empty? || in_codeblock?(str, match, i)
-      out += match
-      prev = i + match.size
-      next
-    end
-    text = url if text.empty?
-    obj = object_match url, str
-    if obj
-      out += link object_url(obj), text
-      prev = i + match.size
-    else
-      out += match
-      prev = i + match.size
-    end
-  end
-  out += str[prev..-1]
-  out
 end
 
 def link_objects str
@@ -293,61 +269,6 @@ def object_url obj
   end
 end
 
-def codeblock_space str
-  end_cap = "\\n\\n!!@#^%&*ZXQZXQZXQ<>?|||"
-  str += end_cap
-  out = ""
-  match_list = []
-  i = 0
-  while i < str.length
-    if str[i..-1] =~ /\A(\s{4}[^\*].*?)(?=\\n\\n\S)/
-      match = Regexp.last_match
-      match_list << [match, i] 
-      i += match[0].size
-    else
-      i += 1
-    end
-  end
-  return str.sub(end_cap, "") if match_list.empty?
-
-  prev = 0
-  match_list.each do |entry|
-    code = entry[0][0]
-    i = entry[1]
-    out += str[prev...i]
-    if in_bulleted_list? str, i
-      out += code
-      prev = i + entry[0][0].size
-      next
-    else
-      code = code.split("\\n").map { |line| line.sub "    ", "" }.join("\\n")
-      code = codeblock code
-      out += code
-      prev = i + entry[0][0].size
-    end
-  end
-  out += str[prev..-1]
-  out.sub end_cap, ""
-end
-
-
-def codeblock_backtick str
-  while str.include? "```"
-    str.sub! "```", code_head
-    str.sub! "```", code_tail
-  end
-  while str.include? "`"
-    a = str.index "`"
-    str.sub! "`", "<code>"
-    b = str.index "`"
-    substr = str[(a + "<code>".size)...b]
-    new_substr = substr.gsub /(?<!\\)\*/, "&#42;"
-    str.sub! substr, new_substr
-    str.sub! "`", "</code>"
-  end
-  str
-end
-
 def in_codeblock? str, sub_str, i
   return false unless str.include?(code_head) && str.include?(code_tail)
 
@@ -359,21 +280,6 @@ def in_codeblock? str, sub_str, i
     i += 1
   end
   false
-end
-
-def in_bulleted_list? str, i
-  return false if i == 0
-
-  sep = "\\n"
-  lines = str.split sep
-  line_number = 0
-  n = 0
-  lines.each do |line|
-    next if n >= i
-    line_number += 1
-    n += (line.size + sep.size)
-  end
-  lines[line_number - 1].start_with? " *  "
 end
 
 def code_head
