9bf9c692a787cc5a2259454284a5ffb6ca5472df
Neha Bajaj <bajajnehaa@google.com>
2021-11-19 15:36:02 +0530

    Adding admin instance client test.

---
+++ google-cloud-spanner/test/google/cloud/spanner/admin/instance/client_test.rb
@@ -0,0 +1,34 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require "helper"
+require "google/cloud/spanner/admin/instance"
+require "google/cloud/spanner/admin/instance/v1/instance_admin"
+require "gapic/common"
+require "gapic/grpc"
+
+class Google::Cloud::Spanner::AdminInstanceClientConstructionMinitest < Minitest::Test
+  def test_instance_admin
+    emulator_host = "localhost:4567"
+    emulator_check = ->(name) { (name == "SPANNER_EMULATOR_HOST") ? emulator_host : nil }
+    # Clear all environment variables, except SPANNER_EMULATOR_HOST
+    ENV.stub :[], emulator_check do
+      Gapic::ServiceStub.stub :new, :stub do
+          client = Google::Cloud::Spanner::Admin::Instance.instance_admin project_id: "1234"
+          assert_kind_of Google::Cloud::Spanner::Admin::Instance::V1::InstanceAdmin::Client, client
+          assert_equal emulator_host, client.configure.endpoint
+      end
+    end
+  end
+end
