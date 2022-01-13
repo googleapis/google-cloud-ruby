0aa94ee2baf8495dd0462a861a4c143333bb2a72
Rebecca Peterson <44721098+rebecca-pete@users.noreply.github.com>
2022-01-13 11:46:37 -0800

    chore: Reduce the permissions needed for the storage_download_file sample

--- google-cloud-storage/samples/storage_download_file.rb
+++ google-cloud-storage/samples/storage_download_file.rb
@@ -26,7 +26,7 @@ def download_file bucket_name:, file_name:, local_file_path:
   require "google/cloud/storage"
 
   storage = Google::Cloud::Storage.new
-  bucket  = storage.bucket bucket_name
+  bucket  = storage.bucket bucket_name, skip_lookup: true
   file    = bucket.file file_name
 
   file.download local_file_path
