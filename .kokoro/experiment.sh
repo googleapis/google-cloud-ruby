e195fde47f9f1572a90d1c8522b6ef9577b86d5a
Neha Bajaj <bajajnehaa@google.com>
2022-01-24 16:45:10 +0530

    Kokoro: Changes to view file structure within docker container.

--- .kokoro/experiment.sh
+++ .kokoro/experiment.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
+echo "Current working directory: "
 pwd
 
+echo "List all files."
 ls -lashR
 
-cd ..
+echo "Change directory to root"
+cd /
 
+echo "List all files."
 ls -lashR
