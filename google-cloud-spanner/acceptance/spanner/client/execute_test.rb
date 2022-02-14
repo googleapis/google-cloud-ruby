69bcb965339c20d45445c839975d3b69c2980c13
Nivedha <nivedhasenthil@gmail.com>
2022-02-14 16:40:47 +0530

    update execute test for pg

--- google-cloud-spanner/acceptance/spanner/client/execute_test.rb
+++ google-cloud-spanner/acceptance/spanner/client/execute_test.rb
@@ -15,178 +15,142 @@
 require "spanner_helper"
 
 describe "Spanner Client", :execute_sql, :spanner do
-  let(:db) { spanner_client }
+  let(:db) { {gsql: spanner_client, pg: spanner_pg_client} }
 
-  it "runs SELECT 1" do
-    results = db.execute_sql "SELECT 1"
-    _(results).must_be_kind_of Google::Cloud::Spanner::Results
+  [:gsql, :pg].each do |dialect|
+    it "runs SELECT 1" do
+      results = db[dialect].execute_sql "SELECT 1"
+      _(results).must_be_kind_of Google::Cloud::Spanner::Results
 
-    _(results.fields).must_be_kind_of Google::Cloud::Spanner::Fields
-    _(results.fields.keys.count).must_equal 1
-    _(results.fields[0]).must_equal :INT64
+      _(results.fields).must_be_kind_of Google::Cloud::Spanner::Fields
+      _(results.fields.keys.count).must_equal 1
+      _(results.fields[0]).must_equal :INT64
 
-    rows = results.rows.to_a # grab all from the enumerator
-    _(rows.count).must_equal 1
-    row = rows.first
-    _(row).must_be_kind_of Google::Cloud::Spanner::Data
-    _(row.keys).must_equal [0]
-    _(row[0]).must_equal 1
-  end
-
-  it "runs a simple query" do
-    results = db.execute_sql "SELECT 42 AS num"
-    _(results).must_be_kind_of Google::Cloud::Spanner::Results
-
-    _(results.fields).must_be_kind_of Google::Cloud::Spanner::Fields
-    _(results.fields.keys.count).must_equal 1
-    _(results.fields[:num]).must_equal :INT64
-
-    rows = results.rows.to_a # grab all from the enumerator
-    _(rows.count).must_equal 1
-    row = rows.first
-    _(row).must_be_kind_of Google::Cloud::Spanner::Data
-    _(row.keys).must_equal [:num]
-    _(row[:num]).must_equal 42
-  end
-
-  it "runs a simple query using a single-use strong option" do
-    results = db.execute_sql "SELECT 42 AS num", single_use: { strong: true }
-    _(results).must_be_kind_of Google::Cloud::Spanner::Results
-
-    _(results.fields).must_be_kind_of Google::Cloud::Spanner::Fields
-    _(results.fields.keys.count).must_equal 1
-    _(results.fields[:num]).must_equal :INT64
-
-    rows = results.rows.to_a # grab all from the enumerator
-    _(rows.count).must_equal 1
-    row = rows.first
-    _(row).must_be_kind_of Google::Cloud::Spanner::Data
-    _(row.keys).must_equal [:num]
-    _(row[:num]).must_equal 42
-  end
-
-  it "runs a simple query using a single-use timestamp option" do
-    results = db.execute_sql "SELECT 42 AS num", single_use: { timestamp: (Time.now - 60) }
-    _(results).must_be_kind_of Google::Cloud::Spanner::Results
-
-    _(results.fields).must_be_kind_of Google::Cloud::Spanner::Fields
-    _(results.fields.keys.count).must_equal 1
-    _(results.fields[:num]).must_equal :INT64
-
-    rows = results.rows.to_a # grab all from the enumerator
-    _(rows.count).must_equal 1
-    row = rows.first
-    _(row).must_be_kind_of Google::Cloud::Spanner::Data
-    _(row.keys).must_equal [:num]
-    _(row[:num]).must_equal 42
-  end
-
-  it "runs a simple query using a single-use staleness option" do
-    results = db.execute_sql "SELECT 42 AS num", single_use: { staleness: 60 }
-    _(results).must_be_kind_of Google::Cloud::Spanner::Results
-
-    _(results.fields).must_be_kind_of Google::Cloud::Spanner::Fields
-    _(results.fields.keys.count).must_equal 1
-    _(results.fields[:num]).must_equal :INT64
-
-    rows = results.rows.to_a # grab all from the enumerator
-    _(rows.count).must_equal 1
-    row = rows.first
-    _(row).must_be_kind_of Google::Cloud::Spanner::Data
-    _(row.keys).must_equal [:num]
-    _(row[:num]).must_equal 42
-  end
-
-  it "runs a simple query using a single-use bounded_timestamp option" do
-    results = db.execute_sql "SELECT 42 AS num", single_use: { bounded_timestamp: (Time.now - 60) }
-    _(results).must_be_kind_of Google::Cloud::Spanner::Results
-
-    _(results.fields).must_be_kind_of Google::Cloud::Spanner::Fields
-    _(results.fields.keys.count).must_equal 1
-    _(results.fields[:num]).must_equal :INT64
-
-    rows = results.rows.to_a # grab all from the enumerator
-    _(rows.count).must_equal 1
-    row = rows.first
-    _(row).must_be_kind_of Google::Cloud::Spanner::Data
-    _(row.keys).must_equal [:num]
-    _(row[:num]).must_equal 42
-  end
-
-  it "runs a simple query using a single-use bounded_staleness option" do
-    results = db.execute_sql "SELECT 42 AS num", single_use: { bounded_staleness: 60 }
-    _(results).must_be_kind_of Google::Cloud::Spanner::Results
-
-    _(results.fields).must_be_kind_of Google::Cloud::Spanner::Fields
-    _(results.fields.keys.count).must_equal 1
-    _(results.fields[:num]).must_equal :INT64
-
-    rows = results.rows.to_a # grab all from the enumerator
-    _(rows.count).must_equal 1
-    row = rows.first
-    _(row).must_be_kind_of Google::Cloud::Spanner::Data
-    _(row.keys).must_equal [:num]
-    _(row[:num]).must_equal 42
-  end
-
-  it "runs a simple query with query options" do
-    query_options = { optimizer_version: "3", optimizer_statistics_package: "latest" }
-    results = db.execute_sql "SELECT 42 AS num", query_options: query_options
-    _(results).must_be_kind_of Google::Cloud::Spanner::Results
-
-    _(results.fields).must_be_kind_of Google::Cloud::Spanner::Fields
-    _(results.fields.keys.count).must_equal 1
-    _(results.fields[:num]).must_equal :INT64
-
-    rows = results.rows.to_a # grab all from the enumerator
-    _(rows.count).must_equal 1
-    row = rows.first
-    _(row).must_be_kind_of Google::Cloud::Spanner::Data
-    _(row.keys).must_equal [:num]
-    _(row[:num]).must_equal 42
-  end
-
-  it "runs a simple query when the client-level config of query options is set" do
-    query_options = { optimizer_version: "3", optimizer_statistics_package: "latest" }
-    new_spanner = Google::Cloud::Spanner.new
-    new_db = new_spanner.client db.instance_id, db.database_id, query_options: query_options
-    _(new_db.query_options).must_equal({ optimizer_version: "3", optimizer_statistics_package: "latest" })
-
-    results = new_db.execute_sql "SELECT 42 AS num"
-    _(results).must_be_kind_of Google::Cloud::Spanner::Results
-
-    _(results.fields).must_be_kind_of Google::Cloud::Spanner::Fields
-    _(results.fields.keys.count).must_equal 1
-    _(results.fields[:num]).must_equal :INT64
-
-    rows = results.rows.to_a # grab all from the enumerator
-    _(rows.count).must_equal 1
-    row = rows.first
-    _(row).must_be_kind_of Google::Cloud::Spanner::Data
-    _(row.keys).must_equal [:num]
-    _(row[:num]).must_equal 42
-  end
-
-  describe "when the environment variable of query options is set" do
-    let(:origin_opt_version) { nil }
-    let(:origin_opt_stats_pkg) { nil }
-
-    before do
-      origin_opt_version = ENV["SPANNER_OPTIMIZER_VERSION"]
-      ENV["SPANNER_OPTIMIZER_VERSION"] = "3"
-      origin_opt_stats_pkg = ENV["SPANNER_OPTIMIZER_STATISTICS_PACKAGE"]
-      ENV["SPANNER_OPTIMIZER_STATISTICS_PACKAGE"] = "latest"
+      rows = results.rows.to_a # grab all from the enumerator
+      _(rows.count).must_equal 1
+      row = rows.first
+      _(row).must_be_kind_of Google::Cloud::Spanner::Data
+      _(row[0]).must_equal 1
     end
 
-    after do
-      ENV["SPANNER_OPTIMIZER_VERSION"] = origin_opt_version
-      ENV["SPANNER_OPTIMIZER_STATISTICS_PACKAGE"] = origin_opt_stats_pkg
+    it "runs a simple query" do
+      results = db[dialect].execute_sql "SELECT 42 AS num"
+      _(results).must_be_kind_of Google::Cloud::Spanner::Results
+
+      _(results.fields).must_be_kind_of Google::Cloud::Spanner::Fields
+      _(results.fields.keys.count).must_equal 1
+      _(results.fields[:num]).must_equal :INT64
+
+      rows = results.rows.to_a # grab all from the enumerator
+      _(rows.count).must_equal 1
+      row = rows.first
+      _(row).must_be_kind_of Google::Cloud::Spanner::Data
+      _(row.keys).must_equal [:num]
+      _(row[:num]).must_equal 42
     end
 
-    it "runs a simple query " do
+    it "runs a simple query using a single-use strong option" do
+      results = db[dialect].execute_sql "SELECT 42 AS num", single_use: { strong: true }
+      _(results).must_be_kind_of Google::Cloud::Spanner::Results
+
+      _(results.fields).must_be_kind_of Google::Cloud::Spanner::Fields
+      _(results.fields.keys.count).must_equal 1
+      _(results.fields[:num]).must_equal :INT64
+
+      rows = results.rows.to_a # grab all from the enumerator
+      _(rows.count).must_equal 1
+      row = rows.first
+      _(row).must_be_kind_of Google::Cloud::Spanner::Data
+      _(row.keys).must_equal [:num]
+      _(row[:num]).must_equal 42
+    end
+
+    it "runs a simple query using a single-use timestamp option" do
+      results = db[dialect].execute_sql "SELECT 42 AS num", single_use: { timestamp: (Time.now - 60) }
+      _(results).must_be_kind_of Google::Cloud::Spanner::Results
+
+      _(results.fields).must_be_kind_of Google::Cloud::Spanner::Fields
+      _(results.fields.keys.count).must_equal 1
+      _(results.fields[:num]).must_equal :INT64
+
+      rows = results.rows.to_a # grab all from the enumerator
+      _(rows.count).must_equal 1
+      row = rows.first
+      _(row).must_be_kind_of Google::Cloud::Spanner::Data
+      _(row.keys).must_equal [:num]
+      _(row[:num]).must_equal 42
+    end
+
+    it "runs a simple query using a single-use staleness option" do
+      results = db[dialect].execute_sql "SELECT 42 AS num", single_use: { staleness: 60 }
+      _(results).must_be_kind_of Google::Cloud::Spanner::Results
+
+      _(results.fields).must_be_kind_of Google::Cloud::Spanner::Fields
+      _(results.fields.keys.count).must_equal 1
+      _(results.fields[:num]).must_equal :INT64
+
+      rows = results.rows.to_a # grab all from the enumerator
+      _(rows.count).must_equal 1
+      row = rows.first
+      _(row).must_be_kind_of Google::Cloud::Spanner::Data
+      _(row.keys).must_equal [:num]
+      _(row[:num]).must_equal 42
+    end
+
+    it "runs a simple query using a single-use bounded_timestamp option" do
+      results = db[dialect].execute_sql "SELECT 42 AS num", single_use: { bounded_timestamp: (Time.now - 60) }
+      _(results).must_be_kind_of Google::Cloud::Spanner::Results
+
+      _(results.fields).must_be_kind_of Google::Cloud::Spanner::Fields
+      _(results.fields.keys.count).must_equal 1
+      _(results.fields[:num]).must_equal :INT64
+
+      rows = results.rows.to_a # grab all from the enumerator
+      _(rows.count).must_equal 1
+      row = rows.first
+      _(row).must_be_kind_of Google::Cloud::Spanner::Data
+      _(row.keys).must_equal [:num]
+      _(row[:num]).must_equal 42
+    end
+
+    it "runs a simple query using a single-use bounded_staleness option" do
+      results = db[dialect].execute_sql "SELECT 42 AS num", single_use: { bounded_staleness: 60 }
+      _(results).must_be_kind_of Google::Cloud::Spanner::Results
+
+      _(results.fields).must_be_kind_of Google::Cloud::Spanner::Fields
+      _(results.fields.keys.count).must_equal 1
+      _(results.fields[:num]).must_equal :INT64
+
+      rows = results.rows.to_a # grab all from the enumerator
+      _(rows.count).must_equal 1
+      row = rows.first
+      _(row).must_be_kind_of Google::Cloud::Spanner::Data
+      _(row.keys).must_equal [:num]
+      _(row[:num]).must_equal 42
+    end
+
+    it "runs a simple query with query options" do
+      query_options = { optimizer_version: "3", optimizer_statistics_package: "latest" }
+      results = db[dialect].execute_sql "SELECT 42 AS num", query_options: query_options
+      _(results).must_be_kind_of Google::Cloud::Spanner::Results
+
+      _(results.fields).must_be_kind_of Google::Cloud::Spanner::Fields
+      _(results.fields.keys.count).must_equal 1
+      _(results.fields[:num]).must_equal :INT64
+
+      rows = results.rows.to_a # grab all from the enumerator
+      _(rows.count).must_equal 1
+      row = rows.first
+      _(row).must_be_kind_of Google::Cloud::Spanner::Data
+      _(row.keys).must_equal [:num]
+      _(row[:num]).must_equal 42
+    end
+
+    it "runs a simple query when the client-level config of query options is set" do
+      query_options = { optimizer_version: "3", optimizer_statistics_package: "latest" }
       new_spanner = Google::Cloud::Spanner.new
-      new_db = new_spanner.client db.instance_id, db.database_id
-      _(new_db.project.query_options).must_equal({ optimizer_version: "3", optimizer_statistics_package: "latest" })
+      new_db = new_spanner.client db[dialect].instance_id, db[dialect].database_id, query_options: query_options
+      _(new_db.query_options).must_equal({ optimizer_version: "3", optimizer_statistics_package: "latest" })
 
       results = new_db.execute_sql "SELECT 42 AS num"
       _(results).must_be_kind_of Google::Cloud::Spanner::Results
@@ -202,14 +166,51 @@ describe "Spanner Client", :execute_sql, :spanner do
       _(row.keys).must_equal [:num]
       _(row[:num]).must_equal 42
     end
-  end
 
-  describe "request options" do
-    it "run sample query with priority" do
-      results = db.execute_sql "SELECT 1", request_options: { priority: :PRIORITY_MEDIUM }
-      _(results).must_be_kind_of Google::Cloud::Spanner::Results
+    describe "when the environment variable of query options is set" do
+      let(:origin_opt_version) { nil }
+      let(:origin_opt_stats_pkg) { nil }
 
-      _(results.rows.count).must_equal 1
+      before do
+        origin_opt_version = ENV["SPANNER_OPTIMIZER_VERSION"]
+        ENV["SPANNER_OPTIMIZER_VERSION"] = "3"
+        origin_opt_stats_pkg = ENV["SPANNER_OPTIMIZER_STATISTICS_PACKAGE"]
+        ENV["SPANNER_OPTIMIZER_STATISTICS_PACKAGE"] = "latest"
+      end
+
+      after do
+        ENV["SPANNER_OPTIMIZER_VERSION"] = origin_opt_version
+        ENV["SPANNER_OPTIMIZER_STATISTICS_PACKAGE"] = origin_opt_stats_pkg
+      end
+
+      it "runs a simple query " do
+        new_spanner = Google::Cloud::Spanner.new
+        new_db = new_spanner.client db[dialect].instance_id, db[dialect].database_id
+        _(new_db.project.query_options).must_equal({ optimizer_version: "3", optimizer_statistics_package: "latest" })
+
+        results = new_db.execute_sql "SELECT 42 AS num"
+        _(results).must_be_kind_of Google::Cloud::Spanner::Results
+
+        _(results.fields).must_be_kind_of Google::Cloud::Spanner::Fields
+        _(results.fields.keys.count).must_equal 1
+        _(results.fields[:num]).must_equal :INT64
+
+        rows = results.rows.to_a # grab all from the enumerator
+        _(rows.count).must_equal 1
+        row = rows.first
+        _(row).must_be_kind_of Google::Cloud::Spanner::Data
+        _(row.keys).must_equal [:num]
+        _(row[:num]).must_equal 42
+      end
     end
-  end
+
+    describe "request options" do
+      it "run sample query with priority" do
+        results = db[dialect].execute_sql "SELECT 1", request_options: { priority: :PRIORITY_MEDIUM }
+        _(results).must_be_kind_of Google::Cloud::Spanner::Results
+
+        _(results.rows.count).must_equal 1
+      end
+    end
+  end  
 end
