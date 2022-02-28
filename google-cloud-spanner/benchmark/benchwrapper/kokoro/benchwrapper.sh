d55acc11b38862d73440872f136ef7f2d5fd2e29
bajajnehaa <bajajnehaa@google.com>
2022-02-28 10:56:08 +0000

    chore(spanner): [Benchmark] debugging why bundle install is failing in kokoro job.

--- google-cloud-spanner/benchmark/benchwrapper/kokoro/benchwrapper.sh
+++ google-cloud-spanner/benchmark/benchwrapper/kokoro/benchwrapper.sh
@@ -15,7 +15,15 @@ export PATH=$GEM_HOME/bin:$PATH
 gem install --no-document bundler
 
 export SPANNER_EMULATOR_HOST=localhost:9010
+
+# print current working directory
+pwd
+
 cd ../
+
+# list all files in the pwd
+ls -lash
+
 bundle install
 bundle exec benchwrapper.rb --port=8081 &
 docker pull gcr.io/cloud-spanner-emulator/emulator
