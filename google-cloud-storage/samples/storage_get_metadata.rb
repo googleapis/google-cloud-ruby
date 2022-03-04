ec60eabdd9af3bc8508d0beda11d6eeea2a94c5a
Rebecca Peterson <44721098+rebecca-pete@users.noreply.github.com>
2022-03-03 21:55:55 -0800

    samples(storage): Skip separate request to received metdata (#17865)

--- google-cloud-storage/samples/storage_get_metadata.rb
+++ google-cloud-storage/samples/storage_get_metadata.rb
@@ -23,7 +23,7 @@ def get_metadata bucket_name:, file_name:
   require "google/cloud/storage"
 
   storage = Google::Cloud::Storage.new
-  bucket  = storage.bucket bucket_name
+  bucket  = storage.bucket bucket_name, skip_lookup: true
   file    = bucket.file file_name
 
   puts "Name: #{file.name}"
