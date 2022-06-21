e0068768399900c0537a1d594577dd5808bae64f
Sandeep Tuniki <sandeep.tnk29@gmail.com>
2022-06-21 12:24:29 +0000

    Modify the tests to use newer syntax

--- google-cloud-datastore/acceptance/datastore/datastore_test.rb
+++ google-cloud-datastore/acceptance/datastore/datastore_test.rb
@@ -449,34 +449,38 @@ describe Google::Cloud::Datastore::Dataset, :datastore do
     end
 
     it "should filter by = operator" do
-      query = Google::Cloud::Datastore::Query.new.
-        kind("Character").
-        where("appearances", "=", 0)
-      entities = dataset.run query
+      datastore = Google::Cloud::Datastore.new
+      kind = "Character"
+      query = datastore.query(kind)
+                 .where("appearances", "=", 0)
+      entities = datastore.run query
       _(entities.count).must_equal 1
     end
 
     it "should filter by != operator" do
-      query = Google::Cloud::Datastore::Query.new.
-        kind("Character").
-        where("appearances", "!=", 0)
-      entities = dataset.run query
+      datastore = Google::Cloud::Datastore.new
+      kind = "Character"
+      query = datastore.query(kind)
+                 .where("appearances", "!=", 0)
+      entities = datastore.run query
       _(entities.count).must_equal 7
     end
 
     it "should filter by in operator" do
-      query = Google::Cloud::Datastore::Query.new.
-        kind("Character").
-        where("name", "in", ["Jon Snow", "Arya"])
-      entities = dataset.run query
+      datastore = Google::Cloud::Datastore.new
+      kind = "Character"
+      query = datastore.query(kind)
+                 .where("name", "in", ["Jon Snow", "Arya"])
+      entities = datastore.run query
       _(entities.count).must_equal 2
     end
 
     it "should filter by not_in operator" do
-      query = Google::Cloud::Datastore::Query.new.
-        kind("Character").
-        where("name", "not_in", ["Jon Snow", "Arya"])
-      entities = dataset.run query
+      datastore = Google::Cloud::Datastore.new
+      kind = "Character"
+      query = datastore.query(kind)
+                 .where("name", "not_in", ["Jon Snow", "Arya"])
+      entities = datastore.run query
       _(entities.count).must_equal 6
     end
 
