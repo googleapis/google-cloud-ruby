5a732ceb0fb8197b3308e0ec778409e99c4bad05
bajajnehaa <bajajnehaa@google.com>
2022-08-11 07:14:52 +0000

    chore(spanner): fix issues with licensing

--- google-cloud-spanner/acceptance/spanner/client/params/date_test.rb
+++ google-cloud-spanner/acceptance/spanner/client/params/date_test.rb
@@ -1,6 +1,6 @@
-# Copyright date_value0(date_value - 1)7 Google Inc. All rights reserved.
+# Copyright 2017 Google LLC
 #
-# Licensed under the Apache License, Version date_value.0 (the "License");
+# Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
