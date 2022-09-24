c9d75603330e6dbde8fab0e706b57071053a1af6
Nivedha <nivedhasenthil@gmail.com>
2022-09-24 15:02:25 +0530

    chore: add sample params

--- google-cloud-pubsub/samples/pubsub_create_bigquery_subscription.rb
+++ google-cloud-pubsub/samples/pubsub_create_bigquery_subscription.rb
@@ -15,16 +15,23 @@
 # [START pubsub_create_bigquery_subscription]
 require "google/cloud/pubsub"
 
+## 
 # Shows how to create a BigQuery subscription where messages published to a topic populates a BigQuery table.
-  def pubsub_create_bigquery_subscription project_id:, topic_id:, subscription_id:, bigquery_table_id:
-    pubsub = Google::Cloud::Pubsub.new project_id: project_id
-    topic = pubsub.topic topic_id
-    subscription = topic.subscribe subscription_id, 
-                                   bigquery_config: {
-                                    table: bigquery_table_id,
-                                    write_metadata: true
-                                   }
-    puts "BigQuery subscription created: #{subscription_id}."
-    puts "Table for subscription is: #{bigquery_table_id}"
-  end
+#
+# @param project_id [String] Your Google Cloud project (e.g. "my-project")
+# @param topic_id [String] Your topic name (e.g. "my-secret")
+# @param subscription_id [String] ID for new subscription to be created (e.g. "my-subscription")
+# @param bigquery_table_id [String] ID of bigquery table (e.g "my-project:dataset-id.table-id")
+#
+def pubsub_create_bigquery_subscription project_id:, topic_id:, subscription_id:, bigquery_table_id:
+  pubsub = Google::Cloud::Pubsub.new project_id: project_id
+  topic = pubsub.topic topic_id
+  subscription = topic.subscribe subscription_id, 
+                                  bigquery_config: {
+                                  table: bigquery_table_id,
+                                  write_metadata: true
+                                  }
+  puts "BigQuery subscription created: #{subscription_id}."
+  puts "Table for subscription is: #{bigquery_table_id}"
+end
 # [END pubsub_create_bigquery_subscription]
