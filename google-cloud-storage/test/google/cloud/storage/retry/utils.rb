7cef4a219fa4842e74ea584b42ccee9cdaadbd8f
Neha Bajaj <bajajnehaa@google.com>
2022-09-28 14:15:08 +0530

    chore(storage): address review comments

--- google-cloud-storage/test/google/cloud/storage/retry/utils.rb
+++ google-cloud-storage/test/google/cloud/storage/retry/utils.rb
@@ -12,7 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-class MethodMapping
+require "securerandom"
+
+module MethodMapping
 
   CONF_TEST_SERVICE_ACCOUNT_EMAIL = "my-service-account@test.iam.gserviceaccount.com"
   CONF_TEST_ACL_ENTITY = "my-entity@example.net"
@@ -144,7 +146,7 @@ class MethodMapping
   end
 
   def self.insert_bucket client, _preconditions, **resources
-    bucket_name = "new_bucket" + Time.now.to_i.to_s
+    bucket_name = "new-bucket-" + Time.now.to_i.to_s + "-" + SecureRandom.hex(4)
     new_bucket = Google::Apis::StorageV1::Bucket.new({name: bucket_name})
     client.insert_bucket(new_bucket)
   end
@@ -242,38 +244,38 @@ class MethodMapping
     client.project_service_account
   end
 
-  def self.patch_bucket client, _preconditions, **resources
+  def self.patch_bucket client, preconditions, **resources
     bucket = resources[:bucket]
     metageneration = resources[:bucket].metageneration
     bucket_gapi = Google::Apis::StorageV1::Bucket.new storage_class: "COLDLINE"
-    if _preconditions
+    if preconditions
       client.patch_bucket bucket.name, bucket_gapi, if_metageneration_match: metageneration
     else
       client.patch_bucket bucket.name, bucket_gapi
     end
   end
 
-  def self.bucket_acl_public client, _preconditions, **resources
+  def self.bucket_acl_public client, preconditions, **resources
     bucket = resources[:bucket]
-    if _preconditions
+    if preconditions
       bucket.acl.public! if_metageneration_match: bucket.metageneration
     else
       bucket.acl.public!
     end
   end
 
-  def self.bucket_acl_private client, _preconditions, **resources
+  def self.bucket_acl_private client, preconditions, **resources
     bucket = resources[:bucket]
-    if _preconditions
+    if preconditions
       bucket.acl.private! if_metageneration_match: bucket.metageneration
     else
       bucket.acl.private!
     end
   end
 
-  def self.default_acl_owner_full client, _preconditions, **resources
+  def self.default_acl_owner_full client, preconditions, **resources
     bucket = resources[:bucket]
-    if _preconditions
+    if preconditions
       bucket.default_acl.owner_full! if_metageneration_match: bucket.metageneration
     else
       bucket.default_acl.owner_full!
@@ -293,12 +295,12 @@ class MethodMapping
     client.set_bucket_policy bucket_name, policy
   end
 
-  def self.update_hmac_key client, _preconditions, **resources
+  def self.update_hmac_key client, preconditions, **resources
     access_id = resources[:hmac_key].access_id
     etag = resources[:hmac_key].etag
 
     hmac_key = Google::Apis::StorageV1::HmacKeyMetadata.new access_id: access_id, state: "INACTIVE"
-    hmac_key.etag = etag if _preconditions
+    hmac_key.etag = etag if preconditions
     client.update_hmac_key access_id, hmac_key
   end
 
@@ -366,10 +368,10 @@ class MethodMapping
     bucket.create_notification pubsub_topic_name
   end
 
-  def self.insert_object client, _preconditions, **resources
+  def self.insert_object client, preconditions, **resources
     bucket = resources[:bucket]
-    file = StringIO.new CONF_TEST_FILE_CONTENT * 1024 * 1024 # 1MB
-    if _preconditions
+    file = StringIO.new CONF_TEST_FILE_CONTENT * 1024 * 1024 # 12MB
+    if preconditions
       bucket.create_file file, CONF_TEST_FILE_PATH, if_generation_match: 0
     else
       bucket.create_file file, CONF_TEST_FILE_PATH
@@ -379,7 +381,7 @@ class MethodMapping
   def self.get_object client, _preconditions, **resources
     bucket = resources[:bucket]
     object = resources[:object]
-    file = bucket.file object.name
+    bucket.file object.name
   end
 
   def self.get_file client, _preconditions, **resources
@@ -391,7 +393,7 @@ class MethodMapping
   def self.download_file client, _preconditions, **resources
     bucket = resources[:bucket]
     object = resources[:object]
-    res = object.download
+    object.download
   end  
 
   def self.delete_file_acl client, _preconditions, **resources
@@ -409,12 +411,12 @@ class MethodMapping
     client.insert_file_acl bucket.name, object.name, entity, role
   end
 
-  def self.compose_file client, _preconditions, **resources
+  def self.compose_file client, preconditions, **resources
     bucket = resources[:bucket]
     object = resources[:object]
     object_2 = bucket.create_file StringIO.new(CONF_TEST_FILE_CONTENT), "my-test-file-2"
     destination = "new-composite-object"
-    if _preconditions
+    if preconditions
       bucket.compose [object.name, object_2.name], destination, if_generation_match: 0 do |f|
         f.content_type = "text/plain"
       end
@@ -425,10 +427,10 @@ class MethodMapping
     end
   end
 
-  def self.delete_file client, _preconditions, **resources
+  def self.delete_file client, preconditions, **resources
     bucket = resources[:bucket]
     object = resources[:object]
-    if _preconditions
+    if preconditions
       client.delete_file bucket.name, object.name, if_generation_match: object.generation
     else
       client.delete_file bucket.name, object.name
@@ -440,20 +442,20 @@ class MethodMapping
     client.list_files bucket.name
   end
 
-  def self.patch_file client, _preconditions, **resources
+  def self.patch_file client, preconditions, **resources
     bucket = resources[:bucket]
     object = resources[:object]
-    if _preconditions
+    if preconditions
       client.patch_file bucket.name, object.name, if_metageneration_match: object.metageneration
     else
       client.patch_file bucket.name, object.name
     end
   end
 
-  def self.rewrite_file client, _preconditions, **resources
+  def self.rewrite_file client, preconditions, **resources
     bucket = resources[:bucket]
     object = resources[:object]
-    if _preconditions
+    if preconditions
       client.rewrite_file bucket.name, object.name, bucket.name, "destination-object", if_generation_match: 0
     else
       client.rewrite_file bucket.name, object.name, bucket.name, "destination-object"
