df960413ea0378ce6ca0df39f8030dd726f6007d
Neha Bajaj <bajajnehaa@google.com>
2022-10-18 19:59:59 +0530

    chore(storage): Add tests to cover empty string/file upload

--- google-cloud-storage/acceptance/storage/file_test.rb
+++ google-cloud-storage/acceptance/storage/file_test.rb
@@ -457,6 +457,23 @@ describe Google::Cloud::Storage::File, :storage do
     uploaded.delete
   end
 
+  it "should upload and download an empty file" do
+    data = ""
+    file = StringIO.new("")
+
+    uploaded = bucket.create_file file, "uploaded/empty-file.txt"
+    _(uploaded.name).must_equal "uploaded/empty-file.txt"
+
+    downloadio = StringIO.new()
+    downloaded = uploaded.download downloadio
+    _(downloaded).must_be_kind_of StringIO
+
+    downloaded_data = downloaded.read
+    _(downloaded_data).must_equal data
+
+    uploaded.delete
+  end
+
   it "should download and verify when Content-Encoding gzip response header with skip_decompress" do
     bucket = bucket_public
     file = bucket.file bucket_public_file_gzip
