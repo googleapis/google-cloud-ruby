6b32b705257bb2fb88f22e3aad866406147c84ce
Diptanshu Mittal <diptanshu@google.com>
2022-12-14 09:26:42 +0000

    Resolved unit test issue

--- google-cloud-bigquery/lib/google/cloud/bigquery/service.rb
+++ google-cloud-bigquery/lib/google/cloud/bigquery/service.rb
@@ -148,7 +148,11 @@ module Google
           metadata_view = table_metadata_view_type_for metadata_view
           # The get operation is considered idempotent
           execute backoff: true do
-            service.get_table project_id, dataset_id, table_id, view: metadata_view
+            if metadata_view.nil?
+              service.get_table project_id, dataset_id, table_id
+            else
+              service.get_table project_id, dataset_id, table_id, view: metadata_view
+            end
           end
         end
 
