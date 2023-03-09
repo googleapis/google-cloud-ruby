01fc08ba282b52964cba194da808b6d6bbaf76a6
Holt Skinner <holtskinner@google.com>
2023-03-09 11:29:30 -0600

    Import minitest

--- google-cloud-document_ai/samples/acceptance/quickstart_test.rb
+++ google-cloud-document_ai/samples/acceptance/quickstart_test.rb
@@ -12,6 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+require "minitest/autorun"
+require "minitest/focus"
+require "minitest/rg"
+
 require_relative "../../../.toys/.lib/sample_loader"
 
 describe "Document AI Quickstart" do
