9091986b81974d13edfb1827c92e7ca2ce73981a
Sandeep Tuniki <sandeep.tnk29@gmail.com>
2023-03-14 09:11:50 +0000

    Add documentation

--- google-cloud-datastore/lib/google/cloud/datastore/filter.rb
+++ google-cloud-datastore/lib/google/cloud/datastore/filter.rb
@@ -3,6 +3,53 @@ require "google/cloud/datastore/v1"
 module Google
   module Cloud
     module Datastore
+      ##
+      # # Filter
+      #
+      # Represents the filter criteria for a datastore query.
+      #
+      # @example Run a query with a simple property filter.
+      #   require "google/cloud/datastore"
+      #
+      #   datastore = Google::Cloud::Datastore.new
+      #
+      #   filter = Google::Cloud::Datastore::Filter.new("done", "=", "false")
+      #
+      #   query = Google::Cloud::Datastore::Query.new
+      #   query.kind("Task")
+      #        .where(filter)
+      #
+      #   tasks = datastore.run query
+      #
+      # @example Construct a composite filter with a logical OR.
+      #   require "google/cloud/datastore"
+      #
+      #   datastore = Google::Cloud::Datastore.new
+      #
+      #   filter = Google::Cloud::Datastore::Filter.new("done", "=", "false")
+      #                                            .or("priority", ">=", "4")
+      #
+      #   query = Google::Cloud::Datastore::Query.new
+      #   query.kind("Task")
+      #        .where(filter)
+      #
+      #   tasks = datastore.run query
+      #
+      # @example Construct a composite filter by combining multiple filters.
+      #   require "google/cloud/datastore"
+      #
+      #   datastore = Google::Cloud::Datastore.new
+      #
+      #   filter_1 = Google::Cloud::Datastore::Filter.new("done", "=", "false")
+      #   filter_2 = Google::Cloud::Datastore::Filter.new("priority", ">=", "4")
+      #   filter = filter_1.or(filter_2)
+      #
+      #   query = Google::Cloud::Datastore::Query.new
+      #   query.kind("Task")
+      #        .where(filter)
+      #
+      #   tasks = datastore.run query
+      #
       class Filter
         ##
         # @private Object of type
@@ -16,12 +63,6 @@ module Google
         #   require "google/cloud/datastore"
         #
         #   filter = Google::Cloud::Datastore::Filter.new("done", "=", "false")
-        #
-        #   query = Google::Cloud::Datastore::Query.new
-        #   query.kind("Task")
-        #        .where(filter)
-        #
-        #   tasks = datastore.run query
         #
         def initialize name_or_filter, operator = nil, value = nil
           @grpc = if name_or_filter.is_a? Google::Cloud::Datastore::V1::Filter
