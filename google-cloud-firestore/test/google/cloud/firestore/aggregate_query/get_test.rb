aa4a9ed9d7b17ec8e6d70c54a9b6c1710b6ddcab
Sandeep Tuniki <sandeep.tnk29@gmail.com>
2023-05-03 13:51:16 +0000

    Add methods for sum/avg aggregate

--- google-cloud-firestore/test/google/cloud/firestore/aggregate_query/get_test.rb
+++ google-cloud-firestore/test/google/cloud/firestore/aggregate_query/get_test.rb
@@ -24,7 +24,7 @@ describe Google::Cloud::Firestore::AggregateQuery, :add_count, :mock_firestore d
     )
   end
 
-  it "gets an aggregate query with a count" do
+  focus; it "gets an aggregate query with a count" do
     expected_params = Google::Cloud::Firestore::V1::RunAggregationQueryRequest.new(
       parent: parent,
       structured_aggregation_query: Google::Cloud::Firestore::V1::StructuredAggregationQuery.new(
@@ -60,7 +60,7 @@ describe Google::Cloud::Firestore::AggregateQuery, :add_count, :mock_firestore d
     end
   end
 
-  it "gets an aggregate query with custom alias" do
+  focus; it "gets an aggregate query with custom alias" do
     expected_params = Google::Cloud::Firestore::V1::RunAggregationQueryRequest.new(
       parent: parent,
       structured_aggregation_query: Google::Cloud::Firestore::V1::StructuredAggregationQuery.new(
@@ -97,7 +97,259 @@ describe Google::Cloud::Firestore::AggregateQuery, :add_count, :mock_firestore d
     end
   end
 
-  it "gets multiple aggregates of query" do
+  # Modify this set for SUM
+  focus; it "gets multiple aggregates of query" do
+    expected_params = Google::Cloud::Firestore::V1::RunAggregationQueryRequest.new(
+      parent: parent,
+      structured_aggregation_query: Google::Cloud::Firestore::V1::StructuredAggregationQuery.new(
+        structured_query: expected_structured_query,
+        aggregations: [
+          Google::Cloud::Firestore::V1::StructuredAggregationQuery::Aggregation.new(
+            count: Google::Cloud::Firestore::V1::StructuredAggregationQuery::Aggregation::Count.new,
+            alias: "alias_1"
+          ),
+          Google::Cloud::Firestore::V1::StructuredAggregationQuery::Aggregation.new(
+            count: Google::Cloud::Firestore::V1::StructuredAggregationQuery::Aggregation::Count.new,
+            alias: "alias_2"
+          ),
+          Google::Cloud::Firestore::V1::StructuredAggregationQuery::Aggregation.new(
+            count: Google::Cloud::Firestore::V1::StructuredAggregationQuery::Aggregation::Count.new,
+            alias: "alias_3"
+          ),
+        ]
+      )
+    )
+    mocked_response = [
+      Google::Cloud::Firestore::V1::RunAggregationQueryResponse.new(
+        result: Google::Cloud::Firestore::V1::AggregationResult.new(
+          aggregate_fields: {
+            "alias_1": Google::Cloud::Firestore::V1::Value.new(integer_value: 3),
+            "alias_2": Google::Cloud::Firestore::V1::Value.new(integer_value: 3),
+            "alias_3": Google::Cloud::Firestore::V1::Value.new(integer_value: 3)
+          }
+        )
+      )
+    ].to_enum
+    firestore_mock.expect :run_aggregation_query, mocked_response, [expected_params]
+
+    aq = query.aggregate_query.add_count(aggregate_alias: "alias_1")
+                              .add_count(aggregate_alias: "alias_2")
+                              .add_count(aggregate_alias: "alias_3")
+    results_enum = aq.get
+
+    _(results_enum).must_be_kind_of Enumerator
+    results = results_enum.to_a
+    _(results.count).must_equal 1
+    results.each do |result|
+      _(result).must_be_kind_of Google::Cloud::Firestore::AggregateQuerySnapshot 
+      _(result.get('alias_1')).must_equal 3
+      _(result.get('alias_2')).must_equal 3
+      _(result.get('alias_3')).must_equal 3
+    end
+  end
+
+  # Modify this set for SUM
+  focus; it "gets an aggregate query with a count" do
+    expected_params = Google::Cloud::Firestore::V1::RunAggregationQueryRequest.new(
+      parent: parent,
+      structured_aggregation_query: Google::Cloud::Firestore::V1::StructuredAggregationQuery.new(
+        structured_query: expected_structured_query,
+        aggregations: [
+          Google::Cloud::Firestore::V1::StructuredAggregationQuery::Aggregation.new(
+            count: Google::Cloud::Firestore::V1::StructuredAggregationQuery::Aggregation::Count.new,
+            alias: "count"
+          )
+        ]
+      )
+    )
+    mocked_response = [
+      Google::Cloud::Firestore::V1::RunAggregationQueryResponse.new(
+        result: Google::Cloud::Firestore::V1::AggregationResult.new(
+          aggregate_fields: {
+            "count": Google::Cloud::Firestore::V1::Value.new(integer_value: 3)
+          }
+        )
+      )
+    ].to_enum
+    firestore_mock.expect :run_aggregation_query, mocked_response, [expected_params]
+
+    aq = query.aggregate_query.add_count
+    results_enum = aq.get
+
+    _(results_enum).must_be_kind_of Enumerator
+    results = results_enum.to_a
+    _(results.count).must_equal 1
+    results.each do |result|
+      _(result).must_be_kind_of Google::Cloud::Firestore::AggregateQuerySnapshot 
+      _(result.get).must_equal 3
+    end
+  end
+
+  # Modify this set for SUM
+  focus; it "gets an aggregate query with custom alias" do
+    expected_params = Google::Cloud::Firestore::V1::RunAggregationQueryRequest.new(
+      parent: parent,
+      structured_aggregation_query: Google::Cloud::Firestore::V1::StructuredAggregationQuery.new(
+        structured_query: expected_structured_query,
+        aggregations: [
+          Google::Cloud::Firestore::V1::StructuredAggregationQuery::Aggregation.new(
+            count: Google::Cloud::Firestore::V1::StructuredAggregationQuery::Aggregation::Count.new,
+            alias: "total_score"
+          )
+        ]
+      )
+    )
+    mocked_response = [
+      Google::Cloud::Firestore::V1::RunAggregationQueryResponse.new(
+        result: Google::Cloud::Firestore::V1::AggregationResult.new(
+          aggregate_fields: {
+            "total_score": Google::Cloud::Firestore::V1::Value.new(integer_value: 3)
+          }
+        )
+      )
+    ].to_enum
+    firestore_mock.expect :run_aggregation_query, mocked_response, [expected_params]
+
+    aq = query.aggregate_query.add_count aggregate_alias: "total_score"
+    results_enum = aq.get
+
+    _(results_enum).must_be_kind_of Enumerator
+    results = results_enum.to_a
+    _(results.count).must_equal 1
+    results.each do |result|
+      _(result).must_be_kind_of Google::Cloud::Firestore::AggregateQuerySnapshot 
+      _(result.get).must_equal 3
+      _(result.get('total_score')).must_equal 3
+    end
+  end
+
+  focus; it "gets multiple aggregates of query" do
+    expected_params = Google::Cloud::Firestore::V1::RunAggregationQueryRequest.new(
+      parent: parent,
+      structured_aggregation_query: Google::Cloud::Firestore::V1::StructuredAggregationQuery.new(
+        structured_query: expected_structured_query,
+        aggregations: [
+          Google::Cloud::Firestore::V1::StructuredAggregationQuery::Aggregation.new(
+            count: Google::Cloud::Firestore::V1::StructuredAggregationQuery::Aggregation::Count.new,
+            alias: "alias_1"
+          ),
+          Google::Cloud::Firestore::V1::StructuredAggregationQuery::Aggregation.new(
+            count: Google::Cloud::Firestore::V1::StructuredAggregationQuery::Aggregation::Count.new,
+            alias: "alias_2"
+          ),
+          Google::Cloud::Firestore::V1::StructuredAggregationQuery::Aggregation.new(
+            count: Google::Cloud::Firestore::V1::StructuredAggregationQuery::Aggregation::Count.new,
+            alias: "alias_3"
+          ),
+        ]
+      )
+    )
+    mocked_response = [
+      Google::Cloud::Firestore::V1::RunAggregationQueryResponse.new(
+        result: Google::Cloud::Firestore::V1::AggregationResult.new(
+          aggregate_fields: {
+            "alias_1": Google::Cloud::Firestore::V1::Value.new(integer_value: 3),
+            "alias_2": Google::Cloud::Firestore::V1::Value.new(integer_value: 3),
+            "alias_3": Google::Cloud::Firestore::V1::Value.new(integer_value: 3)
+          }
+        )
+      )
+    ].to_enum
+    firestore_mock.expect :run_aggregation_query, mocked_response, [expected_params]
+
+    aq = query.aggregate_query.add_count(aggregate_alias: "alias_1")
+                              .add_count(aggregate_alias: "alias_2")
+                              .add_count(aggregate_alias: "alias_3")
+    results_enum = aq.get
+
+    _(results_enum).must_be_kind_of Enumerator
+    results = results_enum.to_a
+    _(results.count).must_equal 1
+    results.each do |result|
+      _(result).must_be_kind_of Google::Cloud::Firestore::AggregateQuerySnapshot 
+      _(result.get('alias_1')).must_equal 3
+      _(result.get('alias_2')).must_equal 3
+      _(result.get('alias_3')).must_equal 3
+    end
+  end
+
+  # Modify this set for AVG
+  focus; it "gets an aggregate query with a count" do
+    expected_params = Google::Cloud::Firestore::V1::RunAggregationQueryRequest.new(
+      parent: parent,
+      structured_aggregation_query: Google::Cloud::Firestore::V1::StructuredAggregationQuery.new(
+        structured_query: expected_structured_query,
+        aggregations: [
+          Google::Cloud::Firestore::V1::StructuredAggregationQuery::Aggregation.new(
+            count: Google::Cloud::Firestore::V1::StructuredAggregationQuery::Aggregation::Count.new,
+            alias: "count"
+          )
+        ]
+      )
+    )
+    mocked_response = [
+      Google::Cloud::Firestore::V1::RunAggregationQueryResponse.new(
+        result: Google::Cloud::Firestore::V1::AggregationResult.new(
+          aggregate_fields: {
+            "count": Google::Cloud::Firestore::V1::Value.new(integer_value: 3)
+          }
+        )
+      )
+    ].to_enum
+    firestore_mock.expect :run_aggregation_query, mocked_response, [expected_params]
+
+    aq = query.aggregate_query.add_count
+    results_enum = aq.get
+
+    _(results_enum).must_be_kind_of Enumerator
+    results = results_enum.to_a
+    _(results.count).must_equal 1
+    results.each do |result|
+      _(result).must_be_kind_of Google::Cloud::Firestore::AggregateQuerySnapshot 
+      _(result.get).must_equal 3
+    end
+  end
+
+  # Modify this set for AVG
+  focus; it "gets an aggregate query with custom alias" do
+    expected_params = Google::Cloud::Firestore::V1::RunAggregationQueryRequest.new(
+      parent: parent,
+      structured_aggregation_query: Google::Cloud::Firestore::V1::StructuredAggregationQuery.new(
+        structured_query: expected_structured_query,
+        aggregations: [
+          Google::Cloud::Firestore::V1::StructuredAggregationQuery::Aggregation.new(
+            count: Google::Cloud::Firestore::V1::StructuredAggregationQuery::Aggregation::Count.new,
+            alias: "total_score"
+          )
+        ]
+      )
+    )
+    mocked_response = [
+      Google::Cloud::Firestore::V1::RunAggregationQueryResponse.new(
+        result: Google::Cloud::Firestore::V1::AggregationResult.new(
+          aggregate_fields: {
+            "total_score": Google::Cloud::Firestore::V1::Value.new(integer_value: 3)
+          }
+        )
+      )
+    ].to_enum
+    firestore_mock.expect :run_aggregation_query, mocked_response, [expected_params]
+
+    aq = query.aggregate_query.add_count aggregate_alias: "total_score"
+    results_enum = aq.get
+
+    _(results_enum).must_be_kind_of Enumerator
+    results = results_enum.to_a
+    _(results.count).must_equal 1
+    results.each do |result|
+      _(result).must_be_kind_of Google::Cloud::Firestore::AggregateQuerySnapshot 
+      _(result.get).must_equal 3
+      _(result.get('total_score')).must_equal 3
+    end
+  end
+
+  # Modify this set for AVG
+  focus; it "gets multiple aggregates of query" do
     expected_params = Google::Cloud::Firestore::V1::RunAggregationQueryRequest.new(
       parent: parent,
       structured_aggregation_query: Google::Cloud::Firestore::V1::StructuredAggregationQuery.new(
