7d4546e76beafc5e981cbdef52655a5996749a4f
Shiv Gautam <shivng@google.com>
2023-05-23 04:47:39 +0000

    chore(samples): Splitting topic samples in pubsub

---
+++ google-cloud-pubsub/samples/create_ordered_pull_subscription.rb
@@ -0,0 +1,36 @@
+# Copyright 2021 Google, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require "google/cloud/pubsub"
+
+def create_ordered_pull_subscription topic_id:, subscription_id:
+  # [START pubsub_enable_subscription_ordering]
+  # topic_id        = "your-topic-id"
+  # subscription_id = "your-subscription-id"
+  require "google/cloud/pubsub"
+
+  pubsub = Google::Cloud::Pubsub.new
+
+  topic        = pubsub.topic topic_id
+  subscription = topic.subscribe subscription_id,
+                                 message_ordering: true
+
+  puts "Pull subscription #{subscription_id} created with message ordering."
+  # [END pubsub_enable_subscription_ordering]
+end
+
+if $PROGRAM_NAME == __FILE__
+  create_ordered_pull_subscription topic_id:        ARGV.shift,
+                                   subscription_id: ARGV.shift
+end
