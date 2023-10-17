d5e837b48780992720866170677ea236ce90ce0b
Sandeep Tuniki <sandeep.tnk29@gmail.com>
2023-10-17 12:19:04 +0530

    Fix unit tests for sum/avg aggregates

--- google-cloud-firestore/test/google/cloud/firestore/aggregate_query/get_test.rb
+++ google-cloud-firestore/test/google/cloud/firestore/aggregate_query/get_test.rb
@@ -16,26 +16,30 @@ require "helper"
 
 describe Google::Cloud::Firestore::AggregateQuery, :add_count, :mock_firestore do
   let (:parent) { "projects/projectID/databases/(default)/documents" }
-  let(:query) { Google::Cloud::Firestore::Query.start(nil, "#{firestore.path}/documents", firestore).select(:name) }
+  let(:query) { Google::Cloud::Firestore::Query.start(nil, "#{firestore.path}/documents", firestore).select(:score) }
 
-  it "creates empty AggregateQuery object" do
-    aggregate_query = query.aggregate_query
+  describe "Common" do
+    focus
+    it "creates empty AggregateQuery object" do
+      aggregate_query = query.aggregate_query
 
-    _(aggregate_query).must_be_kind_of Google::Cloud::Firestore::AggregateQuery
+      _(aggregate_query).must_be_kind_of Google::Cloud::Firestore::AggregateQuery
 
-    grpc = aggregate_query.to_grpc
-    _(grpc).must_be_kind_of Google::Cloud::Firestore::V1::StructuredAggregationQuery
+      grpc = aggregate_query.to_grpc
+      _(grpc).must_be_kind_of Google::Cloud::Firestore::V1::StructuredAggregationQuery
 
-    _(grpc.structured_query).wont_be :nil?
-    _(grpc.structured_query).must_be_kind_of Google::Cloud::Firestore::V1::StructuredQuery
-    _(grpc.structured_query).must_equal query.query
+      _(grpc.structured_query).wont_be :nil?
+      _(grpc.structured_query).must_be_kind_of Google::Cloud::Firestore::V1::StructuredQuery
+      _(grpc.structured_query).must_equal query.query
 
-    _(grpc.aggregations).wont_be :nil?
-    _(grpc.aggregations).must_be_kind_of Google::Protobuf::RepeatedField
-    _(grpc.aggregations.size).must_equal 0
+      _(grpc.aggregations).wont_be :nil?
+      _(grpc.aggregations).must_be_kind_of Google::Protobuf::RepeatedField
+      _(grpc.aggregations.size).must_equal 0
+    end
   end
 
   describe "COUNT" do
+    focus
     it "creates COUNT aggregate with default alias" do
       aggregate_query = query.aggregate_query
                              .add_count
@@ -59,6 +63,7 @@ describe Google::Cloud::Firestore::AggregateQuery, :add_count, :mock_firestore d
       _(grpc.aggregations.first.count).must_be_kind_of Google::Cloud::Firestore::V1::StructuredAggregationQuery::Aggregation::Count
     end
 
+    focus
     it "creates COUNT aggregate with custom alias" do
       aggregate_query = query.aggregate_query
                              .add_count aggregate_alias: 'total'
@@ -82,6 +87,7 @@ describe Google::Cloud::Firestore::AggregateQuery, :add_count, :mock_firestore d
       _(grpc.aggregations.first.count).must_be_kind_of Google::Cloud::Firestore::V1::StructuredAggregationQuery::Aggregation::Count
     end
 
+    focus
     it "creates multiple COUNT aggregates" do
       aggregate_query = query.aggregate_query
                              .add_count(aggregate_alias: 'total_1')
@@ -115,6 +121,7 @@ describe Google::Cloud::Firestore::AggregateQuery, :add_count, :mock_firestore d
       _(grpc.aggregations[2].count).must_be_kind_of Google::Cloud::Firestore::V1::StructuredAggregationQuery::Aggregation::Count
     end
 
+    focus
     it "does not mutate existing AggregateQuery object on adding more aggregates" do
       aggregate_query_1 = query.aggregate_query
                                .add_count(aggregate_alias: 'total_1')
@@ -130,13 +137,14 @@ describe Google::Cloud::Firestore::AggregateQuery, :add_count, :mock_firestore d
                                              .to_grpc)
     end
 
+    focus
     it "gets an aggregate query with custom alias" do
       expected_params = Google::Cloud::Firestore::V1::RunAggregationQueryRequest.new(
         parent: parent,
         structured_aggregation_query: Google::Cloud::Firestore::V1::StructuredAggregationQuery.new(
           structured_query: Google::Cloud::Firestore::V1::StructuredQuery.new(
             select: Google::Cloud::Firestore::V1::StructuredQuery::Projection.new(
-              fields: [Google::Cloud::Firestore::V1::StructuredQuery::FieldReference.new(field_path: "name")])
+              fields: [Google::Cloud::Firestore::V1::StructuredQuery::FieldReference.new(field_path: "score")])
           ),
           aggregations: [
             Google::Cloud::Firestore::V1::StructuredAggregationQuery::Aggregation.new(
@@ -172,7 +180,8 @@ describe Google::Cloud::Firestore::AggregateQuery, :add_count, :mock_firestore d
   end
 
   describe "SUM" do
-    focus; it "creates SUM aggregate with default alias" do
+    focus
+    it "creates SUM aggregate with default alias" do
       aggregate_query = query.aggregate_query
                              .add_sum('score')
 
@@ -195,7 +204,8 @@ describe Google::Cloud::Firestore::AggregateQuery, :add_count, :mock_firestore d
       _(grpc.aggregations.first.sum).must_be_kind_of Google::Cloud::Firestore::V1::StructuredAggregationQuery::Aggregation::Sum
     end
 
-    focus; it "creates SUM aggregate with custom alias" do
+    focus
+    it "creates SUM aggregate with custom alias" do
       aggregate_query = query.aggregate_query
                              .add_sum 'score', aggregate_alias: 'total'
 
@@ -218,7 +228,8 @@ describe Google::Cloud::Firestore::AggregateQuery, :add_count, :mock_firestore d
       _(grpc.aggregations.first.sum).must_be_kind_of Google::Cloud::Firestore::V1::StructuredAggregationQuery::Aggregation::Sum
     end
 
-    focus; it "creates multiple SUM aggregates" do
+    focus
+    it "creates multiple SUM aggregates" do
       aggregate_query = query.aggregate_query
                              .add_sum('score', aggregate_alias: 'total_1')
                              .add_sum('score', aggregate_alias: 'total_2')
@@ -251,7 +262,8 @@ describe Google::Cloud::Firestore::AggregateQuery, :add_count, :mock_firestore d
       _(grpc.aggregations[2].sum).must_be_kind_of Google::Cloud::Firestore::V1::StructuredAggregationQuery::Aggregation::Sum
     end
 
-    focus; it "does not mutate existing AggregateQuery object on adding more aggregates" do
+    focus
+    it "does not mutate existing AggregateQuery object on adding more aggregates" do
       aggregate_query_1 = query.aggregate_query
                                .add_sum('score', aggregate_alias: 'total_1')
       aggregate_query_2 = aggregate_query_1
@@ -266,18 +278,20 @@ describe Google::Cloud::Firestore::AggregateQuery, :add_count, :mock_firestore d
                                              .to_grpc)
     end
 
+    focus
     it "gets an aggregate query with custom alias" do
-      skip # fix it after writing an acceptance test
       expected_params = Google::Cloud::Firestore::V1::RunAggregationQueryRequest.new(
         parent: parent,
         structured_aggregation_query: Google::Cloud::Firestore::V1::StructuredAggregationQuery.new(
           structured_query: Google::Cloud::Firestore::V1::StructuredQuery.new(
             select: Google::Cloud::Firestore::V1::StructuredQuery::Projection.new(
-              fields: [Google::Cloud::Firestore::V1::StructuredQuery::FieldReference.new(field_path: "name")])
+              fields: [Google::Cloud::Firestore::V1::StructuredQuery::FieldReference.new(field_path: "score")])
           ),
           aggregations: [
             Google::Cloud::Firestore::V1::StructuredAggregationQuery::Aggregation.new(
-              sum: Google::Cloud::Firestore::V1::StructuredAggregationQuery::Aggregation::Sum.new,
+              sum: Google::Cloud::Firestore::V1::StructuredAggregationQuery::Aggregation::Sum.new(
+                field: Google::Cloud::Firestore::V1::StructuredQuery::FieldReference.new(field_path: "score")
+              ),
               alias: "total_score" # custom alias
             )
           ]
@@ -294,12 +308,155 @@ describe Google::Cloud::Firestore::AggregateQuery, :add_count, :mock_firestore d
       ].to_enum
       firestore_mock.expect :run_aggregation_query, mocked_response, [expected_params]
 
-      aggregate_query = query.aggregate_query.add_sum aggregate_alias: "total_score" # custom alias
+      aggregate_query = query.aggregate_query.add_sum 'score', aggregate_alias: "total_score" # custom alias
       results_enum = aggregate_query.get
 
       _(results_enum).must_be_kind_of Enumerator
       results = results_enum.to_a
-      _(results.sum).must_equal 1
+      _(results.count).must_equal 1
+      results.each do |result|
+        _(result).must_be_kind_of Google::Cloud::Firestore::AggregateQuerySnapshot
+        _(result.get).must_equal 3
+        _(result.get('total_score')).must_equal 3
+      end
+    end
+  end
+
+  describe "AVG" do
+    focus
+    it "creates AVG aggregate with default alias" do
+      aggregate_query = query.aggregate_query
+                             .add_avg('score')
+
+      _(aggregate_query).must_be_kind_of Google::Cloud::Firestore::AggregateQuery
+
+      grpc = aggregate_query.to_grpc
+      _(grpc).must_be_kind_of Google::Cloud::Firestore::V1::StructuredAggregationQuery
+
+      _(grpc.structured_query).wont_be :nil?
+      _(grpc.structured_query).must_be_kind_of Google::Cloud::Firestore::V1::StructuredQuery
+      _(grpc.structured_query).must_equal query.query
+
+      _(grpc.aggregations).wont_be :nil?
+      _(grpc.aggregations).must_be_kind_of Google::Protobuf::RepeatedField
+      _(grpc.aggregations.size).must_equal 1
+
+      _(grpc.aggregations.first.alias).wont_be :nil?
+      _(grpc.aggregations.first.alias).must_equal 'avg' # default alias
+      _(grpc.aggregations.first.avg).wont_be :nil?
+      _(grpc.aggregations.first.avg).must_be_kind_of Google::Cloud::Firestore::V1::StructuredAggregationQuery::Aggregation::Avg
+    end
+
+    focus
+    it "creates AVG aggregate with custom alias" do
+      aggregate_query = query.aggregate_query
+                             .add_avg 'score', aggregate_alias: 'avg_score'
+
+      _(aggregate_query).must_be_kind_of Google::Cloud::Firestore::AggregateQuery
+
+      grpc = aggregate_query.to_grpc
+      _(grpc).must_be_kind_of Google::Cloud::Firestore::V1::StructuredAggregationQuery
+
+      _(grpc.structured_query).wont_be :nil?
+      _(grpc.structured_query).must_be_kind_of Google::Cloud::Firestore::V1::StructuredQuery
+      _(grpc.structured_query).must_equal query.query
+
+      _(grpc.aggregations).wont_be :nil?
+      _(grpc.aggregations).must_be_kind_of Google::Protobuf::RepeatedField
+      _(grpc.aggregations.size).must_equal 1
+
+      _(grpc.aggregations.first.alias).wont_be :nil?
+      _(grpc.aggregations.first.alias).must_equal 'avg_score' # custom alias
+      _(grpc.aggregations.first.avg).wont_be :nil?
+      _(grpc.aggregations.first.avg).must_be_kind_of Google::Cloud::Firestore::V1::StructuredAggregationQuery::Aggregation::Avg
+    end
+
+    focus
+    it "creates multiple AVG aggregates" do
+      aggregate_query = query.aggregate_query
+                             .add_avg('score', aggregate_alias: 'score_1')
+                             .add_avg('score', aggregate_alias: 'score_2')
+                             .add_avg('score', aggregate_alias: 'score_3')
+
+      grpc = aggregate_query.to_grpc
+      _(grpc).must_be_kind_of Google::Cloud::Firestore::V1::StructuredAggregationQuery
+
+      _(grpc.structured_query).wont_be :nil?
+      _(grpc.structured_query).must_be_kind_of Google::Cloud::Firestore::V1::StructuredQuery
+      _(grpc.structured_query).must_equal query.query
+
+      _(grpc.aggregations).wont_be :nil?
+      _(grpc.aggregations).must_be_kind_of Google::Protobuf::RepeatedField
+      _(grpc.aggregations.size).must_equal 3
+
+      _(grpc.aggregations[0].alias).wont_be :nil?
+      _(grpc.aggregations[0].alias).must_equal 'score_1'
+      _(grpc.aggregations[0].avg).wont_be :nil?
+      _(grpc.aggregations[0].avg).must_be_kind_of Google::Cloud::Firestore::V1::StructuredAggregationQuery::Aggregation::Avg
+
+      _(grpc.aggregations[1].alias).wont_be :nil?
+      _(grpc.aggregations[1].alias).must_equal 'score_2'
+      _(grpc.aggregations[1].avg).wont_be :nil?
+      _(grpc.aggregations[1].avg).must_be_kind_of Google::Cloud::Firestore::V1::StructuredAggregationQuery::Aggregation::Avg
+
+      _(grpc.aggregations[2].alias).wont_be :nil?
+      _(grpc.aggregations[2].alias).must_equal 'score_3'
+      _(grpc.aggregations[2].avg).wont_be :nil?
+      _(grpc.aggregations[2].avg).must_be_kind_of Google::Cloud::Firestore::V1::StructuredAggregationQuery::Aggregation::Avg
+    end
+
+    focus
+    it "does not mutate existing AggregateQuery object on adding more aggregates" do
+      aggregate_query_1 = query.aggregate_query
+                               .add_avg('score', aggregate_alias: 'score_1')
+      aggregate_query_2 = aggregate_query_1
+                               .add_avg('score', aggregate_alias: 'score_2')
+      refute aggregate_query_1.to_grpc == aggregate_query_2.to_grpc
+      _(aggregate_query_1.to_grpc).must_equal(query.aggregate_query
+                                              .add_avg('score', aggregate_alias: 'score_1')
+                                              .to_grpc)
+      _(aggregate_query_2.to_grpc).must_equal(query.aggregate_query
+                                             .add_avg('score', aggregate_alias: 'score_1')
+                                             .add_avg('score', aggregate_alias: 'score_2')
+                                             .to_grpc)
+    end
+
+    focus
+    it "gets an aggregate query with custom alias" do
+      expected_params = Google::Cloud::Firestore::V1::RunAggregationQueryRequest.new(
+        parent: parent,
+        structured_aggregation_query: Google::Cloud::Firestore::V1::StructuredAggregationQuery.new(
+          structured_query: Google::Cloud::Firestore::V1::StructuredQuery.new(
+            select: Google::Cloud::Firestore::V1::StructuredQuery::Projection.new(
+              fields: [Google::Cloud::Firestore::V1::StructuredQuery::FieldReference.new(field_path: "score")])
+          ),
+          aggregations: [
+            Google::Cloud::Firestore::V1::StructuredAggregationQuery::Aggregation.new(
+              avg: Google::Cloud::Firestore::V1::StructuredAggregationQuery::Aggregation::Avg.new(
+                field: Google::Cloud::Firestore::V1::StructuredQuery::FieldReference.new(field_path: "score")
+              ),
+              alias: "total_score" # custom alias
+            )
+          ]
+        )
+      )
+      mocked_response = [
+        Google::Cloud::Firestore::V1::RunAggregationQueryResponse.new(
+          result: Google::Cloud::Firestore::V1::AggregationResult.new(
+            aggregate_fields: {
+              "total_score": Google::Cloud::Firestore::V1::Value.new(integer_value: 3)
+            }
+          )
+        )
+      ].to_enum
+      firestore_mock.expect :run_aggregation_query, mocked_response, [expected_params]
+
+      aggregate_query = query.aggregate_query.add_avg 'score', aggregate_alias: "total_score" # custom alias
+      results_enum = aggregate_query.get
+
+      _(results_enum).must_be_kind_of Enumerator
+      results = results_enum.to_a
+      _(results.count).must_equal 1
       results.each do |result|
         _(result).must_be_kind_of Google::Cloud::Firestore::AggregateQuerySnapshot
         _(result.get).must_equal 3
