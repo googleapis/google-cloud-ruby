4338b837a307921ae542da22892149f2f43d78d5
Holt Skinner <13262395+holtskinner@users.noreply.github.com>
2023-11-06 00:26:00 -0600

    sample(ai_platform): Add Vertex AI text prompt sample (#23499)

--- google-cloud-document_ai/samples/quickstart.rb
+++ google-cloud-document_ai/samples/quickstart.rb
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 # [START documentai_quickstart]
-require "google/cloud/document_ai"
+require "google/cloud/document_ai/v1"
 
 ##
 # Document AI quickstart
@@ -26,7 +26,9 @@ require "google/cloud/document_ai"
 #
 def quickstart project_id:, location_id:, processor_id:, file_path:, mime_type:
   # Create the Document AI client.
-  client = Google::Cloud::DocumentAI.document_processor_service
+  client = ::Google::Cloud::DocumentAI::V1::DocumentProcessorService::Client.new do |config|
+    config.endpoint = "#{location_id}-documentai.googleapis.com"
+  end
 
   # Build the resource name from the project.
   name = client.processor_path(
