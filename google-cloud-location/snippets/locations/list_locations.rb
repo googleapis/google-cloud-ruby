467587a6c20cc54618060aa8cc2a90b0a89f87da
Daniel Azuma <dazuma@google.com>
2024-01-08 13:23:28 -0800

    feat: Support for universe domain in mixin clients (#23746)

--- google-cloud-location/snippets/locations/list_locations.rb
+++ google-cloud-location/snippets/locations/list_locations.rb
@@ -22,9 +22,14 @@ require "google/cloud/location"
 ##
 # Snippet for the list_locations call in the Locations service
 #
+# This snippet has been automatically generated and should be regarded as a code
+# template only. It will require modifications to work:
+# - It may require correct/in-range values for request initialization.
+# - It may require specifying regional endpoints when creating the service
+# client as shown in https://cloud.google.com/ruby/docs/reference.
+#
 # This is an auto-generated example demonstrating basic usage of
-# Google::Cloud::Location::Locations::Client#list_locations. It may require
-# modification in order to execute successfully.
+# Google::Cloud::Location::Locations::Client#list_locations.
 #
 def list_locations
   # Create a client object. The client can be reused for multiple calls.
