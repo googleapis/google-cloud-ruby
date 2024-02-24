cfa492b56eb2810229b92786b6d2e7b69e5ded0f
Daniel Azuma <dazuma@google.com>
2024-02-23 17:02:22 -0800

    chore: Additional minor rubocop fixes (#25055)

--- google-cloud-container_analysis/samples/acceptance/sample_test.rb
+++ google-cloud-container_analysis/samples/acceptance/sample_test.rb
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-require_relative "./helper"
+require_relative "helper"
 require_relative "../sample"
 
 require "pathname"
