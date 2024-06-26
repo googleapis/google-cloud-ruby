0398d424c5534c0c0a92b8d494acd40def348770
Neha Bajaj <bajajneha27@users.noreply.github.com>
2024-06-26 10:42:58 +0530

    samples(storage): add samples for object retention (#26158)

--- google-cloud-storage/samples/acceptance/buckets_test.rb
+++ google-cloud-storage/samples/acceptance/buckets_test.rb
@@ -21,6 +21,7 @@ require_relative "../storage_create_bucket"
 require_relative "../storage_create_bucket_class_location"
 require_relative "../storage_create_bucket_dual_region"
 require_relative "../storage_create_bucket_hierarchical_namespace"
+require_relative "../storage_create_bucket_with_object_retention"
 require_relative "../storage_define_bucket_website_configuration"
 require_relative "../storage_delete_bucket"
 require_relative "../storage_disable_bucket_lifecycle_management"
@@ -45,6 +46,7 @@ require_relative "../storage_remove_bucket_label"
 require_relative "../storage_remove_cors_configuration"
 require_relative "../storage_remove_retention_policy"
 require_relative "../storage_set_bucket_default_kms_key"
+require_relative "../storage_set_object_retention_policy"
 require_relative "../storage_set_public_access_prevention_enforced"
 require_relative "../storage_set_public_access_prevention_inherited"
 require_relative "../storage_set_retention_policy"
@@ -160,6 +162,43 @@ describe "Buckets Snippets" do
 
       refute_nil storage_client.bucket bucket_name
 
+      delete_bucket_helper bucket_name
+    end
+  end
+
+  describe "storage_create_bucket_with_object_retention" do
+    it "creates a bucket with object retention enabled." do
+      bucket_name = random_bucket_name
+      refute storage_client.bucket bucket_name
+
+      expected = "Created bucket #{bucket_name} with object retention setting: Enabled\n"
+
+      retry_resource_exhaustion do
+        assert_output expected do
+          create_bucket_with_object_retention bucket_name: bucket_name
+        end
+      end
+
+      refute_nil storage_client.bucket bucket_name
+
+      file_name = "test_object_retention"
+
+      bucket = storage_client.bucket bucket_name
+
+      out, _err = capture_io do
+        set_object_retention_policy bucket_name: bucket.name,
+                                    content: "hello world",
+                                    destination_file_name: file_name
+      end
+
+      assert_includes out, "Retention policy for file #{file_name}"
+
+      file = bucket.file file_name
+      file.retention = {
+        mode: nil,
+        retain_until_time: nil,
+        override_unlocked_retention: true
+      }
       delete_bucket_helper bucket_name
     end
   end
