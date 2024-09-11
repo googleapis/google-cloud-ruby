7cb87825ba2df8d11a0c5aba8532a858132e7bc9
Shubhangi Singh <shubhangisin@google.com>
2024-09-11 11:16:41 +0000

    removing unwanted samples created

--- google-cloud-storage/samples/acceptance/files_test.rb
+++ google-cloud-storage/samples/acceptance/files_test.rb
@@ -60,7 +60,6 @@ describe "Files Snippets" do
   let(:downloaded_file)  { "test_download_#{SecureRandom.hex}" }
   let(:file_1_name) { "path/file_1_name_#{SecureRandom.hex}.txt" }
   let(:file_2_name) { "path/file_2_name_#{SecureRandom.hex}.txt" }
-  let(:gs_url) { "gs://my-todo-app/test_sub_folder/heidi.jpeg?params1=test1&params2=test2" }
   let(:bucket) { @bucket }
   let(:secondary_bucket) { @secondary_bucket }
   let(:file_content) { "some content" }
@@ -273,12 +272,6 @@ describe "Files Snippets" do
     end
 
     assert_nil bucket.file remote_file_name
-  end
-
-  it "fetch_from_gs_url" do
-    fetch_from_gs_url gs_url
-
-    assert_output "Output json {'bucket_name'=>'test_nucket_2', 'file_name'=>'floer.jpeg'}"
   end
 
   it "get_metadata" do
