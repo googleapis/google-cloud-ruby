a331bbdd32a1cbd02997be72c9d5c52ac5bf5388
Shubhangi Singh <shubhangisin@google.com>
2024-09-30 07:54:30 +0000

    adding system env vars to test file

--- google-cloud-storage/acceptance/storage/universe_domain_test.rb
+++ google-cloud-storage/acceptance/storage/universe_domain_test.rb
@@ -16,6 +16,13 @@ require "storage_helper"
 
 describe Google::Cloud::Storage do
 
+  system(
+    "export TEST_UNIVERSE_DOMAIN_CREDENTIAL=$(realpath ${KOKORO_GFILE_DIR}/secret_manager/client-library-test-universe-domain-credential)
+    export TEST_UNIVERSE_DOMAIN=$(gcloud secrets versions access latest --project cloud-devrel-kokoro-resources --secret=client-library-test-universe-domain)
+    export TEST_UNIVERSE_PROJECT_ID=$(gcloud secrets versions access latest --project cloud-devrel-kokoro-resources --secret=client-library-test-universe-project-id)
+    export TEST_UNIVERSE_LOCATION=$(gcloud secrets versions access latest --project cloud-devrel-kokoro-resources --secret=client-library-test-universe-storage-location)"
+  )
+            
   # Universe Domain Test project Credentials
   $test_universe_domain = ENV["TEST_UNIVERSE_DOMAIN"]
   $test_universe_project_id = ENV["TEST_UNIVERSE_PROJECT_ID"]
