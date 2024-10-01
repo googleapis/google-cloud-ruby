538a75bb889e151657bf86046101127fa95d0c80
Shubhangi Singh <shubhangisin@google.com>
2024-10-01 07:11:16 +0000

    trying env vars

--- google-cloud-storage/acceptance/storage/universe_domain_test.rb
+++ google-cloud-storage/acceptance/storage/universe_domain_test.rb
@@ -15,14 +15,6 @@
 require "storage_helper"
 
 describe Google::Cloud::Storage do
-
-  system(
-    "export TEST_UNIVERSE_DOMAIN_CREDENTIAL=$(realpath ${KOKORO_GFILE_DIR}/secret_manager/client-library-test-universe-domain-credential)
-    export TEST_UNIVERSE_DOMAIN=$(gcloud secrets versions access latest --project cloud-devrel-kokoro-resources --secret=client-library-test-universe-domain)
-    export TEST_UNIVERSE_PROJECT_ID=$(gcloud secrets versions access latest --project cloud-devrel-kokoro-resources --secret=client-library-test-universe-project-id)
-    export TEST_UNIVERSE_LOCATION=$(gcloud secrets versions access latest --project cloud-devrel-kokoro-resources --secret=client-library-test-universe-storage-location)"
-  )
-
   let :storage do
     # Universe Domain Test project Credentials
     Google::Cloud::Storage.new(
