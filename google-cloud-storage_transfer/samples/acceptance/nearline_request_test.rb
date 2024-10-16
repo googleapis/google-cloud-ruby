3fb5173d9bf71836923456628795cd0d454a025e
Shubhangi Singh <shubhangisin@google.com>
2024-10-16 19:26:36 +0000

    wip

---
+++ google-cloud-storage_transfer/samples/acceptance/nearline_request_test.rb
@@ -0,0 +1,42 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+require "pry"
+
+require_relative "helper"
+require_relative "../nearline_request"
+
+describe "Storage Transfer Service To Nearline Transfer" do
+  let(:project) { Google::Cloud::Storage.new }
+  let(:source_bucket) { create_bucket_helper random_bucket_name }
+  let(:sink_bucket) { create_nearline_bucket_helper random_bucket_name }
+
+  before do
+    grant_sts_permissions project_id: project.project_id, bucket_name: source_bucket.name
+    grant_sts_permissions project_id: project.project_id, bucket_name: sink_bucket.name
+  end
+
+  after do
+    delete_bucket_helper source_bucket.name
+    delete_bucket_helper sink_bucket.name
+  end
+
+  it "creates a transfer job" do
+    out, _err = capture_io do
+      create_daily_nearline_30_day_migration project_id: project.project_id, gcs_source_bucket: source_bucket.name, gcs_sink_bucket: sink_bucket.name, start_date: Time.now
+    end
+    assert_includes out, "transferJobs"
+    job_name = out.scan(%r{(transferJobs/.*)}).flatten.first
+    delete_transfer_job project_id: project.project_id, job_name: job_name
+  end
+end
---
+++ google-cloud-storage_transfer/samples/nearline_request.rb
@@ -0,0 +1,75 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# [Start storagetransfer_transfer_to_nearline]
+def create_daily_nearline_30_day_migration project_id:, gcs_source_bucket:, gcs_sink_bucket:, start_date:
+    # Your Google Cloud Project ID
+    # project_id = "your-project_id"
+  
+    # The name of the source GCS bucket to transfer objects from
+    # gcs_source_bucket = "your-source-gcs-source-bucket"
+  
+    # The name of the  GCS bucket to transfer objects to
+    # gcs_sink_bucket = "your-sink-gcs-bucket"
+  
+    require "google/cloud/storage_transfer"
+  
+    transfer_job = {
+      project_id: project_id,
+      transfer_spec: {
+        gcs_data_source: {
+          bucket_name: gcs_source_bucket
+        },
+        gcs_data_sink: {
+          bucket_name: gcs_sink_bucket
+        },
+        object_conditions: {
+            min_time_elapsed_since_last_modification: {
+                seconds: 86400  # Minimum time of 1 day (24 hours)
+              }
+        },
+        transfer_options: {
+            delete_objects_from_source_after_transfer: true,
+        }
+      },
+      schedule: {
+        schedule_start_date: {
+            day: start_date.day,
+            month: start_date.month,
+            year: start_date.year
+        }
+
+      },
+      status: :ENABLED
+    }
+  
+    client = Google::Cloud::StorageTransfer.storage_transfer_service
+  
+    transfer_job_response = client.create_transfer_job transfer_job: transfer_job
+  
+    run_request = {
+      project_id: project_id,
+      job_name: transfer_job_response.name
+    }
+    client.run_transfer_job run_request
+  
+    puts "Created and ran transfer job between #{gcs_source_bucket} and #{gcs_sink_bucket} with name #{transfer_job_response.name}"
+  end
+  # [END storagetransfer_transfer_to_nearline]
+
+  
+  if $PROGRAM_NAME == __FILE__
+    create_daily_nearline_30_day_migration project_id: ARGV.shift, gcs_source_bucket: ARGV.shift, gcs_sink_bucket: ARGV.shift, start_date: ARGV.shift
+  end
+  
