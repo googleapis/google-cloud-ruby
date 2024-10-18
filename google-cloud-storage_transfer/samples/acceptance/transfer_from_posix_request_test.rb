e8db0f66d74d0b449fdd7c7ac351676d18a8587f
Shubhangi Singh <shubhangisin@google.com>
2024-10-18 11:48:35 +0000

    transfer from posix sample tests  passed

--- google-cloud-storage_transfer/samples/acceptance/transfer_from_posix_request_test.rb
+++ google-cloud-storage_transfer/samples/acceptance/transfer_from_posix_request_test.rb
@@ -14,14 +14,12 @@
 
 require_relative "helper"
 require_relative "../transfer_from_posix_request"
-require "pry"
-
 
 describe "Storage Transfer Service from POSIX" do
   let(:project) { Google::Cloud::Storage.new }
-  #{}let(:source_bucket) { create_bucket_helper random_bucket_name }
   let(:sink_bucket) { create_bucket_helper random_bucket_name }
-  let(:root_directory) { "/tmp/" }
+  let(:agent_pool_name) { "" }
+  let(:root_directory) { "/tmp/uploads" }
   let(:dummy_file_name) { "ruby_storagetransfer_samples_dummy_#{SecureRandom.hex}.txt" }
   let(:dummy_file_path) { "#{root_directory}/#{dummy_file_name}" }
   let(:create_dummy_file) {
@@ -29,31 +27,14 @@ describe "Storage Transfer Service from POSIX" do
     File.open dummy_file_path, "w" do |file|
       file.write "this is dummy"
     end
-   puts "Dummy file created"
-   binding.pry
-
   }
-#   let(:manifestfile_path) { "manifest.csv" }
-#   let(:manifest_location) { "gs://#{source_bucket.name}/#{manifestfile_path}" }
-  let(:agent_pool_name) { "projects/storage-sdk-vendor/agentPools/shubhangi-test-pool" }
-#   let(:data_csv) {
-#     create_dummy_file
-#     # create manifestcsv file
-#     CSV.generate { |csv| csv << [dummy_file_name] }
-#   }
-#   let(:data_io) { StringIO.new data_csv }
-#   let(:create_manifest_file) { source_bucket.file(manifestfile_path) || source_bucket.create_file(data_io, manifestfile_path) }
 
   before do
-    # create_manifest_file
-       binding.pry
-
     create_dummy_file
     puts "Dummy file created"
-    binding.pry
-    #{}grant_sts_permissions project_id: project.project_id, bucket_name: source_bucket.name
     grant_sts_permissions project_id: project.project_id, bucket_name: sink_bucket.name
   end
+
   after do
     # delete dummy file
     if File.exist? dummy_file_path
@@ -62,25 +43,24 @@ describe "Storage Transfer Service from POSIX" do
     else
       puts "File not found: #{dummy_file_path}"
     end
-   #{} delete_bucket_helper source_bucket.name
     delete_bucket_helper sink_bucket.name
   end
 
-#   it "creates a transfer job" do
-#     out, _err = capture_io do
-#       retry_resource_exhaustion do
-#         posix_request project_id: project.project_id, gcs_sink_bucket: sink_bucket.name, source_agent_pool_name: agent_pool_name, root_directory: root_directory
-#       end
-#     end
-#     assert_includes out, "transferJobs"
-#     job_name = out.scan(%r{(transferJobs/.*)}).flatten.first
-#     delete_transfer_job project_id: project.project_id, job_name: job_name
-#   end
+  it "creates a transfer job" do
+    out, _err = capture_io do
+      retry_resource_exhaustion do
+        posix_request project_id: project.project_id, gcs_sink_bucket: sink_bucket.name, source_agent_pool_name: agent_pool_name, root_directory: root_directory
+      end
+    end
+    assert_includes out, "transferJobs"
+    job_name = out.scan(%r{(transferJobs/.*)}).flatten.first
+    delete_transfer_job project_id: project.project_id, job_name: job_name
+  end
 
   it "checks the file is created in destination bucket" do
     out, _err = capture_io do
       retry_resource_exhaustion do
-        posix_request project_id: project.project_id, gcs_sink_bucket: sink_bucket.name, source_agent_pool_name: agent_pool_name, root_directory: dummy_file_path
+        posix_request project_id: project.project_id, gcs_sink_bucket: sink_bucket.name, source_agent_pool_name: agent_pool_name, root_directory: root_directory
       end
     end
 
@@ -90,10 +70,8 @@ describe "Storage Transfer Service from POSIX" do
         sink_bucket.file dummy_file_name
       end
     end
+    refute_nil file, "#{dummy_file_name} should exist on bucket"
 
-    binding.pry
-    assert file
     job_name = out.scan(%r{(transferJobs/.*)}).flatten.first
     delete_transfer_job project_id: project.project_id, job_name: job_name
   end
-end
--- google-cloud-storage_transfer/samples/transfer_from_posix_request.rb
+++ google-cloud-storage_transfer/samples/transfer_from_posix_request.rb
@@ -12,107 +12,49 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# [START storagetransfer_manifest_request]
-
-require "google/cloud/storage"
-
-# def grant_sts_permissions project_id:, bucket_name:
-#   client = Google::Cloud::StorageTransfer.storage_transfer_service
-#   request = { project_id: project_id }
-#   response = client.get_google_service_account request
-#   email = response.account_email
-
-#   storage = Google::Cloud::Storage.new
-#   bucket = storage.bucket bucket_name
-
-#   object_viewer = "roles/storage.objectViewer"
-#   bucket_reader = "roles/storage.legacyBucketReader"
-#   bucket_writer = "roles/storage.legacyBucketWriter"
-#   member = "serviceAccount:#{email}"
-
-#   bucket.policy requested_policy_version: 3 do |policy|
-#     policy.version = 3
-#     policy.bindings.insert(
-#       role:      object_viewer,
-#       members:   member
-#     )
-#   end
-
-#   bucket.policy requested_policy_version: 3 do |policy|
-#     policy.version = 3
-#     policy.bindings.insert(
-#       role:      bucket_reader,
-#       members:   member
-#     )
-#   end
-
-#   bucket.policy requested_policy_version: 3 do |policy|
-#     policy.version = 3
-#     policy.bindings.insert(
-#       role:      bucket_writer,
-#       members:   member
-#     )
-#   end
-# end
-
-
+# [START storagetransfer_transfer_from_posix]
 def posix_request project_id:, gcs_sink_bucket:, source_agent_pool_name:, root_directory:
-    # Your Google Cloud Project ID
-    # # project_id = "your-project_id"
-  
-    # The name of the  GCS bucket to transfer objects to
-    # gcs_sink_bucket = "your-sink-gcs-bucket"
-  
-    # The agent pool associated with the POSIX data source.
-    # Defaults to 'projects/{project_id}/agentPools/transfer_service_default'
-    # source_agent_pool_name = 'projects/your-project/agentPools/myouragent'
-  
-    # The root directory path on the source filesystem
-    # root_directory = '/directory/to/transfer/source'
-  
-    # Google Cloud Storage destination bucket name
-    # sink_bucket = 'your-destination-bucket'
+  # Your Google Cloud Project ID
+  # # project_id = "your-project_id"
 
-  
-    require "google/cloud/storage_transfer"
-  
-    transfer_job = {
-      project_id: project_id,
-      transfer_spec: {
-        source_agent_pool_name: source_agent_pool_name,
-        posix_data_source: {
-          root_directory: root_directory
-        },
-        gcs_data_sink: {
-          bucket_name: gcs_sink_bucket
-        }
+  # The name of the  GCS bucket to transfer objects to
+  # gcs_sink_bucket = "your-sink-gcs-bucket"
+
+  # The agent pool associated with the POSIX data source.
+  # Defaults to 'projects/{project_id}/agentPools/transfer_service_default'
+  # source_agent_pool_name = 'projects/your-project/agentPools/myouragent'
+
+  # The root directory path on the source filesystem
+  # root_directory = '/directory/to/transfer/source'
+
+  require "google/cloud/storage_transfer"
+
+  transfer_job = {
+    project_id: project_id,
+    transfer_spec: {
+      source_agent_pool_name: source_agent_pool_name,
+      posix_data_source: {
+        root_directory: root_directory
       },
-      status: :ENABLED
-    }
-    client = Google::Cloud::StorageTransfer.storage_transfer_service
-  
-    transfer_job_response = client.create_transfer_job transfer_job: transfer_job
-    run_request = {
-      project_id: project_id,
-      job_name: transfer_job_response.name
-    }
-    client.run_transfer_job run_request
-    puts "Transfered a file from #{root_directory} to #{gcs_sink_bucket} via job name #{transfer_job_response.name}"
-  end
-  # [END storagetransfer_manifest_request]
+      gcs_data_sink: {
+        bucket_name: gcs_sink_bucket
+      }
+    },
+    status: :ENABLED
+  }
+  client = Google::Cloud::StorageTransfer.storage_transfer_service
 
-  project_id= "storage-sdk-vendor"
-  gcs_sink_bucket= "ruby-storagetransfer-samples-test-2024-10-18t07-52-55z-a6e2492c"
-  source_agent_pool_name= "projects/storage-sdk-vendor/agentPools/shubhangi-test-pool"
-  root_directory= "/tmp/try/ruby_storagetransfer_samples_dummy_1b2d68c56d5c6d856f40446bf93e4794.txt"
+  transfer_job_response = client.create_transfer_job transfer_job: transfer_job
+  run_request = {
+    project_id: project_id,
+    job_name: transfer_job_response.name
+  }
+  client.run_transfer_job run_request
+  puts "Transfered a file from #{root_directory} to #{gcs_sink_bucket} via job name #{transfer_job_response.name}"
+end
 
+# [END storagetransfer_transfer_from_posix]
 
-#   if $PROGRAM_NAME == __FILE__
-#     posix_request project_id: project_id, gcs_sink_bucket: gcs_sink_bucket, source_agent_pool_name: source_agent_pool_name, root_directory: root_directory
-#   end
-
-
-  
-  if $PROGRAM_NAME == __FILE__
-    posix_request project_id: ARGV.shift, gcs_sink_bucket: ARGV.shift, source_agent_pool_name: ARGV.shift, root_directory: ARGV.shift
-  end
+if $PROGRAM_NAME == __FILE__
+  posix_request project_id: ARGV.shift, gcs_sink_bucket: ARGV.shift, source_agent_pool_name: ARGV.shift, root_directory: ARGV.shift
+end
