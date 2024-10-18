03064718376cd8a1b50ae9ed6308bdb86f822ef7
Shubhangi Singh <shubhangisin@google.com>
2024-10-18 10:32:31 +0000

    adding tests to secret values

--- google-cloud-storage/acceptance/storage/universe_domain_test.rb
+++ google-cloud-storage/acceptance/storage/universe_domain_test.rb
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 require "storage_helper"
+require "pry"
 
 describe Google::Cloud::Storage, :universe_domain do
   
@@ -40,12 +41,18 @@ describe Google::Cloud::Storage, :universe_domain do
     end
   end
 
+  it "validates presense of test project secret values" do
+    refute_nil TEST_UNIVERSE_PROJECT_ID, "TEST_UNIVERSE_PROJECT_ID should not be nil"
+    refute_nil TEST_UNIVERSE_LOCATION, "TEST_UNIVERSE_LOCATION should not be nil"
+    refute_nil TEST_UNIVERSE_DOMAIN, "TEST_UNIVERSE_DOMAIN should not be nil"
+    refute_nil TEST_UNIVERSE_DOMAIN_CREDENTIAL, "TEST_UNIVERSE_DOMAIN_CREDENTIAL should not be nil"
+  end
+
   it "creates a new bucket and uploads an object with universe_domain" do
     # Create a bucket
     ud_bucket =  ud_storage.create_bucket ud_bucket_name, location: TEST_UNIVERSE_LOCATION 
     _(ud_bucket).wont_be_nil
     _(ud_bucket.name).must_equal ud_bucket_name
-
     # Upload an object
     ud_file_name = "ud-test-file"
     ud_payload = StringIO.new "Hello world!"
@@ -58,11 +65,12 @@ describe Google::Cloud::Storage, :universe_domain do
 
     # Delete the object
     uploaded_ud_file.delete
-    _(uploaded_ud_file).must_be_nil
+    _(ud_bucket.file uploaded_ud_file.name).must_be_nil
 
     # Delete the bucket
     ud_bucket.delete
-    _(ud_bucket).must_be_nil
+    bucket= ud_storage.bucket ud_bucket_name
+    assert_nil bucket, "Bucket #{ud_bucket_name} should not exist"
 
   end
 end
