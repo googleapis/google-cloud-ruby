2219935c6ba6fd3995ac471bbfc7cf027069f90f
Shubhangi Singh <shubhangisin@google.com>
2025-01-15 06:38:57 +0000

    removing unwanted code

--- google-cloud-storage/samples/storage_get_soft_deleted_bucket.rb
+++ google-cloud-storage/samples/storage_get_soft_deleted_bucket.rb
@@ -22,8 +22,6 @@ def get_soft_deleted_bucket bucket_name:, generation:
   require "google/cloud/storage"
 
   storage = Google::Cloud::Storage.new
-  bucket_name = bucket_name.gsub(/[^a-zA-Z0-9\- ]/, "")
-
   # fetching soft deleted bucket with soft_delete_time and hard_delete_time
   deleted_bucket_fetch = storage.bucket bucket_name, generation: generation, soft_deleted: true
 
