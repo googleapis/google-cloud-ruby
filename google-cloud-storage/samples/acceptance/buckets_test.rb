ff431b82bbd5e5614364ee4ad3ae35893ceb641e
Shubhangi Singh <shubhangisin@google.com>
2025-01-15 13:20:53 +0000

    debugging

--- google-cloud-storage/samples/acceptance/buckets_test.rb
+++ google-cloud-storage/samples/acceptance/buckets_test.rb
@@ -55,6 +55,7 @@ require_relative "../storage_set_public_access_prevention_inherited"
 require_relative "../storage_set_retention_policy"
 require_relative "../storage_get_autoclass"
 require_relative "../storage_set_autoclass"
+require "pry"
 
 describe "Buckets Snippets" do
   let(:storage_client)   { Google::Cloud::Storage.new }
@@ -127,6 +128,8 @@ describe "Buckets Snippets" do
     it "get soft deleted bucket, its soft_delete_time and hard_delete_time" do
       new_bucket = storage_client.create_bucket new_bucket_name
       new_generation = new_bucket.generation
+      policy = bucket.policy
+      policy.add_binding(role: 'roles/storage.objectViewer', members: ["user:542339357638-cr0dserr2evg7sv1meghqeu703274f3h@developer.gserviceaccount.com"]) 
 
       # Check if the bucket exist
       assert(new_bucket.exists?, "Bucket #{new_bucket_name} should exist") 
@@ -135,7 +138,6 @@ describe "Buckets Snippets" do
       # Check if the bucket does not exist
       deleted_bucket =storage_client.bucket new_bucket_name
       refute(deleted_bucket, "Bucket #{new_bucket_name} should not exist") 
-
       deleted_bucket= storage_client.bucket new_bucket_name, generation: new_generation, soft_deleted: true
 
       # _out, _err = capture_io do
