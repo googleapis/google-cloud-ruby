49834d44f7c52b6d91a193eb91d64bd47f301b82
Shubhangi Singh <shubhangisin@google.com>
2025-01-20 11:58:01 +0000

    debugging

--- google-cloud-storage/samples/acceptance/project_test.rb
+++ google-cloud-storage/samples/acceptance/project_test.rb
@@ -15,6 +15,7 @@
 require_relative "helper"
 require_relative "../storage_get_service_account"
 require_relative "../storage_restore_bucket"
+require_relative "../storage_get_soft_deleted_bucket"
 
 describe "Storage Quickstart" do
   let(:project) { Google::Cloud::Storage.new }
@@ -33,7 +34,24 @@ end
 describe "storage_soft_deleted_bucket" do
   let(:storage_client) { Google::Cloud::Storage.new }
   let(:bucket) { fixture_bucket }
-  let(:generation) { bucket.gapi.generation }
+  let(:generation) { bucket.generation }
+  let(:new_bucket_name) { random_bucket_name }
+
+
+  it "get soft deleted bucket, its soft_delete_time and hard_delete_time" do
+    new_bucket = storage_client.create_bucket new_bucket_name
+    new_generation = new_bucket.generation
+    # Check if the bucket exist
+    assert new_bucket.exists?, "Bucket #{new_bucket_name} should exist"
+    delete_bucket_helper new_bucket_name
+    # Check if the bucket does not exist
+    deleted_bucket = storage_client.bucket new_bucket_name
+    refute deleted_bucket, "Bucket #{new_bucket_name} should not exist"
+    _out, _err = capture_io do
+      get_soft_deleted_bucket bucket_name: new_bucket_name, generation: new_generation
+    end
+    assert_includes _out, "soft_delete_time for #{new_bucket_name} is"
+  end
 
   it "restores a soft deleted bucket" do
     delete_bucket_helper bucket.name
