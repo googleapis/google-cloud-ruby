2a7f238b3d7723f55fcae7a4612f720b807e9922
Shubhangi Singh <shubhangisin@google.com>
2025-01-22 09:59:12 +0000

    debugging

--- google-cloud-storage/samples/acceptance/project_test.rb
+++ google-cloud-storage/samples/acceptance/project_test.rb
@@ -19,6 +19,9 @@ require_relative "../storage_get_soft_deleted_bucket"
 
 describe "Storage Quickstart" do
   let(:project) { Google::Cloud::Storage.new }
+  let(:bucket) { fixture_bucket }
+  let(:generation) { bucket.generation }
+  let(:new_bucket_name) { random_bucket_name }
 
   it "get_service_account" do
     email = nil
@@ -28,29 +31,43 @@ describe "Storage Quickstart" do
     assert_includes out,
                     "The GCS service account for project #{project.project_id} is: #{project.service_account_email}"
     assert_includes out, "@gs-project-accounts.iam.gserviceaccount.com"
+
+    new_bucket = project.create_bucket new_bucket_name
+    new_generation = new_bucket.generation
+    # Check if the bucket exist
+    assert new_bucket.exists?, "Bucket #{new_bucket_name} should exist"
+    delete_bucket_helper new_bucket_name
+    # Check if the bucket does not exist
+    deleted_bucket = project.bucket new_bucket_name
+    refute deleted_bucket, "Bucket #{new_bucket_name} should not exist"
+    output, _err = capture_io do
+      get_soft_deleted_bucket bucket_name: new_bucket_name, generation: new_generation
+    end
+    assert_includes output, "soft_delete_time for #{new_bucket_name} is"
   end
 end
 
 describe "storage_soft_deleted_bucket" do
-  let(:storage_client) { Google::Cloud::Storage.new }
+  let(:project) { Google::Cloud::Storage.new }
   let(:bucket) { fixture_bucket }
   let(:generation) { bucket.generation }
   let(:new_bucket_name) { random_bucket_name }
 
 
   it "get soft deleted bucket, its soft_delete_time and hard_delete_time" do
-    new_bucket = storage_client.create_bucket new_bucket_name
+    new_bucket = project.create_bucket new_bucket_name
     new_generation = new_bucket.generation
     # Check if the bucket exist
     assert new_bucket.exists?, "Bucket #{new_bucket_name} should exist"
     delete_bucket_helper new_bucket_name
     # Check if the bucket does not exist
-    deleted_bucket = storage_client.bucket new_bucket_name
+    deleted_bucket = project.bucket new_bucket_name
     refute deleted_bucket, "Bucket #{new_bucket_name} should not exist"
-    _out, _err = capture_io do
+    output, _err = capture_io do
       get_soft_deleted_bucket bucket_name: new_bucket_name, generation: new_generation
     end
-    assert_includes _out, "soft_delete_time for #{new_bucket_name} is"
+    binding.pry
+    assert_includes output, "soft_delete_time for #{new_bucket_name} is"
   end
 
   it "restores a soft deleted bucket" do
