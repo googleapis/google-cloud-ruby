633c1acfe128973e05d01a463774844b3d324689
Daniel Azuma <dazuma@google.com>
2025-02-24 10:54:20 -0800

    feat(bigtable): Support for setting the universe domain (#29173)

--- google-cloud-bigtable/test/google/cloud/bigtable/project_test.rb
+++ google-cloud-bigtable/test/google/cloud/bigtable/project_test.rb
@@ -20,4 +20,27 @@ describe Google::Cloud::Bigtable::Project, :mock_bigtable do
     _(bigtable).must_be_kind_of Google::Cloud::Bigtable::Project
     _(bigtable.project_id).must_equal project_id
   end
+
+  it "returns the default universe domain" do
+    _(bigtable.universe_domain).must_equal "googleapis.com"
+    tables_stub = bigtable.service.tables.instance_variable_get :@bigtable_table_admin_stub
+    _(tables_stub.endpoint).must_equal "bigtableadmin.googleapis.com"
+    instances_stub = bigtable.service.instances.instance_variable_get :@bigtable_instance_admin_stub
+    _(instances_stub.endpoint).must_equal "bigtableadmin.googleapis.com"
+    main_stub = bigtable.service.client("mytable", "myprofile").instance_variable_get :@bigtable_stub
+    _(main_stub.endpoint).must_equal "bigtable.googleapis.com"
+  end
+
+  it "returns a custom universe domain" do
+    universe = "myuniverse.com"
+    service = Google::Cloud::Bigtable::Service.new project_id, credentials, universe_domain: universe
+    project = Google::Cloud::Bigtable::Project.new service
+    _(project.universe_domain).must_equal universe
+    tables_stub = service.tables.instance_variable_get :@bigtable_table_admin_stub
+    _(tables_stub.endpoint).must_equal "bigtableadmin.myuniverse.com"
+    instances_stub = service.instances.instance_variable_get :@bigtable_instance_admin_stub
+    _(instances_stub.endpoint).must_equal "bigtableadmin.myuniverse.com"
+    main_stub = service.client("mytable", "myprofile").instance_variable_get :@bigtable_stub
+    _(main_stub.endpoint).must_equal "bigtable.myuniverse.com"
+  end
 end
