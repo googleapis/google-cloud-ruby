cbd7ae36187c08009a9a4ce1f240635a847a776f
vatsal-vora-crestdata <vatsal.vora@crestdata.ai>
2025-03-28 06:37:19 +0530

    samples(parametermanager): Added global samples for parameter manager (#29324)

---
+++ google-cloud-parameter_manager/samples/create_structured_param_version.rb
@@ -0,0 +1,56 @@
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# [START parametermanager_create_structured_param_version]
+require "google/cloud/parameter_manager"
+
+##
+# Create a parameter version
+#
+# @param project_id [String] The Google Cloud project (e.g. "my-project")
+# @param parameter_id [String] The parameter name (e.g. "my-parameter")
+# @param version_id [String] The version name (e.g. "my-version")
+# @param json_payload [String] The parameter payload in JSON format \
+# (e.g."{\"username\": \"test-user\", \"host\": \"localhost\"}")
+#
+def create_structured_param_version project_id:, parameter_id:, version_id:, json_payload:
+  # Create a Parameter Manager client.
+  client = Google::Cloud::ParameterManager.parameter_manager
+
+  # Build the resource name of the parent project.
+  parent = client.parameter_path project: project_id, location: "global", parameter: parameter_id
+
+  parameter_version = {
+    payload: {
+      data: json_payload
+    }
+  }
+
+  # Create the parameter version.
+  param_version = client.create_parameter_version parent: parent, parameter_version_id: version_id,
+                                                  parameter_version: parameter_version
+
+  # Print the new parameter version name.
+  puts "Created parameter version #{param_version.name}"
+end
+# [END parametermanager_create_structured_param_version]
+
+if $PROGRAM_NAME == __FILE__
+  create_structured_param_version(
+    project_id: ENV["GOOGLE_CLOUD_PROJECT"] || raise("missing GOOGLE_CLOUD_PROJECT"),
+    parameter_id: ARGV.shift,
+    version_id: ARGV.shift,
+    json_payload: ARGV.shift
+  )
+end
---
+++ google-cloud-parameter_manager/samples/enable_param_version.rb
@@ -0,0 +1,56 @@
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# [START parametermanager_enable_param_version]
+require "google/cloud/parameter_manager"
+
+##
+# Enable a parameter version
+#
+# @param project_id [String] The Google Cloud project (e.g. "my-project")
+# @param parameter_id [String] The parameter name (e.g. "my-parameter")
+# @param version_id [String] The version name (e.g. "my-version")
+#
+def enable_param_version project_id:, parameter_id:, version_id:
+  # Create a Parameter Manager client.
+  client = Google::Cloud::ParameterManager.parameter_manager
+
+  # Build the resource name of the parent project.
+  name = client.parameter_version_path project: project_id, location: "global", parameter: parameter_id,
+                                       parameter_version: version_id
+
+  parameter_version = {
+    name: name,
+    disabled: false
+  }
+
+  update_mask = {
+    paths: ["disabled"]
+  }
+
+  # Enabled the parameter version.
+  param_version = client.update_parameter_version parameter_version: parameter_version, update_mask: update_mask
+
+  # Print the parameter version name.
+  puts "Enabled parameter version #{version_id} for parameter #{parameter_id}"
+end
+# [END parametermanager_enable_param_version]
+
+if $PROGRAM_NAME == __FILE__
+  enable_param_version(
+    project_id: ENV["GOOGLE_CLOUD_PROJECT"] || raise("missing GOOGLE_CLOUD_PROJECT"),
+    parameter_id: ARGV.shift,
+    version_id: ARGV.shift
+  )
+end
