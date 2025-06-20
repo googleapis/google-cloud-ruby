7f5d121c3b0497921c19f371dc3ca1d383dfbcc1
Shubhangi Singh <shubhangisin@google.com>
2025-06-20 11:10:46 +0000

    get and list working

--- google-cloud-storage-control/samples/acceptance/storage_control_anywhere_cache_test.rb
+++ google-cloud-storage-control/samples/acceptance/storage_control_anywhere_cache_test.rb
@@ -14,22 +14,42 @@
 
 require_relative "helper"
 require_relative "../storage_control_create_anywhere_cache"
+require_relative "../storage_control_list_anywhere_caches"
+require_relative "../storage_control_get_anywhere_cache"
+
 require 'pry'
 
 describe "Storage Control Anywhere Cache" do
-  let(:bucket_name) { random_bucket_name }
+  let(:bucket_name) { 'ruby_7' }
   let(:storage_client) { Google::Cloud::Storage.new }
-  let(:project_name)   { storage_client.project }
+  let(:zone) {'us-east1-b'}
+#   let(:zone) {@bucket.location}
 
   before :all do
-     @bucket = create_bucket_helper bucket_name
+    @bucket= storage_client.bucket bucket_name
+    #  @bucket = create_bucket_helper bucket_name
   end
 
-  after do
-    delete_bucket_helper bucket_name
+#   after do
+#     delete_bucket_helper bucket_name
+#   end
+
+#   it "create Anywhere cache" do
+#     create_anywhere_cache bucket_name: bucket_name, project_name: project_name, zone: @bucket.location
+#   end
+
+  it "list Anywhere cache" do
+    out, _err = capture_io do
+        list_anywhere_caches bucket_name: bucket_name
+    end
+
+    assert_includes out, "#{bucket_name}/anywhereCaches/#{zone}"
   end
 
-  it "create Anywhere cache" do
-    create_anywhere_cache bucket_name: bucket_name, project_name: project_name, zone: @bucket.location
+  it "Get Anywhere cache" do
+    out, _err = capture_io do
+        get_anywhere_cache bucket_name: bucket_name, zone: zone
+    end
+    assert_includes out, "#{bucket_name}/anywhereCaches/#{zone}"
   end
 end
