fae8845fe735b19565c46916f59d7856d95a60b7
Shubhangi Singh <shubhangisin@google.com>
2025-08-21 09:11:45 +0000

    removing commented line

--- google-cloud-storage/test/google/cloud/storage/bucket_test.rb
+++ google-cloud-storage/test/google/cloud/storage/bucket_test.rb
@@ -1454,7 +1454,6 @@ describe Google::Cloud::Storage::Bucket, :mock_storage do
                       storage_class: nil, temporary_hold: nil,
                       event_based_hold: nil
 
-  # new_file_contents = StringIO.new
   # Set crc32c if both md5 and crc32c are not provided
   if md5.nil? && crc32c.nil?
     crc32c = Google::Cloud::Storage::File::Verifier.crc32c_for(StringIO.new("Hello world"))
