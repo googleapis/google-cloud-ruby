c9c92237ee3855842226084aa4cf8a1e71a001a7
Shubhangi Singh <shubhangisin@google.com>
2025-08-25 16:42:33 +0530

    Update bucket.rb

--- google-cloud-storage/lib/google/cloud/storage/bucket.rb
+++ google-cloud-storage/lib/google/cloud/storage/bucket.rb
@@ -1511,7 +1511,6 @@ module Google
         #     puts file.name
         #   end
         #
-
         def files prefix: nil, delimiter: nil, token: nil, max: nil,
                   versions: nil, match_glob: nil, include_folders_as_prefixes: nil,
                   soft_deleted: nil
