1ebcc784750d99eb272187b7bf3f2878ca763493
Charlotte Yun <yejiyun@google.com>
2025-11-14 18:14:48 -0800

    chore(PubSub): making all logs block-based and catching and killing all exceptions

--- google-cloud-pubsub/lib/google/cloud/pubsub/logger_helper.rb
+++ google-cloud-pubsub/lib/google/cloud/pubsub/logger_helper.rb
@@ -16,45 +16,69 @@ require "logger"
 module Google
   module Cloud
     module PubSub
-      def self.logger name = ""
-        @loggers ||= {}
-        @loggers[name] ||= begin
-          if is_logging_enabled
-            logger = Logger.new $stdout
-            prog_name = "pubsub"
-            prog_name = "#{prog_name}:#{name}" unless name.nil? || name.empty?
-            logger.progname = prog_name
-            logger
-          else
-            Logger.new nil
-          end
-        end
+      LOG_NAME = "pubsub".freeze
+      private_constant :LOG_NAME
+
+      def self.logger
+        is_logging_enabled ? configure.logger : Logger.new(nil)
       end
 
       def self.is_logging_enabled
-        packages = ENV["GOOGLE_SDK_RUBY_LOGGING"]&.split(",") || []
-        packages.include?("pubsub") || packages.include?("all")
+        begin
+          env_var = ENV["GOOGLE_SDK_RUBY_LOGGING_GEMS"]
+          return false if (configure.logger.nil? || env_var == "none")
+          return true if env_var == "all"
+          # parse env var by removing whitespace and splitting by comma
+          packages = env_var&.gsub(/\s+/, "")&.split(",") || []
+          packages.include?(LOG_NAME)
+        rescue StandardError => e
+          warn "Failed to determine logging configuration. Logging disabled. Error: #{e.class}: #{e.message}"
+          false
+        end
       end
 
       ##
       # @private
       module LoggerHelper
+        VALID_LOG_LEVELS = [:debug, :info, :warn, :error, :fatal].freeze
+        private_constant :VALID_LOG_LEVELS
+
         private
 
-        def log_batch logger_name, reason, type, num_messages, total_bytes
-          Google::Cloud::PubSub.logger(logger_name).info(
-            "#{reason} triggered #{type} batch of #{num_messages} messages, a total of #{total_bytes} bytes"
-          )
-        end
-
-        def log_ack_nack_safely ack_ids, type
-          return unless Google::Cloud::PubSub.is_logging_enabled
+        # rubocop:disable Naming/BlockForwarding
+        def log_safely level, subtag, &message_block
+          return unless VALID_LOG_LEVELS.include?(level) && block_given?
           begin
-            ack_ids.each do |ack_id|
-              Google::Cloud::PubSub.logger("ack-nack").info "message (ackID #{ack_id}) #{type}"
-            end
+            Google::Cloud::PubSub.logger.public_send(level, "#{LOG_NAME}:#{subtag}", &message_block)
           rescue StandardError
             # Ignore all logging errors.
+          end
+        end
+        # rubocop:enable Naming/BlockForwarding
+
+        def log_batch logger_name, reason, type, num_messages, total_bytes
+          log_safely :info, logger_name do
+            "#{reason} triggered #{type} batch of #{num_messages} messages, a total of #{total_bytes} bytes"
+          end
+        end
+
+        def log_ack_nack ack_ids, type
+          # exit early to avoid unnecessary loop
+          return unless Google::Cloud::PubSub.is_logging_enabled
+          ack_ids.each do |ack_id|
+            log_safely :info, "ack-nack" do
+              "message (ackID #{ack_id}) #{type}"
+            end
+          end
+        end
+
+        def log_expiry expired
+          # exit early to avoid unnecessary loop
+          return unless Google::Cloud::PubSub.is_logging_enabled
+          expired.each do |ack_id, item|
+            log_safely :info, "expiry" do
+              "message (ID #{item.message_id}, ackID #{ack_id}) has been dropped from leasing due to a timeout"
+            end
           end
         end
       end
