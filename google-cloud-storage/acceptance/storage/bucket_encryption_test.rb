6e270a2fdd5244650c6dae413998ff2da2b45ef5
Shubhangi Singh <shubhangisin@google.com>
2026-03-02 17:53:09 +0000

    try

--- google-cloud-storage/acceptance/storage/bucket_encryption_test.rb
+++ google-cloud-storage/acceptance/storage/bucket_encryption_test.rb
@@ -25,9 +25,16 @@ describe Google::Cloud::Storage::Bucket, :encryption, :storage do
     ENV["GCLOUD_TEST_STORAGE_KMS_KEY_2"] ||
       "projects/#{storage.project_id}/locations/#{bucket_location}/keyRings/ruby-test/cryptoKeys/ruby-test-key-2"
   }
+  let(:customer_supplied_config) { Google::Apis::StorageV1::Bucket::Encryption::CustomerSuppliedEncryptionEnforcementConfig.new restriction_mode: "FullyRestricted" }
+  let(:customer_managed_config) { Google::Apis::StorageV1::Bucket::Encryption::CustomerManagedEncryptionEnforcementConfig.new restriction_mode: "NotRestricted" }
+  let(:google_managed_config) { Google::Apis::StorageV1::Bucket::Encryption::GoogleManagedEncryptionEnforcementConfig.new restriction_mode: "FullyRestricted" }
+  let(:customer_supplied_config1) { Google::Apis::StorageV1::Bucket::Encryption::CustomerSuppliedEncryptionEnforcementConfig.new restriction_mode: "test" }
   let :bucket do
-    b = safe_gcs_execute { storage.create_bucket(bucket_name, location: bucket_location) }
+    b = storage.bucket(bucket_name) || storage.create_bucket(bucket_name, location: bucket_location)
     b.default_kms_key = kms_key
+    b.google_managed_encryption_enforcement_config = google_managed_config
+    b.customer_supplied_encryption_enforcement_config = customer_supplied_config
+    b.customer_managed_encryption_enforcement_config = customer_managed_config
     b
   end
 
@@ -45,14 +52,27 @@ describe Google::Cloud::Storage::Bucket, :encryption, :storage do
     { logo: { path: "acceptance/data/CloudPlatform_128px_Retina.png" },
       big:  { path: "acceptance/data/three-mb-file.tif" } }
   end
-  describe "KMS customer-managed encryption key (CMEK)" do
+  describe "bucket encryption configuration" do
 
     it "knows its encryption configuration" do
       _(bucket.default_kms_key).wont_be :nil?
       _(bucket.default_kms_key).must_equal kms_key
+      _(bucket.google_managed_encryption_enforcement_config).wont_be :nil?
+      _(bucket.google_managed_encryption_enforcement_config.restriction_mode).must_equal "FullyRestricted"
+      _(bucket.customer_supplied_encryption_enforcement_config).wont_be :nil?
+      _(bucket.customer_supplied_encryption_enforcement_config.restriction_mode).must_equal "FullyRestricted"
+      _(bucket.customer_managed_encryption_enforcement_config).wont_be :nil?
+      _(bucket.customer_managed_encryption_enforcement_config.restriction_mode).must_equal "NotRestricted"
+    
       bucket.reload!
       _(bucket.default_kms_key).wont_be :nil?
       _(bucket.default_kms_key).must_equal kms_key
+      _(bucket.google_managed_encryption_enforcement_config).wont_be :nil?
+      _(bucket.google_managed_encryption_enforcement_config.restriction_mode).must_equal "FullyRestricted"
+      _(bucket.customer_supplied_encryption_enforcement_config).wont_be :nil?
+      _(bucket.customer_supplied_encryption_enforcement_config.restriction_mode).must_equal "FullyRestricted"
+      _(bucket.customer_managed_encryption_enforcement_config).wont_be :nil?
+      _(bucket.customer_managed_encryption_enforcement_config.restriction_mode).must_equal "NotRestricted"
     end
 
     it "can update its default kms key to another key" do
@@ -70,5 +90,30 @@ describe Google::Cloud::Storage::Bucket, :encryption, :storage do
       bucket.reload!
       _(bucket.default_kms_key).must_be :nil?
     end
+
+    it "can update its google managed encryption config to nil" do
+      _(bucket.google_managed_encryption_enforcement_config).wont_be :nil?
+      bucket.google_managed_encryption_enforcement_config = nil
+      _(bucket.google_managed_encryption_enforcement_config).must_be :nil?
+      bucket.reload!
+      _(bucket.google_managed_encryption_enforcement_config).must_be :nil?
+    end
+
+    it "can update its customer supplied encryption config to nil" do
+      _(bucket.customer_supplied_encryption_enforcement_config).wont_be :nil?
+      bucket.customer_supplied_encryption_enforcement_config = nil
+      _(bucket.customer_supplied_encryption_enforcement_config).must_be :nil?
+      bucket.reload!
+      _(bucket.customer_supplied_encryption_enforcement_config).must_be :nil?
+    end
+
+    it "can update its customer managed encryption config to nil" do
+      _(bucket.customer_managed_encryption_enforcement_config).wont_be :nil?
+      bucket.customer_managed_encryption_enforcement_config = nil
+      _(bucket.customer_managed_encryption_enforcement_config).must_be :nil?
+      bucket.reload!
+      _(bucket.customer_managed_encryption_enforcement_config).must_be :nil?
+    end
+
   end
 end
