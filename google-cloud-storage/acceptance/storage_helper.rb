5aaefc28abf9d1475c6983fce00ba2f4c829fdbd
Chris Smith <chrisdsmith@google.com>
2026-07-09 23:58:20 +0000

    cleanup: Remove obsolete 2018 JUnit config blocks (PR #2695) from 5 acceptance helpers

--- google-cloud-storage/acceptance/storage_helper.rb
+++ google-cloud-storage/acceptance/storage_helper.rb
@@ -37,12 +37,6 @@ PAP_SKIP_MESSAGE = "Skipping this test due to a change in GCS behavior that disa
                    "access prevention is enforced. See " \
                    "https://cloud.google.com/storage/docs/public-access-prevention for more details.".freeze
 
-# Generate JUnit format test reports
-if ENV["GCLOUD_TEST_GENERATE_XML_REPORT"]
-  require "minitest/reporters"
-  Minitest::Reporters.use! [Minitest::Reporters::SpecReporter.new, Minitest::Reporters::JUnitReporter.new]
-end
-
 # Create shared storage object so we don't create new for each test
 scopes = ["https://www.googleapis.com/auth/devstorage.full_control", "https://www.googleapis.com/auth/iam"]
 $storage = Google::Cloud.new.storage retries: 10, scope: scopes
