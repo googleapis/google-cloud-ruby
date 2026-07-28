fd3b7be0d5882773db49ea9fefd22874604ad560
Torrey Payne <11740989+torreypayne@users.noreply.github.com>
2026-07-28 05:29:41 +0000

    feat(language-v2): track native Gemfile.lock from generator updates

--- google-cloud-language-v2/lib/google/cloud/language/v2/version.rb
+++ google-cloud-language-v2/lib/google/cloud/language/v2/version.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# Copyright 2023 Google LLC
+# Copyright 2026 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -21,7 +21,7 @@ module Google
   module Cloud
     module Language
       module V2
-        VERSION = "1.5.0"
+        VERSION = "0.0.1"
       end
     end
   end
